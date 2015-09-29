c0034380c2b45c133987c203f03e55561ac0b581
jonas.gao <jonas.gao@voyageone.cn>
2015-09-29 13:45:58 +0800

    属性匹配 0.1.1 添加搜索 第三方品牌属性的值，需要添加条件。 第三方品牌属性的值，没有初始化为空。 属性列表页面，列【值】里面希望有内容（只要设置过属性，就显示“已设置”）

--- voyageone-static-cms/resources/static/components/directives/enterClick.js
+++ voyageone-static-cms/resources/static/components/directives/enterClick.js
@@ -12,13 +12,31 @@ define(["components/app"], function (app) {
             restrict: "A",
             link: function (scope, elem, attr) {
                 elem.keyup(function (e) {
+
+                    if (e.keyCode !== 13) return;
+
                     var selectExp = attr.enterClick;
+                    var targetElem, handler = function () {
+                        targetElem.click();
+                    };
 
-                    var targetElem = angular.element(selectExp);
+                    try {
+                        targetElem = angular.element(selectExp);
+                    } catch (e) {
+                        targetElem = null
+                    }
 
-                    if (!targetElem || !targetElem.length || targetElem.attr("disabled")) return;
+                    if (!targetElem || !targetElem.length) {
+                        // 如果取不到元素，则尝试作为表达式执行
+                        handler = function () {
+                            scope.$eval(selectExp);
+                        };
+                    } else if (targetElem.attr("disabled")) {
+                        // 如果元素存在，但是是禁用状态的，放弃执行
+                        return;
+                    }
 
-                    if (e.keyCode == 13) targetElem.click();
+                    handler();
                 });
             }
         };
