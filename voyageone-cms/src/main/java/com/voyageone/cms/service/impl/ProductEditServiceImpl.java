0d9db82222374c3d456862e6f08e1ffe2f2df29c
james <james>
2015-10-20 18:49:56 +0800

    cnGroupId设为1

--- voyageone-cms/src/main/java/com/voyageone/cms/service/impl/ProductEditServiceImpl.java
+++ voyageone-cms/src/main/java/com/voyageone/cms/service/impl/ProductEditServiceImpl.java
@@ -628,6 +628,7 @@ public class ProductEditServiceImpl implements ProductEditService {
 			data.put("isPublished", false);
 			if (productDao.doUpdatePublishStatus(data) == 0) {
 				data.put("code", productCNPriceInfo.getCode());
+				data.put("cnGroupId", 1);
 				productDao.doInsertPublishStatus(data);
 			}
 		}
