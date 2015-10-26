a9d0c9a4bb659acb05b801fd435b8effaeb2d785
lewis <lewis.liu@voyageone.cn>
2015-10-26 15:59:53 +0800

    属性值批量更新（上下架）

---
+++ voyageone-cms/src/main/java/com/voyageone/cms/dao/MasterCatComPropBatchUpdateDao.java
@@ -0,0 +1,164 @@
+package com.voyageone.cms.dao;
+
+import com.voyageone.base.dao.BaseDao;
+import com.voyageone.cms.modelbean.CmsCategoryModel;
+import com.voyageone.cms.modelbean.PropertyValue;
+import com.voyageone.common.Constants;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class MasterCatComPropBatchUpdateDao extends BaseDao {
+	
+	/**
+	 * 日志
+	 */
+	private static Log logger = LogFactory.getLog(MasterCatComPropBatchUpdateDao.class);
+
+	/**
+	 * 获取类目属性已完成类目id.
+	 * @param propName
+	 * @return
+	 */
+	public List<Map<String,String>> getComPropOptions(String propName){
+
+		logger.info("查询主类目共通属性选项开始...");
+
+		List<Map<String,String>> resultList = selectList(Constants.DAO_NAME_SPACE_CMS + "get_Com_Prop_Options", propName);
+
+		logger.info("查询主类目共通属性选项结束...");
+
+		return resultList;
+	}
+
+	/**
+	 * 批量更新model的属性值.
+	 * @param modelIdList
+	 * @param propName
+	 * @param channelId
+	 * @param level
+	 * @param propValue
+	 * @param user
+	 * @return
+	 */
+	public boolean batchUpdate(List<String> modelIdList,String propName,String channelId,int level,String propValue,String user){
+		logger.info("["+propName+"]"+"属性值批量更新开始...");
+		Map<String, Object> parmMap = new HashMap<String, Object>();
+
+		parmMap.put("modelIdList", modelIdList);
+		parmMap.put("propName",propName);
+		parmMap.put("channelId",channelId);
+		parmMap.put("level",level);
+		parmMap.put("propValue",propValue);
+		parmMap.put("user", user);
+
+		int updateCount = super.update(Constants.DAO_NAME_SPACE_CMS + "update_batch_cat_com_propValue", parmMap);
+
+		logger.info("["+propName+"]"+"属性值批量更新结束，"+updateCount+"条记录被更新...");
+
+		if(updateCount>1)
+			return true;
+
+		return false;
+	}
+
+	/**
+	 * 验证当前model的属性值是否存在.
+	 * @param modelIdList
+	 * @param modelId
+	 * @param channelId
+	 * @param level
+	 * @param referencePropId
+	 * @return
+	 */
+	public List<Map<String,String>> verifyPropValueExist(String propName,List<String> modelIdList,String modelId,String channelId,int level,String referencePropId){
+
+		logger.info("验证当前model的属性值是否存在开始...");
+
+		Map<String, Object> parmMap = new HashMap<String, Object>();
+		parmMap.put("propName",propName);
+		parmMap.put("modelIdList",modelIdList);
+		parmMap.put("modelId",modelId);
+		parmMap.put("channelId",channelId);
+		parmMap.put("level",level);
+		if(referencePropId!=null){
+			String refPropId = "%{\"extraPropId\":\""+referencePropId+"\"%";
+			parmMap.put("referencePropId", refPropId);
+		}else {
+			parmMap.put("referencePropId", referencePropId);
+		}
+
+
+		List<Map<String,String>> verifyList = super.selectList(Constants.DAO_NAME_SPACE_CMS + "verify_prop_value_exist", parmMap);
+		logger.info("验证当前model的属性值是否存在结束...");
+		return verifyList;
+	}
+
+	/**
+	 *
+	 * @param propertyValue
+	 * @return
+	 */
+	public int addPropValue(PropertyValue propertyValue){
+		logger.info("添加属性值开始...");
+		int count = super.insert(Constants.DAO_NAME_SPACE_CMS + "insert_prop_value", propertyValue);
+		logger.info("添加属性值结束.属性值为: " + propertyValue.getProp_id());
+		return count;
+	}
+
+	/**
+	 *
+	 * @param propId
+	 * @return
+	 */
+	public int removePorpValue(String propId,String channelId,int level,String levelValue){
+		logger.info("删除值开始...");
+		Map<String, Object> parmMap = new HashMap<String, Object>();
+		parmMap.put("propId",propId);
+		parmMap.put("channelId",channelId);
+		parmMap.put("level",level);
+		parmMap.put("levelValue", levelValue);
+		int count = super.delete(Constants.DAO_NAME_SPACE_CMS + "delete_sub_prop_value", parmMap);
+
+		logger.info("删除了"+count+"条");
+
+		return count;
+	}
+
+	/**
+	 *
+	 * @param propValue
+	 * @param user
+	 * @param propId
+	 * @param channelId
+	 * @param level
+	 * @param levelValue
+	 * @return
+	 */
+	public int updateSubPropValue(String propValue,String user,String propId,String channelId,int level,String levelValue){
+
+		logger.info("更新从属性值开始...");
+
+		Map<String, Object> parmMap = new HashMap<String, Object>();
+
+		parmMap.put("propValue",propValue);
+		parmMap.put("user", user);
+		parmMap.put("propId",propId);
+		parmMap.put("channelId",channelId);
+		parmMap.put("level",level);
+		parmMap.put("levelValue",levelValue);
+
+		int count = super.update(Constants.DAO_NAME_SPACE_CMS + "update_sub_prop_value", parmMap);
+
+		logger.info("更新了"+count+"条");
+
+		return count;
+	}
+
+}
---
+++ voyageone-cms/src/main/java/com/voyageone/cms/service/impl/MasterCatComPropBatchUpdateServiceImpl.java
@@ -0,0 +1,161 @@
+package com.voyageone.cms.service.impl;
+
+import com.voyageone.cms.dao.MasterCatComPropBatchUpdateDao;
+import com.voyageone.cms.formbean.MasterCatPropBatchUpdateBean;
+import com.voyageone.cms.formbean.MasterCategoryPropBean;
+import com.voyageone.cms.modelbean.PropertyValue;
+import com.voyageone.cms.service.MasterCatComPropBatchUpdateService;
+import com.voyageone.core.modelbean.UserSessionBean;
+import com.voyageone.ims.enums.MasterPropTypeEnum;
+import com.voyageone.ims.rule_expression.RuleExpression;
+import com.voyageone.ims.rule_expression.RuleJsonMapper;
+import com.voyageone.ims.rule_expression.TextWord;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class MasterCatComPropBatchUpdateServiceImpl implements MasterCatComPropBatchUpdateService {
+	
+	/**
+	 * 日志
+	 */
+	private static Log logger = LogFactory.getLog(MasterCatComPropBatchUpdateServiceImpl.class);
+
+	@Autowired
+	MasterCatComPropBatchUpdateDao masterCatComPropBatchUpdateDao;
+
+	/**
+	 * 获取需要批量更新的主属性的选项值.
+	 * @param PropName
+	 * @return
+	 */
+	@Override
+	public List<Map<String,String>> getPropOptions(String PropName) {
+		return masterCatComPropBatchUpdateDao.getComPropOptions(PropName);
+	}
+
+	/**
+	 * 批量更新属性值.
+	 * @param formData
+	 * @param userSession
+	 * @return
+	 */
+	@Override
+	@Transactional
+	public boolean batchUpdate(MasterCatPropBatchUpdateBean formData, UserSessionBean userSession) {
+
+		boolean isSuccess =false;
+
+		RuleJsonMapper ruleJsonMapper = new RuleJsonMapper();
+
+		//需要更新的主属性.
+		MasterCategoryPropBean masterProperty = formData.getMasterProperty();
+
+		//需要更新的model列表.
+		List<String> modelIdList = formData.getTarModelList();
+
+		//需要更新的从属性列表.
+		List<MasterCategoryPropBean> propBeans = formData.getProperties();
+
+		//需要删除的从属性列表.
+		List<MasterCategoryPropBean> delPropBeans = formData.getDelProperties();
+
+		//格式化主属性属性值.
+		RuleExpression ruleExpression = new RuleExpression();
+		ruleExpression.addRuleWord(new TextWord(masterProperty.getPropValue()));
+		String masterEncodePropValue = ruleJsonMapper.serializeRuleExpression(ruleExpression);
+
+
+		//更新主属性列表.
+		masterCatComPropBatchUpdateDao.batchUpdate(modelIdList, masterProperty.getPropName(), userSession.getSelChannel(), 2, masterEncodePropValue, userSession.getUserName());
+		//验证主属性的值是否已经存在.
+		List<Map<String,String>> verifyPropList = masterCatComPropBatchUpdateDao.verifyPropValueExist(masterProperty.getPropName(), modelIdList, null, userSession.getSelChannel(), 2, null);
+		//如果不存在就插入.
+		for (Map<String,String> propMap:verifyPropList){
+			if(propMap.get("valPropId")==null){
+				PropertyValue value = new PropertyValue();
+
+				value.setChannel_id(userSession.getSelChannel());
+				value.setLevel(2);
+				value.setLevel_value(String.valueOf(propMap.get("modelId")));
+				value.setParent(null);
+				value.setProp_id(Integer.valueOf(String.valueOf(propMap.get("propId"))));
+				value.setUuid(UUID.randomUUID().toString().replace("-", ""));
+				value.setProp_value(masterEncodePropValue);
+				value.setCreater(userSession.getUserName());
+				value.setModifier(userSession.getUserName());
+				masterCatComPropBatchUpdateDao.addPropValue(value);
+			}
+		}
+
+		//删除需要删除的从属性的属性值.
+		if(delPropBeans.size()>0){
+			for (MasterCategoryPropBean delBean:delPropBeans){
+				for (Map<String,String> propMap:verifyPropList){
+					String propName = delBean.getPropName();
+					String modelId = String.valueOf(propMap.get("modelId"));
+					String channelId = userSession.getSelChannel();
+					String propId = String.valueOf(String.valueOf(propMap.get("propId")));
+					List<Map<String,String>> delPropList = masterCatComPropBatchUpdateDao.verifyPropValueExist(propName,null,modelId,channelId,2,propId);
+					if(delPropList.size()>0){
+						for (Map<String,String> delMap:delPropList){
+							if(delMap.get("valPropId")!=null)
+								masterCatComPropBatchUpdateDao.removePorpValue(String.valueOf(delMap.get("valPropId")),userSession.getSelChannel(),2,String.valueOf(delMap.get("modelId")));
+						}
+					}
+				}
+			}
+		}
+
+
+		//更新从属性列表.
+		for (MasterCategoryPropBean propBean:propBeans){
+			RuleExpression updRuleExpression = new RuleExpression();
+			updRuleExpression.addRuleWord(new TextWord(propBean.getPropValue()));
+			String updEncodePropValue = ruleJsonMapper.serializeRuleExpression(updRuleExpression);
+			for (Map<String,String> propMap:verifyPropList){
+				List<Map<String,String>> updatePropList = masterCatComPropBatchUpdateDao.verifyPropValueExist(propBean.getPropName(),null,String.valueOf(propMap.get("modelId")),userSession.getSelChannel(),2,String.valueOf(propMap.get("propId")));
+				if(updatePropList.size()>0){
+					for (Map<String,String> updMap:updatePropList){
+
+						if(updMap.get("valPropId")==null){
+
+							PropertyValue subValue = new PropertyValue();
+
+							subValue.setChannel_id(userSession.getSelChannel());
+							subValue.setLevel(2);
+							subValue.setLevel_value(String.valueOf(updMap.get("modelId")));
+							subValue.setParent(null);
+							subValue.setProp_id(Integer.valueOf(String.valueOf(updMap.get("propId"))));
+							subValue.setUuid(UUID.randomUUID().toString().replace("-", ""));
+							subValue.setProp_value(updEncodePropValue);
+							subValue.setCreater(userSession.getUserName());
+							subValue.setModifier(userSession.getUserName());
+
+							//插入.
+							masterCatComPropBatchUpdateDao.addPropValue(subValue);
+
+							isSuccess=true;
+						}else {
+							//更新.
+							masterCatComPropBatchUpdateDao.updateSubPropValue(updEncodePropValue,userSession.getUserName(),String.valueOf(updMap.get("valPropId")),userSession.getSelChannel(),2,String.valueOf(updMap.get("modelId")));
+
+							isSuccess=true;
+						}
+
+					}
+				}
+			}
+		}
+
+		return isSuccess;
+	}
+}
