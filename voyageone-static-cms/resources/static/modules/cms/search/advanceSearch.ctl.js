3b1d979badc4705b6964234a0bad3efcb90af700
james <james>
2015-10-28 17:13:16 +0800

    高级检索 code条件追加 bug1

--- voyageone-static-cms/resources/static/modules/cms/search/advanceSearch.ctl.js
+++ voyageone-static-cms/resources/static/modules/cms/search/advanceSearch.ctl.js
@@ -91,6 +91,8 @@ define (function (require) {
               if($scope.search.codesString){
                   var codes = $scope.search.codesString.split("\n");
                   $scope.search.codes = codes;
+              }else{
+                  $scope.search.codes = null;
               }
               return searchService.doAdvanceSearch(data,$scope.search)
                   .then (function (data) {
