c464ea38d16cbc2edacef69fdf9bc0d46893e597
jonas.gao <jonas.gao@voyageone.cn>
2015-11-09 14:55:54 +0800

    拆分单独测试 BCBG 文件导入部分

--- voyageone-batchjob-all/voyageone-batchjob-cms/src/test/java/com/voyageone/batch/cms/service/feed/BcbgAnalysisServiceTest.java
+++ voyageone-batchjob-all/voyageone-batchjob-cms/src/test/java/com/voyageone/batch/cms/service/feed/BcbgAnalysisServiceTest.java
@@ -1,18 +1,13 @@
 package com.voyageone.batch.cms.service.feed;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.Assert;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.regex.Pattern;
-
-import static com.voyageone.batch.cms.service.feed.BcbgWsdlConstants.apparels_duty;
-import static com.voyageone.batch.cms.service.feed.BcbgWsdlConstants.fixed_exchange_rate;
+import java.io.FileNotFoundException;
 
 /**
  * 解析测试..
@@ -24,45 +19,18 @@ public class BcbgAnalysisServiceTest {
     @Autowired
     private BcbgAnalysisService bcbgAnalysisService;
 
+    @Before
+    public void setUp() throws Exception {
+        BcbgWsdlConstants.init();
+    }
+
+    @Test
+    public void testAppendDataFromFile() throws FileNotFoundException {
+        bcbgAnalysisService.appendDataFromFile();
+    }
+
     @Test
     public void testBcbgAnalysisService() {
         bcbgAnalysisService.startup();
-    }
-
-    @Test
-    public void testBigDecimal() {
-        BcbgWsdlConstants.init();
-        System.out.println("");
-        System.out.println("=================");
-
-        BigDecimal c = new BigDecimal("231.0000");
-        System.out.println(c.toString());
-        System.out.println(c.intValue());
-        System.out.println(fixed_exchange_rate);
-        System.out.println(apparels_duty);
-
-        BigDecimal ten = new BigDecimal(10);
-
-        BigDecimal d = c.multiply(fixed_exchange_rate).divide(apparels_duty, BigDecimal.ROUND_DOWN);
-
-        d = d.setScale(0, RoundingMode.DOWN);
-
-        BigDecimal e1 = d.divide(ten, BigDecimal.ROUND_DOWN).multiply(ten);
-        BigDecimal e2 = new BigDecimal(d.toString().substring(0, d.toString().length() - 1) + "0");
-        System.out.println(e1.intValue());
-        System.out.println(e2.intValue());
-        Assert.isTrue(e1.compareTo(e2) == 0);
-
-        BigDecimal a = new BigDecimal(1);
-        BigDecimal b = new BigDecimal(2);
-        Assert.isTrue(a.divide(b, BigDecimal.ROUND_DOWN).toString().equals("0"));
-        System.out.println("=================");
-    }
-
-    @Test
-    public void testClear() {
-        Pattern pattern = Pattern.compile("[%\\[~@\\#\\$&\\*_'/‘’\\^\\(\\)\\]\\\\]");
-        String result = pattern.matcher("%1[2]3~4@5#6\\7$8&9_10*11'12)13").replaceAll("");
-        System.out.print(result);
     }
 }
