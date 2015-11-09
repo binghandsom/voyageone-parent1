3dd0469231afaab3ab6829818580889f8f0e24d7
jonas.gao <jonas.gao@voyageone.cn>
2015-11-09 19:50:19 +0800

    修正打开新页面就登出的问题

--- voyageone-static-cms/resources/static/modules/app/common/index.ctl.js
+++ voyageone-static-cms/resources/static/modules/app/common/index.ctl.js
@@ -3,19 +3,27 @@
  * @Date:    2015/2/3
  *
  * @User:    Edward
- * @Version: 1.0.0
+ * @Version: 1.1
  */
 
-define (function (require) {
+define([
+  'modules/app/app.module',
+  'components/util/commonUtil',
+  'modules/app/common/common.service',
+  'modules/app/services/user.service'
+], function(app, common) {
+  app.controller('indexController', [
+    '$scope', 'commonService', 'userService', '$location',
+    indexController
+  ]);
 
-    var mainApp = require ("modules/app/app.module");
-    require ('modules/app/common/common.service');
-
-    mainApp.controller ('indexController', ['$scope', 'commonService',
-        function ($scope, commonService) {
-
-            $scope.initialize = function () {
-                commonService.doLogout ();
-            };
-        }]);
+  function indexController($scope, commonService, userService, $location) {
+    // 如果没登陆
+    // 跳至登陆页
+    if (!userService.isLogin()) {
+      common.goToLoginPage();
+      return;
+    }
+    $location.path('/cms/common/index');
+  }
 });
