3b7ae5e3afc85e4682758dcbca889678523a6284
chuanyu.liang <chuanyu.liang@voyageone.cn>
2015-11-26 11:12:05 +0800

    remove Spring Data MongoDB

--- voyageone-com/src/test/java/com/voyageone/base/dao/mongodb/test/dao/CustomerDao.java
+++ voyageone-com/src/test/java/com/voyageone/base/dao/mongodb/test/dao/CustomerDao.java
@@ -5,23 +5,24 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface CustomerDao extends CrudRepository<Customer, String> {
-
-    public Customer findOneByFirstName(String firstName);
-
-    public Customer findOneByLastName(String firstName);
-
-    public Iterable<Customer> findByFirstName(String firstName);
-
-
-    public Iterable<Customer> findByFirstNameRegex(String firstName);
-
-
-    public Integer countByFirstName(String firstName);
-
-
-    public Iterable<Customer> findByFirstNameAndLastName(String firstName,String lastName);
-
-    @Query(value="{ 'firstName' : ?0 ,'lastName' : ?1}", fields="{ 'firstName' : 1, 'lastName' : 1}")
-    public Iterable<Customer> findByThePersonsFirstName(String firstName,String lastName);
+public interface CustomerDao {
+//        extends CrudRepository<Customer, String> {
+//
+//    public Customer findOneByFirstName(String firstName);
+//
+//    public Customer findOneByLastName(String firstName);
+//
+//    public Iterable<Customer> findByFirstName(String firstName);
+//
+//
+//    public Iterable<Customer> findByFirstNameRegex(String firstName);
+//
+//
+//    public Integer countByFirstName(String firstName);
+//
+//
+//    public Iterable<Customer> findByFirstNameAndLastName(String firstName,String lastName);
+//
+//    @Query(value="{ 'firstName' : ?0 ,'lastName' : ?1}", fields="{ 'firstName' : 1, 'lastName' : 1}")
+//    public Iterable<Customer> findByThePersonsFirstName(String firstName,String lastName);
 }
