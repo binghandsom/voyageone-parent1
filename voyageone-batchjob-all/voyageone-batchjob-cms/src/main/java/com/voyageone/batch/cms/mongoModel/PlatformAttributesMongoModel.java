93498d3ff82fa2e75a761de4534be8c0ed4f244b
lewis <lewis.liu@voyageone.cn>
2015-11-27 17:34:02 +0800

    Merge from ims

---
+++ voyageone-batchjob-all/voyageone-batchjob-cms/src/main/java/com/voyageone/batch/cms/mongoModel/PlatformAttributesMongoModel.java
@@ -0,0 +1,68 @@
+package com.voyageone.batch.cms.mongoModel;
+
+
+import com.voyageone.base.dao.mongodb.model.BaseMongoModel;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class PlatformAttributesMongoModel extends BaseMongoModel {
+
+    private String cartId;
+    private String categoryId;
+    private String propsProduct;
+    private String propsItem;
+
+    public PlatformAttributesMongoModel() {
+    }
+
+    public PlatformAttributesMongoModel(
+            String categoryId,
+            String propsProduct,
+            String propsItem
+    ) {
+        this.categoryId = categoryId;
+        this.propsProduct = propsProduct;
+        this.propsItem = propsItem;
+    }
+
+    public String getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(String cartId) {
+        this.cartId = cartId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getPropsProduct() {
+        return propsProduct;
+    }
+
+    public void setPropsProduct(String propsProduct) {
+        this.propsProduct = propsProduct;
+    }
+
+    public String getPropsItem() {
+        return propsItem;
+    }
+
+    public void setPropsItem(String propsItem) {
+        this.propsItem = propsItem;
+    }
+
+    /**
+     * Get collection name.
+     * @return
+     */
+    public static String getCollectionName() {
+        return "platform_attributes";
+    }
+
+}
