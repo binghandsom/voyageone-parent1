ea1c0caf76e1addaaf8463562fc0efa5e81bf565
jonas.gao <jonas.gao@voyageone.cn>
2015-11-28 17:55:45 +0800

    split gulp

---
+++ voyageone-static/2.0/gulp-publish.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp');
+var ngAnnotate = require('gulp-ng-annotate');
+var header = require('gulp-header');
+var minifyHtml = require('gulp-minify-html');
+var uglify = require('gulp-uglify');
+var sourceMaps = require('gulp-sourcemaps');
+
+var vars = require('./gulp-vars');
+var publish = vars.publish;
+var build = vars.build;
+var tasks = vars.tasks;
+
+// release static
+gulp.task(tasks.publish.statics, [tasks.build.css.all], function () {
+
+  gulp.src(publish.static.fonts.src)
+    .pipe(gulp.dest(publish.static.fonts.dist))
+    .pipe(gulp.dest(publish.release.static.fonts));
+  gulp.src(publish.static.img.src)
+    .pipe(gulp.dest(publish.static.img.dist))
+    .pipe(gulp.dest(publish.release.static.img));
+  gulp.src(publish.static.css.src)
+    .pipe(gulp.dest(publish.static.css.dist))
+    .pipe(gulp.dest(publish.release.static.css));
+});
+
+// release voyageone.angular.com.js
+gulp.task(tasks.publish.angular, [tasks.build.angular], function () {
+  gulp.src([build.common.angular.dist + '/' + build.common.angular.concat,
+      build.common.angular.dist + '/' + build.common.angular.map])
+    .pipe(gulp.dest(publish.components.angular.dist))
+    .pipe(gulp.dest(publish.release.components));
+});
+
+// release voyageone.com.js.
+gulp.task(tasks.publish.com, [tasks.build.com], function () {
+  gulp.src([build.common.native.dist + '/' + build.common.native.concat,
+      build.common.native.dist + '/' + build.common.native.map])
+    .pipe(gulp.dest(publish.components.native.dist))
+    .pipe(gulp.dest(publish.release.components));
+});
+
+// release libs.
+gulp.task(tasks.publish.libs, function () {
+  gulp.src(publish.libs.src)
+    .pipe(gulp.dest(publish.release.libs));
+});
+
+// release views.
+gulp.task(tasks.publish.views, function () {
+
+  // build login.app and channel.app
+  gulp.src(publish.loginAndChannel.js)
+    .pipe(ngAnnotate())
+    .pipe(sourceMaps.init())
+    .pipe(uglify())
+    .pipe(sourceMaps.write('./'))
+    .pipe(gulp.dest(publish.release.loginAndChannel));
+
+  gulp.src(publish.loginAndChannel.html)
+    .pipe(minifyHtml())
+    .pipe(gulp.dest(publish.release.loginAndChannel));
+
+  // 压缩js文件
+  gulp.src(publish.views.js)
+    .pipe(uglify())
+    .pipe(gulp.dest(publish.release.views));
+
+  // 压缩html文件
+  gulp.src(publish.views.html)
+    .pipe(minifyHtml())
+    .pipe(gulp.dest(publish.release.views));
+
+  // copy json文件
+  gulp.src(publish.views.json)
+    .pipe(gulp.dest(publish.release.views));
+
+});
+
+gulp.task(tasks.publish.all, [tasks.publish.statics, tasks.publish.angular, tasks.publish.com, tasks.publish.libs, tasks.publish.views]);
---
+++ voyageone-static/2.0/gulp-vars.js
@@ -0,0 +1,126 @@
+var extend = require('extend');
+
+var versions = {
+  // 整个工程版本号
+  publish: "2.0.0",
+  // voyageone-angular-com.js的版本号
+  angularCom: "2.0.0",
+  // voyageone-com.js的版本号
+  com: "2.0.0",
+  // 静态资源的版本号
+  statics: "2.0.1"
+};
+var build = {
+  version: versions.statics,
+  common: {
+    angular: {
+      src: 'develop/components/angular/*/*.js',
+      dist: 'develop/components/dist',
+      footerFile: 'develop/components/angular/voyageone.angular.com.suffix',
+      concat: 'voyageone.angular.com.min.js',
+      map: 'voyageone.angular.com.min.js.map',
+      version: versions.angularCom
+    },
+    native: {
+      src: 'develop/components/js/*.js',
+      dist: 'develop/components/dist',
+      concat: 'voyageone.com.min.js',
+      map: 'voyageone.com.min.js.map',
+      version: versions.com
+    },
+    appCss: {
+      src: [
+        'develop/static/css/twitter-bootstrap/3.3.5/bootstrap.css',
+        'develop/static/css/animate.css',
+        'develop/static/css/simple-line-icons.css',
+        'develop/static/css/font.css',
+        'develop/static/css/font-awesome.css',
+        'develop/static/css/app.css'
+      ],
+      dist: 'develop/static/',
+      concat: 'app.min.css'
+    },
+    loginCss: {
+      src: [
+        'develop/static/css/login.css',
+        'develop/static/css/font-awesome.css'
+      ],
+      dist: 'develop/static/',
+      concat: 'login.min.css'
+    }
+  }
+};
+var publish = {
+  version: versions.publish,
+  static: {
+    fonts: {
+      src: 'develop/static/fonts/**',
+      dist: 'publish/static/' + versions.statics + '/fonts'
+    },
+    img: {
+      src: 'develop/static/img/**',
+      dist: 'publish/static/' + versions.statics + '/img'
+    },
+    css: {
+      src: 'develop/static/*.css',
+      dist: 'publish/static/' + versions.statics
+    }
+  },
+  components: {
+    angular: {
+      dist: 'publish/voaygeone-angular-com/' + versions.angularCom
+    },
+    native: {
+      dist: 'publish/voyageone-com/' + versions.com
+    }
+  },
+  libs: {
+    src: 'develop/libs/**'
+  },
+  loginAndChannel: {
+    js: 'develop/*.js',
+    html: 'develop/*.html'
+  },
+  views: {
+    js: 'develop/views/**/*.js',
+    html: 'develop/views/**/*.html',
+    json: 'develop/views/**/*.json'
+  },
+  release: {
+    static: {
+      fonts: 'publish/release/' + versions.publish + '/static/fonts',
+      img: 'publish/release/' + versions.publish + '/static/img',
+      css: 'publish/release/' + versions.publish + '/static'
+    },
+    components: 'publish/release/' + versions.publish + '/components/dist',
+    libs: 'publish/release/' + versions.publish + '/libs',
+    views: 'publish/release/' + versions.publish + '/views',
+    loginAndChannel: 'publish/release/' + versions.publish
+  }
+};
+var tasks = {
+  build: {
+    angular: 'build-angular-com',
+    com: 'build-com',
+    css: {
+      all: 'build-css',
+      app : 'build-css-app',
+      login : 'build-css-login'
+    },
+    watch: 'watch-build'
+  },
+  publish: {
+    all: 'publish',
+    statics: 'publish-static',
+    angular: 'publish-angular-com',
+    com: 'publish-com',
+    libs: 'publish-libs',
+    views: 'publish-views'
+  }
+};
+
+module.exports = {
+  build: build,
+  publish: publish,
+  tasks: tasks
+};
