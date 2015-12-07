d0691ea243f4b69b364816db0ce84504b9087b62
chuanyu.liang <chuanyu.liang@voyageone.cn>
2015-12-07 17:02:08 +0800

    add CatId int->String

--- voyageone-com/src/main/java/com/voyageone/cms/service/model/CmsBtProductModel_Feed.java
+++ voyageone-com/src/main/java/com/voyageone/cms/service/model/CmsBtProductModel_Feed.java
@@ -5,4 +5,12 @@ import com.voyageone.base.dao.mongodb.model.BaseMongoMap;
 
 public class CmsBtProductModel_Feed extends BaseMongoMap {
 
+    public String getCatId() {
+        return (String) getAttribute("catId");
+    }
+
+    public void setCatId(String catId) {
+        setAttribute("catId", catId);
+    }
+
 }
