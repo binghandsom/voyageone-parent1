0efeb7e1778862849772a0ab3ddb3992bed91596
chuanyu.liang <chuanyu.liang@voyageone.cn>
2015-12-07 16:19:25 +0800

    add  Field equals()

--- voyageone-md-schema/src/main/java/com/voyageone/common/masterdate/schema/field/Field.java
+++ voyageone-md-schema/src/main/java/com/voyageone/common/masterdate/schema/field/Field.java
@@ -264,4 +264,16 @@ public abstract class Field {
     public void setProperties(List<Property> properties) {
         this.properties = properties;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (this.id == null || obj == null || !(this.getClass().equals(obj.getClass()))) {
+            return false;
+        }
+        Field that = (Field) obj;
+        return this.id.equals(that.getId());
+    }
 }
