22560670a7dbb240b8fd101e5a82f43c228364a7
jonas.gao <jonas.gao@voyageone.cn>
2015-12-07 14:10:33 +0800

    ajax.service for new message, part

--- voyageone-static/2.0/develop/components/angular/services/ajax.service.js
+++ voyageone-static/2.0/develop/components/angular/services/ajax.service.js
@@ -21,15 +21,15 @@ AjaxService.prototype.post = function (url, data) {
       defer.reject(null);
       return;
     }
-    // 对后端通知的跳转进行自动处理
-    if (autoRedirect(res) || sessionTimeout(res)) {
+    // 对系统特殊内容进行处理
+    if (sysMessage(res)) {
       return;
     }
     if (res.message || res.code) {
       defer.reject(res);
       return;
     }
-    defer.resolve(res.result);
+    defer.resolve(res);
   }, function (response) {
     defer.reject(null, response);
   });
@@ -40,18 +40,45 @@ AjaxService.prototype.post = function (url, data) {
 var CODE_SYS_REDIRECT = "SYS_REDIRECT";
 // 和 JAVA 同步,回话过期的信息
 var MSG_TIMEOUT = "300001";
+// 同步,DisplayType 枚举
+var DISPLAY_TYPES = {
+  /**
+   * 弹出提示
+   */
+  ALERT: 1,
+  /**
+   * 顶端弹出自动关闭
+   */
+  NOTIFY: 2,
+  /**
+   * 右下弹出自动关闭
+   */
+  POP: 3,
+  /**
+   * 用户自定义处理
+   */
+  CUSTOM: 4
+};
 
+/**
+ * 对系统自动跳转的响应,执行跳转
+ * @param {{code:string,redirectTo:string}} res
+ * @returns {boolean}
+ */
 function autoRedirect(res) {
   if (res.code != CODE_SYS_REDIRECT) {
     return false;
   }
   // 如果跳转数据异常,则默认跳转登陆页
-  location.href = (!res.data || !res.data.redirectTo)
-    ? '/login.html'
-    : res.data.redirectTo;
+  location.href = res.redirectTo || '/login.html';
   return true;
 }
 
+/**
+ * 对会话超时和未登录进行特殊处理
+ * @param {{code:string}} res
+ * @returns {boolean}
+ */
 function sessionTimeout(res) {
   if (res.code != MSG_TIMEOUT) {
     return false;
@@ -59,4 +86,22 @@ function sessionTimeout(res) {
   // 会话超时,默认跳转到登陆页
   location.href = '/login.html';
   return true;
+}
+
+/**
+ *
+ * @param res
+ */
+function autoDisplayMessage(res) {
+  // todo
+  return true;
+}
+
+/**
+ * 对特定类型(依据 Code 判断)的信息进行处理
+ * @param {{message:string,code:string,redirectTo:string,displayType:DISPLAY_TYPES}} res
+ * @returns {boolean}
+ */
+function sysMessage(res) {
+  return autoRedirect(res) || sessionTimeout(res) || autoDisplayMessage(res);
 }
