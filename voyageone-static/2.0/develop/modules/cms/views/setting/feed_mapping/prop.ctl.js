3f3a2af7b744761638dba983ebff26c616a281b6
yoenka <yoenka.yu@voyageone.cn>
2015-12-08 15:04:35 +0800

    add new html

---
+++ voyageone-static/2.0/develop/modules/cms/views/setting/feed_mapping/prop.ctl.js
@@ -0,0 +1,12 @@
+/**
+ * Created by linanbin on 15/12/7.
+ */
+define([
+    'modules/cms/controller/popup.ctl'
+], function () {
+
+    return function ($scope) {
+
+        $scope.vm = {"searchInfo": {}, "masterData": {}};
+    };
+});
