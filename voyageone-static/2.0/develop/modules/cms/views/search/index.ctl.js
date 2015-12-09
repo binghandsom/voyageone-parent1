a07a1f068677a498e099559e6bce871729e7e2d1
edward.lin <edward.lin@voyageone.cn>
2015-12-09 20:37:50 +0800

    20151209  edward.lin                          add vpagination directive

--- voyageone-static/2.0/develop/modules/cms/views/search/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/search/index.ctl.js
@@ -14,6 +14,10 @@ define([
         $scope.clear = clear;
         $scope.search = search;
         $scope.export = exportFile;
+        $scope.getGroupList = getGroupList;
+        $scope.getProductList = getProductList;
+        $scope.groupPageOption = {curr: 1, total: 250, size: 20, fetch: getGroupList};
+        $scope.productPageOption = {curr: 1, total: 250, size: 20, fetch: getProductList};
 
         function clear () {
             $scope.vm.searchInfo = {};
@@ -24,6 +28,15 @@ define([
         }
 
         function exportFile () {
+
+        }
+
+        function getGroupList () {
+
+            console.log($scope.groupPageOption);
+        }
+
+        function getProductList () {
 
         }
     };
