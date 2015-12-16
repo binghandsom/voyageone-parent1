f15eaa6e14f510bc3ccb2c843c8f9985cf975502
edward.lin <edward.lin@voyageone.cn>
2015-12-16 16:01:18 +0800

    20151216  edward.lin add type with blank option

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/search/CmsSearchIndexService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/search/CmsSearchIndexService.java
@@ -4,12 +4,12 @@ import com.voyageone.common.Constants;
 import com.voyageone.common.configs.Enums.TypeConfigEnums;
 import com.voyageone.common.configs.TypeChannel;
 import com.voyageone.web2.cms.dao.CmsPromotionDao;
+import com.voyageone.web2.cms.model.CmsBtPromotionModel;
 import com.voyageone.web2.core.bean.UserSessionBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Edward
@@ -43,16 +43,39 @@ public class CmsSearchIndexService {
         masterData.put("priceTypeList", TypeConfigEnums.MastType.priceType.getList(language));
 
         // 获取compare type
-        masterData.put("compareTypeList", TypeConfigEnums.MastType.compareType.getList(language));
+        masterData.put("compareTypeList", TypeConfigEnums.MastType.compareType.getListWithBlank(language));
 
         // 获取brand list
-        masterData.put("brandList", TypeChannel.getOptions(Constants.comMtType.BRAND, userInfo.getSelChannelId()));
+        masterData.put("brandList", TypeChannel.getOptionsWithBlank(Constants.comMtType.BRAND, userInfo.getSelChannelId(), language));
 
         // 获取promotion list
         Map<String, Object> params = new HashMap<>();
         params.put("channelId", userInfo.getSelChannelId());
-        masterData.put("promotionList", cmsPromotionDao.getPromotionList(params));
+
+        List<CmsBtPromotionModel> promotionList = cmsPromotionDao.getPromotionList(params);
+
+        masterData.put("promotionList", promotionList);
+        masterData.put("promotionWithBlankList", addBlankInPromotionList(promotionList));
 
         return masterData;
+    }
+
+    /**
+     * 添加
+     * @param promotionList
+     * @return
+     */
+    private List<CmsBtPromotionModel> addBlankInPromotionList (List<CmsBtPromotionModel> promotionList) {
+        List<CmsBtPromotionModel> returnInfo = new ArrayList<CmsBtPromotionModel>();
+
+        CmsBtPromotionModel blankPromotion = new CmsBtPromotionModel();
+        blankPromotion.setPromotionId(null);
+        blankPromotion.setPromotionName("Select...");
+
+        returnInfo.add(blankPromotion);
+        if(promotionList != null && promotionList.size() > 0)
+            returnInfo.addAll(promotionList);
+
+        return returnInfo;
     }
 }
