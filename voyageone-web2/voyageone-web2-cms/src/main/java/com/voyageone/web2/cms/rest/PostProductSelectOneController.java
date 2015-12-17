0f28187220b9ba2f36deff26118cca32b1856040
james <james>
2015-12-17 14:00:56 +0800

    chuanyu.liang add param

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/rest/PostProductSelectOneController.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/rest/PostProductSelectOneController.java
@@ -2,8 +2,10 @@ package com.voyageone.web2.cms.rest;
 
 import com.voyageone.cms.service.model.CmsBtProductModel;
 import com.voyageone.web2.cms.CmsRestController;
+import com.voyageone.web2.sdk.api.request.PostProductSelectOneRequest;
 import com.voyageone.web2.sdk.api.response.PostProductSelectOneResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,8 +33,8 @@ public class PostProductSelectOneController extends CmsRestController {
      * @return
      */
     @RequestMapping("selectOne")
-    public PostProductSelectOneResponse selectOne() {
-        CmsBtProductModel model = productService.selectOne(null);
+    public PostProductSelectOneResponse selectOne(@RequestBody PostProductSelectOneRequest responseMode) {
+        CmsBtProductModel model = productService.selectOne(responseMode);
 
         PostProductSelectOneResponse result = new PostProductSelectOneResponse();
         result.setProduct(model);
