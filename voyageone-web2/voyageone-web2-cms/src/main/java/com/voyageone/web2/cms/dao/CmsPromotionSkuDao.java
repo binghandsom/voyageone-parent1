9051331b44302d12fe23ade208a0362566850dd9
Jerry.ji <jerry.ji@voyageone.cn>
2015-12-29 10:49:00 +0800

    Promotion 下载文件作成

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/dao/CmsPromotionSkuDao.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/dao/CmsPromotionSkuDao.java
@@ -2,11 +2,13 @@ package com.voyageone.web2.cms.dao;
 
 import com.voyageone.base.dao.BaseDao;
 import com.voyageone.web2.cms.model.CmsBtPromotionCodeModel;
+import com.voyageone.web2.cms.model.CmsBtInventoryOutputTmpModel;
 import com.voyageone.web2.cms.model.CmsBtPromotionGroupModel;
 import com.voyageone.web2.cms.model.CmsBtPromotionSkuModel;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -49,5 +51,35 @@ public class CmsPromotionSkuDao extends BaseDao{
         params.setPromotionId(promotionId);
         params.setProductId(productId);
         return updateTemplate.delete("delete_cms_bt_promotion_sku", params);
+    }
+    public boolean insertSkuInventoryInfo(String values) {
+        boolean ret = true;
+
+        Map<String, String> dataMap = new HashMap<String, String>();
+        dataMap.put("values", values);
+
+        int retCount = updateTemplate.insert("wms_bt_inventory_center_output_tmp_insertSKUData", dataMap);
+
+        return  ret;
+    }
+
+    public boolean delSkuInventoryInfo() {
+        updateTemplate.delete("wms_bt_inventory_center_output_tmp_deleteAll");
+
+        return true;
+    }
+
+    public int getSkuInventoryInfoRecCount() {
+        return selectOne("wms_bt_inventory_center_output_tmp_getRecCount");
+    }
+
+    public List<CmsBtInventoryOutputTmpModel> getSkuInventoryInfoRecInfo(int offset,int pagesize) {
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        dataMap.put("offset", offset * pagesize);
+        dataMap.put("pagesize", pagesize);
+
+        List<CmsBtInventoryOutputTmpModel> ret = selectList("wms_bt_inventory_center_output_tmp_getRecInfo", dataMap);
+
+        return  ret;
     }
 }
---
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/dao/mongodb/CmsBtProductDao.java
@@ -0,0 +1,97 @@
+package com.voyageone.web2.cms.dao.mongodb;
+
+import com.mongodb.*;
+import com.voyageone.base.dao.mongodb.BaseMongoDao;
+import com.voyageone.base.dao.mongodb.JomgoQuery;
+import com.voyageone.base.dao.mongodb.model.BulkUpdateModel;
+import com.voyageone.cms.service.model.CmsBtProductModel;
+import com.voyageone.cms.service.model.CmsBtProductModel_Field;
+import com.voyageone.cms.service.model.CmsBtProductModel_Group_Platform;
+import com.voyageone.cms.service.model.CmsBtProductModel_Sku;
+import com.voyageone.common.util.DateTimeUtil;
+import net.minidev.json.JSONObject;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@Repository("web2.cms.CmsBtProductDao")
+public class CmsBtProductDao extends BaseMongoDao {
+
+    @Override
+    public Class getEntityClass() {
+        return CmsBtProductModel.class;
+    }
+
+    /**
+     * 获取商品List 根据GroupId
+     */
+    public List<CmsBtProductModel> selectProductByGroupId(String channelId, long groupId) {
+        String query = "{\"groups.platforms.groupId\":" + groupId + "}";
+        return select(query, channelId);
+    }
+
+    /**
+     * 获取商品Code List 根据CartId（含SKU）
+     * @param channelId
+     * @param cartId
+     * @param pageNo 当前页
+     * @param pageSize 页面大小
+     *
+     */
+    public List<CmsBtProductModel> selectProductByCartId(String channelId, String cartId, int pageNo, int pageSize) {
+        JomgoQuery where = new JomgoQuery();
+        where.setQuery("{\"groups.platforms.cartId\":" + cartId + "}");
+        where.setSkip(pageNo * pageSize);
+        where.setLimit(pageSize);
+        where.setProjection("{\"prodId\":1,\"groups.platforms.cartId.$\":1,\"fields.model\":1,\"fields.code\":1,\"fields.productName\":1,\"batch_update.code_qty\":1,\"fields.salePriceStart\":1,\"fields.salePriceEnd\":1,\"catPath\":1,\"skus\":1}");
+
+        return select(where, channelId);
+    }
+
+    /**
+     * 获取商品Code 数量 根据CartId（含SKU）
+     * @param channelId
+     * @param cartId
+     *
+     */
+    public long selectProductByCartIdRecCount(String channelId, String cartId) {
+        String query = "{\"groups.platforms.cartId\":" + cartId + "}";
+
+        return countByQuery(query, channelId);
+    }
+
+    /**
+     * 获取商品Model List 根据CartId
+     * @param channelId
+     * @param cartId
+     * @param pageNo 当前页
+     * @param pageSize 页面大小
+     *
+     */
+    public List<CmsBtProductModel> selectModelByCartId(String channelId, String cartId, int pageNo, int pageSize) {
+        JomgoQuery where = new JomgoQuery();
+//        where.setQuery("{\"groups.platforms.cartId\":" + cartId + ",\"groups.platforms.isMain\":1}");
+        where.setQuery("{\"groups.platforms.cartId\":" + cartId + ",\"groups.platforms.isMain\":0}");
+        where.setSkip(pageNo * pageSize);
+        where.setLimit(pageSize);
+        where.setProjection("{\"groups.platforms.cartId.$\":1,\"groups.salePriceStart\":1,\"groups.salePriceEnd\":1,\"prodId\":1,\"fields.model\":1}");
+
+        return select(where, channelId);
+    }
+
+    /**
+     * 获取商品Model 数量 根据CartId（含SKU）
+     * @param channelId
+     * @param cartId
+     *
+     */
+    public long selectModelByCartIdRecCount(String channelId, String cartId) {
+//        String query = "{\"groups.platforms.cartId\":" + cartId + ",\"groups.platforms.isMain\":1}";
+        String query = "{\"groups.platforms.cartId\":" + cartId + ",\"groups.platforms.isMain\":0}";
+
+        return countByQuery(query, channelId);
+    }
+}
