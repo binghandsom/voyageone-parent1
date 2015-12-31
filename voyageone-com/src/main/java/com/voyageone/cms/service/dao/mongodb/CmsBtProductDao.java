09aabb8f18b46cffe16641fe6854c5400be3c975
Jerry.ji <jerry.ji@voyageone.cn>
2015-12-31 16:41:36 +0800

    Tag SDK方式 作成

--- voyageone-com/src/main/java/com/voyageone/cms/service/dao/mongodb/CmsBtProductDao.java
+++ voyageone-com/src/main/java/com/voyageone/cms/service/dao/mongodb/CmsBtProductDao.java
@@ -63,6 +63,14 @@ public class CmsBtProductDao extends BaseMongoPartDao {
     }
 
     /**
+     * 获取商品List 根据TagId
+     */
+    public List<CmsBtProductModel> selectProductByTagId(String channelId, Integer tagId) {
+        String query = "{\"tags\":{$regex:\".*-" + tagId + "-.*\"}}";
+        return select(query, channelId);
+    }
+
+    /**
      * 获取SKUList 根据prodId
      */
     public List<CmsBtProductModel_Sku> selectSKUById(String channelId, long prodId) {
