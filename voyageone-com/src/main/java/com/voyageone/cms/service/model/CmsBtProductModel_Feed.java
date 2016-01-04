26c47699fdfbb6f3ca68806f872efa6c02f4b177
chuanyu.liang <chuanyu.liang@voyageone.cn>
2016-01-04 14:34:59 +0800

    add customIds

--- voyageone-com/src/main/java/com/voyageone/cms/service/model/CmsBtProductModel_Feed.java
+++ voyageone-com/src/main/java/com/voyageone/cms/service/model/CmsBtProductModel_Feed.java
@@ -3,6 +3,9 @@ package com.voyageone.cms.service.model;
 
 import com.voyageone.base.dao.mongodb.model.BaseMongoMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 的商品Model feedAtts
  * @author chuanyu.liang, 12/11/15
@@ -10,6 +13,12 @@ import com.voyageone.base.dao.mongodb.model.BaseMongoMap;
  * @since 2.0.0
  */
 public class CmsBtProductModel_Feed extends BaseMongoMap<String, Object> {
+
+    private BaseMongoMap<String, Object> orgAtts = new BaseMongoMap<>();
+
+    private BaseMongoMap<String, Object> cnAtts = new BaseMongoMap<>();
+
+    private List<String> customIds = new ArrayList<>();
 
 //    public String getCatId() {
 //        return getAttribute("catId");
@@ -19,14 +28,36 @@ public class CmsBtProductModel_Feed extends BaseMongoMap<String, Object> {
 //        setAttribute("catId", catId);
 //    }
 
-    public String getModelCode() {
-        return getAttribute("modelCode");
+//    public String getModelCode() {
+//        return getAttribute("modelCode");
+//    }
+//
+//    public void setModelCode(String modelCode) {
+//        setAttribute("modelCode", modelCode);
+//    }
+
+
+    public BaseMongoMap<String, Object> getOrgAtts() {
+        return orgAtts;
     }
 
-    public void setModelCode(String modelCode) {
-        setAttribute("modelCode", modelCode);
+    public void setOrgAtts(BaseMongoMap<String, Object> orgAtts) {
+        this.orgAtts = orgAtts;
     }
 
+    public BaseMongoMap<String, Object> getCnAtts() {
+        return cnAtts;
+    }
 
+    public void setCnAtts(BaseMongoMap<String, Object> cnAtts) {
+        this.cnAtts = cnAtts;
+    }
 
+    public List<String> getCustomIds() {
+        return customIds;
+    }
+
+    public void setCustomIds(List<String> customIds) {
+        this.customIds = customIds;
+    }
 }
