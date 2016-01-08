f0107025c0d3aaa657b9e106657ed4b85b46d8d3
jack.zhao <jack.zhao@voyageone.cn>
2016-01-08 18:19:34 +0800

    WMF状态的定义

---
+++ voyageone-wsc1/voyageone-wsc1-magento/src/main/java/com/voyageone/common/magento/api/MagentoConstants.java
@@ -0,0 +1,14 @@
+package com.voyageone.common.magento.api;
+
+/**
+ * Created by jack.zhao on 2015-12-01.
+ */
+public class MagentoConstants {
+
+    //WmFStatus
+    public static final class WMF_Status {
+        public final static String Shipped = "Shipped";
+        public final static String Cancelled =  "Cancelled";
+    }
+
+}
