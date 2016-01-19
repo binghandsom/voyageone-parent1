1d3a5db441eb10747b7d11f8ee1d60f297b8afdb
Jonas <jonas.gao@voyageone.cn>
2016-01-19 20:18:50 +0800

    format cookie.service.js & channel.js

--- voyageone-static/2.0/develop/channel.js
+++ voyageone-static/2.0/develop/channel.js
@@ -7,49 +7,53 @@
  */
 
 require.config({
-  paths: {
-    'angular': 'libs/angular.js/1.5.0-RC.0/angular.min',
-    'angular-block-ui': 'libs/angular-block-ui/0.2.1/angular-block-ui.min',
-    'angular-cookies': 'libs/angular.js/1.4.7/angular-cookies.min',
-    'voyageone-angular-com' : 'components/dist/voyageone.angular.com',
-    'css': 'libs/require-css/0.1.8/css.min'
-  },
-  shim: {
-    'angular-block-ui': ['angular', 'css!libs/angular-block-ui/0.2.1/angular-block-ui.min.css'],
-    'voyageone-angular-com': ['angular'],
-    'angular-cookies': ['angular'],
-    'angular': {exports: 'angular'}
-  }
+    paths: {
+        'angular': 'libs/angular.js/1.5.0-RC.0/angular.min',
+        'angular-block-ui': 'libs/angular-block-ui/0.2.1/angular-block-ui.min',
+        'angular-cookies': 'libs/angular.js/1.4.7/angular-cookies.min',
+        'voyageone-angular-com': 'components/dist/voyageone.angular.com',
+        'css': 'libs/require-css/0.1.8/css.min'
+    },
+    shim: {
+        'angular-block-ui': ['angular', 'css!libs/angular-block-ui/0.2.1/angular-block-ui.min.css'],
+        'voyageone-angular-com': ['angular'],
+        'angular-cookies': ['angular'],
+        'angular': {exports: 'angular'}
+    }
 });
 
 // Bootstrap App !!
 require([
-  'angular',
-  'angular-block-ui',
-  'angular-cookies',
-  'voyageone-angular-com'
+    'angular',
+    'angular-block-ui',
+    'angular-cookies',
+    'voyageone-angular-com'
 ], function (angular) {
-  angular.module('voyageone.cms.channel', [
-    'blockUI',
-    'ngCookies',
-    'voyageone.angular'
-  ]).controller('channelController', function($scope, $ajax, cookieService, $window) {
-    $ajax.post('/core/access/user/getChannel').then(function(res) {
-      $scope.channels = res.data;
-    }, function(res) { alert(res.message || res.code); });
-    $scope.choose = function(channel, app) {
-      $ajax.post('/core/access/user/selectChannel', {channelId: channel.channelId}).then(function(res) {
-        cookieService.application(app);
-        location.href = 'modules/' + app + '/app.html';
-      }, function(res) { alert(res.message || res.code); })
-    };
-    $scope.logout = function () {
-      $ajax.post('/core/access/user/logout')
-          .then(function () {
-            cookieService.removeAll();
-            $window.location = '/login.html#/';
-          });
-    }
-  });
-  return angular.bootstrap(document, ['voyageone.cms.channel']);
+    angular.module('voyageone.cms.channel', [
+        'blockUI',
+        'ngCookies',
+        'voyageone.angular'
+    ]).controller('channelController', function ($scope, $ajax, cookieService, $window) {
+        $ajax.post('/core/access/user/getChannel').then(function (res) {
+            $scope.channels = res.data;
+        }, function (res) {
+            alert(res.message || res.code);
+        });
+        $scope.choose = function (channel, app) {
+            $ajax.post('/core/access/user/selectChannel', {channelId: channel.channelId}).then(function (res) {
+                cookieService.application(app);
+                location.href = 'modules/' + app + '/app.html';
+            }, function (res) {
+                alert(res.message || res.code);
+            })
+        };
+        $scope.logout = function () {
+            $ajax.post('/core/access/user/logout')
+                .then(function () {
+                    cookieService.removeAll();
+                    $window.location = '/login.html#/';
+                });
+        }
+    });
+    return angular.bootstrap(document, ['voyageone.cms.channel']);
 });
