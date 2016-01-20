e5264ff9271d3e000571e7b269dd8c06c73d3204
aooer <bingbing.gao@voyageone.cn>
2016-01-20 19:59:45 +0800

    错误管理

---
+++ voyageone-web2/voyageone-web2-wsdl/src/main/java/com/voyageone/web2/cms/wsdl/service/BusinessLogService.java
@@ -0,0 +1,36 @@
+package com.voyageone.web2.cms.wsdl.service;
+
+import com.voyageone.web2.cms.wsdl.BaseService;
+import com.voyageone.web2.cms.wsdl.dao.CmsBtBusinessLogDao;
+import com.voyageone.web2.sdk.api.domain.CmsBtBusinessLogModel;
+import com.voyageone.web2.sdk.api.request.BusinessLogGetRequest;
+import com.voyageone.web2.sdk.api.response.BusinessLogGetResponse;
+import com.voyageone.web2.sdk.api.util.SdkBeanUtils;
+import org.apache.commons.beanutils.BeanMap;
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author aooer 2016/1/20.
+ * @version 2.0.0
+ * @since 2.0.0
+ */
+@Service
+public class BusinessLogService extends BaseService {
+
+    @Autowired
+    private CmsBtBusinessLogDao cmsBtBusinessLogDao;
+
+    public BusinessLogGetResponse findList(BusinessLogGetRequest request){
+        request.check();
+        List<CmsBtBusinessLogModel> models=cmsBtBusinessLogDao.findByCondition(new BeanMap(request));
+        BusinessLogGetResponse response =new BusinessLogGetResponse();
+        response.setCmsBtBusinessLogModels(models);
+        response.setTotalCount(Integer.toUnsignedLong(models.size()));
+        return response;
+    }
+
+}
