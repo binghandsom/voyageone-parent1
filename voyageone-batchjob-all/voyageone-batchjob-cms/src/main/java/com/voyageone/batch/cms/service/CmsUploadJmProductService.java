cc3b4528b562c702f75d541d82f09f156f1cc723
james <james>
2016-01-25 15:00:57 +0800

    JM 上新

---
+++ voyageone-batchjob-all/voyageone-batchjob-cms/src/main/java/com/voyageone/batch/cms/service/CmsUploadJmProductService.java
@@ -0,0 +1,30 @@
+package com.voyageone.batch.cms.service;
+
+import com.voyageone.batch.base.BaseTaskService;
+import com.voyageone.batch.core.modelbean.TaskControlBean;
+import com.voyageone.common.components.issueLog.enums.SubSystem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author james.li on 2016/1/25.
+ * @version 2.0.0
+ */
+@Service
+public class CmsUploadJmProductService extends BaseTaskService {
+    @Override
+    public SubSystem getSubSystem() {
+        return SubSystem.CMS;
+    }
+
+    @Override
+    public String getTaskName() {
+        return "CmsUploadJmProductJob";
+    }
+
+    @Override
+    protected void onStartup(List<TaskControlBean> taskControlList) throws Exception {
+
+    }
+}
