596025e00c30561c105a694d37d4198e2af3e6c6
james <james>
2016-01-26 17:02:03 +0800

    update

--- voyageone-batchjob-all/voyageone-batchjob-cms/src/main/java/com/voyageone/batch/cms/model/JmBtProductImportModel.java
+++ voyageone-batchjob-all/voyageone-batchjob-cms/src/main/java/com/voyageone/batch/cms/model/JmBtProductImportModel.java
@@ -23,11 +23,17 @@ public class JmBtProductImportModel {
 
     private Integer brandId;
 
+    private String brandName;
+
+    private String sizeType;
+
     private String productName;
 
     private String foreignLanguageName;
 
     private String functionIds;
+
+    private String uploadErrorInfo;
 
     private String synFlg;
 
@@ -207,5 +213,29 @@ public class JmBtProductImportModel {
 
     public void setJmBtDealImportModel(JmBtDealImportModel jmBtDealImportModel) {
         this.jmBtDealImportModel = jmBtDealImportModel;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getSizeType() {
+        return sizeType;
+    }
+
+    public void setSizeType(String sizeType) {
+        this.sizeType = sizeType;
+    }
+
+    public String getUploadErrorInfo() {
+        return uploadErrorInfo;
+    }
+
+    public void setUploadErrorInfo(String uploadErrorInfo) {
+        this.uploadErrorInfo = uploadErrorInfo;
     }
 }
