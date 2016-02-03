9ce7dcb6c8a9fd66eeb47bec75837fee489da5df
RobertTong <robert.tong@voyageone.cn>
2016-02-03 16:13:39 +0800

    Update GiltSalesServiceTest

--- voyageone-com/src/test/java/com/voyageone/common/components/gilt/GiltSalesServiceTest.java
+++ voyageone-com/src/test/java/com/voyageone/common/components/gilt/GiltSalesServiceTest.java
@@ -5,6 +5,7 @@ import com.voyageone.common.components.gilt.bean.GiltSale;
 import com.voyageone.common.components.gilt.bean.GiltSku;
 import com.voyageone.common.configs.beans.ShopBean;
 import com.voyageone.common.util.JsonUtil;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
-
+import static org.junit.Assert.*;
 /**
  * @author aooer 2016/2/3.
  * @version 2.0.0
@@ -21,17 +22,21 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class GiltSalesServiceTest {
+    private static final String url="https://api-sandbox.gilt.com/global/";
+    private static final String tokenKey="YTE5N2YzM2M1ZmFhZmRjZDY3YmZiNjgxMzJiYTgzNGY6";
+    private ShopBean shopBean;
 
+    @Before
+    public void before() {
+        shopBean=new ShopBean();
+        shopBean.setApp_url(url);
+        shopBean.setAppKey(tokenKey);
+    }
     @Autowired
     private GiltSalesService giltSalesService;
 
     @Test
     public void testGetSaleSkusById() throws Exception {
-
-        ShopBean shopBean=new ShopBean();
-        shopBean.setApp_url("https://api-sandbox.gilt.com/global/");
-        shopBean.setAppKey("YTE5N2YzM2M1ZmFhZmRjZDY3YmZiNjgxMzJiYTgzNGY6");
-
         GiltPageGetSaleAttrRequest request=new GiltPageGetSaleAttrRequest();
         request.setLimit(2);
         request.setOffset(0);
@@ -40,15 +45,11 @@ public class GiltSalesServiceTest {
 
         List<GiltSku> skus= giltSalesService.getSaleSkusById(shopBean,request);
         System.out.println("Retrun:"+ JsonUtil.getJsonString(skus));
+        assertTrue(skus.size()>0);
     }
 
     @Test
     public void testGetAllSales() throws Exception {
-        ShopBean shopBean=new ShopBean();
-        shopBean.setApp_url("https://api-sandbox.gilt.com/global/");
-        shopBean.setAppKey("YTE5N2YzM2M1ZmFhZmRjZDY3YmZiNjgxMzJiYTgzNGY6");
-
-
         List<GiltSale> skus= giltSalesService.getAllSales(shopBean);
         System.out.println("Retrun:"+ JsonUtil.getJsonString(skus));
 
@@ -56,10 +57,6 @@ public class GiltSalesServiceTest {
 
     @Test
     public void testGetSaleById() throws Exception {
-        ShopBean shopBean=new ShopBean();
-        shopBean.setApp_url("https://api-sandbox.gilt.com/global/");
-        shopBean.setAppKey("YTE5N2YzM2M1ZmFhZmRjZDY3YmZiNjgxMzJiYTgzNGY6");
-
         GiltPageGetSaleAttrRequest request=new GiltPageGetSaleAttrRequest();
         request.setLimit(2);
         request.setOffset(0);
