b5a281d67162e4d5d021b0249f18808fd97b833d
james <james>
2016-02-29 13:08:39 +0800

    bug fix

--- voyageone-static/2.0/develop/modules/cms/views/pop/file/import.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/file/import.ctl.js
@@ -16,21 +16,26 @@ define([
         $scope.initialize  = function () {
 
         }
-        $scope.upload = function(){
-            uploader.queue[0].formData = [{"promotionId":data}];
-            uploader.queue[0].upload();
-            $scope.vm.messager ="读入中";
-        }
+$scope.upload = function(){
+    uploader.queue[0].formData = [{"promotionId":data}];
+    uploader.queue[0].upload();
+    $scope.vm.messager ="读入中";
+}
 
-        uploader.onProgressItem = function(fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
-        };
+uploader.onProgressItem = function(fileItem, progress) {
+    console.info('onProgressItem', fileItem, progress);
+};
 
-        uploader.onSuccessItem = function(fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-            $scope.vm.messager ="读入完毕！成功"+response.data.succeed.length +"条  失败"+response.data.fail.length +"条";
-        };
-    });
+uploader.onSuccessItem = function(fileItem, response, status, headers) {
+    console.info('onSuccessItem', fileItem, response, status, headers);
+    if(response.data){
+        $scope.vm.messager ="读入完毕！成功"+response.data.succeed.length +"条  失败"+response.data.fail.length +"条";
+    }else{
+        $scope.vm.messager =response.code;
+    }
+
+};
+});
 
     //return function ($scope,promotionService) {
     //
