3ba8932c8aca3524840b5b9856f5478547607784
dell <s@fed.com>
2016-03-01 19:13:15 +0800

    ant

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/channel/CmsFeedCustPropService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/channel/CmsFeedCustPropService.java
@@ -4,13 +4,17 @@ import com.voyageone.cms.service.dao.mongodb.CmsMtFeedCategoryTreeDao;
 import com.voyageone.cms.service.model.CmsFeedCategoryModel;
 import com.voyageone.cms.service.model.CmsMtFeedCategoryTreeModelx;
 import com.voyageone.web2.base.BaseAppService;
-import com.voyageone.web2.core.bean.UserSessionBean;
 import com.voyageone.web2.cms.dao.CmsBtFeedCustomPropDao;
+import com.voyageone.web2.core.bean.UserSessionBean;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Service;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,6 +37,14 @@ public class CmsFeedCustPropService extends BaseAppService {
     public List<CmsFeedCategoryModel> getTopCategories(UserSessionBean user) {
         CmsMtFeedCategoryTreeModelx treeModelx = cmsMtFeedCategoryTreeDao.findFeedCategoryx(user.getSelChannelId());
         return treeModelx.getCategoryTree();
+    }
+
+    // 根据类目路径查询自定义已翻译属性信息
+    public List<Map<String, Object>> selectAllAttr(String channelId, String catPath) {
+        Map<String, Object> params = new HashMap<String, Object>(2);
+        params.put("channelId", channelId);
+        params.put("feedCatPath", catPath);
+        return cmsBtFeedCustomPropDao.selectAllAttr(params);
     }
 
     // 根据类目路径查询自定义已翻译属性信息
@@ -65,5 +77,42 @@ public class CmsFeedCustPropService extends BaseAppService {
         Criteria criteria = Criteria.where("channelId").is(channelId);
         query.addCriteria(criteria);
         return mongoTemplate.find(query, Object.class, "cms_mt_feed_category_tree");
+        //TODO-- 这里只能使用Object对象来影射，不能使用Map.class，可能是spring mongoTemplate的问题
+    }
+
+    public boolean isAttrExist(Map<String, Object> params, String catPath, String chnId) {
+        Map<String, Object> sqlPara = new HashMap<String, Object>();
+        sqlPara.putAll(params);
+        sqlPara.put("cat_path", catPath);
+        sqlPara.put("channelId", chnId);
+        return cmsBtFeedCustomPropDao.isAttrExist(sqlPara);
+    }
+
+    @Transactional
+    public void saveAttr( List<Map<String, Object>> addList,  List<Map<String, Object>> updList, String catPath, UserSessionBean userInfo) {
+        if (addList.size() > 0) {
+            Map<String, Object> params = new HashMap<String, Object>(4);
+            params.put("channelId", userInfo.getSelChannelId());
+            params.put("cat_path", catPath);
+            params.put("userName", userInfo.getUserName());
+            params.put("list", addList);
+            int tslt = cmsBtFeedCustomPropDao.addAttr(params);
+            if (tslt != addList.size()) {
+                logger.error("添加属性结果与期望不符：添加条数=" + addList.size() + " 实际更新件数=" + tslt);
+            } else {
+                logger.debug("添加属性成功 实际更新件数=" + tslt);
+            }
+        }
+        if (updList.size() > 0) {
+            Map<String, Object> params = new HashMap<String, Object>(2);
+            params.put("userName", userInfo.getUserName());
+            params.put("list", updList);
+            int tslt = cmsBtFeedCustomPropDao.updateAttr(params);
+            if (tslt != addList.size()) {
+                logger.error("修改属性结果与期望不符：添加条数=" + addList.size() + " 实际更新件数=" + tslt);
+            } else {
+                logger.debug("修改属性成功 实际更新件数=" + tslt);
+            }
+        }
     }
 }
