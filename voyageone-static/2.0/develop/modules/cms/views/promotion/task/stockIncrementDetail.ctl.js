e2e4749e450230efee4196d1ddb87998d35ae4bf
edward.lin <edward.lin@voyageone.cn>
2016-03-08 15:07:21 +0800

    add cms2 - new stock task

--- voyageone-static/2.0/develop/modules/cms/views/promotion/task/stockIncrementDetail.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/promotion/task/stockIncrementDetail.ctl.js
@@ -2,7 +2,18 @@
  * Created by linanbin on 15/12/7.
  */
 define([
-    'angularAMD',
+    'cms',
     'modules/cms/controller/popup.ctl'
-], function (angularAMD) {
+], function (cms) {
+    cms.controller("taskStockIncrementDetailController", (function () {
+
+        function TaskStockIncrementDetailController() {
+        }
+
+        TaskStockIncrementDetailController.prototype = {
+        };
+
+        return TaskStockIncrementDetailController;
+
+    })())
 });
