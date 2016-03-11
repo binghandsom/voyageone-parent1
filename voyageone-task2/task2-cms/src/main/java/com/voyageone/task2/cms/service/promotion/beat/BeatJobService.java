8a4fd39d045acd93461ee7a21a3bb45517b1d502
Jonas <jonas.gao@voyageone.cn>
2016-03-11 14:39:12 +0800

    beat 2: fix revert

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/promotion/beat/BeatJobService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/promotion/beat/BeatJobService.java
@@ -221,8 +221,15 @@ public class BeatJobService extends BaseTaskService {
             if (configBean.isNeed_vimage()) {
                 if (tbItemSchema.hasVerticalImage())
                     tbItemSchema.setVerticalImage(getTaobaoVerticalImageUrl());
-                else
-                    images.put(2, getTaobaoImageUrl(beatInfoModel.getPromotion_code().getImage_url_2()));
+                else {
+                    String url_key2 = beatInfoModel.getPromotion_code().getImage_url_2();
+                    if (!StringUtils.isEmpty(url_key2))
+                        images.put(2, getTaobaoImageUrl(beatInfoModel.getPromotion_code().getImage_url_2()));
+                    else {
+                        // 如果不为空, 则还原和刷图有区别, 还原需要清空
+                        images.put(2, beatInfoModel.getBeatFlag().equals(BeatFlag.REVERT) ? "" : images.get(1));
+                    }
+                }
             }
             tbItemSchema.setMainImage(images);
             TmallItemSchemaUpdateResponse updateResponse = tbItemService.updateFields(shopBean, tbItemSchema);
