8176de762a6c7da9d97ed40c61db0c0accd054c6
jeff.duan <jeff.duan@voyageone.cn>
2016-03-15 15:17:36 +0800

    库存隔离apidoc设计

--- voyageone-static/2.0/develop/modules/cms/views/pop/promotion/newMrbStockSku.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/promotion/newMrbStockSku.ctl.js
@@ -1,11 +1,50 @@
-/**
- * Created by linanbin on 15/12/7.
- */
 define([
-    'angularAMD',
-    'modules/cms/controller/popup.ctl'
-], function (angularAMD) {
+    'cms'
+], function (cms) {
+    cms.controller('popNewMrbStockSkuCtl', (function () {
 
-    angularAMD.controller('popNewMrbStockSkuCtl', function ($scope) {
-    });
+        function PopNewMrbStockSkuCtl(data, taskStockService, confirm, alert, notify) {
+            this.alert = alert;
+            this.notify = notify;
+            this.confirm = confirm;
+            this.taskId = data.taskId;
+            this.platformList = data.platformList;
+            this.taskStockService = taskStockService;
+            this.model;
+            this.code;
+            this.sku;
+            this.usableStock;
+            this.platformStockList;
+        }
+
+        PopNewMrbStockSkuCtl.prototype = {
+            getUsableStock: function () {
+                var main = this;
+                main.taskStockService.getUsableStock({
+                    "taskId": main.task_id,
+                    "sku": main.sku
+                }).then(function (res) {
+                    main.usableStock = res.data.usableStock;
+                });
+            },
+
+            saveNewRecord: function () {
+                var main = this;
+                main.taskStockService.saveNewRecord({
+                    "taskId": main.task_id,
+                    "model": main.model,
+                    "code": main.code,
+                    "sku": main.sku,
+                    "usableStock": main.usableStock,
+                    "platformStockList": main.platformStockList
+                }).then(function (res) {
+                    main.notify.success('更新成功');
+                }, function (err) {
+                    //main.alert('TXT_MSG_UPDATE_FAIL');
+                });
+            },
+        };
+
+        return PopNewMrbStockSkuCtl;
+    })());
 });
