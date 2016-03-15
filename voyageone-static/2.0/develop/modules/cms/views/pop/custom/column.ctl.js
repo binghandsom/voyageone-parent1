0e5fbf336792e359f565d85a50927f2ba963e3ab
carrie <carrie.zhang@voyageone.cn>
2016-03-15 18:39:59 +0800

    search advance add popup

--- voyageone-static/2.0/develop/modules/cms/views/pop/custom/column.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/custom/column.ctl.js
@@ -2,10 +2,26 @@
  * Created by linanbin on 15/12/7.
  */
 define([
-    'angularAMD',
-    'modules/cms/controller/popup.ctl'
+    'angularAMD'
 ], function (angularAMD) {
 
-    angularAMD.controller('popCustomColumnCtl', function ($scope) {
+    angularAMD.controller('popCustomColumnCtl', function ($scope,$searchAdvanceService) {
+        $scope.cus = {
+            customProps:[],
+            commonProps:[]
+        };
+        $scope.initialize = initialize;
+        /**
+         * 初始化数据.
+         */
+        function initialize () {
+            $searchAdvanceService.getCustColumnsInfo()
+                .then(function (res) {
+                $scope.cus.customProps = res.data.customProps;
+                $scope.cus.commonProps = res.data.commonProps;
+            })
+        }
+
     });
+
 });
