e2731be0d78424e841fc0da7a9bd496d6d51352f
carrie <carrie.zhang@voyageone.cn>
2016-03-16 13:42:01 +0800

    search advance add popup

--- voyageone-static/2.0/develop/modules/cms/views/pop/custom/column.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/custom/column.ctl.js
@@ -8,9 +8,16 @@ define([
     angularAMD.controller('popCustomColumnCtl', function ($scope,$searchAdvanceService) {
         $scope.cus = {
             customProps:[],
+            commonProps:[],
+            feed_prop_original:"",
+            propId:""
+        };
+        $scope.cusData = {
+            customProps:[],
             commonProps:[]
         };
         $scope.initialize = initialize;
+        //$scope.isSelect = isSelect;
         /**
          * 初始化数据.
          */
@@ -21,6 +28,41 @@ define([
                 $scope.cus.commonProps = res.data.commonProps;
             })
         }
+
+        /**
+         * 确定返回，数据传回前端及用户自定义保存
+         */
+
+
+        $scope.ok = function(){
+
+            console.log($scope.cusData.customProps);
+        };
+
+        var updateSelected = function(action,item,inx){
+            if(action == 'add'){
+                $scope.cusData.customProps.push(item);
+            }
+            if(action == 'remove'){
+
+                $scope.cusData.customProps.splice(inx,1);
+            }
+        };
+
+        $scope.updateSelection = function($event,item,inx){
+            var checkbox = $event.target;
+            var action = (checkbox.checked?'add':'remove');
+
+            updateSelected(action,item,inx);
+        };
+
+        /**
+         * check 是否选中
+         * To do 需要添加一个checkedValue属性
+         */
+        /*$scope.isSelected = function(sel){
+            return sel;
+        };*/
 
     });
 
