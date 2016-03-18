5cd82417b5c161555273f1567cb134d545c4c005
edward.lin <edward.lin@voyageone.cn>
2016-03-18 19:47:31 +0800

    clean gulp-build

--- voyageone-static/2.0/node_scripts/gulp-build.js
+++ voyageone-static/2.0/node_scripts/gulp-build.js
@@ -1,22 +1,11 @@
 var gulp = require('gulp');
-var ngAnnotate = require('gulp-ng-annotate');
-var concat = require('gulp-concat');
 var debug = require('gulp-debug');
-var header = require('gulp-header');
-var footer = require('gulp-footer');
-var minifyCss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var fs = require('fs');
 var suffBuilder = require('./gulp-build-suff');
 var actionsDesc = require('./gulp-build-actions');
 
 var build = require('./vars').build;
 var tasks = require('./vars').tasks;
 
-var definePrefix = 'define(function(){\n';
-var defineSuffix = '});';
-var headerSingle = '(function(){\n';
-var footerSingle = '})();';
 var encode = 'utf-8';
 
 gulp.task(tasks.build.actions, function() {
