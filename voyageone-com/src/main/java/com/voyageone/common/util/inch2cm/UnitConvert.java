ca6cbfbaa3d39d5138d1c92802f3919925eff529
tom <tom.zhu@voyageone.cn>
2016-03-18 14:01:29 +0800

    in2cm

---
+++ voyageone-com/src/main/java/com/voyageone/common/util/inch2cm/UnitConvert.java
@@ -0,0 +1,11 @@
+package com.voyageone.common.util.inch2cm;
+
+/**
+ * Created by dell on 2016/3/4.
+ */
+public class UnitConvert {
+    //英寸转厘米
+    public double inchToCM(double value) {
+        return value * 2.54;
+    }
+}
