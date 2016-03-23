cf886458a873925213cad07c197cb9cdc7983ad2
jeff.duan <jeff.duan@voyageone.cn>
2016-03-23 14:10:51 +0800

    库存隔离代码提交

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/promotion/task/CmsTaskStockIncrementDetailService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/promotion/task/CmsTaskStockIncrementDetailService.java
@@ -4,6 +4,7 @@ import com.voyageone.web2.base.BaseAppService;
 import com.voyageone.web2.cms.dao.CmsBtStockSeparateIncrementItemDao;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,6 +13,7 @@ import java.util.Map;
 /**
  * Created by morse.lu on 2016/3/23.
  */
+@Service
 public class CmsTaskStockIncrementDetailService extends BaseAppService {
 
     @Autowired
