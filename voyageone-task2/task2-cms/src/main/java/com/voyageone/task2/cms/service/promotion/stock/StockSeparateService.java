955658ab6e2aa9988e225e977c0c470176e36259
morse <morse.lu@voyageone.cn>
2016-03-31 11:28:38 +0800

    0331 库存隔离batch temp

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/promotion/stock/StockSeparateService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/promotion/stock/StockSeparateService.java
@@ -1,6 +1,7 @@
 package com.voyageone.task2.cms.service.promotion.stock;
 
 import com.voyageone.common.components.issueLog.enums.SubSystem;
+import com.voyageone.common.components.transaction.TransactionRunner;
 import com.voyageone.service.dao.cms.CmsBtBeatInfoDao;
 import com.voyageone.service.dao.cms.CmsBtStockSeparateItemDao;
 import com.voyageone.service.model.cms.CmsBtBeatInfoModel;
@@ -9,6 +10,7 @@ import com.voyageone.task2.base.modelbean.TaskControlBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +29,9 @@ public class StockSeparateService extends BaseTaskService {
 
     @Autowired
     private CmsBtStockSeparateItemDao cmsBtStockSeparateItemDao;
+
+    @Autowired
+    private TransactionRunner transactionRunner;
 
     /**
      * 获取子系统
@@ -51,11 +56,54 @@ public class StockSeparateService extends BaseTaskService {
      */
     @Override
     protected void onStartup(List<TaskControlBean> taskControlList) throws Exception {
-        Map<String, Object> param = new HashMap<String, Object>();
+        Map<String, Object> param = new HashMap<>();
         param.put("status", stockInfoService.STATUS_WAITING_SEPARATE);
+        $info("开始取得等待隔离数据");
         List<Map<String, Object>> resultData = cmsBtStockSeparateItemDao.selectStockSeparateItem(param);
+        $info("等待隔离数据取得完毕");
+        // 按渠道整理Map<channelId, resultData>
+        Map<String, List<Map<String, Object>>> resultDataByChannel = new HashMap<>();
+        for (Map<String, Object> rowData : resultData) {
+            String channelId = (String) rowData.get("channel_id");
+            if (resultDataByChannel.containsKey(channelId)) {
+                resultDataByChannel.get(channelId).add(rowData);
+            } else {
+                List<Map<String, Object>> listData = new ArrayList<>();
+                listData.add(rowData);
+                resultDataByChannel.put(channelId, listData);
+            }
+        }
+
+        for (String channelId : resultDataByChannel.keySet()) {
+            executeByChannel(channelId, resultDataByChannel.get(channelId));
+        }
+
+    }
+
+    /**
+     * 按渠道把等待隔离数据进行更新
+     *
+     * @param channelId   渠道id
+     * @param listSkuData 该渠道下的sku数据
+     */
+    private void executeByChannel(String channelId, List<Map<String, Object>> listSkuData) {
+        // 渠道对应的所有销售平台取得
+
 
         // 取得可用库存
-//        String usableStock = stockInfoService.getUsableStock();
+        $info("开始取得可用库存数据");
+        Map<String,Integer> skuStockUsableAll = stockInfoService.getUsableStock(channelId);
+        $info("可用库存数据取得完毕");
+
+        $info("更新处理开始,渠道是%s", channelId);
+        try {
+            transactionRunner.runWithTran(() -> {
+
+            });
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            logIssue("cms 库存隔离batch", "渠道是"+ channelId +"的等待隔离数据更新失败. " + e.getMessage());
+        }
+        $info("更新处理结束,渠道是%s", channelId);
     }
 }
