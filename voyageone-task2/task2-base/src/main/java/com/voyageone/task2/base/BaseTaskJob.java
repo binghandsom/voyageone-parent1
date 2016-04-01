41fede6de0fea3dc8c2463f23071dee1c9a73391
aooer <bingbing.gao@voyageone.cn>
2016-04-01 16:57:40 +0800

    aop动态切入日志，

--- voyageone-task2/task2-base/src/main/java/com/voyageone/task2/base/BaseTaskJob.java
+++ voyageone-task2/task2-base/src/main/java/com/voyageone/task2/base/BaseTaskJob.java
@@ -1,10 +1,17 @@
 package com.voyageone.task2.base;
 
+import com.voyageone.base.exception.BusinessException;
 import com.voyageone.common.components.issueLog.IssueLog;
 import com.voyageone.common.components.issueLog.enums.SubSystem;
+import com.voyageone.task2.base.Enums.TaskControlEnums;
+import com.voyageone.task2.base.dao.TaskDao;
+import com.voyageone.task2.base.modelbean.TaskControlBean;
+import com.voyageone.task2.base.util.TaskControlUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * 基础 Job 类
@@ -47,10 +54,55 @@ public abstract class BaseTaskJob {
 
         logger.info(taskCheck + "任务开始");
 
-        getTaskService().startup();
+        startup();
 
         getLogger().info(taskCheck + "任务结束");
 
         running = false;
+    }
+
+    @Autowired
+    protected TaskDao taskDao;
+
+    /**
+     * 获取 job 配置
+     */
+    protected List<TaskControlBean> getControls() {
+        return taskDao.getTaskControlList(getTaskName());
+    }
+
+    public void startup() {
+        // 先获取配置
+        List<TaskControlBean> taskControlList = getControls();
+
+        if (taskControlList.size() < 1) {
+            return;
+        }
+
+        // 是否可以运行的判断
+        if (!TaskControlUtils.isRunnable(taskControlList)) {
+            return;
+        }
+
+        String taskID = TaskControlUtils.getTaskId(taskControlList);
+
+        TaskControlEnums.Status status = TaskControlEnums.Status.START;
+
+        // 任务监控历史记录添加:启动
+        taskDao.insertTaskHistory(taskID, status.getIs());
+
+        try {
+            getTaskService().onStartup(taskControlList);
+            status = TaskControlEnums.Status.SUCCESS;
+
+        } catch (BusinessException be) {
+            status = TaskControlEnums.Status.ERROR;
+        } catch (Exception e) {
+            status = TaskControlEnums.Status.ERROR;
+            logger.error("出现异常，任务退出", e);
+        }
+
+        // 任务监控历史记录添加:结束
+        taskDao.insertTaskHistory(taskID, status.getIs());
     }
 }
