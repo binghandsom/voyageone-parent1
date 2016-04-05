0b7dab968b49fafcf5603a6ec038c6e2f2e73df3
aooer <bingbing.gao@voyageone.cn>
2016-04-05 14:07:04 +0800

    还原

--- voyageone-com/src/main/java/com/voyageone/common/redis/CacheHelper.java
+++ voyageone-com/src/main/java/com/voyageone/common/redis/CacheHelper.java
@@ -1,15 +1,12 @@
 package com.voyageone.common.redis;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import com.voyageone.common.configs.Enums.CacheKeyEnums;
 import com.voyageone.common.masterdate.schema.utils.StringUtil;
-import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
@@ -41,15 +38,11 @@ public class CacheHelper {
         return template.opsForHash();
     }
 
-    /* 本地缓存(windows环境使用) linux环境自动忽略 */
-    private static Map<String, Map<String,Object>> localCache = Maps.newConcurrentMap();
-
     @SuppressWarnings("unchecked")
     private static void reloadData(String key, Class self) {
         try {
-            /* window系统未缓存，或linux系统redis未缓存 */
-            if((onWindows()&&!localCache.containsKey(key))||(!onWindows()&&!getCacheTemplate().hasKey(key))){
-                Method method =self.getMethod(RELAOD);
+            if (!getCacheTemplate().hasKey(key)) {
+                Method method = self.getMethod(RELAOD);
                 method.invoke(null);
             }
         } catch (Exception e) {
@@ -59,39 +52,31 @@ public class CacheHelper {
 
     @SuppressWarnings("unchecked")
     public static long getSize(String key) {
-        return onWindows()?localCache.get(key).size():getHashOperation().size(key);
+        return getHashOperation().size(key);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T getBean(String key, String cellKey, Class self) {
         reloadData(key, self);
-        return (T) (onWindows()?localCache.get(key).get(cellKey):getHashOperation().get(key, cellKey));
-    }
-
-    @SuppressWarnings("unchecked")
-    public static boolean isExists(String key,String cellKey,Class self){
-        reloadData(key, self);
-        return onWindows()?localCache.get(key).containsKey(cellKey):getHashOperation().hasKey(key,cellKey);
+        return (T) getHashOperation().get(key, cellKey);
     }
 
     @SuppressWarnings("unchecked")
     public static Set<String> getKeySet(String key, Class self) {
         reloadData(key, self);
-        return onWindows()?localCache.get(key).keySet():getHashOperation().keys(key);
+        return getHashOperation().keys(key);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> List<T> getBeans(String key, List<String> keyList, Class self) {
         reloadData(key, self);
-        List<T> beans= Lists.newCopyOnWriteArrayList();
-        if(onWindows()) keyList.forEach(k->{ beans.add((T) localCache.get(key).get(k)); });
-        return onWindows() ? beans : getHashOperation().multiGet(key, keyList);
+        return getHashOperation().multiGet(key, keyList);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> List<T> getAllBeans(String key, Class self) {
         reloadData(key, self);
-        return onWindows()? (List<T>) localCache.get(key).values() :getHashOperation().values(key);
+        return getHashOperation().values(key);
     }
 
     /* SSB表示缓存数据类型为<String,String,Bean>  */
@@ -101,7 +86,7 @@ public class CacheHelper {
 
     /* 删除RedisKey */
     public static void delete(String key) {
-        if(onWindows()) localCache.remove(key); else getCacheTemplate().delete(key);
+        getCacheTemplate().delete(key);
     }
 
     /**
@@ -109,13 +94,9 @@ public class CacheHelper {
      */
     @SuppressWarnings("unchecked")
     private static void callCache(String cacheKey, Map mapData) {
-        if (StringUtil.isEmpty(cacheKey)||MapUtils.isEmpty(mapData)) return;
-        if(onWindows()) localCache.put(cacheKey, mapData);
-        else getHashOperation().putAll(cacheKey, mapData);
+        if (StringUtil.isEmpty(cacheKey)) return;
+        if (mapData != null) {
+            getHashOperation().putAll(cacheKey, mapData);
+        }
     }
-
-    /**
-     * 获取系统环境
-     */
-    private static boolean onWindows(){ return File.separatorChar=='\\'; }
 }
