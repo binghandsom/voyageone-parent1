0542e4e2967531a2003eb8f4edd952eb02e0411d
james <james>
2016-04-08 13:27:45 +0800

    特价宝 优化

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsPromotrionService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsPromotrionService.java
@@ -68,8 +68,8 @@ public class CmsPromotrionService extends BaseTaskService {
 //        shopBean.setAppKey("21008948");
 //        shopBean.setAppSecret("0a16bd08019790b269322e000e52a19f");
 //        shopBean.setSessionKey("6201d2770dbfa1a88af5acfd330fd334fb4ZZa8ff26a40b2641101981");
-        List<String> succeedProduct = new ArrayList<>();
-        Map<String, List<String>> failProduct = new HashMap<>();
+//        List<String> succeedProduct = new ArrayList<>();
+//        Map<String, List<String>> failProduct = new HashMap<>();
         items.forEach(item -> {
             TaobaoResponse response;
             try {
@@ -136,7 +136,8 @@ public class CmsPromotrionService extends BaseTaskService {
 
                 // 成功的场合把product_id保存起来
                 if (response != null && response.getErrorCode() == null) {
-                    succeedProduct.add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
+                    updateStatus(Integer.parseInt(item.get("promotionId").toString()), item.get("num_iid").toString(), 2, "");
+//                    succeedProduct.add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
                 } else {
                     // 失败的场合 错误信息取得
                     String fail = "";
@@ -147,51 +148,64 @@ public class CmsPromotrionService extends BaseTaskService {
                         System.out.println("getSubMsg" + response.getSubMsg());
                         fail = response.getSubMsg();
                     }
-                    if (failProduct.get(fail) == null) {
-                        List<String> temp = new ArrayList<>();
-                        temp.add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
-                        failProduct.put(fail, temp);
-                    } else {
-                        failProduct.get(fail).add((item.get("promotionId").toString() + "," + item.get("num_iid").toString()));
-                    }
+                    updateStatus(Integer.parseInt(item.get("promotionId").toString()),item.get("num_iid").toString(),3,fail);
+//                    if (failProduct.get(fail) == null) {
+//                        List<String> temp = new ArrayList<>();
+//                        temp.add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
+//                        failProduct.put(fail, temp);
+//                    } else {
+//                        failProduct.get(fail).add((item.get("promotionId").toString() + "," + item.get("num_iid").toString()));
+//                    }
                 }
             } catch (Exception e) {
                 $error(e);
-                if (failProduct.get(e.getMessage()) == null) {
-                    List<String> temp = new ArrayList<>();
-                    temp.add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
-                    failProduct.put(e.getMessage(), temp);
-                } else {
-                    failProduct.get(e.getMessage()).add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
-                }
+                updateStatus(Integer.parseInt(item.get("promotionId").toString()), item.get("num_iid").toString(), 3, e.getMessage());
+//                if (failProduct.get(e.getMessage()) == null) {
+//                    List<String> temp = new ArrayList<>();
+//                    temp.add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
+//                    failProduct.put(e.getMessage(), temp);
+//                } else {
+//                    failProduct.get(e.getMessage()).add(item.get("promotionId").toString() + "," + item.get("num_iid").toString());
+//                }
                 $info(e.getMessage());
             }
         });
-        // 把成功的产品更新数据库
-        succeedProduct.forEach(s -> {
-            Map<String, Object> parameter = new HashMap<>();
-            parameter.put("promotionId", s.split(",")[0]);
-            parameter.put("taskType", 0);
-            parameter.put("num_iid", s.split(",")[1]);
-            parameter.put("synFlg", "2");
-            parameter.put("errMsg", "");
-            parameter.put("modifier", getTaskName());
-            promotionDao.updatePromotionStatus(parameter);
-        });
+//        // 把成功的产品更新数据库
+//        succeedProduct.forEach(s -> {
+//            Map<String, Object> parameter = new HashMap<>();
+//            parameter.put("promotionId", s.split(",")[0]);
+//            parameter.put("taskType", 0);
+//            parameter.put("num_iid", s.split(",")[1]);
+//            parameter.put("synFlg", "2");
+//            parameter.put("errMsg", "");
+//            parameter.put("modifier", getTaskName());
+//            promotionDao.updatePromotionStatus(parameter);
+//        });
+//
+//
+//        // 把错误信息更新到数据库中
+//        failProduct.forEach((s, numIids) -> {
+//            numIids.forEach(numIid -> {
+//                Map<String, Object> parameter = new HashMap<>();
+//                parameter.put("promotionId", numIid.split(",")[0]);
+//                parameter.put("taskType", 0);
+//                parameter.put("num_iid", numIid.split(",")[1]);
+//                parameter.put("synFlg", "3");
+//                parameter.put("errMsg", s);
+//                parameter.put("modifier", getTaskName());
+//                promotionDao.updatePromotionStatus(parameter);
+//            });
+//        });
+    }
 
-
-        // 把错误信息更新到数据库中
-        failProduct.forEach((s, numIids) -> {
-            numIids.forEach(numIid -> {
-                Map<String, Object> parameter = new HashMap<>();
-                parameter.put("promotionId", numIid.split(",")[0]);
-                parameter.put("taskType", 0);
-                parameter.put("num_iid", numIid.split(",")[1]);
-                parameter.put("synFlg", "3");
-                parameter.put("errMsg", s);
-                parameter.put("modifier", getTaskName());
-                promotionDao.updatePromotionStatus(parameter);
-            });
-        });
+    private void updateStatus(Integer promotionId, String num_iid, Integer synFlg, String errMsg){
+        Map<String, Object> parameter = new HashMap<>();
+        parameter.put("promotionId", promotionId);
+        parameter.put("taskType", 0);
+        parameter.put("num_iid", num_iid);
+        parameter.put("synFlg", synFlg);
+        parameter.put("errMsg", errMsg);
+        parameter.put("modifier", getTaskName());
+        promotionDao.updatePromotionStatus(parameter);
     }
 }
