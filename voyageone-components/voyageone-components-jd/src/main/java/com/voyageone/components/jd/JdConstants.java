22319774859c8ddb274bc80d075f538e21d6e4df
desmond <desmond.li@voyageone.cn>
2016-04-12 21:25:48 +0800

    review

---
+++ voyageone-components/voyageone-components-jd/src/main/java/com/voyageone/components/jd/JdConstants.java
@@ -0,0 +1,15 @@
+package com.voyageone.components.jd;
+
+public class JdConstants {
+
+	// C_JD_RETURN_SUCCESS_OK
+	public static final String C_JD_RETURN_SUCCESS_OK = "0";
+	// 父属性（有子属性）
+	public static final int C_JD_ISPARENT_TRUE = 1;
+	// 叶子属性（无子属性）
+	public static final int C_JD_ISPARENT_FALSE = 0;
+//	public static final String url = "https://api.jd.com/routerjson";
+
+	public final static String C_PROP_PATH_SPLIT_CHAR = ">";
+	
+}
