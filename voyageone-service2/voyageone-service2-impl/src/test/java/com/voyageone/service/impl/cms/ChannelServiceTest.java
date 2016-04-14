df438899f39de1982f5e1b2210f9adf6ef2338ef
holysky.zhao <holysky.zhao@voyageone.cn>
2016-04-14 16:23:46 +0800

    Channel List page and backend fix

---
+++ voyageone-service2/voyageone-service2-impl/src/test/java/com/voyageone/service/impl/cms/ChannelServiceTest.java
@@ -0,0 +1,44 @@
+package com.voyageone.service.impl.cms;
+
+import com.voyageone.BaseTest;
+import com.voyageone.common.configs.beans.OrderChannelBean;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @description
+ * @author: holysky
+ * @date: 2016/4/13 10:47
+ * COPYRIGHT © 2001 - 2016 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ */
+public class ChannelServiceTest extends BaseTest {
+
+
+    @Resource ChannelService service;
+
+    @Test
+    public void testGetChannelList() throws Exception {
+        List result = service.getChannelListBy(null, null, null);
+        Assert.assertTrue(result.size()>0);
+
+        List<OrderChannelBean> result1 = service.getChannelListBy("001", null, null);
+        Assert.assertTrue(result1.get(0).getOrder_channel_id().equalsIgnoreCase("001"));
+        List<OrderChannelBean> result2 = service.getChannelListBy("001", "SN", null);
+        Assert.assertTrue(result2.get(0).getOrder_channel_id().equals("001") && result2.get(0).getName().equalsIgnoreCase("SN"));
+
+
+        List<OrderChannelBean> result3 = service.getChannelListBy("001", "SN", -1);
+
+        Assert.assertTrue(result3.size()>0);
+
+        List<OrderChannelBean> result4 = service.getChannelListBy("001", "SN", 0);
+        Assert.assertTrue(result4.size()>0);
+        List<OrderChannelBean> result5 = service.getChannelListBy("001", "SN", 1);
+        Assert.assertTrue(result5.size()==0);
+
+
+    }
+}
