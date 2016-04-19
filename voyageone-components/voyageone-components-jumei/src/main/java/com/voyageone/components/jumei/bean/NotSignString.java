863589a471bb4180037cd1c89954126f11fa35bd
edward.lin <edward.lin@voyageone.cn>
2016-04-19 11:33:47 +0800

    delete the name about jumei files

--- voyageone-components/voyageone-components-jumei/src/main/java/com/voyageone/components/jumei/bean/NotSignString.java
+++
@@ -1,9 +0,0 @@
-package com.voyageone.components.jumei.bean;
-
-public class NotSignString {
-    public String content;
-
-    public NotSignString(String content) {
-        this.content = content;
-    }
-}
