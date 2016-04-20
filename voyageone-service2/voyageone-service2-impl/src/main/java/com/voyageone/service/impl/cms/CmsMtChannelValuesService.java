b91a98ff73187e8c725a9dc21852c08dfbdde836
james <james>
2016-04-20 17:00:27 +0800

    format

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/CmsMtChannelValuesService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/CmsMtChannelValuesService.java
@@ -19,16 +19,29 @@ public class CmsMtChannelValuesService {
     @Autowired
     CmsMtChannelValuesDao cmsMtChannelValuesDao;
 
+    /**
+     * 插入ChannelValues
+     * @param cmsMtChannelValuesModel cmsMtChannelValuesModel
+     */
     public int insertCmsMtChannelValues(CmsMtChannelValuesModel cmsMtChannelValuesModel){
         return cmsMtChannelValuesDao.insert(cmsMtChannelValuesModel);
     }
 
+    /**
+     * 根据channelId 获取channelValuesList
+     * @param channelId 根据channelId
+     */
     public List<CmsMtChannelValuesModel> getCmsMtChannelValuesListByChannelId(String channelId){
         Map<String, Object> param = new HashMap<>();
         param.put("channelId",channelId);
         return cmsMtChannelValuesDao.selectList(param);
     }
 
+    /**
+     * 根据channelId和类型获取 获取channelValuesList
+     * @param channelId 根据channelId
+     * @param type （0:brand 1:sizeType 2:productType）
+     */
     public List<CmsMtChannelValuesModel> getCmsMtChannelValuesListByChannelIdType(String channelId, int type){
         Map<String, Object> param = new HashMap<>();
         param.put("channelId",channelId);
