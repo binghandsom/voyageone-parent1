9d1d45795572f27aae70bb7256d7989ee4b854b0
morse <morse.lu@voyageone.cn>
2016-04-22 10:41:31 +0800

    sx共通  cms_bt_platform_images的Dao,Mapper,Model做成

---
+++ voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/CmsBtPlatformImagesDao.java
@@ -0,0 +1,35 @@
+package com.voyageone.service.dao.cms;
+
+import com.voyageone.service.dao.ServiceBaseDao;
+import com.voyageone.service.model.cms.CmsBtPlatformImagesModel;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author morse 16/04/21
+ */
+
+@Repository
+public class CmsBtPlatformImagesDao extends ServiceBaseDao {
+
+    public List<CmsBtPlatformImagesModel> selectPlatformImagesList(String channelId, int cartId, String searchId) {
+        Map<String, Object> param = new HashMap<>();
+        param.put("channelId", channelId);
+        param.put("cartId", cartId);
+        param.put("cartId", searchId);
+        return selectList("cms_bt_platform_images_select", param);
+    }
+
+    public int insertPlatformImagesByList(List<CmsBtPlatformImagesModel> listData) {
+        return insert("cms_bt_platform_images_insertByList", listData);
+    }
+
+    public int updatePlatformImagesBySeq(CmsBtPlatformImagesModel platformImagesModel, String modifier) {
+        platformImagesModel.setModifier(modifier);
+        return update("cms_bt_platform_images_updateBySeq", platformImagesModel);
+    }
+
+}
