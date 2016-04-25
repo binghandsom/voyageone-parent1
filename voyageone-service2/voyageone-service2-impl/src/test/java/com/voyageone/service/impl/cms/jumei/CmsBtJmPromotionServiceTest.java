46c027fcb3dacbf05f281b44aee260340289300d
holysky.zhao <holysky.zhao@voyageone.cn>
2016-04-25 10:45:28 +0800

    修改一些导致测试失败的问题.这里的类型换成全限定类型

---
+++ voyageone-service2/voyageone-service2-impl/src/test/java/com/voyageone/service/impl/cms/jumei/CmsBtJmPromotionServiceTest.java
@@ -0,0 +1,27 @@
+package com.voyageone.service.impl.cms.jumei;
+
+import com.voyageone.BaseTest;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+
+/**
+ * @description
+ * @author: holysky
+ * @date: 2016/4/25 10:36
+ * COPYRIGHT © 2001 - 2016 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ */
+public class CmsBtJmPromotionServiceTest extends BaseTest{
+
+    @Resource
+    CmsBtJmPromotionService jmPromotionService;
+
+
+    @Test
+    public void testAddProductionToPromotion() throws Exception {
+
+    }
+}
