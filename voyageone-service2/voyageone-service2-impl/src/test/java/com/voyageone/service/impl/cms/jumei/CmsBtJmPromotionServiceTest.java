5793abddadfa97ebee06d8c9845e9cdd02b2c6cf
holysky.zhao <holysky.zhao@voyageone.cn>
2016-04-25 12:40:21 +0800

    聚美添加活动的test-case 添加

--- voyageone-service2/voyageone-service2-impl/src/test/java/com/voyageone/service/impl/cms/jumei/CmsBtJmPromotionServiceTest.java
+++ voyageone-service2/voyageone-service2-impl/src/test/java/com/voyageone/service/impl/cms/jumei/CmsBtJmPromotionServiceTest.java
@@ -2,9 +2,14 @@ package com.voyageone.service.impl.cms.jumei;
 
 import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
+import com.jd.open.api.sdk.request.order.OrderGetRequest;
 import com.voyageone.BaseTest;
+import com.voyageone.service.model.jumei.CmsBtJmProductImagesModel;
+import com.voyageone.service.model.jumei.CmsBtJmProductModel;
 import com.voyageone.service.model.jumei.CmsBtJmPromotionModel;
+import com.voyageone.service.model.jumei.CmsBtJmSkuModel;
 import com.voyageone.service.model.jumei.businessmodel.CmsBtJmImportSaveInfo;
+import com.voyageone.service.model.jumei.businessmodel.CmsBtJmProductImportSaveInfo;
 import com.voyageone.service.model.jumei.businessmodel.JmProductImportAllInfo;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,12 +17,20 @@ import org.junit.Test;
 import javax.annotation.Resource;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static org.junit.Assert.*;
 import static org.junit.Assume.*;
 
 /**
  * 添加聚美活动的集成测试
+ *
  * @description
  * @author: holysky
  * @date: 2016/4/25 10:36
@@ -52,6 +65,35 @@ public class CmsBtJmPromotionServiceTest extends BaseTest {
         JmProductImportAllInfo importInfos = jmPromotionService.buildJmProductImportAllInfo(productIds, promotion, channelId, discount, priceType);
 
         CmsBtJmImportSaveInfo realSaveInfos = jmPromotionImportTaskService.loadListSaveInfo(importInfos, "will");
-        Assert.assertTrue(realSaveInfos!=null);
+        assertTrue(realSaveInfos != null);
+        List<CmsBtJmProductImportSaveInfo> saveInfos = realSaveInfos.getListProductSaveInfo();
+        assertTrue(saveInfos != null && saveInfos.size() > 0);
+        IntStream.range(0, saveInfos.size()).forEach((i) -> {
+            CmsBtJmProductImportSaveInfo saveInfo = saveInfos.get(i);
+            CmsBtJmProductModel product = saveInfo.getProductModel();
+            List<CmsBtJmSkuModel> skus = saveInfo.getListSkuModel();
+            List<CmsBtJmProductImagesModel> images = saveInfo.getListCmsBtJmProductImagesModel();
+            assertTrue(product != null);
+            assertTrue(skus.size() > 0);
+            assertTrue(images.size() > 0); //error
+        });
+
+
+        //数据case
+        Set<String> originProductCodes = importInfos.getListProductModel().stream().map(bean -> bean.getProductCode()).collect(toSet());
+        Set<String> newProductCodes = saveInfos.stream().map(bean -> bean.getProductModel().getProductCode()).collect(toSet());
+        assertEquals(originProductCodes,newProductCodes);
+
+        Set<String> skuProductCodes = saveInfos.stream().map(bean -> {
+            return bean.getListSkuModel().stream().map(sku -> sku.getProductCode()).collect(toList());
+        }).flatMap(l->l.stream()).collect(Collectors.toSet());
+
+        assertEquals(originProductCodes,skuProductCodes); //所有的sku中的productCode 必须和原先选定的productCode相等
+
+        //images
+
+
+
+
     }
 }
