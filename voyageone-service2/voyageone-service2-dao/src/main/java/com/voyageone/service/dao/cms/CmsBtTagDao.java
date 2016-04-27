15ad4c208f377d8712cf699a6a5748c42d54683e
edward.lin <edward.lin@voyageone.cn>
2016-04-27 12:50:31 +0800

    修改 回滚的CmsBtTagDao

--- voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/CmsBtTagDao.java
+++ voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/CmsBtTagDao.java
@@ -1,66 +1,22 @@
 package com.voyageone.service.dao.cms;
 
-import com.voyageone.service.dao.ServiceBaseDao;
 import com.voyageone.service.model.cms.CmsBtTagModel;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
-
-/**
- * @author jerry 15/12/14
- * @version 2.0.0
- */
+import java.util.Map;
 
 @Repository
-public class CmsBtTagDao extends ServiceBaseDao {
+public interface CmsBtTagDao {
+    List<CmsBtTagModel> selectList(Map<String, Object> map);
 
-    public int insertCmsBtTag(CmsBtTagModel cmsBtTagModel){
-        return insert("insert_cms_bt_tag", cmsBtTagModel);
-    }
+    CmsBtTagModel selectOne(Map<String, Object> map);
 
-    public int updateCmsBtTag(CmsBtTagModel cmsBtTagModel){
-        return update("update_cms_bt_tag", cmsBtTagModel);
-    }
+    CmsBtTagModel select(long id);
 
-    public int deleteCmsBtTagByTagId(CmsBtTagModel cmsBtTagModel){
-        return delete("delete_cms_bt_tag_by_tag_id", cmsBtTagModel);
-    }
+    int insert(CmsBtTagModel entity);
 
-    public int deleteCmsBtTagByParentTagId(CmsBtTagModel cmsBtTagModel){
-        return delete("delete_cms_bt_tag_by_parent_tag_id", cmsBtTagModel);
-    }
+    int update(CmsBtTagModel entity);
 
-    public CmsBtTagModel selectCmsBtTagByTagId(int tagId) {
-        HashMap<String, Object> paraIn = new HashMap<>();
-        paraIn.put("tagId", tagId);
-        return selectOne("select_one_by_tag_id", paraIn);
-    }
-
-    public CmsBtTagModel selectCmsBtTagByParentTagId(int tagId) {
-        HashMap<String, Object> paraIn = new HashMap<>();
-        paraIn.put("tagId", tagId);
-        return selectOne("select_one_by_parent_tag_id", paraIn);
-    }
-
-    public List<CmsBtTagModel> selectListByParentTagId(int parentTagId) {
-        return selectList("select_list_by_parent_tag_id", parentTagId);
-    }
-
-    public List<CmsBtTagModel> selectListByChannelId(String channelId) {
-        return selectList("select_list_by_channel_id", channelId);
-    }
-
-    public List<CmsBtTagModel> selectListByParentTagId(String channelId, int parentTagId, String tagName) {
-        HashMap<String, Object> paraIn = new HashMap<>();
-        paraIn.put("channelId", channelId);
-        paraIn.put("parentTagId", parentTagId);
-        paraIn.put("tagName", tagName);
-
-        return selectList("select_one_by_tag_name", paraIn);
-    }
-
-    public List<CmsBtTagModel> selectListByChannelIdAndTagType(String channelId,String tagType) {
-        return selectList("select_list_by_channel_id_and_tag_type", parameters("channelId", channelId, "tagType", tagType));
-    }
+    int delete(long id);
 }
