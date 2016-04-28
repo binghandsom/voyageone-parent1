a388688603dd509d24b9111a6b09955dd208fd13
gjl <gump.gan@voyageone.cn>
2016-04-28 11:17:33 +0800

    价格履历补充修正

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/ProductService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/ProductService.java
@@ -227,24 +227,18 @@ public class ProductService extends BaseService {
     /**
      * get prices log list
      */
-    public List<CmsBtPriceLogModel> getPriceLog(String channelId, Map<String, Object> params) {
+    public List<CmsBtPriceLogModel> getPriceLog(String sku,String channelId, Map<String, Object> params) {
         params.put("channelId", channelId);
-        String flag = (String) params.get("flag");
-        if ("sku".equals(flag)) {
-            params.put("sku", "0");
-        }
+        params.put("sku", sku);
         return cmsBtPriceLogDaoExt.selectPriceLogByCode(params);
     }
 
     /**
      * get prices log list
      */
-    public int getPriceLogCnt(String channelId, Map<String, Object> params) {
+    public int getPriceLogCnt(String sku,String channelId, Map<String, Object> params) {
         params.put("channelId", channelId);
-        String flag = (String) params.get("flag");
-        if ("sku".equals(flag)) {
-            params.put("sku", "0");
-        }
+        params.put("sku", sku);
         return cmsBtPriceLogDaoExt.selectPriceLogByCodeCnt(params);
     }
 
