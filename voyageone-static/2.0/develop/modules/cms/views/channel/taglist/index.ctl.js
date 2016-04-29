9bd98d9164487525d41fb4e0730c488f0835d7df
wenjie.piao@voyageone.cn <wj7405049>
2016-04-29 18:05:52 +0800

    2016-4-26     全店操作和tag management的修改

--- voyageone-static/2.0/develop/modules/cms/views/channel/taglist/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/channel/taglist/index.ctl.js
@@ -9,11 +9,20 @@ define([
         $scope.vm = {
             tagTypeSelectValue:"1",
             tagTree: null,
+            tagTree2:null,
+            tagTree3:null,
             tagSelectObject: null,
             id: "",
             parentTagId: "",
             indexThree:0,
             indexTwo: 0,
+            tagOneText : "",
+            tagTwoText : "",
+            tagThreeText:"",
+            selectedId1:0,
+            selectedId2:0,
+            selectedId3:0,
+            childs:[]
         };
         /**
          *初始化数据:
@@ -23,6 +32,9 @@ define([
             //默认选中店铺类分类
             channelTagService.init($scope.vm).then(function (res) {
                 $scope.vm.tagTree = res.data;
+                $scope.vm.tagTree2 = $scope.vm.tagTree[0];
+                $scope.vm.tagTree3 = $scope.vm.tagTree[0].children[0];
+
             });
         };
 
@@ -36,6 +48,23 @@ define([
             channelTagService.del($scope.vm).then(function (res) {
                 $scope.vm = res.data;
 
+                if($scope.vm.selectedId1 == 0)
+                    $scope.vm.tagTree2 = $scope.vm.tagTree[0];
+                else
+                {
+                    $scope.vm.childs = [];
+                    dGselect($scope.vm.selectedId1);
+                    $scope.vm.tagTree2 = $scope.vm.childs;
+                }
+                if($scope.vm.selectedId2 == 0)
+                    $scope.vm.tagTree3 = $scope.vm.tagTree2.children[0];
+                else
+                {
+                    $scope.vm.childs = [];
+                    dGselect($scope.vm.selectedId2);
+                    $scope.vm.tagTree3 = $scope.vm.childs;
+                }
+
             });
         }
         /**
@@ -45,6 +74,24 @@ define([
         function save(savedata) {
             channelTagService.save(savedata.vm).then(function (res) {
                 $scope.vm.tagTree = res.data.tagInfo.tagTree;
+
+                if($scope.vm.selectedId1 == 0)
+                    $scope.vm.tagTree2 = $scope.vm.tagTree[0];
+                else
+                {
+                    $scope.vm.childs = [];
+                    dGselect($scope.vm.selectedId1);
+                    $scope.vm.tagTree2 = $scope.vm.childs;
+                }
+                if($scope.vm.selectedId2 == 0)
+                    $scope.vm.tagTree3 = $scope.vm.tagTree2.children[0];
+                else
+                {
+                    $scope.vm.childs = [];
+                    dGselect($scope.vm.selectedId2);
+                    $scope.vm.tagTree3 = $scope.vm.childs;
+                }
+
                 savedata.$close();
             },
             function (err) {
@@ -53,7 +100,84 @@ define([
                 }
             })
         }
+        $scope.tagOneFilter = tagOneFilter;
+        $scope.tagTwoFilter = tagTwoFilter;
+        $scope.tagThreeFilter = tagThreeFilter;
+        $scope.findChilds = findChilds;
+
+        function tagOneFilter(item){
+            var searText = $scope.vm.tagOneText;
+            if(searText == "" || searText == null)
+                return true;
+            if(item.tagPathName.indexOf($scope.vm.tagOneText) >= 0)
+            {
+                $scope.vm.tagTree2 = item;
+                $scope.vm.tagTree3 = item.children[0];
+                return true;
+            }
+        }
+
+        function tagTwoFilter(item){
+            var searText = $scope.vm.tagTwoText;
+            if(searText == "" || searText == null)
+                return true;
+            if(item.tagPathName.indexOf($scope.vm.tagTwoText) >= 0)
+            {
+                $scope.vm.tagTree3 = item;
+                return true;
+            }
+        }
+
+        function tagThreeFilter(item){
+            var searText = $scope.vm.tagThreeText;
+            if(searText == "" || searText == null && $scope.vm.tagOneText == "")
+                return true;
+            if(item.tagPathName.indexOf($scope.vm.tagThreeText) >= 0)
+            {
+                return true;
+            }
+        }
+
+        function findChilds(index,level){
+
+            //首先清空子集合
+            $scope.vm.childs = [];
+            if(level != 3)
+                dGselect(index);
+            switch(level){
+                case 1:
+                    $scope.vm.selectedId1 = index;
+                    $scope.vm.tagTree2 = $scope.vm.childs;
+                    $scope.vm.tagTree3 = $scope.vm.tagTree2.children[0];
+                    break;
+                case 2:
+                    $scope.vm.selectedId2 = index;
+                    $scope.vm.tagTree3 = $scope.vm.childs;
+                    break;
+                case 3:
+                    $scope.vm.selectedId3 = index;
+                    break;
+            }
+        }
+
+        /**
+         * 递归查找该index下的子集合
+         * @param index 下行index属性
+         * @param data 下行集合
+         */
+        function dGselect(index,data){
+            data = data == null ? $scope.vm.tagTree : data;
+            //递归访问子属性
+            for(var i=0,length=data.length;i<length;i++){
+                 if(data[i].id == index)
+                 {
+                     $scope.vm.childs = data[i];
+                 }
+                dGselect(index,data[i].children);
+            }
+        }
     }
+
     cmsChannelTagController.$inject = ['$scope', 'channelTagService'];
     //返回的数据类型
     return cmsChannelTagController;
