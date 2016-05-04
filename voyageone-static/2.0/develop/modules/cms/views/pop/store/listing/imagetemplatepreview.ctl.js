a05680651ed90fa3bdddc4a5e25b6dd580ec2607
yoenka <yoenka.yu@voyageone.cn>
2016-05-04 20:18:10 +0800

    新增店铺管理下的listing叶子目录下的页面

---
+++ voyageone-static/2.0/develop/modules/cms/views/pop/store/listing/imagetemplatepreview.ctl.js
@@ -0,0 +1,23 @@
+/**
+ * Created by 123 on 2016/5/4.
+ */
+define([
+    'angularAMD',
+    'modules/cms/controller/popup.ctl'
+], function (angularAMD) {
+
+    angularAMD.controller('popImageTemplatePreviewCtl', function ($scope, $routeParams) {
+
+        /**
+         * 初始化数据.
+         */
+        $scope.initialize = function () {
+            if ($scope.vm == undefined) {
+                $scope.vm = {};
+            }
+            $scope.vm.imageMain = $routeParams.imageMain;
+            $scope.vm.imageList = $routeParams.imageList;
+        };
+
+    });
+});
