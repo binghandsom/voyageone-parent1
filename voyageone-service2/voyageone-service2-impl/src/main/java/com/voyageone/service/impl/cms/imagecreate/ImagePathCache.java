f7134dcb26e8337c347f5db044093aae51ec294b
sunpeitao <peitaosun@voyageone.cn>
2016-05-05 16:49:10 +0800

    LiquidFireClient   AliYunOSSClient

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/imagecreate/ImagePathCache.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/imagecreate/ImagePathCache.java
@@ -3,7 +3,10 @@ package com.voyageone.service.impl.cms.imagecreate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 /**
  * Created by dell on 2016/4/28.
@@ -11,10 +14,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class ImagePathCache {
     private static HashOperations<String, Long, String> hashOperation;
+    private static ZSetOperations<String,Long> ZSetOperation;
     public static final String HashtableName = "voyageone_image_create_hashcode_file";
+    public static final String ZSetName = "voyageone_image_create_hashcode_zset";
     @Autowired
     public  void setTemplate(RedisTemplate template) {
         hashOperation = template.opsForHash();
+        ZSetOperation=template.opsForZSet();
+        //template.execute()
     }
 
     /**
@@ -24,6 +31,13 @@ public class ImagePathCache {
      */
     public void set(long key, String value) {
         hashOperation.put(HashtableName, key, value);
+//        ZSetOperation.add(ZSetName, key, System.currentTimeMillis());
+//        if (ZSetOperation.size(ZSetName) > 10000) {
+//            Set<Long> hashCodeList = ZSetOperation.range(ZSetName, 1, 100);
+//            Object[] keys = hashCodeList.toArray();
+//            hashOperation.delete(HashtableName, keys);
+//            ZSetOperation.remove(ZSetName,keys);
+//        }
     }
     public String get(Long key) {
         return hashOperation.get(HashtableName, key);
