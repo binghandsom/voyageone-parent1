4904a53218ad07e365c1ed5da5d2b7d2712fe9cc
gjl <gump.gan@voyageone.cn>
2016-05-06 17:03:03 +0800

    sizeChartView修正

--- voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/model/cms/mongo/channel/CmsBtSizeChartModel.java
+++ voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/model/cms/mongo/channel/CmsBtSizeChartModel.java
@@ -9,14 +9,13 @@ import java.util.List;
  * Created by gjl on 2016/5/5.
  */
 public class CmsBtSizeChartModel extends ChannelPartitionModel {
-
     private String sizeChartId;
     private String sizeChartName;
     private String finish;
     private List<String> brandName;
     private List<String>productType;
     private List<String> sizeType;
-    private List<CmsBtSizeChartModelSizeMap> carts = new ArrayList<>();
+    private List<CmsBtSizeChartModelSizeMap> sizeMap = new ArrayList<>();
     private String active;
 
     public String getSizeChartId() {
@@ -35,28 +34,12 @@ public class CmsBtSizeChartModel extends ChannelPartitionModel {
         this.sizeChartName = sizeChartName;
     }
 
-    public String getFinish() {
-        return finish;
-    }
-
-    public void setFinish(String finish) {
-        this.finish = finish;
-    }
-
     public List<String> getBrandName() {
         return brandName;
     }
 
     public void setBrandName(List<String> brandName) {
         this.brandName = brandName;
-    }
-
-    public List<String> getProductType() {
-        return productType;
-    }
-
-    public void setProductType(List<String> productType) {
-        this.productType = productType;
     }
 
     public List<String> getSizeType() {
@@ -67,14 +50,6 @@ public class CmsBtSizeChartModel extends ChannelPartitionModel {
         this.sizeType = sizeType;
     }
 
-    public List<CmsBtSizeChartModelSizeMap> getCarts() {
-        return carts;
-    }
-
-    public void setCarts(List<CmsBtSizeChartModelSizeMap> carts) {
-        this.carts = carts;
-    }
-
     public String getActive() {
         return active;
     }
@@ -82,4 +57,32 @@ public class CmsBtSizeChartModel extends ChannelPartitionModel {
     public void setActive(String active) {
         this.active = active;
     }
+
+    public List<CmsBtSizeChartModelSizeMap> getSizeMap() {
+        return sizeMap;
+    }
+
+    public void setSizeMap(List<CmsBtSizeChartModelSizeMap> sizeMap) {
+        this.sizeMap = sizeMap;
+    }
+
+    public List<String> getProductType() {
+        return productType;
+    }
+
+    public void setProductType(List<String> productType) {
+        this.productType = productType;
+    }
+
+    public String getFinish() {
+        return finish;
+    }
+
+    public void setFinish(String finish) {
+        this.finish = finish;
+    }
+
+
+
+
 }
