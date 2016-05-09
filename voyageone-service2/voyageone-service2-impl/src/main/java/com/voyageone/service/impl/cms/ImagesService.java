1317d7d289dd41c86ecc112f5510ede0d8795dd6
sunpeitao <peitaosun@voyageone.cn>
2016-05-09 16:16:54 +0800

    config

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/ImagesService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/ImagesService.java
@@ -14,5 +14,18 @@ import java.util.List;
  */
 public class ImagesService extends BaseService {
 
+    @Autowired
+    private CmsBtImagesDao cmsBtImagesDao;
 
+    public List<CmsBtImagesModel> getImageList (CmsBtImagesModel model) {
+        return cmsBtImagesDao.selectList(JacksonUtil.jsonToMap(JacksonUtil.bean2Json(model)));
+    }
+
+    public int insert(CmsBtImagesModel model) {
+        return cmsBtImagesDao.insert(model);
+    }
+
+    public int update(CmsBtImagesModel model) {
+        return cmsBtImagesDao.update(model);
+    }
 }
