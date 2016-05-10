71521cb6644b53aa73f75833467ce4b3684b585c
Jonas <jonas.gao@voyageone.cn>
2016-05-10 15:03:09 +0800

    拆分 PlatformPropMapping Service, 指定不重复打开 field mapping 页面

--- voyageone-static/2.0/develop/modules/cms/views/mapping/platform/prop.list.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/mapping/platform/prop.list.ctl.js
@@ -4,21 +4,13 @@
  * @name platformPropMappingController
  */
 
-/**
- * @typedef {object} PlatformInfo
- * @property {object} category CategorySchema
- * @property {object} properties FieldMap
- * @property {object} mappingInfo
- * @property {object} mappingTypes MappingTypeMap
- */
-
 define([
     'cms',
     'underscore',
-    'modules/cms/enums/MappingTypes',
     'modules/cms/controller/popup.ctl',
-    'modules/cms/views/mapping/platform/prop.item.d'
-], function (cms, _, MappingTypes) {
+    './prop.item.d',
+    './PlatformPropMapping.service'
+], function (cms, _) {
     'use strict';
     return cms.controller('platformPropMappingController', (function () {
 
@@ -43,8 +35,8 @@ define([
             this.platform = null;
 
             this.selected = {
-                required: null,
-                matched: null,
+                required: true,
+                matched: false,
                 keyWord: null
             };
         }
@@ -69,6 +61,7 @@ define([
 
                 $service.getPlatformData($mainCate.id, $.cartId).then(function (data) {
                     $.platform = data;
+                    $.filteringData();
                 });
 
                 $service.getMainCategorySchema($mainCate.id).then(function (mainCategory) {
@@ -82,8 +75,8 @@ define([
             },
             clear: function () {
                 this.selected = {
-                    required: null,
-                    matched: null,
+                    required: true,
+                    matched: false,
                     keyWord: null
                 };
             },
@@ -173,177 +166,6 @@ define([
         };
 
         return PlatformPropMappingController;
-
-    })()).service('platformPropMappingService', (function () {
-
-        /**
-         * 用于封装属性 Mapping 相关的数据操作, 用于在画面和 item directive 间共享
-         * @constructor
-         */
-        function PlatformPropMappingService(platformMappingService, $q) {
-            this.$service = platformMappingService;
-            this.$q = $q;
-
-            /**
-             * @type {PlatformInfo}
-             */
-            this.platform = null;
-
-            /**
-             * 主数据类目缓存 Map.
-             * 已查询的会存入.
-             * @type {object}
-             */
-            this.mainCategories = {};
-
-            this.mappingMap = {};
-        }
-
-        PlatformPropMappingService.prototype = {
-
-            /**
-             * 根据主数据目录,通过Mapping,获取完整的平台数据信息
-             * @param categoryId MainCategory ID
-             * @param cartId
-             * @returns {Promise.<PlatformInfo|null>}
-             */
-            getPlatformData: function (categoryId, cartId) {
-                var that = this;
-
-                // 因为后续的操作理论上都是在画面加载后进行的.所以后续可能不传递参数
-                // 因此需要在此处特殊检查
-                if (that.platform || !categoryId || !cartId) {
-                    var deferred = that.$q.defer();
-                    deferred.resolve(that.platform);
-                    return deferred.promise;
-                }
-
-                // 木有数据,再去后台拿
-                return that.$service.getPlatformCategory({
-                    categoryId: categoryId,
-                    cartId: cartId
-                }).then(function (res) {
-                    that.platform = {
-                        category: res.data.categorySchema,
-                        properties: res.data.properties,
-                        mappingInfo: res.data.mapping,
-                        matchOver: res.data.matchOver
-                    };
-                    that.getPlatformMapping(categoryId, that.platform.category.catId, cartId);
-                }).then(function () {
-                    var platform = that.platform;
-                    that.$service.getMappingTypes({
-                        cartId: cartId,
-                        platformCategoryId: platform.catId
-                    }).then(function (res) {
-                        platform.mappingTypes = res.data;
-                    });
-                    return platform;
-                });
-            },
-
-            /**
-             * @param property
-             * @returns {Promise.<string|null>}
-             */
-            getMappingType: function (property) {
-                return this.getPlatformData().then(function (platform) {
-                    if (!platform) return null;
-                    return platform.mappingTypes[property.id];
-                });
-            },
-
-            /**
-             * @param {string} mainCategoryId
-             * @return {Promise}
-             */
-            getMainCategorySchema: function (mainCategoryId) {
-
-                var deferred = this.$q.defer();
-                var mainCategorySchema = this.mainCategories[mainCategoryId];
-
-                if (mainCategorySchema) {
-                    deferred.resolve(mainCategorySchema);
-                    return deferred.promise;
-                }
-
-                this.$service.getMainCategorySchema({
-                    mainCategoryId: mainCategoryId
-                }).then(function (res) {
-                    deferred.resolve(this.mainCategories[mainCategoryId] = res.data);
-                }.bind(this));
-
-                return deferred.promise;
-            },
-
-            saveMatchOver: function (mainCategoryId, matchOver, cartId) {
-                return this.$service.$saveMatchOverByMainCategory({
-                    mainCategoryId: mainCategoryId, matchOver: matchOver, cartId: cartId
-                }).then(function(res) {
-                    return res.data;
-                });
-            },
-
-            /**
-             * @param {string} mainCategoryId
-             * @param {string} platformCategoryId
-             * @param {number} cartId
-             */
-            getPlatformMapping: function (mainCategoryId, platformCategoryId, cartId) {
-
-                var deferred = this.$q.defer();
-                var key = mainCategoryId + '->' + platformCategoryId + '@' + cartId;
-                var mapping = this.mappingMap[key];
-
-                if (mapping) {
-                    deferred.resolve(mapping);
-                    return deferred.promise;
-                }
-
-                this.$service.getPlatformMapping({
-                    mainCategoryId: mainCategoryId,
-                    platformCategoryId: platformCategoryId,
-                    cartId: cartId
-                }).then(function (res) {
-                    deferred.resolve(this.mappingMap[key] = res.data);
-                }.bind(this));
-
-                return deferred.promise;
-            },
-
-            isMatched: function(property) {
-
-                var that = this;
-                var path = [property];
-                var parent = property.parent;
-                while (parent) {
-                    path.unshift(parent);
-                    parent = parent.parent;
-                }
-
-                return that.getPlatformData().then(function(platform) {
-                    var mappingInfo = platform.mappingInfo;
-                    var result = false;
-                    _.find(path, function(p) {
-                        result = mappingInfo[p.id];
-                        if (_.isBoolean(result)) return true;
-                        if (_.isArray(result)) {
-                            result = !!result.length;
-                            return true;
-                        }
-                        if (_.isObject(result)) {
-                            mappingInfo = result;
-                            result = !!_.keys(result).length;
-                        }
-                        return false;
-                    });
-                    return result;
-                });
-            }
-
-        };
-
-        return PlatformPropMappingService;
 
     })());
 });
