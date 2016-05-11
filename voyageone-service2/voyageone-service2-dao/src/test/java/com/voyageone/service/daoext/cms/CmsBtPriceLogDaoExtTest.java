71e036ec77f7b44d6a94eebdf25f4c0ceeb90b0b
ethan.shi <ethan.shi@vovageone.cn>
2016-05-11 17:30:25 +0800

    fix cms_bt_price_log mapper

--- voyageone-service2/voyageone-service2-dao/src/test/java/com/voyageone/service/daoext/cms/CmsBtPriceLogDaoExtTest.java
+++ voyageone-service2/voyageone-service2-dao/src/test/java/com/voyageone/service/daoext/cms/CmsBtPriceLogDaoExtTest.java
@@ -1,12 +1,17 @@
 package com.voyageone.service.daoext.cms;
 
+import com.voyageone.service.model.cms.CmsBtPriceLogModel;
+import org.apache.avro.generic.GenericData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Ethan Shi on 2016/4/26.
@@ -29,6 +34,38 @@ public class CmsBtPriceLogDaoExtTest {
         map.put("rows", 1);
 
         cmsBtPriceLogDaoExt.selectPriceLogByCode(map);
+
+    }
+
+    @Test
+    public void testSelectPriceLogByCodeCnt() throws Exception {
+
+    }
+
+
+
+    @Test
+    public void testInsertCmsBtPriceLogList() throws Exception {
+        List<CmsBtPriceLogModel> list  =  new ArrayList<CmsBtPriceLogModel>();
+        CmsBtPriceLogModel cmsBtPriceLogModel = new CmsBtPriceLogModel();
+        cmsBtPriceLogModel.setChannelId("1");
+        cmsBtPriceLogModel.setProductId(1);
+        cmsBtPriceLogModel.setCode("1");
+        cmsBtPriceLogModel.setSku("1");
+        cmsBtPriceLogModel.setSalePrice("555");
+        cmsBtPriceLogModel.setMsrpPrice("11111");
+        cmsBtPriceLogModel.setRetailPrice("2222");
+        cmsBtPriceLogModel.setClientMsrpPrice("333");
+        cmsBtPriceLogModel.setClientRetailPrice("44");
+        cmsBtPriceLogModel.setClientNetPrice("4444");
+        cmsBtPriceLogModel.setComment("5555555");
+        cmsBtPriceLogModel.setCreated(new Date());
+        cmsBtPriceLogModel.setCreater("testuser");
+        cmsBtPriceLogModel.setModified(new Date());
+        cmsBtPriceLogModel.setModifier("testuser");
+
+        list.add(cmsBtPriceLogModel);
+        cmsBtPriceLogDaoExt.insertCmsBtPriceLogList(list);
 
     }
 }
