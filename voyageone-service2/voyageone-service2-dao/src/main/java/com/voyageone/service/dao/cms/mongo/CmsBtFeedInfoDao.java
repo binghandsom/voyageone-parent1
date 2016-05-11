ac3e88dd252e433fcd796b356d49027bd8ee077d
Jonas <jonas.gao@voyageone.cn>
2016-05-11 10:37:20 +0800

    format CmsBtFeedInfoDao

--- voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/mongo/CmsBtFeedInfoDao.java
+++ voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/mongo/CmsBtFeedInfoDao.java
@@ -2,7 +2,6 @@ package com.voyageone.service.dao.cms.mongo;
 
 import com.mongodb.WriteResult;
 import com.voyageone.base.dao.mongodb.BaseMongoChannelDao;
-import com.voyageone.base.dao.mongodb.JomgoQuery;
 import com.voyageone.common.util.MongoUtils;
 import com.voyageone.service.model.cms.mongo.feed.CmsBtFeedInfoModel;
 import org.springframework.stereotype.Repository;
@@ -11,8 +10,9 @@ import java.util.List;
 
 /**
  * Created by james.li on 2015/11/27.
+ *
+ * @author james.li
  */
-
 @Repository
 public class CmsBtFeedInfoDao extends BaseMongoChannelDao<CmsBtFeedInfoModel> {
 
@@ -23,8 +23,9 @@ public class CmsBtFeedInfoDao extends BaseMongoChannelDao<CmsBtFeedInfoModel> {
 
     /**
      * 根据updFlg来获取商品列表(updFlg=0: 等待反映到主数据, updFlg=1: 已经反映到主数据)
+     *
      * @param channelId channel id
-     * @param updFlg updFlg=0: 等待反映到主数据, updFlg=1: 已经反映到主数据
+     * @param updFlg    updFlg=0: 等待反映到主数据, updFlg=1: 已经反映到主数据
      * @return 商品列表
      */
     public List<CmsBtFeedInfoModel> selectProductByUpdFlg(String channelId, int updFlg) {
@@ -36,27 +37,25 @@ public class CmsBtFeedInfoDao extends BaseMongoChannelDao<CmsBtFeedInfoModel> {
     /**
      * updateFeedInfoUpdFlg
      */
-    public int updateFeedInfoUpdFlg(String channelId,String[] modelCode){
+    public int updateFeedInfoUpdFlg(String channelId, String[] modelCode) {
 
-        StringBuilder sbQuery = new StringBuilder();
-        sbQuery.append("{");
-        sbQuery.append(MongoUtils.splicingValue("channelId", channelId));
-        sbQuery.append(",");
-        sbQuery.append(MongoUtils.splicingValue("model", modelCode));
-        sbQuery.append("}");
+        String sbQuery = "{" +
+                MongoUtils.splicingValue("channelId", channelId) +
+                "," +
+                MongoUtils.splicingValue("model", modelCode) +
+                "}";
 
         String collectionName = getCollectionName(channelId);
 
-        WriteResult updateRes = mongoTemplate.updateMulti(sbQuery.toString(),"{ $set: {updFlg: 0}}", collectionName);
+        WriteResult updateRes = mongoTemplate.updateMulti(sbQuery, "{$set: {updFlg: 0}}", collectionName);
 
         return updateRes.getN();
     }
 
-
     /**
      * 更新所有FeedInfo的UpdFlg
      */
-    public int updateAllUpdFlg(String channelId,int updFlg){
+    public int updateAllUpdFlg(String channelId, int updFlg) {
 
         WriteResult updateRes = mongoTemplate.updateMulti("{}", String.format("{ $set: {updFlg: %d}}", updFlg),
                 getCollectionName(channelId));
