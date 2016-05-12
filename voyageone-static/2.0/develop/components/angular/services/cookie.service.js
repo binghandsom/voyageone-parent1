a08de02160fd484f7ffb9afa31e9bc20c041e434
Jonas <jonas.gao@voyageone.cn>
2016-05-12 14:45:33 +0800

    fix cookie service

--- voyageone-static/2.0/develop/components/angular/services/cookie.service.js
+++ voyageone-static/2.0/develop/components/angular/services/cookie.service.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     /**
      * @Date:    2015-11-16 20:30:37
      * @User:    Jonas
@@ -11,8 +11,9 @@
         channel: "voyageone.user.channel",
         application: "voyageone.user.application"
     };
-    function gentProps(key) {
-        return function(val) {
+
+    function makeProps(key) {
+        return function (val) {
             if (arguments.length === 1) {
                 return this.set(key, val);
             } else if (arguments.length > 1) {
@@ -21,24 +22,34 @@
             return this.get(key);
         };
     }
+
     function CookieService($cookies) {
         this.$cookies = $cookies;
     }
-    CookieService.prototype.get = function(key) {
+
+    CookieService.prototype.get = function (key) {
         var result = this.$cookies.get(key);
-        return result == undefined || result == null ? "" : this.$cookies.get(key);
+        if (result === undefined || result === null)
+            return '';
+        return JSON.parse(result);
     };
-    CookieService.prototype.set = function(key, value) {
-        return this.$cookies.put(key, value);
+
+    CookieService.prototype.set = function (key, value) {
+        return this.$cookies.put(key, angular.toJson(value));
     };
-    CookieService.prototype.language = gentProps(keys.language);
-    CookieService.prototype.company = gentProps(keys.company);
-    CookieService.prototype.channel = gentProps(keys.channel);
-    CookieService.prototype.application = gentProps(keys.application);
-    CookieService.prototype.removeAll = function() {
+
+    CookieService.prototype.removeAll = function () {
         this.$cookies.remove(keys.language);
         this.$cookies.remove(keys.company);
         this.$cookies.remove(keys.channel);
         this.$cookies.remove(keys.application);
     };
+
+    CookieService.prototype.language = makeProps(keys.language);
+
+    CookieService.prototype.company = makeProps(keys.company);
+
+    CookieService.prototype.channel = makeProps(keys.channel);
+
+    CookieService.prototype.application = makeProps(keys.application);
 })();
