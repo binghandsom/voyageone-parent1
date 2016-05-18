8f82d73246f60e2f54a494333eacfda4ce635ec8
Jonas <jonas.gao@voyageone.cn>
2016-05-18 11:08:52 +0800

    更新 mock server 的配置方式

--- voyageone-static/2.0/test_server/app.js
+++ voyageone-static/2.0/test_server/app.js
@@ -5,23 +5,29 @@ var path = require('path');
 var logger = require('morgan');
 var request = require('request');
 
+var config = require('./config');
+
 var app = express();
-var port = 9090;
-var proxy = 'http://localhost:8080';
+var port = config.port;
+var proxy = config.http_proxy;
+
+if (!port || !proxy) {
+    throw '没有设置正确的配置, 启动前需要设置 port 和 http_proxy 属性到 config.js 文件中, 如果没有这个文件, 请自行创建。';
+}
 
 app.use(logger('dev'));
 
-app.get('/', function(req, res) {
-  res.redirect('/login.html');
+app.get('/', function (req, res) {
+    res.redirect('/login.html');
 });
 
 app.use(express.static(path.join(__dirname, '../develop')));
 
 app.use(function (req, res) {
-  req.pipe(request(proxy + req.path)).pipe(res);
+    req.pipe(request(proxy + req.path)).pipe(res);
 });
 
-app.listen(port, function() {
-  console.log('Listening' + port);
-  console.log('Started !');
+app.listen(port, function () {
+    console.log('Listening' + port);
+    console.log('Started !');
 });
