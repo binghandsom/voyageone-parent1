c8221cc90a605a2e0dd7a4e6833da7ce91603bae
desmond <desmond.li@voyageone.cn>
2016-05-20 14:42:09 +0800

    追加addProductAndDeal（）方法     （国际POP - 创建商品并同时创建Deal）

--- voyageone-components/voyageone-components-jumei/src/main/java/com/voyageone/components/jumei/JumeiHtProductService.java
+++ voyageone-components/voyageone-components-jumei/src/main/java/com/voyageone/components/jumei/JumeiHtProductService.java
@@ -1,7 +1,9 @@
 package com.voyageone.components.jumei;
 
 import com.voyageone.common.configs.beans.ShopBean;
+import com.voyageone.components.jumei.reponse.HtProductAddResponse;
 import com.voyageone.components.jumei.reponse.HtProductUpdateResponse;
+import com.voyageone.components.jumei.request.HtProductAddRequest;
 import com.voyageone.components.jumei.request.HtProductUpdateRequest;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,16 @@ import java.util.Map;
 
 @Service
 public class JumeiHtProductService extends JmBase {
+
+    public HtProductAddResponse addProductAndDeal(ShopBean shopBean, HtProductAddRequest request) throws Exception {
+        Map<String, Object> params = request.getParameter();
+        String reqResult = reqJmApi(shopBean, request.getUrl(), params);
+        logger.info("国际POP - 创建商品并同时创建Deal返回：" + reqResult);
+        HtProductAddResponse response = new HtProductAddResponse();
+        response.setBody(reqResult);
+        return response;
+    }
+
     public HtProductUpdateResponse update(ShopBean shopBean, HtProductUpdateRequest request) throws Exception {
         Map<String, Object> params = request.getParameter();
         String reqResult = reqJmApi(shopBean, request.getUrl(), params);
