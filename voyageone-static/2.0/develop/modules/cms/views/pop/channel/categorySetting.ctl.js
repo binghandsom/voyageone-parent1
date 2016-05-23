6f32916d77d569ed036ed2fb954cc3fd5bd7a0cc
wenjie.piao@voyageone.cn <wj7405049>
2016-05-23 11:20:37 +0800

    增加店铺分类静态页面

---
+++ voyageone-static/2.0/develop/modules/cms/views/pop/channel/categorySetting.ctl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 123 on 2016/5/20.
+ */
+define([
+    'angularAMD',
+    'modules/cms/controller/popup.ctl'
+], function (angularAMD) {
+
+    angularAMD.controller('categorySettingCtl', function ($scope) {
+    });
+});
