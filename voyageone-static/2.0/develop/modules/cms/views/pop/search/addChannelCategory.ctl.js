2e5646a87fb990d4053cfb6fb100088ca6c8d1f3
sofia <sofia.yu@voyageone.cn>
2016-05-24 20:37:51 +0800

    Merge remote-tracking branch 'origin/dev/cms2' into dev/cms2

--- voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
@@ -2,19 +2,42 @@
  * Created by sofia on 5/19/2016.
  */
 define([
-    'cms'
+    'cms',
+    './addChannelCategory.service.dev'
 ], function (cms) {
-
-    cms.controller('popAddChannelCategoryCtrl', (function(){
-
-        function PopAddChannelCategoryCtrl() {
-
+    cms.controller('popAddChannelCategoryCtrl', (function () {
+        function PopAddChannelCategoryCtrl(context, addChannelCategoryService) {
+            this.code = context.productIds;
+            this.channelCategoryList = null;
+            this.cartList = [];
+            this.cartId = 0;
+            this.addChannelCategoryService = addChannelCategoryService;
         }
 
         PopAddChannelCategoryCtrl.prototype = {
+            /**
+             * 画面初始化时
+             */
 
+
+
+            init: function () {
+                var self = this;
+                self.addChannelCategoryService.init().then(function (res) {
+                    self.channelCategoryList = res.data;
+                });
+                self.cartList=['TM','TG','JD','JG']
+            },
+            /**
+             * 点击保存按钮时
+             */
+            save: function () {
+                var self = this;
+                self.addChannelCategoryService.save().then(function () {
+
+                })
+            }
         };
-
         return PopAddChannelCategoryCtrl;
     })());
 });
