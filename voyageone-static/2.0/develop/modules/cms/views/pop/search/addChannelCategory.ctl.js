7317760f637940c6b17d1ceb54e45014f10b16fc
sofia <sofia.yu@voyageone.cn>
2016-05-25 10:31:42 +0800

    update the channelCategory

--- voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
@@ -2,16 +2,16 @@
  * Created by sofia on 5/19/2016.
  */
 define([
-    'cms',
-    './addChannelCategory.service.dev'
+    'cms'
 ], function (cms) {
     cms.controller('popAddChannelCategoryCtrl', (function () {
-        function PopAddChannelCategoryCtrl(context, addChannelCategoryService) {
+        function PopAddChannelCategoryCtrl(context, $addChannelCategoryService) {
             this.code = context.productIds;
             this.channelCategoryList = null;
             this.cartList = [];
+            this.isSelectCid = [];
             this.cartId = 0;
-            this.addChannelCategoryService = addChannelCategoryService;
+            this.addChannelCategoryService = $addChannelCategoryService;
         }
 
         PopAddChannelCategoryCtrl.prototype = {
@@ -20,10 +20,11 @@ define([
              */
             init: function () {
                 var self = this;
-                self.addChannelCategoryService.init().then(function (res) {
-                    self.channelCategoryList = res.data;
+                self.addChannelCategoryService.init({"code": self.code,"cartId":self.cartId}).then(function (res) {
+                    self.channelCategoryList = res.data.channelCategoryList;
+                    self.cartList = res.data.cartList;
+                    self.isSelectCid = res.data.isSelectCid;
                 });
-                self.cartList=['TM','TG','JD','JG']
             },
             /**
              * 点击保存按钮时
