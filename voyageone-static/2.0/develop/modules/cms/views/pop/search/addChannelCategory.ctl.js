b41dc511ce5388754f066e2d8c62c8932e46864f
sofia <sofia.yu@voyageone.cn>
2016-05-26 21:25:30 +0800

    update the ChannelCategory page add new welcome  Page

--- voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
@@ -2,20 +2,29 @@
  * Created by sofia on 5/19/2016.
  */
 define([
-    'cms'
+    'cms',
+    './addChannelCategory.service.dev'
 ], function (cms) {
+
+    function flatCategories(categories, parent) {
+        return categories.reduce(function (map, curr) {
+            curr.parent = parent;
+            map[curr.catId] = curr;
+            if (curr.children && curr.children.length)
+                map = angular.extend(map, flatCategories(curr.children, curr));
+            return map;
+        }, {});
+    }
+
     cms.controller('popAddChannelCategoryCtrl', (function () {
-        function PopAddChannelCategoryCtrl(context, $addChannelCategoryService, selectRowsFactory) {
+
+        function PopAddChannelCategoryCtrl(context, $rootScope, addChannelCategoryService) {
             this.code = context.productIds;
-            this.channelCategoryList = null;
             this.cartList = [];
+            this.channelCategoryList = null;
             this.isSelectCid = [];
-            this.cartId = "20";
-            this.addChannelCategoryService = $addChannelCategoryService;
-            this.catPath = null;
-            this.fullCatCId = null;
-            this.categorySelList = { selList: []};
-            this.tempCategorySelect = new selectRowsFactory();
+            this.cartId = $rootScope.platformType.cartId.toString();
+            this.addChannelCategoryService = addChannelCategoryService;
         }
 
         PopAddChannelCategoryCtrl.prototype = {
@@ -26,8 +35,8 @@ define([
                 var self = this;
                 self.addChannelCategoryService.init({"code": self.code, "cartId": self.cartId}).then(function (res) {
                     self.cartList = res.data.cartList;
-                    self.channelCategoryList = res.data.channelCategoryList;
                     self.isSelectCid = res.data.isSelectCid;
+                    self.channelCategoryList = res.data.channelCategoryList;
                 });
             },
 
@@ -35,27 +44,43 @@ define([
              * 点击保存按钮时
              */
             save: function () {
-                var self = this;
-                // 重新初始化选中标签
-                self.tempCategorySelect = new selectRowsFactory();
-                self.addChannelCategoryService.save({"cartId": self.cartId, "cIds": self.cIds, "cNames": self.cNames,"fullCNames": self.catPath, "fullCIds": self.fullCIds}).then(function () {
-                    return self.cartId;
+
+                var cIds = [], cNames = [], fullCNames = [], fullCIds = [];
+
+                var map = flatCategories(this.channelCategoryList);
+
+                _.map(this.isSelectCid, function (value, key) {
+                    return {categoryId:key, selected:value};
+                }).filter(function(item) {
+                    return item.selected;
+                }).forEach(function(item) {
+                    var category = map[item.categoryId];
+
+                    while(category) {
+                        if (cIds.indexOf(category.catId) < 0) cIds.unshift(category.catId);
+                        if (cNames.indexOf(category.catName) < 0) cNames.unshift(category.catName);
+
+                        if (category.parent) {
+                            if (fullCNames.indexOf(category.catPath) < 0) fullCNames.unshift(category.catPath);
+                            if (fullCIds.indexOf(category.fullCatCId) < 0)  fullCIds.unshift(category.fullCatCId);
+                        }
+
+                        category = category.parent;
+                    }
                 });
-                self.categorySelList = tempCategorySelect.selectRowsInfo;
-                if(self.categorySelList.selList.length>0){
-                   return function () {
-                            var parameter = [];
-                            _.forEach(self.categorySelList.selList, function (object) {
-                                parameter.push(object.data);
-                            });
-                        };
-                }
+
+                cIds = cIds.map(function(i){return {cId:i};});
+                cNames = cNames.map(function(i){return {cName:i};});
+                fullCNames = fullCNames.map(function(i){return {fullCName:i};});
+                fullCIds = fullCIds.map(function(i){return {fullCId:i};});
+
+                console.log(cIds);
+                console.log(cNames);
+                console.log(fullCNames);
+                console.log(fullCIds);
             }
         };
+
         return PopAddChannelCategoryCtrl;
     })());
 });
-
-
-
-
