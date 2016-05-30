d5bfe477576594e746dc2567404ab4077f610691
sofia <sofia.yu@voyageone.cn>
2016-05-30 15:57:01 +0800

    add new notice  alert about the invalid operation

--- voyageone-static/2.0/develop/modules/cms/translate/en.js
+++ voyageone-static/2.0/develop/modules/cms/translate/en.js
@@ -551,6 +551,7 @@ define(function() {
         "TXT_STORE_CATEGORY_NAME":"store",
         "MAX_SELLER_CAT_DEPTH":"max level",
         "MAX_SELLER_CAT_CNT":"max tag counts less than 10",
-        "TXT_STORE_CATEGORY_SELECT":"Please select a tag"
+        "TXT_STORE_CATEGORY_SELECT":"Please select a tag",
+        "INVALID_OPERATION_IN_CURRENT_CART_PATH":"当前目录下操作无效"
     };
 });
--- voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/search/addChannelCategory.ctl.js
@@ -37,8 +37,13 @@ define([
                 var self = this;
                 self.addChannelCategoryService.init({"code": self.code, "cartId": self.cartId}).then(function (res) {
                     self.cartList = res.data.cartList;
-                    self.isSelectCid = res.data.isSelectCid;
-                    self.channelCategoryList = res.data.channelCategoryList;
+                    if (self.cartId == "0" || self.cartId == "1") {
+                        self.isSelectCid = [];
+                        self.channelCategoryList = null;
+                    } else {
+                        self.isSelectCid = res.data.isSelectCid;
+                        self.channelCategoryList = res.data.channelCategoryList;
+                    }
                 });
             },
 
@@ -47,42 +52,42 @@ define([
              */
             save: function () {
                 var self = this;
-
-                var cIds = [], cNames = [], fullCNames = [], fullCIds = [];
-
-                var map = flatCategories(this.channelCategoryList);
-
-                _.map(this.isSelectCid, function (value, key) {
-                    return {categoryId: key, selected: value};
-                }).filter(function (item) {
-                    return item.selected;
-                }).forEach(function (item) {
-                    var category = map[item.categoryId];
-                    if (category && category.isParent == 0) {
-                        if (fullCNames.indexOf(category.catPath) < 0) fullCNames.push(category.catPath);
-                        if (fullCIds.indexOf(category.fullCatId) < 0)  fullCIds.push(category.fullCatId);
+                if (self.channelCategoryList == null) {
+                    self.alert('INVALID_OPERATION_IN_CURRENT_CART_PATH');
+                } else {
+                    var cIds = [], cNames = [], fullCNames = [], fullCIds = [];
+                    var map = flatCategories(this.channelCategoryList);
+                    _.map(this.isSelectCid, function (value, key) {
+                        return {categoryId: key, selected: value};
+                    }).filter(function (item) {
+                        return item.selected;
+                    }).forEach(function (item) {
+                        var category = map[item.categoryId];
+                        if (category && category.isParent == 0) {
+                            if (fullCNames.indexOf(category.catPath) < 0) fullCNames.push(category.catPath);
+                            if (fullCIds.indexOf(category.fullCatId) < 0)  fullCIds.push(category.fullCatId);
+                        }
+                        while (category && category.isParent == 0) {
+                            if (cIds.indexOf(category.catId) < 0) cIds.push(category.catId);
+                            if (cNames.indexOf(category.catName) < 0) cNames.push(category.catName);
+                            category = category.parent;
+                        }
+                    });
+                    if (fullCIds.length > 10) {
+                        self.alert('MAX_SELLER_CAT_CNT');
                     }
-                    while (category && category.isParent == 0) {
-                        if (cIds.indexOf(category.catId) < 0) cIds.push(category.catId);
-                        if (cNames.indexOf(category.catName) < 0) cNames.push(category.catName);
-                        category = category.parent;
-                    }
-                });
-                if (fullCIds.length > 10) {
-                    self.alert('MAX_SELLER_CAT_CNT');
+                    self.addChannelCategoryService.save({
+                        "cIds": cIds,
+                        "cNames": cNames,
+                        "fullCNames": fullCNames,
+                        "fullCatId": fullCIds,
+                        "code": self.code,
+                        "cartId": self.cartId
+                    }).then(function () {
+                        self.notify.success('TXT_MSG_UPDATE_SUCCESS');
+                        self.$uibModalInstance.close();
+                    });
                 }
-
-                self.addChannelCategoryService.save({
-                    "cIds": cIds,
-                    "cNames": cNames,
-                    "fullCNames": fullCNames,
-                    "fullCatId": fullCIds,
-                    "code": self.code,
-                    "cartId": self.cartId
-                }).then(function () {
-                    self.notify.success('TXT_MSG_UPDATE_SUCCESS');
-                    self.$uibModalInstance.close();
-                });
             }
         };
         return PopAddChannelCategoryCtrl;
