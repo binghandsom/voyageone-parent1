a9b712d2711ed28e523dc8ad6976d0152fac10ae
desmond <desmond.li@voyageone.cn>
2016-05-30 19:50:00 +0800

    京东价格判断BUG修正   if (Double.compare(minPrice, jdPrice) < 0)   --> if (Double.compare(minPrice, jdPrice) > 0)

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/sx/SxProductService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/sx/SxProductService.java
@@ -563,6 +563,11 @@ public class SxProductService extends BaseService {
                 }
             }
 
+            if (!productModel.getFields().getStatus().equals(CmsConstants.ProductStatus.Approved.name())) {
+                removeProductList.add(productModel);
+                continue;
+            }
+
             List<CmsBtProductModel_Sku> productModelSku = productModel.getSkus();
             List<CmsBtProductModel_Sku> skus = new ArrayList<>(); // 该product下，允许在该平台上上架的sku
             productModelSku.forEach(sku -> {
@@ -787,7 +792,7 @@ public class SxProductService extends BaseService {
                 optionDisplayName = optionDisplayName.trim();
 
                 Double minPrice = Double.parseDouble(optionDisplayName);
-                if (Double.compare(minPrice, jdPrice) < 0) {
+                if (Double.compare(minPrice, jdPrice) > 0) {
                     // 不符合
                     continue;
                 }
@@ -1435,7 +1440,7 @@ public class SxProductService extends BaseService {
     }
 
     // 20160513 tom 图片服务器切换 START
-    public String getImageByTemplateId(String channelId, String imageTemplate, String imageName) {
+    public String getImageByTemplateId(String channelId, String imageTemplate, String imageName) throws Exception {
 
         ImageCreateGetRequest request = new ImageCreateGetRequest();
         request.setChannelId(channelId);
@@ -1446,11 +1451,10 @@ public class SxProductService extends BaseService {
         ImageCreateGetResponse response = null;
         try {
             response = imageCreateService.getImage(request);
+            return imageCreateService.getOssHttpURL(response.getResultData().getFilePath());
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new BusinessException("图片取得失败! 模板id:" + imageTemplate + ", 图片名:" + imageName);
         }
-
-        return imageCreateService.getOssHttpURL(response.getResultData().getFilePath());
     }
     // 20160513 tom 图片服务器切换 END
 
