d8b87a71baaf0edcc7b6e183691d9716a1251824
gjl <gump.gan@voyageone.cn>
2016-06-01 16:00:37 +0800

    tag bug 对应

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/pop/bulkUpdate/CmsAddChannelCategoryService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/pop/bulkUpdate/CmsAddChannelCategoryService.java
@@ -114,7 +114,7 @@ public class CmsAddChannelCategoryService extends BaseAppService {
         //根据codeList取得相关联的code
         List<String> allCodeList=getAllCodeList(codeList, channelId, cartId);
         //取得类目达标下面的个数
-        String cnt = Codes.getCodeName("SELLER_CATS", String.valueOf(cartId));
+        String cnt = Codes.getCodeName("SELLER_CATS_FULL_CIDS", String.valueOf(cartId));
         List<String> editFullCNamesList = new ArrayList<>();
         if(cnt!=null){
             for(String fullCatId:fullCatIdList){
