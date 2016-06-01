08081dffc724f28c542738808beaa81be97a4ce9
Jonas <gaopinsong@live.cn>
2016-06-01 20:03:53 +0800

    update(component/angular): simple angular component

--- voyageone-static/2.0/node_scripts/npm-build-dev-package.js
+++ voyageone-static/2.0/node_scripts/npm-build-dev-package.js
@@ -1,78 +1,58 @@
 var fs = require('fs');
-var common = require('./vars').build.common;
 var glob = require('glob');
-var def = 'define(%deps%%body%);';
-var body = ', function () {\n\n%code%\n\n}';
-var angularFiles = common.angular.src;
+
+var common = require('./vars').build.common;
 var angularPackage = common.angular.dist + '/' + common.angular.concat;
+
 var commonFiles = common.native.src;
 var commonPackage = common.native.dist + '/' + common.native.concat;
-var suffPath = common.native.dist + '/' + common.angular.footerFile;
-var staticRoot = 'develop/';
 
-/**
- * 转换指定路径到相对路径, 并删除后缀名
- */
-function clearPaths(pathArray) {
-    return pathArray.map(i => i.replace('.js', '').replace(staticRoot, ''));
-}
+glob('develop/components/angular/*/*.js', function (err, files) {
+    if (err) {
+        console.error(err);
+        return;
+    }
 
-/**
- * 获取指定目录下的所有文件的相对路径
- */
-function build(src, callback) {
+    if (!files || !files.length) {
+        console.warn('cant find any files !!! -> ' + src);
+        return;
+    }
 
-    console.log("target path   -> " + src);
+    let code = '';
 
-    glob(src, function (err, files) {
-        if (err) {
-            console.error(err);
-            return;
-        }
+    code += 'define(function (require) {\n';
 
-        if (!files || !files.length) {
-            console.warn('cant find any files !!! -> ' + src);
-            return;
-        }
+    code += '  require(\'components/angular/angular.modules\');\n';
 
-        var deps = '[\n  \'' + clearPaths(files).join("',\n  '") + '\'\n]';
-
-        callback(deps);
+    files.map(file => file.replace('develop/', '').replace('.js', '')).forEach(file => {
+        code += '  require(\'' + file + '\');\n';
     });
-}
+    
+    code += '});';
 
-console.log("suff file     -> " + suffPath);
-
-build(angularFiles, function (deps) {
-
-    // 读取后缀文件内容
-    // 并和所有相对路径拼接
-    // 组合成完整的 angular component 声明文件
-    fs.readFile(suffPath, function (err, data) {
-
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        var angular = def.replace('%deps%', deps).replace('%body%', body.replace('%code%', data));
-
-        console.log("will write to -> " + angularPackage);
-
-        fs.writeFile(angularPackage, angular);
-    });
+    fs.writeFile(angularPackage, code);
 });
 
-build(commonFiles, function (deps) {
+glob(commonFiles, function (err, files) {
+    if (err) {
+        console.error(err);
+        return;
+    }
 
-    // 类似 angular component 的编译过程
-    // 普通 component 不需要组合代码, 所以没有后缀
-    var code = def.replace('%deps%', deps).replace('%body%', '');
+    if (!files || !files.length) {
+        console.warn('cant find any files !!! -> ' + src);
+        return;
+    }
 
-    console.log("will write to -> " + angularPackage);
+    let code = '';
+
+    code += 'define(function (require) {\n';
+
+    files.map(file => file.replace('develop/', '').replace('.js', '')).forEach(file => {
+        code += '  require(\'' + file + '\');\n';
+    });
+    
+    code += '});';
 
     fs.writeFile(commonPackage, code);
 });
-
-
-
