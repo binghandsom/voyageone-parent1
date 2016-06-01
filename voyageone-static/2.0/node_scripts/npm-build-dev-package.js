e87457eb02dadf996e345c815439689b94931883
Jonas <gaopinsong@live.cn>
2016-06-01 14:26:19 +0800

    update(npm-build-dev): add comment

--- voyageone-static/2.0/node_scripts/npm-build-dev-package.js
+++ voyageone-static/2.0/node_scripts/npm-build-dev-package.js
@@ -10,10 +10,16 @@ var commonPackage = common.native.dist + '/' + common.native.concat;
 var suffPath = common.native.dist + '/' + common.angular.footerFile;
 var staticRoot = 'develop/';
 
+/**
+ * 转换指定路径到相对路径, 并删除后缀名
+ */
 function clearPaths(pathArray) {
     return pathArray.map(i => i.replace('.js', '').replace(staticRoot, ''));
 }
 
+/**
+ * 获取指定目录下的所有文件的相对路径
+ */
 function build(src, callback) {
 
     console.log("target path   -> " + src);
@@ -39,6 +45,9 @@ console.log("suff file     -> " + suffPath);
 
 build(angularFiles, function (deps) {
 
+    // 读取后缀文件内容
+    // 并和所有相对路径拼接
+    // 组合成完整的 angular component 声明文件
     fs.readFile(suffPath, function (err, data) {
 
         if (err) {
@@ -56,6 +65,8 @@ build(angularFiles, function (deps) {
 
 build(commonFiles, function (deps) {
 
+    // 类似 angular component 的编译过程
+    // 普通 component 不需要组合代码, 所以没有后缀
     var code = def.replace('%deps%', deps).replace('%body%', '');
 
     console.log("will write to -> " + angularPackage);
