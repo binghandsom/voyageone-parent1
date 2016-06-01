9b078b1685ba6019bd2656da1e51884f2dd0c7e6
wenjie.piao@voyageone.cn <wj7405049>
2016-06-01 11:16:17 +0800

    图片

--- voyageone-static/2.0/develop/modules/cms/views/product/detail.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/product/detail.ctl.js
@@ -97,7 +97,6 @@ define([
                         self._isminimall = res.data.isminimall;
                         self._isMain = res.data.isMain;
                         self.currentImage = res.data.defaultImage;
-                        console.log(self.currentImage);
                         self.productDetailsCopy = angular.copy(self.productDetails);
                         self.showInfoFlag = self.productDetails.productDataIsReady
 
