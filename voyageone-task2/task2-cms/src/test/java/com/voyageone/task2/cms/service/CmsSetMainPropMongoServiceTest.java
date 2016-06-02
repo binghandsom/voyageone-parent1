da559b78d5193d0d866e3a42afbee5c3e6a6c481
jeff.duan <jeff.duan@voyageone.cn>
2016-06-02 12:31:45 +0800

    Code拆分对应

--- voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/CmsSetMainPropMongoServiceTest.java
+++ voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/CmsSetMainPropMongoServiceTest.java
@@ -1,11 +1,15 @@
 package com.voyageone.task2.cms.service;
 
+import com.voyageone.task2.base.modelbean.TaskControlBean;
 import com.voyageone.task2.cms.service.CmsSetMainPropMongoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhujiaye on 15/12/21.
@@ -19,6 +23,11 @@ public class CmsSetMainPropMongoServiceTest {
 
     @Test
     public void testOnStartup() throws Exception {
-        cmsSetMainPropMongoService.startup();
+        List<TaskControlBean> taskControlList = new ArrayList<>();
+        TaskControlBean taskControlBean = new TaskControlBean();
+        taskControlBean.setCfg_name("order_channel_id");
+        taskControlBean.setCfg_val1("010");
+        taskControlList.add(taskControlBean);
+        cmsSetMainPropMongoService.onStartup(taskControlList);
     }
 }
