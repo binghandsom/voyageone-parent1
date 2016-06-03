60a5e02916961ddd987f3bdfd113eaa7d240f5f4
chuanyu.liang <chuanyu.liang@voyageone.cn>
2016-06-03 19:38:54 +0800

    add ImageCreateGetListResultRequest

--- voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/model/cms/mongo/product/CmsBtProductModel_Field.java
+++ voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/model/cms/mongo/product/CmsBtProductModel_Field.java
@@ -456,21 +456,14 @@ public class CmsBtProductModel_Field extends BaseMongoMap<String, Object> {
         if (lock == null) {
             return "";
         }
-        return getAttribute("images7");
-    }
-    public void setImages7(List<CmsBtProductModel_Field_Image> images7) {
-        setAttribute("images7", images7);
+        return lock.toString();
     }
 
-    //商品自定义HANG_TAG_IMAGE
-    public List<CmsBtProductModel_Field_Image> getImages8() {
-        if (!this.containsKey("images8") || getAttribute("images8") == null) {
-            setAttribute("images8", new ArrayList<CmsBtProductModel_Field_Image>());
+    public void setLock(String lock) {
+        if (lock == null) {
+            lock = "";
         }
-        return getAttribute("images8");
-    }
-    public void setImages8(List<CmsBtProductModel_Field_Image> images8) {
-        setAttribute("images8", images8);
+        setAttribute("lock", lock);
     }
 
     //状态 new/pending/ready/approved/deleted
