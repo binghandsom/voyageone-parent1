cac6cad7c68fa173c8c61a03dd7f536bb3ef2429
sofia <sofia.yu@voyageone.cn>
2016-06-08 14:41:08 +0800

    New changelist

--- voyageone-static/2.0/develop/modules/cms/views/tools/product/hsCodeList.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/tools/product/hsCodeList.ctl.js
@@ -2,8 +2,51 @@
  * Created by sofia on 6/2/2016.
  */
 define([
-    'angularAMD',
-    'modules/cms/controller/popup.ctl'
-], function () {
+    'cms'
+], function (cms) {
+    cms.controller('HsCodeController', (function () {
+        function HsCodeController(hsCodeInfoService) {
+            this.hsCodeInfoService = hsCodeInfoService;
+            this.prodPageOption = {curr: 1, total: 0, fetch: searchHistoryTasks};
+            this.searchHistoryTasks = searchHistoryTasks;
+            this.getTask = getTask;
+            this.totalHsCodeCnt = 0;
+            this.hsCodeTaskCnt = "";
+            this.hsCodeStatus = "0";
+            this.hsCodeList = [];
+            this.productCode = "";
+            this.productName = "";
+            this.productMaterial = "";
+            this.hsCodeValue = [];
 
+        }
+
+        HsCodeController.prototype = {
+            init: function () {
+                var self = this;
+                self.hsCodeInfoService.init({hsCodeStatus: self.hsCodeStatus}).then(function (res) {
+                    self.totalHsCodeCnt = res.data.totalHsCodeCnt;
+                    self.hsCodeTaskCnt = res.data.hsCodeTaskCnt;
+                    self.hsCodeList = res.data.hsCodeList;
+                    self.productCode = res.data.productCode;
+                    self.productName = res.data.productName;
+                    self.productMaterial = res.data.productMaterial;
+                    self.hsCodeValue = res.data.hsCodeValue;
+                })
+            },
+            search: function () {
+                var self = this;
+                self.hsCodeInfoService.search().then()
+            },
+            getTask: function () {
+                var self = this;
+                self.hsCodeInfoService.get().then()
+            }
+        };
+        function searchHistoryTasks() {
+
+        }
+
+        return HsCodeController;
+    })())
 });
