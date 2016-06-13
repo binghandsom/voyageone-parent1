9a4473a07581d4521ff09ca3736912cb4200c876
sunpeitao <peitaosun@voyageone.cn>
2016-06-13 16:05:57 +0800

    jmpromotiondetaillist

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/jumei2/JmBtDealImportService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/jumei2/JmBtDealImportService.java
@@ -3,17 +3,26 @@ import com.voyageone.service.bean.cms.businessmodel.JMImportData.JMProductDealBe
 import com.voyageone.service.bean.cms.businessmodel.JMImportData.JmBtDealImportModel;
 import com.voyageone.service.bean.cms.businessmodel.JMImportData.JmBtProductModel;
 import com.voyageone.service.bean.cms.businessmodel.JMImportData.JmBtSkuModel;
+import com.voyageone.service.dao.cms.CmsBtJmProductDao;
+import com.voyageone.service.dao.cms.CmsBtJmSkuDao;
 import com.voyageone.service.daoext.cms.JmBtDealImportDaoExt;
+import com.voyageone.service.model.cms.CmsBtJmProductModel;
+import com.voyageone.service.model.cms.CmsBtJmSkuModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class JmBtDealImportService {
     @Autowired
     JmBtDealImportDaoExt daoExtJmBtDealImport;
-
+    @Autowired
+    CmsBtJmProductDao daoCmsBtJmProduct;
+    @Autowired
+    CmsBtJmSkuDao daoCmsBtJmSku;
     public void importJM(String channelId) {
         List<JMProductDealBean> listJMProductDealBean = daoExtJmBtDealImport.selectListProductDealByChannelId(channelId);
         for (JMProductDealBean productDeal : listJMProductDealBean) {
@@ -23,9 +32,68 @@ public class JmBtDealImportService {
 
     private void importProduct(JMProductDealBean productDeal) {
         JmBtDealImportModel modelJmBtDealImport = daoExtJmBtDealImport.selectJmBtDealImportModel(productDeal.getChannelId(), productDeal.getDealId(), productDeal.getProductCode());
-
         JmBtProductModel modelJmBtProduct = daoExtJmBtDealImport.selectJmBtProductModel(productDeal.getChannelId(), productDeal.getDealId(), productDeal.getProductCode());
-
         JmBtSkuModel modelJmBtSku = daoExtJmBtDealImport.selectJmBtSkuModel(productDeal.getChannelId(), productDeal.getDealId(), productDeal.getProductCode());
+        CmsBtJmProductModel modelCmsBtJmProduct = new CmsBtJmProductModel();
+        modelCmsBtJmProduct.setAddressOfProduce(modelJmBtProduct.getAddressOfProduce());
+        // modelCmsBtJmProduct.setApplicableCrowd(modelJmBtProduct.get);
+        modelCmsBtJmProduct.setAttribute(modelJmBtProduct.getAttribute());
+        //modelCmsBtJmProduct.setAvailablePeriod();
+        modelCmsBtJmProduct.setBrandName(modelJmBtProduct.getBrandName());
+        modelCmsBtJmProduct.setChannelId(modelJmBtProduct.getChannelId());
+        //modelCmsBtJmProduct.setColorEn(modelJmBtProduct.get);
+        modelCmsBtJmProduct.setForeignLanguageName(modelJmBtProduct.getForeignLanguageName());
+        modelCmsBtJmProduct.setHsCode(modelJmBtProduct.getHsCode());
+        modelCmsBtJmProduct.setHsName(modelJmBtProduct.getHsName());
+        modelCmsBtJmProduct.setHsUnit(modelJmBtProduct.getHsUnit());
+        //modelCmsBtJmProduct.setImage1();
+        modelCmsBtJmProduct.setJumeiProductId(modelJmBtProduct.getJumeiProductId());
+        //modelCmsBtJmProduct.setMaterialCn();
+        //modelCmsBtJmProduct.setMaterialEn(modelJmBtProduct.getm);
+        //modelCmsBtJmProduct.setMsrpRmb();
+        //modelCmsBtJmProduct.setMsrpUsd();
+        //modelCmsBtJmProduct.setOrigin();
+        modelCmsBtJmProduct.setOriginJmHashId(modelJmBtDealImport.getJumeiHashId());
+        modelCmsBtJmProduct.setProductCode(modelJmBtProduct.getProductCode());
+        modelCmsBtJmProduct.setProductDesCn(modelJmBtProduct.getProductDes());
+        modelCmsBtJmProduct.setProductDesEn("");
+        modelCmsBtJmProduct.setJumeiProductId(modelJmBtProduct.getJumeiProductId());
+        modelCmsBtJmProduct.setProductLongName(modelJmBtDealImport.getProductLongName());
+        modelCmsBtJmProduct.setProductMediumName(modelJmBtDealImport.getProductMediumName());
+        modelCmsBtJmProduct.setProductNameCn(modelJmBtProduct.getProductName());
+        modelCmsBtJmProduct.setProductShortName(modelJmBtDealImport.getProductShortName());
+        modelCmsBtJmProduct.setProductType("");
+        modelCmsBtJmProduct.setRetailPrice(new BigDecimal(0));
+        modelCmsBtJmProduct.setSalePrice(new BigDecimal(0));
+        modelCmsBtJmProduct.setSizeType(modelJmBtProduct.getSizeType());
+        modelCmsBtJmProduct.setSearchMetaTextCustom(modelJmBtDealImport.getSearchMetaTextCustom());
+        modelCmsBtJmProduct.setSpecialnote(modelJmBtProduct.getSpecialNote());
+        modelCmsBtJmProduct.setVoBrandName(modelJmBtProduct.getBrandName());
+        modelCmsBtJmProduct.setCreater("system");
+        modelCmsBtJmProduct.setCreated(new Date());
+        modelCmsBtJmProduct.setModifier("system");
+        modelCmsBtJmProduct.setModified(new Date());
+
+        CmsBtJmSkuModel modelCmsBtJmSku = new CmsBtJmSkuModel();
+        modelCmsBtJmSku.setChannelId(modelJmBtSku.getChannelId());
+        modelCmsBtJmSku.setCmsSize("");
+        modelCmsBtJmSku.setFormat("");
+        modelCmsBtJmSku.setJmSize(modelJmBtSku.getSize());
+        modelCmsBtJmSku.setJmSkuNo(modelJmBtSku.getJumeiSkuNo());
+        modelCmsBtJmSku.setJmSpuNo(modelJmBtSku.getJumeiSpuNo());
+        modelCmsBtJmSku.setProductCode(modelJmBtSku.getProductCode());
+        modelCmsBtJmSku.setSkuCode(modelJmBtSku.getSku());
+        modelCmsBtJmSku.setMsrpRmb(new BigDecimal(0));
+        modelCmsBtJmSku.setMsrpUsd(new BigDecimal(0));
+        modelCmsBtJmSku.setRetailPrice(new BigDecimal(0));
+        modelCmsBtJmSku.setSalePrice(new BigDecimal(0));
+        modelCmsBtJmSku.setUpc(modelJmBtSku.getUpcCode());
+        modelCmsBtJmSku.setModifier("system");
+        modelCmsBtJmSku.setModified(new Date());
+        modelCmsBtJmSku.setCreated(new Date());
+        modelCmsBtJmSku.setCreater("system");
+
+        daoCmsBtJmProduct.insert(modelCmsBtJmProduct);
+        daoCmsBtJmSku.insert(modelCmsBtJmSku);
     }
 }
