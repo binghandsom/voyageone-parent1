224753c993b3ab0af8643b604c7c4a3b1ed9a227
james <james>
2016-06-13 15:18:48 +0800

    产品编辑页 改造

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/product/CmsProductPlatformDetailService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/product/CmsProductPlatformDetailService.java
@@ -127,6 +127,11 @@ public class CmsProductPlatformDetailService extends BaseAppService {
         mastData.put("model", cmsBtProduct.getFields().getModel());
         mastData.put("groupId", cmsBtProductGroup.getGroupId());
         mastData.put("skus", cmsBtProduct.getSkus());
+
+        // TODO 取得Sku的库存
+        Map<String, Integer> skuInventoryList = productService.getProductSkuQty(channelId, cmsBtProduct.getFields().getCode());
+        cmsBtProduct.getSkus().forEach(cmsBtProductModel_sku -> cmsBtProductModel_sku.setQty(skuInventoryList.get(cmsBtProductModel_sku.getSkuCode())));
+
         if (cmsBtProduct.getCommon().getFields() != null) {
             mastData.put("translateStatus", cmsBtProduct.getCommon().getFields().getTranslateStatus());
             mastData.put("hsCodeStatus", cmsBtProduct.getCommon().getFields().getHsCodeStatus());
