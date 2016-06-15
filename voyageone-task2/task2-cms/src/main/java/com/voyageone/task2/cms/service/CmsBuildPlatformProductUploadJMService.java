7c413903b66099bf20dfca1cd18d54317ae3117b
ethan.shi <ethan.shi@vovageone.cn>
2016-06-15 17:47:23 +0800

    JM Upload Product

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsBuildPlatformProductUploadJMService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsBuildPlatformProductUploadJMService.java
@@ -317,7 +317,7 @@ public class CmsBuildPlatformProductUploadJMService extends BaseTaskService {
                     }
 
                     //获取jm_hash_id列表
-                    List<String> jmHashIdList = cmsBtJmPromotionProductDaoExt.selectJmHashIds(productCode, channelId);
+                    List<String> jmHashIdList = cmsBtJmPromotionProductDaoExt.getJmHashIds(productCode, channelId);
                     jmHashIdList.add(originHashId);
 
                     skuList = getCmsBtJmSkuModels(channelId, productCode);
