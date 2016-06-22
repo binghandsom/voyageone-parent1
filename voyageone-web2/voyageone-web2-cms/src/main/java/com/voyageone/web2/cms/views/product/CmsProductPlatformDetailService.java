3b8142e7a1674adc5a31eae79b5c713a7a91eeef
james <james>
2016-06-22 19:13:48 +0800

    bug fix

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/product/CmsProductPlatformDetailService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/product/CmsProductPlatformDetailService.java
@@ -60,9 +60,26 @@ public class CmsProductPlatformDetailService extends BaseAppService {
     public Map<String, Object> getProductPlatform(String channelId, Long prodId, int cartId) {
         CmsBtProductModel cmsBtProduct = productService.getProductById(channelId, prodId);
         CmsBtProductModel_Platform_Cart platformCart = cmsBtProduct.getPlatform(cartId);
-
-        if (platformCart != null && !StringUtil.isEmpty(platformCart.getpCatId())) {
+        if(platformCart == null){
+            platformCart = new CmsBtProductModel_Platform_Cart();
             platformCart.setCartId(cartId);
+        }
+
+        // platform 品牌名
+        if (StringUtil.isEmpty(platformCart.getpBrandId())) {
+            Map<String, Object> parm = new HashMap<>();
+            parm.put("channelId", channelId);
+            parm.put("cartId", cartId);
+            parm.put("cmsBrand", cmsBtProduct.getFields().getBrand());
+            parm.put("active", 1);
+            CmsMtBrandsMappingModel cmsMtBrandsMappingModel = cmsMtBrandsMappingDao.selectOne(parm);
+            if (cmsMtBrandsMappingModel != null) {
+                platformCart.setpBrandId(cmsMtBrandsMappingModel.getBrandId());
+                platformCart.setpBrandName(cmsMtBrandsMappingModel.getCmsBrand());
+            }
+        }
+
+        if (!StringUtil.isEmpty(platformCart.getpCatId())) {
             CmsMtPlatformCategorySchemaModel platformCategorySchemaModel;
             // JM的场合schema就一条
             if (cartId == Integer.parseInt(CartEnums.Cart.JM.getId())) {
@@ -76,19 +93,6 @@ public class CmsProductPlatformDetailService extends BaseAppService {
                 FieldUtil.setFieldsValueFromMap(fields, fieldsValue);
             }
             platformCart.put("schemaFields", fields);
-            // platform 品牌名
-            if (StringUtil.isEmpty(platformCart.getpBrandId())) {
-                Map<String, Object> parm = new HashMap<>();
-                parm.put("channelId", channelId);
-                parm.put("cartId", cartId);
-                parm.put("cmsBrand", cmsBtProduct.getFields().getBrand());
-                parm.put("active", 1);
-                CmsMtBrandsMappingModel cmsMtBrandsMappingModel = cmsMtBrandsMappingDao.selectOne(parm);
-                if (cmsMtBrandsMappingModel != null) {
-                    platformCart.setpBrandId(cmsMtBrandsMappingModel.getBrandId());
-                    platformCart.setpBrandName(cmsMtBrandsMappingModel.getCmsBrand());
-                }
-            }
         }
         return platformCart;
     }
