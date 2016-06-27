f67c25c2f3ad3b7a0b7ab253651d310fabec2d14
Jonas <gaopinsong@live.cn>
2016-06-27 11:55:59 +0800

    update(login): use systemjs

--- voyageone-static/src/login.js
+++ voyageone-static/src/login.js
@@ -2,34 +2,37 @@
  * @Date: 2016-06-24 17:35:36
  * @User: Jonas
  */
-define([
-    'angular',
-    'angular-block-ui',
-    'angular-translate',
-    'angular-vo'
-], function (angular) {
-    angular.module('voyageone.cms.login', [
-        'pascalprecht.translate',
-        'blockUI',
-        'vo.ng'
-    ]).controller('loginController', function ($scope, $ajax) {
-        $scope.username = '';
-        $scope.password = '';
 
-        $scope.login = function () {
+require('assets/js/angular.js/1.5.6/angular.js');
+require('assets/js/angular-translate/2.8.1/angular-translate.js');
+require('shared/components.ng.js');
+require('assets/js/angular-block-ui/0.2.1/angular-block-ui.js');
 
-            $scope.dirty = true;
+angular.module('vo.cms.login', [
+    'pascalprecht.translate',
+    'blockUI',
+    'vo.ng'
+]).controller('loginController', function ($scope, $ajax) {
+    $scope.username = '';
+    $scope.password = '';
 
-            if ($scope.loginForm.$invalid)
-                return;
+    $scope.login = function () {
+
+        $scope.dirty = true;
+
+        if ($scope.loginForm.$invalid)
+            return;
 
 
-            if ($scope.username === 'a') {
-                $scope.message = 'aaaaaaa';
-                return;
-            }
-            location.href = '/app/app.html';
-        };
-    });
-    return angular.bootstrap(document, ['voyageone.cms.login']);
+        if ($scope.username === 'a') {
+
+            alert(1);
+
+            $scope.message = 'aaaaaaa';
+            return;
+        }
+        location.href = '/app/app.html';
+    };
 });
+
+angular.bootstrap(document, ['vo.cms.login']);
