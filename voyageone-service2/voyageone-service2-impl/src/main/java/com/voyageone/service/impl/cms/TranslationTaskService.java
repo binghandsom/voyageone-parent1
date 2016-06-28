d644f1df231940e3f20272ab409ea91f0fa82b15
ethan.shi <ethan.shi@vovageone.cn>
2016-06-28 15:18:27 +0800

    删除冗余文件

---
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/TranslationTaskService.java
@@ -0,0 +1,14 @@
+package com.voyageone.service.impl.cms;
+
+import com.voyageone.service.impl.BaseService;
+
+/**
+ * Created by Ethan Shi on 2016/6/28.
+ *
+ * @author Ethan Shi
+ * @version 2.2.0
+ * @since 2.2.0
+ */
+public class TranslationTaskService extends BaseService {
+
+}
