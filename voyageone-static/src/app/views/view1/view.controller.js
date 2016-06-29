93322282bc1226712380c2433cb9402d587fac45
Jonas <gaopinsong@live.cn>
2016-06-29 15:51:57 +0800

    update(view1)

--- voyageone-static/src/app/views/view1/view.controller.js
+++ voyageone-static/src/app/views/view1/view.controller.js
@@ -1,11 +1,38 @@
 define(['vms'], function (vms) {
     vms.controller('View1Controller', (function () {
-        
+
         function View1Controller() {
-            
+            this.data = [
+                {
+                    orderId: 89567824, orderDate: '2016-06-29 15:30:07', details: [
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000},
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000},
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000},
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000}
+                ]
+                },
+                {
+                    orderId: 89567824, orderDate: '2016-06-29 15:30:07', details: [
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000}
+                ]
+                },
+                {
+                    orderId: 89567824, orderDate: '2016-06-29 15:30:07', details: [
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000},
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000}
+                ]
+                },
+                {
+                    orderId: 89567824, orderDate: '2016-06-29 15:30:07', details: [
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000},
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000},
+                    {sku: 'LKF3958', desc: 'dqaiojgioqjiojdiowqjiojwioqjfiojqwio', status: 'Open', price: 1000}
+                ]
+                }
+            ];
         }
-        
+
         return View1Controller;
-        
+
     }()));
 });
