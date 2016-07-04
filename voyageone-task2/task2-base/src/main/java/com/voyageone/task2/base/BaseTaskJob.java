3e9b08819b1eeb3e764aacb4a59712cdae18b92c
jindong.yang <jindong.yang@voyageone.cn>
2016-07-04 11:25:25 +0800

    rollback for miss push

--- voyageone-task2/task2-base/src/main/java/com/voyageone/task2/base/BaseTaskJob.java
+++ voyageone-task2/task2-base/src/main/java/com/voyageone/task2/base/BaseTaskJob.java
@@ -11,15 +11,13 @@ import com.voyageone.task2.base.modelbean.TaskControlBean;
 import com.voyageone.task2.base.util.TaskControlUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.MDC;
-import org.apache.log4j.NDC;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 /**
  * 基础 Job 类
- * <p>
+ *
  * Created by jonas on 15/6/6.
  */
 public abstract class BaseTaskJob extends VOAbsIssueLoggable {
@@ -39,9 +37,7 @@ public abstract class BaseTaskJob extends VOAbsIssueLoggable {
     public void run() {
 
         String taskCheck = getTaskName();
-//        MDC.put("TaskName", taskCheck);
-//        Object o2 = MDC.get("TaskName");
-        NDC.push(taskCheck);
+
         if (running) {
             $info(taskCheck + "正在运行，忽略");
             return;
@@ -53,15 +49,9 @@ public abstract class BaseTaskJob extends VOAbsIssueLoggable {
 
         startup();
 
-        TestService srv1 = new TestService();
-        srv1.test("模拟调用Service。。。");
-
-
         $info(taskCheck + "任务结束");
 
         running = false;
-//        MDC.clear();
-        NDC.remove();
     }
 
     @Autowired
@@ -75,46 +65,40 @@ public abstract class BaseTaskJob extends VOAbsIssueLoggable {
     }
 
     public void startup() {
-        try {
-            getTaskService().onStartup(null);
-        } catch (Exception e) {
-            e.printStackTrace();
+        // 先获取配置
+        List<TaskControlBean> taskControlList = getControls();
+
+        if (taskControlList.size() < 1) {
+            return;
         }
 
-//        // 先获取配置
-//        List<TaskControlBean> taskControlList = getControls();
-//
-//        if (taskControlList.size() < 1) {
-//            return;
-//        }
-//
-//        // 是否可以运行的判断
-//        if (!TaskControlUtils.isRunnable(taskControlList)) {
-//            return;
-//        }
-//
-//        String taskID = TaskControlUtils.getTaskId(taskControlList);
-//
-//        TaskControlEnums.Status status = TaskControlEnums.Status.START;
-//
-//        // 任务监控历史记录添加:启动
-//        taskDao.insertTaskHistory(taskID, status.getIs());
-//
-//        try {
-//            getTaskService().onStartup(taskControlList);
-//            status = TaskControlEnums.Status.SUCCESS;
-//
-//        } catch (BusinessException be) {
-//            status = TaskControlEnums.Status.ERROR;
-//            $error("出现业务异常，任务退出", be);
-//            issueLog.log(be, ErrorType.BatchJob,getSubSystem());
-//        } catch (Exception e) {
-//            $error("出现异常，任务退出", e);
-//            status = TaskControlEnums.Status.ERROR;
-//            issueLog.log(e, ErrorType.BatchJob, getSubSystem());
-//        }
-//
-//        // 任务监控历史记录添加:结束
-//        taskDao.insertTaskHistory(taskID, status.getIs());
+        // 是否可以运行的判断
+        if (!TaskControlUtils.isRunnable(taskControlList)) {
+            return;
+        }
+
+        String taskID = TaskControlUtils.getTaskId(taskControlList);
+
+        TaskControlEnums.Status status = TaskControlEnums.Status.START;
+
+        // 任务监控历史记录添加:启动
+        taskDao.insertTaskHistory(taskID, status.getIs());
+
+        try {
+            getTaskService().onStartup(taskControlList);
+            status = TaskControlEnums.Status.SUCCESS;
+
+        } catch (BusinessException be) {
+            status = TaskControlEnums.Status.ERROR;
+            $error("出现业务异常，任务退出", be);
+            issueLog.log(be, ErrorType.BatchJob,getSubSystem());
+        } catch (Exception e) {
+            $error("出现异常，任务退出", e);
+            status = TaskControlEnums.Status.ERROR;
+            issueLog.log(e, ErrorType.BatchJob, getSubSystem());
+        }
+
+        // 任务监控历史记录添加:结束
+        taskDao.insertTaskHistory(taskID, status.getIs());
     }
 }
