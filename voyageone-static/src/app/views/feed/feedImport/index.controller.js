8c3392816a30bcef31fe02855a8885d50e9a7834
sofia <sofia.yu@voyageone.cn>
2016-07-06 20:10:16 +0800

    update the home page

--- voyageone-static/src/app/views/feed/feedImport/index.controller.js
+++ voyageone-static/src/app/views/feed/feedImport/index.controller.js
@@ -6,11 +6,7 @@ define([
 ], function (vms) {
     vms.controller('FeedImportResultController', (function () {
 
-        function FeedImportResultController(alert, notify, orderListService, popups) {
-            this.alert = alert;
-            this.notify = notify;
-            this.orderListService = orderListService;
-            this.popups = popups;
+        function FeedImportResultController() {
         }
         return FeedImportResultController;
 
