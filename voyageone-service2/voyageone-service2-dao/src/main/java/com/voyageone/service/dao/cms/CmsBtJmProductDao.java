d282fc44924fefe2c24451861ef5c5dfc2e6a353
chuanyu.liang <chuanyu.liang@voyageone.cn>
2016-07-06 17:33:59 +0800

    add selectCount

--- voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/CmsBtJmProductDao.java
+++ voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/CmsBtJmProductDao.java
@@ -18,6 +18,8 @@ public interface CmsBtJmProductDao {
 
     CmsBtJmProductModel selectOne(Map<String, Object> map);
 
+    int selectCount(Map<String, Object> map);
+
     CmsBtJmProductModel select(Integer id);
 
     int insert(CmsBtJmProductModel record);
