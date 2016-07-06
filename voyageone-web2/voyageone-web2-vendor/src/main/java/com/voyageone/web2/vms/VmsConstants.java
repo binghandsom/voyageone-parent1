17359b47c688ebbeb00e74c937e2d2437efe2a1c
jeff.duan <jeff.duan@voyageone.cn>
2016-07-06 13:43:38 +0800

    vendor开发

---
+++ voyageone-web2/voyageone-web2-vendor/src/main/java/com/voyageone/web2/vms/VmsConstants.java
@@ -0,0 +1,35 @@
+package com.voyageone.web2.vms;
+
+public class VmsConstants {
+
+	// 逗号
+	public final static char COMMA = ',';
+	// utf-8
+	public final static String UTF_8 = "utf-8";
+
+
+	/**
+	 * Feed文件导入状态
+	 */
+	public final static class FeedFileStatus {
+		public final static String WAITING_IMPORT = "1";
+		public final static String IMPORT_COMPLETED = "2";
+		public final static String IMPORT_WITH_ERROR = "3";
+	}
+
+	/**
+	 * ChannelConfig
+	 */
+	public interface ChannelConfig {
+
+		// 通常ConfigCode
+		String COMMON_CONFIG_CODE = "0";
+
+		// CSV分隔符
+		String FEED_CSV_SPLIT_SYMBOL = "FEED_CSV_SPLIT_SYMBOL";
+		// CSV文件编码
+		String FEED_CSV_ENCODE = "FEED_CSV_ENCODE";
+
+
+	}
+}
