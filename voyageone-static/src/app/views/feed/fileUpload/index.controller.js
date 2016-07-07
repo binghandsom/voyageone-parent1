5be7f2b9962b65266cad262ca28a729e21441e97
jeff.duan <jeff.duan@voyageone.cn>
2016-07-07 10:51:11 +0800

    vendor开发

--- voyageone-static/src/app/views/feed/fileUpload/index.controller.js
+++ voyageone-static/src/app/views/feed/fileUpload/index.controller.js
@@ -3,12 +3,41 @@ define([
 ], function (vms) {
     vms.controller('FeedFileUploadController', (function () {
 
-        function FeedFileUploadController(alert, notify, orderListService, popups) {
+        function FeedFileUploadController($scope, alert, notify, blockUI) {
             this.alert = alert;
             this.notify = notify;
-            this.orderListService = orderListService;
-            this.popups = popups;
+            this.blockUI = blockUI;
+            var uploader = $scope.uploader = new FileUploader({
+                url: "/vms/feed/file_upload/importFeedFile"
+            });
         }
+
+        FeedFileUploadController.prototype = {
+            upload: function () {
+                var main = this;
+                var uploadQueue = uploader.queue;
+                var uploadItem = uploadQueue[uploadQueue.length - 1];
+                if (!uploadItem) {
+                    return alert('TXT_MSG_NO_UPLOAD');
+                }
+                var uploadIt = function () {
+                    this.uploadItem = uploadItem;
+                    uploadItem.onSuccess = function (res) {
+                        main.blockUI.stop();
+                        if (res.message) {
+                            alert(res.message);
+                            return;
+                        }
+                        main.notify.success('TXT_MSG_UPLOAD_SUCCESS');
+                    };
+                    uploadItem.formData = [];
+                    uploadItem.upload();
+                    main.blockUI.start();
+                };
+                uploadIt();
+            }
+
+        };
         return FeedFileUploadController;
 
     }()));
