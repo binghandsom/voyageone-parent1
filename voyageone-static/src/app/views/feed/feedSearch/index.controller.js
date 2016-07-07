a9802a695b61533f44d60c1c5393ebc7b797a1a3
sofia <sofia.yu@voyageone.cn>
2016-07-07 10:23:39 +0800

    update the home page

---
+++ voyageone-static/src/app/views/feed/feedSearch/index.controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sofia on 7/6/2016.
+ */
+define([
+    'vms'
+], function (vms) {
+    vms.controller('FeedInFoSearchController', (function () {
+
+        function FeedInFoSearchController() {
+        }
+        return FeedInFoSearchController;
+
+    }()));
+});
