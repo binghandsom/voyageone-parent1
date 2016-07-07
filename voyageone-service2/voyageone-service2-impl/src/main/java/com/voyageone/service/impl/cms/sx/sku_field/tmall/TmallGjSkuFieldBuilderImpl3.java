846302471475d6b5abdec8984362466a69180ae0
morse.lu <morse.lu@voyageone.cn>
2016-07-07 11:00:03 +0800

    0707 天猫上新sku的尺码别名修正

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/sx/sku_field/tmall/TmallGjSkuFieldBuilderImpl3.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/sx/sku_field/tmall/TmallGjSkuFieldBuilderImpl3.java
@@ -71,11 +71,17 @@ public class TmallGjSkuFieldBuilderImpl3 extends AbstractSkuFieldBuilder {
         Map<String, CmsBtProductModel> colorCmsPropductMap;
         Map<String, CmsBtProductModel_Sku> sizeCmsSkuPropMap;
         Map<CmsBtProductModel, String> cmsPropductColorMap;
+        // added by morse.lu 2016/07/07 start
+        Map<CmsBtProductModel_Sku, CmsBtProductModel> skuProductMap;
+        // added by morse.lu 2016/07/07 end
 
         public BuildSkuResult() {
             colorCmsPropductMap = new HashMap<>();
             sizeCmsSkuPropMap = new HashMap<>();
             cmsPropductColorMap = new HashMap<>();
+            // added by morse.lu 2016/07/07 start
+            skuProductMap = new HashMap<>();
+            // added by morse.lu 2016/07/07 end
         }
 
         public Map<CmsBtProductModel, String> getCmsPropductColorMap() {
@@ -89,6 +95,12 @@ public class TmallGjSkuFieldBuilderImpl3 extends AbstractSkuFieldBuilder {
         public Map<String, CmsBtProductModel_Sku> getSizeCmsSkuPropMap() {
             return sizeCmsSkuPropMap;
         }
+
+        // added by morse.lu 2016/07/07 start
+        public Map<CmsBtProductModel_Sku, CmsBtProductModel> getSkuProductMap() {
+            return skuProductMap;
+        }
+        // added by morse.lu 2016/07/07 end
     }
 
     @Override
@@ -268,6 +280,9 @@ public class TmallGjSkuFieldBuilderImpl3 extends AbstractSkuFieldBuilder {
         for (CmsBtProductModel sxProduct : sxProducts) {
             List<CmsBtProductModel_Sku> cmsSkuPropBeans = sxProduct.getCommon().getSkus();
             for (CmsBtProductModel_Sku cmsSkuProp : cmsSkuPropBeans) {
+                // added by morse.lu 2016/07/07 start
+                buildSkuResult.getSkuProductMap().put(cmsSkuProp, sxProduct);
+                // added by morse.lu 2016/07/07 end
                 //CmsBtProductModel_Sku 是Map<String, Object>的子类
                 // deleted by morse.lu 2016/07/04 start
                 // 不Mapping了,不用了吧,虽然留着也不影响,但是set的对象不对,应该是common下的skus + 各平台下面的skus
@@ -493,7 +508,15 @@ public class TmallGjSkuFieldBuilderImpl3 extends AbstractSkuFieldBuilder {
 //            }
             if (skuExtend_aliasnameField != null) {
                 // 别名,用size
-                complexValue.setInputFieldValue(skuExtend_aliasnameField.getId(), cmsSkuProp.getSize());
+                // modified by morse.lu 2016/07/07 start
+                // 先看看sizeNick有没有值，有的话直接用，没的话用size去尺码表里转换一下
+//                complexValue.setInputFieldValue(skuExtend_aliasnameField.getId(), cmsSkuProp.getSize());
+                String size = cmsSkuProp.getSizeNick();
+                if (StringUtils.isEmpty(size)) {
+                    size = getChangeSize(expressionParser, buildSkuResult.getSkuProductMap().get(cmsSkuProp), cmsSkuProp.getSize());
+                }
+                complexValue.setInputFieldValue(skuExtend_aliasnameField.getId(), size);
+                // modified by morse.lu 2016/07/07 end
             }
             // modified by morse.lu 2016/07/04 end
 
