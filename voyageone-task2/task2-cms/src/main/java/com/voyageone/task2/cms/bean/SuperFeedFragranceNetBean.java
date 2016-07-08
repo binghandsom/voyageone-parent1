22231e743b8acd79a26017b61628978275e14689
gjl <gump.gan@voyageone.cn>
2016-07-08 14:56:45 +0800

    fragranceNet

---
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/bean/SuperFeedFragranceNetBean.java
@@ -0,0 +1,228 @@
+package com.voyageone.task2.cms.bean;
+
+/**
+ * Created by gjl on 2016/7/5.
+ */
+public class SuperFeedFragranceNetBean extends SuperFeedBean {
+
+    private String sku;
+
+    private String upc;
+
+    private String voyageoneprice;
+
+    private String msrp;
+
+    private String listingprice;
+
+    private String category;
+
+    private String quantity;
+
+    private String md5;
+
+    private Integer updateflag;
+
+    private String title;
+
+    private String description;
+
+    private String shortdescriptions;
+
+    private String brand;
+
+    private String height;
+
+    private String length;
+
+    private String width;
+
+    private String productweight;
+
+    private String manufacturer;
+
+    private String itemgender;
+
+    private String image1;
+
+    private String image2;
+
+    private String countryoforigin;
+
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku == null ? null : sku.trim();
+    }
+
+    public String getUpc() {
+        return upc;
+    }
+
+    public void setUpc(String upc) {
+        this.upc = upc == null ? null : upc.trim();
+    }
+
+    public String getVoyageoneprice() {
+        return voyageoneprice;
+    }
+
+    public void setVoyageoneprice(String voyageoneprice) {
+        this.voyageoneprice = voyageoneprice == null ? null : voyageoneprice.trim();
+    }
+
+    public String getMsrp() {
+        return msrp;
+    }
+
+    public void setMsrp(String msrp) {
+        this.msrp = msrp == null ? null : msrp.trim();
+    }
+
+    public String getListingprice() {
+        return listingprice;
+    }
+
+    public void setListingprice(String listingprice) {
+        this.listingprice = listingprice == null ? null : listingprice.trim();
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category == null ? null : category.trim();
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity == null ? null : quantity.trim();
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5 == null ? null : md5.trim();
+    }
+
+    public Integer getUpdateflag() {
+        return updateflag;
+    }
+
+    public void setUpdateflag(Integer updateflag) {
+        this.updateflag = updateflag;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public String getShortdescriptions() {
+        return shortdescriptions;
+    }
+
+    public void setShortdescriptions(String shortdescriptions) {
+        this.shortdescriptions = shortdescriptions == null ? null : shortdescriptions.trim();
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand == null ? null : brand.trim();
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height == null ? null : height.trim();
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length == null ? null : length.trim();
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width == null ? null : width.trim();
+    }
+
+    public String getProductweight() {
+        return productweight;
+    }
+
+    public void setProductweight(String productweight) {
+        this.productweight = productweight == null ? null : productweight.trim();
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer == null ? null : manufacturer.trim();
+    }
+
+    public String getItemgender() {
+        return itemgender;
+    }
+
+    public void setItemgender(String itemgender) {
+        this.itemgender = itemgender == null ? null : itemgender.trim();
+    }
+
+    public String getImage1() {
+        return image1;
+    }
+
+    public void setImage1(String image1) {
+        this.image1 = image1 == null ? null : image1.trim();
+    }
+
+    public String getImage2() {
+        return image2;
+    }
+
+    public void setImage2(String image2) {
+        this.image2 = image2 == null ? null : image2.trim();
+    }
+
+    public String getCountryoforigin() {
+        return countryoforigin;
+    }
+
+    public void setCountryoforigin(String countryoforigin) {
+        this.countryoforigin = countryoforigin == null ? null : countryoforigin.trim();
+    }
+}
