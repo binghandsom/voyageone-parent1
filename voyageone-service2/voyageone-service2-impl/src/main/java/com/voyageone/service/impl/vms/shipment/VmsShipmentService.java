1db61651ec0fbb6f88f7f24ac16898eb3d245e8f
vantis <vantis.zhang@voyageone.cn>
2016-07-11 18:39:42 +0800

    订单取消完成一半;

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/vms/shipment/VmsShipmentService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/vms/shipment/VmsShipmentService.java
@@ -3,6 +3,7 @@ package com.voyageone.service.impl.vms.shipment;
 import com.voyageone.service.dao.vms.VmsBtShipmentDao;
 import com.voyageone.service.model.vms.VmsBtShipmentModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
@@ -10,6 +11,7 @@ import java.util.Map;
  * =。=
  * Created by vantis on 16-7-6.
  */
+@Service
 public class VmsShipmentService {
 
     private VmsBtShipmentDao vmsBtShipmentDao;
