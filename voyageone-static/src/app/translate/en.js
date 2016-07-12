c7bc98081b07b3b3af701b0bf426cc0814e77fe1
jeff.duan <jeff.duan@voyageone.cn>
2016-07-12 11:02:39 +0800

    vendor开发

--- voyageone-static/src/app/translate/en.js
+++ voyageone-static/src/app/translate/en.js
@@ -1,6 +1,10 @@
 define(function () {
     return {
         "TXT_ALERT": "Alert",
+        "TXT_FILE_NAME": "File Name",
+        "TXT_FEED": "Feed",
+        "TXT_FEED_IMPORT_RESULT": "Feed Import Result",
+        "TXT_HOME": "Home",
         "TXT_ITEMS": "items",
         "TXT_MSG_NO_UPLOAD": "Please choose a file",
         "TXT_MSG_UPLOAD_SUCCESS": "Upload feed file success",
@@ -11,8 +15,13 @@ define(function () {
         "TXT_PAGE": "Page",
         "TXT_PLEASE_INPUT_A_VALID_DATE": "Please input a valid date.",
         "TXT_SHOWING": "showing",
+        "TXT_SEARCH": "Search",
+        "TXT_STATUS": "Status",
         "TXT_SELECT_NO_VALUE": "Select...",
         "TXT_SHOWING_NO": "The",
+        "TXT_UPLOAD_DATE": "Upload Date",
+        "TXT_UPLOAD_USER": "Upload User",
+        "TXT_EXPORT_ERROR_FILE": "Export Error File",
 
 
 
