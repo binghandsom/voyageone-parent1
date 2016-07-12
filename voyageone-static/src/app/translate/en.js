2ac5d72e5b8d884ce37318822d3354acdaacc14b
jeff.duan <jeff.duan@voyageone.cn>
2016-07-12 10:51:34 +0800

    vendor开发

--- voyageone-static/src/app/translate/en.js
+++ voyageone-static/src/app/translate/en.js
@@ -1,19 +1,25 @@
 define(function () {
     return {
-        "TXT_SHOWING": "showing",
-        "TXT_PAGER_SIZE": "page size",
+        "TXT_ALERT": "Alert",
+        "TXT_ITEMS": "items",
         "TXT_MSG_NO_UPLOAD": "Please choose a file",
         "TXT_MSG_UPLOAD_SUCCESS": "Upload feed file success",
+        "TXT_MSG_NO_DATA": "No Data...",
+        "TXT_NO_CURRENT_SHIPMENT": "No Current Shipment.",
         "TXT_OF": "of",
-        "TXT_ITEMS": "items",
-        "TXT_ALERT": "Alert",
+        "TXT_PAGER_SIZE": "page size",
+        "TXT_PAGE": "Page",
+        "TXT_PLEASE_INPUT_A_VALID_DATE": "Please input a valid date.",
+        "TXT_SHOWING": "showing",
         "TXT_SELECT_NO_VALUE": "Select...",
         "TXT_SHOWING_NO": "The",
-        "TXT_PAGE": "Page",
+
+
+
         "BTN_GO": "GO",
         "BTN_OK": "OK",
-        "TXT_PLEASE_INPUT_A_VALID_DATE": "Please input a valid date.",
-        "TXT_NO_CURRENT_SHIPMENT": "No Current Shipment.",
-        "TXT_MSG_NO_DATA": "No Data..."
+        "BTN_SEARCH": "SEARCH",
+        "BTN_UPLOAD": "UPLOAD"
+
     }
 });
