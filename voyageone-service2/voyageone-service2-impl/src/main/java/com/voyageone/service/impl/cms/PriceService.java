ca2ea3729f3707e789358353d4a5a8980dafe8e1
ethan.shi <ethan.shi@vovageone.cn>
2016-07-13 14:16:12 +0800

    增加价格计算相关表

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/PriceService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/PriceService.java
@@ -1,6 +1,7 @@
 package com.voyageone.service.impl.cms;
 
 import com.voyageone.service.impl.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Ethan Shi on 2016/7/13.
@@ -8,6 +9,16 @@ import com.voyageone.service.impl.BaseService;
  * @since 2.3.0
  */
 public class PriceService  extends BaseService {
+
+
+    @Autowired
+
+
+    private double getShippingFee(String channelId, int cartId, String shippingType, double weight)
+    {
+
+        return 0;
+    }
 
 
 }
