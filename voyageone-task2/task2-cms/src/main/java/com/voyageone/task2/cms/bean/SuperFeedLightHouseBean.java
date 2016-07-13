e172632d6d4b8f5b13a3eab2efd891cfd32f8857
gjl <gump.gan@voyageone.cn>
2016-07-13 15:05:03 +0800

    LightHouse

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/bean/SuperFeedLightHouseBean.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/bean/SuperFeedLightHouseBean.java
@@ -1,5 +1,8 @@
 package com.voyageone.task2.cms.bean;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by gjl on 2016/7/5.
  */
@@ -101,7 +104,11 @@ public class SuperFeedLightHouseBean extends SuperFeedBean {
     }
 
     public String getMd5() {
-        return md5;
+        StringBuffer temp = new StringBuffer();
+        Set<String> noMd5Fields = new HashSet<>();
+        noMd5Fields.add("md5");
+        noMd5Fields.add("updateflag");
+        return  beanToMd5(this,noMd5Fields);
     }
 
     public void setMd5(String md5) {
