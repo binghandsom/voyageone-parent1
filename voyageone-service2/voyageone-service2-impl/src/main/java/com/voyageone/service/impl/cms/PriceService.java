ff30592783aaa60cc38c80cdeda875653cb74dc0
ethan.shi <ethan.shi@vovageone.cn>
2016-07-13 13:24:54 +0800

    增加价格计算相关表

---
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/PriceService.java
@@ -0,0 +1,13 @@
+package com.voyageone.service.impl.cms;
+
+import com.voyageone.service.impl.BaseService;
+
+/**
+ * Created by Ethan Shi on 2016/7/13.
+ * @author Ethan Shi
+ * @since 2.3.0
+ */
+public class PriceService  extends BaseService {
+
+
+}
