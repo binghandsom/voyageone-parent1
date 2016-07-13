e02abeefe98e7faf8ac26498666a792052ea8ea4
Jonas <gaopinsong@live.cn>
2016-07-13 13:56:19 +0800

    update(CmsImportCategoryTreeService): 格式化以下, 换下循环的代码, 换下过滤的位置....

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsImportCategoryTreeService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsImportCategoryTreeService.java
@@ -1,13 +1,18 @@
 package com.voyageone.task2.cms.service;
+
+import com.voyageone.base.exception.BusinessException;
 import com.voyageone.common.components.issueLog.enums.SubSystem;
-import com.voyageone.common.configs.*;
+import com.voyageone.common.configs.Carts;
 import com.voyageone.common.util.ExcelUtils;
 import com.voyageone.common.util.FileUtils;
 import com.voyageone.common.util.MD5;
 import com.voyageone.common.util.StringUtils;
-import com.voyageone.service.dao.cms.mongo.*;
-import com.voyageone.service.impl.cms.*;
-import com.voyageone.service.model.cms.mongo.*;
+import com.voyageone.service.dao.cms.mongo.CmsMtCategoryTreeAllDao;
+import com.voyageone.service.impl.cms.CategoryTreeAllService;
+import com.voyageone.service.impl.cms.PlatformCategoryService;
+import com.voyageone.service.model.cms.mongo.CmsMtCategoryTreeAllModel;
+import com.voyageone.service.model.cms.mongo.CmsMtCategoryTreeAllModel_Platform;
+import com.voyageone.service.model.cms.mongo.CmsMtPlatformCategoryTreeModel;
 import com.voyageone.task2.base.BaseTaskService;
 import com.voyageone.task2.base.modelbean.TaskControlBean;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -18,31 +23,38 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * 将主类目/对应的平台类目信息导入到cms_mt_category_tree_all表
+ * <p>
+ * 2016-07-13 13:32:37
+ * 追加 sku 分割属性字段
  *
  * @author jeff.duan on 2016/6/12.
- * @version 2.1.0
+ * @author jonas.gao on 2016-07-13 13:32:24.
+ * @version 2.3.0
+ * @since 2.1.0
  */
 @Service
 public class CmsImportCategoryTreeService extends BaseTaskService {
 
     @Autowired
     private CmsMtCategoryTreeAllDao cmsMtCategoryTreeAllDao;
+
     @Autowired
     private CategoryTreeAllService categoryTreeAllService;
+
     @Autowired
     private PlatformCategoryService platformCategoryService;
 
-    Map<String, Map<String, CmsMtPlatformCategoryTreeModel>> platformsCategoryTreeMap = new HashMap<>();
-
+    private Map<String, Map<String, CmsMtPlatformCategoryTreeModel>> platformsCategoryTreeMap = new HashMap<>();
 
     private final static String CATEGORY_SHEET_NAME = "主数据类目层次";
     private final static String TMALL_MAPPING_SHEET_NAME = "天猫-Master";
@@ -53,7 +65,6 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
     private final static int CATEGORY_START_INDEX = 1;
     // 主类目的列数
     private final static int CATEGORY_MAX_LEVEL = 10;
-
     // 平台类目开始列
     private final static int PLATFORM_CATEGORY_START_INDEX = 12;
 
@@ -67,73 +78,75 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
         return SubSystem.CMS;
     }
 
-
     public void onStartup(List<TaskControlBean> taskControlList) throws Exception {
-
         // 取得导入文件
         String filePath = com.voyageone.common.configs.Properties.readValue("CmsImportCategoryTreeService_import_file_path");
+
         if (StringUtils.isEmpty(filePath)) {
             filePath = "/usr/category";
         }
+
         File allFiles = new File(filePath);
-        for (File file : allFiles.listFiles()) {
-            if (file.isFile() && file.getName().toLowerCase().contains(".xlsx")) {
-                importCategoryTree(file);
-                // 移动文件到bak
-                FileUtils.mkdirPath(filePath + "/bak");
-                FileUtils.moveFile(file.getAbsolutePath(), filePath + "/bak/" + file.getName());
-            }
+
+        File[] fileArray = allFiles.listFiles(((dir, name) -> name.toLowerCase().endsWith(".xlsx")));
+
+        if (fileArray == null || fileArray.length < 1)
+            return;
+
+        for (File file : fileArray) {
+
+            if (!file.isFile()) continue;
+
+            importCategoryTree(file);
+
+            // 移动文件到bak
+            FileUtils.mkdirPath(filePath + "/bak");
+            FileUtils.moveFile(file.getAbsolutePath(), filePath + "/bak/" + file.getName());
         }
     }
 
     /**
      * 根据xls文件导入主类目树
-     *
      */
-    private void  importCategoryTree(File file) throws IOException, InvalidFormatException {
+    private void importCategoryTree(File file) {
 
         $info("处理文件：" + file.getAbsolutePath());
 
-        Workbook wb = null;
-        try {
-             wb = WorkbookFactory.create(new FileInputStream(file));
-        } catch (Exception e) {
-            $error("导入文件出现异常：" + e.getMessage());
-            throw e;
+        try (InputStream inputStream = new FileInputStream(file);
+             Workbook wb = WorkbookFactory.create(inputStream)) {
+
+            // 导入主类目数据
+            Sheet categorySheet = wb.getSheet(CATEGORY_SHEET_NAME);
+            if (categorySheet != null) {
+                importCategoryData(categorySheet);
+            }
+
+            // 导入天猫国际类目与主类目的匹配关系
+            Sheet tmallCategorySheet = wb.getSheet(TMALL_MAPPING_SHEET_NAME);
+            if (tmallCategorySheet != null) {
+                importPlatformCategoryData(tmallCategorySheet, 23);
+            }
+
+            // 导入京东类目与主类目的匹配关系
+            Sheet jdCategorySheet = wb.getSheet(JD_MAPPING_SHEET_NAME);
+            if (jdCategorySheet != null) {
+                importPlatformCategoryData(jdCategorySheet, 26);
+            }
+
+            // 导入聚美类目与主类目的匹配关系
+            Sheet jumeiCategorySheet = wb.getSheet(JUMEI_MAPPING_SHEET_NAME);
+            if (jdCategorySheet != null) {
+                importPlatformCategoryData(jumeiCategorySheet, 27);
+            }
+        } catch (InvalidFormatException | IOException e) {
+            throw new BusinessException("在导入任务中, 打开 excel 文件是出现错误。", e);
         }
-
-         // 导入主类目数据
-        Sheet categorySheet = wb.getSheet(CATEGORY_SHEET_NAME);
-        if (categorySheet!= null) {
-            importCategoryData(categorySheet);
-        }
-
-        // 导入天猫国际类目与主类目的匹配关系
-        Sheet tmallCategorySheet = wb.getSheet(TMALL_MAPPING_SHEET_NAME);
-        if (tmallCategorySheet != null) {
-            importPlatformCategoryData(tmallCategorySheet, 23);
-        }
-
-        // 导入京东类目与主类目的匹配关系
-        Sheet jdCategorySheet = wb.getSheet(JD_MAPPING_SHEET_NAME);
-        if (jdCategorySheet != null) {
-            importPlatformCategoryData(jdCategorySheet, 26);
-        }
-
-        // 导入聚美类目与主类目的匹配关系
-        Sheet jumeiCategorySheet = wb.getSheet(JUMEI_MAPPING_SHEET_NAME);
-        if (jdCategorySheet != null) {
-            importPlatformCategoryData(jumeiCategorySheet, 27);
-        }
-
-
     }
 
     /**
      * 导入平台类目与主类目的匹配关系
-     *
      */
-    private void  importPlatformCategoryData(Sheet sheet, int cartId) {
+    private void importPlatformCategoryData(Sheet sheet, int cartId) {
         int index = 0;
 
         // 根据cartId取得platformId
@@ -143,7 +156,7 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
             return;
         }
 
-        Map<String, CmsMtPlatformCategoryTreeModel> platformCategoryTreeMap = null;
+        Map<String, CmsMtPlatformCategoryTreeModel> platformCategoryTreeMap;
         if (platformsCategoryTreeMap.containsKey(platformId)) {
             platformCategoryTreeMap = platformsCategoryTreeMap.get(platformId);
         } else {
@@ -155,17 +168,14 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
             // 转换成Map
             platformCategoryTreeMap = new HashMap<>();
             platformCategoryTreeList = platformCategoryTreeStream.collect(Collectors.toList());
-            for (CmsMtPlatformCategoryTreeModel platformCategoryTree : platformCategoryTreeList) {
-                if (!platformCategoryTreeMap.containsKey(platformCategoryTree.getCatPath())) {
-                    platformCategoryTreeMap.put(platformCategoryTree.getCatPath(), platformCategoryTree);
-                }
-            }
+            platformCategoryTreeList.stream()
+                    .filter(platformCategoryTree -> !platformCategoryTreeMap.containsKey(platformCategoryTree.getCatPath()))
+                    .forEach(platformCategoryTree -> platformCategoryTreeMap.put(platformCategoryTree.getCatPath(), platformCategoryTree));
             platformsCategoryTreeMap.put(platformId, platformCategoryTreeMap);
         }
 
         for (Row row : sheet) {
-            boolean insertFlg = false;
-            if(index == 0) {
+            if (index == 0) {
                 // 跳过第一行header
                 index++;
                 continue;
@@ -181,18 +191,17 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
             }
             // 去掉最后一个">"
             if (categoryPath.length() > 0) {
-                categoryPath = categoryPath.substring(0,categoryPath.length() -1);
+                categoryPath = categoryPath.substring(0, categoryPath.length() - 1);
             }
 
             // 根据主类目路径取得对应的类目的一级类目对象
             CmsMtCategoryTreeAllModel categoryTreeFistLevel = categoryTreeAllService.getFirstLevelCategoryObjectByCatPath(categoryPath);
 
-            if (categoryTreeFistLevel == null ) {
-                $error("找不到主类目,sheet名：" + sheet.getSheetName() + ",行号：" +(index + 1) + ",主类目路径：" + categoryPath);
+            if (categoryTreeFistLevel == null) {
+                $error("找不到主类目,sheet名：" + sheet.getSheetName() + ",行号：" + (index + 1) + ",主类目路径：" + categoryPath);
                 index++;
                 continue;
             }
-
 
             // 这一行的平台类目路径
             String platformCategoryPath = "";
@@ -204,19 +213,19 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
             }
             // 去掉最后一个">"
             if (platformCategoryPath.length() > 0) {
-                platformCategoryPath = platformCategoryPath.substring(0,platformCategoryPath.length() -1);
+                platformCategoryPath = platformCategoryPath.substring(0, platformCategoryPath.length() - 1);
             }
 
             CmsMtPlatformCategoryTreeModel platformCategoryTree = platformCategoryTreeMap.get(platformCategoryPath);
 
             if (platformCategoryTree == null) {
-                $error("找不到平台类目,sheet名：" + sheet.getSheetName() + ",行号：" +(index + 1) + ",平台类目路径：" + platformCategoryPath);
+                $error("找不到平台类目,sheet名：" + sheet.getSheetName() + ",行号：" + (index + 1) + ",平台类目路径：" + platformCategoryPath);
                 index++;
                 continue;
             }
 
             // 把对应的平台类目匹配关系，设定到主类目下(包含其子类目)
-            CmsMtCategoryTreeAllModel categoryTree = null;
+            CmsMtCategoryTreeAllModel categoryTree;
             if (categoryPath.equals(categoryTreeFistLevel.getCatPath())) {
                 categoryTree = categoryTreeFistLevel;
             } else {
@@ -228,7 +237,6 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
             cmsMtCategoryTreeAllDao.update(categoryTreeFistLevel);
 
             index++;
-
         }
     }
 
@@ -252,7 +260,6 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
 
     /**
      * 把对应的平台类目匹配关系，设定到主类目下(包含其子类目)
-     *
      */
     private void addPlatformMappingToCategoryTree(CmsMtCategoryTreeAllModel categoryTree, CmsMtPlatformCategoryTreeModel platformCategoryTree, String platformId) {
 
@@ -278,9 +285,8 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
 
     /**
      * 取得某个平台的平台类目匹配关系对象
-     *
      */
-    private CmsMtCategoryTreeAllModel_Platform  getPlatformCategory(List<CmsMtCategoryTreeAllModel_Platform> platformCategoryList, String platformId) {
+    private CmsMtCategoryTreeAllModel_Platform getPlatformCategory(List<CmsMtCategoryTreeAllModel_Platform> platformCategoryList, String platformId) {
         for (CmsMtCategoryTreeAllModel_Platform platformCategory : platformCategoryList) {
             if (platformId.equals(platformCategory.getPlatformId())) {
                 return platformCategory;
@@ -293,14 +299,13 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
 
     /**
      * 导入主类目数据
-     *
      */
-    private void  importCategoryData(Sheet sheet) {
+    private void importCategoryData(Sheet sheet) {
         int index = 0;
 
         for (Row row : sheet) {
             boolean insertFlg = false;
-            if(index == 0) {
+            if (index == 0) {
                 // 跳过第一行header
                 index++;
                 continue;
@@ -359,13 +364,11 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
                 cmsMtCategoryTreeAllDao.update(categoryTree);
             }
             index++;
-
         }
     }
 
     /**
      * 根据类目路径 增加类目对象
-     *
      */
     private CmsMtCategoryTreeAllModel makeCategoryObject(CmsMtCategoryTreeAllModel modelParent, String categoryName, String categoryPath, Row row, int index) {
 
@@ -378,7 +381,7 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
             }
         }
         // 找不到新加一个
-        if (findCategoryTree == null)  {
+        if (findCategoryTree == null) {
             findCategoryTree = new CmsMtCategoryTreeAllModel();
             findCategoryTree.setCatId(MD5.getMD5(categoryPath));
             findCategoryTree.setCatName(categoryName);
@@ -398,33 +401,23 @@ public class CmsImportCategoryTreeService extends BaseTaskService {
         }
 
         return findCategoryTree;
-
     }
 
     /**
      * 更新对象的情况下，删除child对象的Creater/Updater/Created/Updated这几个属性
-     *
      */
     private void removeCreateUpdateInfo(CmsMtCategoryTreeAllModel categoryTree) {
-        for (CmsMtCategoryTreeAllModel child : categoryTree.getChildren()) {
-            removeItemCreateUpdate(child);
-        }
-
+        categoryTree.getChildren().forEach(this::removeItemCreateUpdate);
     }
 
     /**
      * 更新对象的情况下，删除child对象的Creater/Updater/Created/Updated这几个属性
-     *
      */
     private void removeItemCreateUpdate(CmsMtCategoryTreeAllModel categoryTree) {
         categoryTree.setModifier(null);
         categoryTree.setModified(null);
         categoryTree.setCreater(null);
         categoryTree.setCreated(null);
-        for (CmsMtCategoryTreeAllModel child : categoryTree.getChildren()) {
-            removeItemCreateUpdate(child);
-        }
+        categoryTree.getChildren().forEach(this::removeItemCreateUpdate);
     }
-
-
 }
