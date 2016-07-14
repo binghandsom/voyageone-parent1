438fdd3cc6d462a116b66eb7b801c28eaa754c1f
Vantis <vantis.zhang@voyageone.cn>
2016-07-14 20:05:28 +0800

    修正了OrderInfo前端页面时间的兼容性问题(喵了个咪谁把Date类型的Json转换改成String的给我站出来我保证不打死你=。=)

--- voyageone-static/src/app/views/pop/orderInfo/newShipment/index.ctl.js
+++ voyageone-static/src/app/views/pop/orderInfo/newShipment/index.ctl.js
@@ -6,34 +6,39 @@ define([
 ], function (vms) {
     vms.controller('NewShipmentController', (function () {
 
-        function NewShipmentController(alert, notify, confirm, shipmentPopupService, context) {
-            this.alert = alert;
-            this.notify = notify;
-            this.confirm = confirm;
-            this.shipmentPopupService = shipmentPopupService;
-            this.shipmentExisted = false;
-            this.expressCompanies = [
-                {
-                    name: "test",
-                    value: 1
-                }
-            ];
-            this.shipment = angular.copy(context.shipment);
-            if (!this.shipment) {
-                this.shipment = {status: "1"}
-            } else this.shipmentExisted = true;
+            function NewShipmentController(alert, notify, confirm, shipmentPopupService, context) {
+                this.alert = alert;
+                this.notify = notify;
+                this.confirm = confirm;
+                this.shipmentPopupService = shipmentPopupService;
+                this.shipmentExisted = false;
+                this.expressCompanies = [
+                    {
+                        name: "test",
+                        value: 1
+                    }
+                ];
+                this.shipment = angular.copy(context.shipment);
+                if (!this.shipment) {
+                    this.shipment = {status: "1"}
+                } else this.shipmentExisted = true;
+            }
 
-            this.shipmentPopupService.init().then((data) => {
-                this.expressCompanies = data.expressCompanies;
-            })
-        }
+            NewShipmentController.prototype.init = function () {
+                var self = this;
 
-        NewShipmentController.prototype.submit = function () {
-            // this.shipmentPopupService.submit(this.shipment).then(function (res) {
+                this.shipmentPopupService.init().then(function (data) {
+                    self.expressCompanies = data.expressCompanies;
+                })
+            };
+
+
+            NewShipmentController.prototype.submit = function () {
+                // this.shipmentPopupService.submit(this.shipment).then(function (res) {
                 console.info("sdfassa");
-            // })
-        };
+                // })
+            };
 
-        return NewShipmentController;
-    })());
+            return NewShipmentController;
+        })());
 });
