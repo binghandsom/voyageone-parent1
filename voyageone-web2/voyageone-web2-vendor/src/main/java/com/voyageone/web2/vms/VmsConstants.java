1bde84eaf4db68e4606c1724a2be5ace77c5ac02
jeff.duan <jeff.duan@voyageone.cn>
2016-07-15 15:06:57 +0800

    vendor开发

--- voyageone-web2/voyageone-web2-vendor/src/main/java/com/voyageone/web2/vms/VmsConstants.java
+++ voyageone-web2/voyageone-web2-vendor/src/main/java/com/voyageone/web2/vms/VmsConstants.java
@@ -14,6 +14,7 @@ public class VmsConstants {
     public final static int DEFAULT_PAGE_SIZE = 10;
 
     public final static String ORDER_TIME = "order_time";
+    public final static String CONSOLIDATION_ORDER_TIME = "consolidation_order_time";
     /**
      * Feed文件导入状态
      */
