594b5ad8d9dfe8def6d867162cddcc5ecb7ff796
sofia <sofia.yu@voyageone.cn>
2016-07-18 18:15:54 +0800

    fix the style of the old pages

--- voyageone-static/src/app/views/pop/orderInfo/addToShipment/index.ctl.js
+++ voyageone-static/src/app/views/pop/orderInfo/addToShipment/index.ctl.js
@@ -7,18 +7,17 @@ define([
     vms.controller('AddToShipmentController', (function () {
         function AddToShipmentController(context) {
             this.context = context;
+            this.barcode = null;
         }
 
-        AddToShipmentController.prototype = {
-            init: function () {
-                var self = this;
-                self.summary = self.context;
-            },
-            scan: function (barcode) {
-                var self = this;
+        AddToShipmentController.prototype.init = function () {
+            var self = this;
+            self.summary = self.context;
+        };
 
-            }
-
+        AddToShipmentController.prototype.scan = function (barcode) {
+            var self = this;
+            self.barcode = null;
         };
         return AddToShipmentController;
     })());
