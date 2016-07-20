924c1f96d01a547e72065d43051e51377a922340
jindong.yang <jindong.yang@voyageone.cn>
2016-07-20 16:01:07 +0800

    rollback modification for flume test

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/VOCms2Startup.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/VOCms2Startup.java
@@ -3,9 +3,6 @@
  */
 package com.voyageone.task2;
 
-import com.voyageone.common.spring.SpringContext;
-import com.voyageone.task2.cms.job.AttributeTranslateJob;
-import com.voyageone.task2.cms.job.CmsBuildMasterCategoryJob;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
@@ -15,7 +12,6 @@ import org.springframework.context.support.GenericXmlApplicationContext;
  * @author jacky.zhu
  * @version 2.0.0
  */
-
 public class VOCms2Startup {
 
     private static final Log log = LogFactory.getLog(VOCms2Startup.class);
@@ -25,16 +21,9 @@ public class VOCms2Startup {
     public static void main(String[] args) {
         try {
             log.info(NAME + " -> 启动中......");
-//            Context context = Context.getContext();
+            Context context = Context.getContext();
             ApplicationContext ctx = new GenericXmlApplicationContext("applicationContext.xml");
-//            context.putAttribute("springContext", ctx);
-            AttributeTranslateJob job = ctx.getBean(AttributeTranslateJob.class);
-
-            job.run();
-
-            CmsBuildMasterCategoryJob job2 = ctx.getBean(CmsBuildMasterCategoryJob.class);
-            job2.run();
-
+            context.putAttribute("springContext", ctx);
             log.info(NAME + " -> 启动完成......");
         } catch (Exception ex) {
             ex.printStackTrace();
