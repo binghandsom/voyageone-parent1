c6a7578dd78e25434db6f225f8f882761df59f76
Gubuchun <gubuchun@voyageone.cn>
2016-07-26 11:03:00 +0800

    update brand mapping

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/BrandBtMappingService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/BrandBtMappingService.java
@@ -9,7 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.voyageone.common.configs.Enums.CartEnums;
-import com.voyageone.service.bean.cms.BrandBtMappingBean;
+import com.voyageone.service.bean.cms.CmsBtBrandBean;
+import com.voyageone.service.bean.cms.CmsBtBrandMappingBean;
 import com.voyageone.service.daoext.cms.CmsBtBrandMappingDaoExt;
 import com.voyageone.service.impl.BaseService;
 
@@ -22,7 +23,15 @@ public class BrandBtMappingService extends BaseService {
 	@Autowired
 	private CmsBtBrandMappingDaoExt cmsBtBrandMappingDaoExt;
 	
-	public List<BrandBtMappingBean> searchBrandsByPage(String channelId, int cartId, String langId,
+	public List<CmsBtBrandBean> searchMasterBrands(String channelId, int cartId, String brandName) {
+		return cmsBtBrandMappingDaoExt.searchMasterBrands(channelId, cartId, brandName);
+	}
+	
+	public List<CmsBtBrandBean> searchJmBrands(String langId, String brandName) {
+		return cmsBtBrandMappingDaoExt.searchJmBrands(langId, brandName);
+	}
+	
+	public List<CmsBtBrandMappingBean> searchBrandsByPage(String channelId, int cartId, String langId,
 			Map<String, Object> others) {
 		Map<String, Object> params = formatBrandsQueryParameter(channelId, cartId, langId, others);
 		return cmsBtBrandMappingDaoExt.searchBrandsByPage(params);
@@ -31,6 +40,11 @@ public class BrandBtMappingService extends BaseService {
 	public Long searchBrandsCount(String channelId, Integer cartId, String langId, Map<String, Object> others) {
 		Map<String, Object> params = formatBrandsQueryParameter(channelId, cartId, langId, others);
 		return cmsBtBrandMappingDaoExt.searchBrandsCount(params);
+	}
+	
+	public List<CmsBtBrandMappingBean> searchMatchedBrands(String channelId, Integer cartId, String langId,
+			String brandId) {
+		return cmsBtBrandMappingDaoExt.searchMatchedBrands(channelId, cartId, langId, brandId);
 	}
 	
 	private Map<String, Object> formatBrandsQueryParameter(String channelId, int cartId, String langId,
@@ -43,9 +57,9 @@ public class BrandBtMappingService extends BaseService {
 		params.put("typeId", 41); // TODO 品牌类型ID为41，常量在哪里定义？
 		// 区分聚美品牌与其他品牌
 		if (CartEnums.Cart.JM.getId().equals(String.valueOf(cartId))) {
-			params.put("isJMPlatform", true);
+			params.put("isJmBrand", true);
 		} else {
-			params.put("isJMPlatform", false);
+			params.put("isJmBrand", false);
 		}
 		// 添加其他非必要参数
 		if (MapUtils.isNotEmpty(others)) {
--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/mapping/brand/BrandMappingService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/mapping/brand/BrandMappingService.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.voyageone.service.bean.cms.BrandBtMappingBean;
+import com.voyageone.common.configs.Enums.CartEnums;
+import com.voyageone.service.bean.cms.CmsBtBrandBean;
+import com.voyageone.service.bean.cms.CmsBtBrandMappingBean;
 import com.voyageone.service.impl.cms.BrandBtMappingService;
 import com.voyageone.web2.base.BaseAppService;
 import com.voyageone.web2.cms.bean.BrandMappingBean;
@@ -22,9 +24,9 @@ public class BrandMappingService extends BaseAppService {
 	private BrandBtMappingService brandBtMappingService;
 
 	/**
-	 * 搜索品牌匹配数据
+	 * 检索品牌匹配数据
 	 */
-	public List<BrandBtMappingBean> searchBrandsByPage(BrandMappingBean brandMapping) {
+	public List<CmsBtBrandMappingBean> searchBrandsByPage(BrandMappingBean brandMapping) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("mappingState", brandMapping.getMappingState());
 		params.put("brandName", brandMapping.getBrandName());
@@ -35,11 +37,9 @@ public class BrandMappingService extends BaseAppService {
 				brandMapping.getLangId(), params);
 	}
 
-	public Object searchCustBrands(BrandMappingBean brandMapping) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * 检索品牌数量
+	 */
 	public Long searchBrandsCount(BrandMappingBean brandMapping) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("mappingState", brandMapping.getMappingState());
@@ -47,6 +47,31 @@ public class BrandMappingService extends BaseAppService {
 		// 查询品牌匹配关系
 		return brandBtMappingService.searchBrandsCount(brandMapping.getChannelId(), brandMapping.getCartId(),
 				brandMapping.getLangId(), params);
+	}
+
+	/**
+	 * 检索商户品牌数据
+	 */
+	public List<CmsBtBrandBean> searchCustBrands(BrandMappingBean brandMapping) {
+		List<CmsBtBrandBean> brandList = null;
+		if (CartEnums.Cart.JM.getId().equals(String.valueOf(brandMapping.getCartId()))) {
+			// 聚美的品牌数据
+			brandList = brandBtMappingService.searchJmBrands(brandMapping.getLangId(), brandMapping.getBrandName());
+		} else {
+			// 非聚美的品牌数据
+			brandList = brandBtMappingService.searchMasterBrands(brandMapping.getChannelId(), brandMapping.getCartId(),
+					brandMapping.getBrandName());
+		}
+		
+		return brandList;
+	}
+	
+	/**
+	 * 检索品牌数量 
+	 */
+	public List<CmsBtBrandMappingBean> searchMatchedBrands(BrandMappingBean brandMapping) {
+		return brandBtMappingService.searchMatchedBrands(brandMapping.getChannelId(), brandMapping.getCartId(),
+				brandMapping.getLangId(), brandMapping.getBrandId());
 	}
 
 }
