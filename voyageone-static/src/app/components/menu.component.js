86cb649ec7e1f2ef2bd88eb30cbb75ae31fa378e
jeff.duan <jeff.duan@voyageone.cn>
2016-07-26 19:04:35 +0800

    vendor系统开发

--- voyageone-static/src/app/components/menu.component.js
+++ voyageone-static/src/app/components/menu.component.js
@@ -1,12 +1,24 @@
 define(function () {
     return angular.module('vms.menu', []).component('voMenu', {
         templateUrl: 'components/menu.html',
-        controller: function MenuController(menuService) {
+        controller: 'MenuController'
+    }).controller('MenuController', (function () {
+        function MenuController(menuService, $window) {
             this.menus = [];
+            this.$window = $window;
             var main = this;
             menuService.getVendorMenuHeaderInfo().then(function (res) {
                 main.menus = res.menuInfo;
             })
         }
-    });
+
+        MenuController.prototype = {
+            selectMenu: function (url) {
+                sessionStorage.clear();
+                this.$window.location = url;
+            }
+        }
+
+        return MenuController;
+    }()));
 });
