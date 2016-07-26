80321003c4d6908b636a7260869f377e310cc6ab
sofia <sofia.yu@voyageone.cn>
2016-07-26 17:06:57 +0800

    fix a bug on the platform-brand mapping

--- voyageone-static/2.0/develop/modules/cms/views/pop/channel/platform_brand_setting.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/channel/platform_brand_setting.ctl.js
@@ -22,19 +22,13 @@ define([
             },
             selectedPlatformBrand: function (item) {
                 var self = this;
-                self.selectedPlatform = item.name;
+                self.selectedPlatform = item.brandName;
             },
             refresh: function () {
                 var self = this;
                 self.brandMappingService.searchCustBrands({'cartId': self.platformData.cartId}).then(function (res) {
                     self.custBrandList = res.data.custBrandList;
                 });
-                self.platformList = [
-                    {id: '01', name: "Vans"}, {id: '02', name: "耐克"}, {id: '03', name: "阿迪达斯"},
-                    {id: '04', name: "NewBalance"}, {id: '05', name: "Skechers"}, {id: '06', name: "Vansss"},
-                    {id: '07', name: "NewBalance3"}, {id: '08', name: "Skechers22"}, {id: '09', name: "NewBalance444"},
-                    {id: '010', name: "耐克a"}, {id: '011', name: "阿迪达斯2"}, {id: '012', name: "阿迪达斯43"}
-                ];
             },
             submitSet: function () {
                 var self = this;
