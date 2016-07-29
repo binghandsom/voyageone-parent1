89086cf0614e63ef515ca5d0e958a320af9fa379
gjl <gump.gan@voyageone.cn>
2016-07-29 11:14:40 +0800

    yoga

--- voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/feed/YogaDemocracyAnalysisServiceTest.java
+++ voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/feed/YogaDemocracyAnalysisServiceTest.java
@@ -8,8 +8,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author gump on 2016/07/27.
@@ -23,21 +21,6 @@ public class YogaDemocracyAnalysisServiceTest {
 
     @Test
     public void testOnStartup() throws Exception {
-
-
-        String str = "7.1oz".trim();
-
-        Pattern pattern = Pattern.compile("[^0-9.]");
-        Matcher matcher = pattern.matcher(str);
-        if (matcher.find()) {
-            int index = str.indexOf(matcher.group());
-            if(index != -1){
-                String a= str.substring(0,index);
-                String b=str.substring(index,str.length());
-                System.out.println(a+"   "+b);
-            }
-        }
-
         yogaDemocracyAnalysisService.onStartup(new ArrayList<TaskControlBean>());
     }
 }
