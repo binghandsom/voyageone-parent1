aa4a8a57c1fbb96e5e2b8d117318e7fb37696c10
jeff.duan <jeff.duan@voyageone.cn>
2016-08-10 15:03:39 +0800

    vendor系统开发

--- voyageone-task2/task2-vms/src/test/java/com/voyageone/task2/vms/service/VmsFeedFileImportServiceTest.java
+++ voyageone-task2/task2-vms/src/test/java/com/voyageone/task2/vms/service/VmsFeedFileImportServiceTest.java
@@ -1,24 +1,20 @@
 package com.voyageone.task2.vms.service;
 
 import com.voyageone.service.impl.com.mq.MqSender;
-import com.voyageone.task2.base.modelbean.TaskControlBean;
-import com.voyageone.task2.cms.service.CmsSetMainPropMongoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * Created by jeff.duan on 16/06/29.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:context-cms-test.xml")
+@ContextConfiguration("classpath:context-vms-test.xml")
 public class VmsFeedFileImportServiceTest {
 
     @Autowired
--- voyageone-task2/task2-vms/src/test/java/com/voyageone/task2/vms/service/VmsMakeFinancialReportServiceTest.java
+++ voyageone-task2/task2-vms/src/test/java/com/voyageone/task2/vms/service/VmsMakeFinancialReportServiceTest.java
@@ -14,22 +14,30 @@ import java.util.List;
  * Created by jeff.duan on 16/06/29.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:context-cms-test.xml")
-public class VmsGetHomeInfoServiceTest {
+@ContextConfiguration("classpath:context-vms-test.xml")
+public class VmsMakeFinancialReportServiceTest {
 
     @Autowired
-    private VmsGetHomeInfoService vmsGetHomeInfoService;
+    private VmsMakeFinancialReportService vmsMakeFinancialReportService;
     @Test
     public void testOnStartup() throws Exception {
         List<TaskControlBean> taskControlList = new ArrayList<>();
         TaskControlBean taskControlBean = new TaskControlBean();
         taskControlBean.setCfg_name("order_channel_id");
-        taskControlBean.setCfg_val1("090");
+        taskControlBean.setCfg_val1("088");
         taskControlList.add(taskControlBean);
         TaskControlBean taskControlBean1 = new TaskControlBean();
         taskControlBean1.setCfg_name("order_channel_id");
-        taskControlBean1.setCfg_val1("091");
+        taskControlBean1.setCfg_val1("089");
         taskControlList.add(taskControlBean1);
-        vmsGetHomeInfoService.onStartup(taskControlList);
+        TaskControlBean taskControlBean2 = new TaskControlBean();
+        taskControlBean2.setCfg_name("order_channel_id");
+        taskControlBean2.setCfg_val1("090");
+        taskControlList.add(taskControlBean2);
+        TaskControlBean taskControlBean3 = new TaskControlBean();
+        taskControlBean3.setCfg_name("order_channel_id");
+        taskControlBean3.setCfg_val1("091");
+        taskControlList.add(taskControlBean3);
+        vmsMakeFinancialReportService.onStartup(taskControlList);
     }
 }
