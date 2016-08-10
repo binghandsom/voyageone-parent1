0acbdacd42584f1aae960305b314ac1dd8c75989
wangtiedao <tiedao.wang@voyageone.cn>
2016-08-10 16:43:32 +0800

    save local

--- voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/views/channel/ChannelController.java
+++ voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/views/channel/ChannelController.java
@@ -3,6 +3,8 @@ package com.voyageone.web2.admin.views.channel;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.base.Preconditions;
+import com.voyageone.service.impl.admin.channel.ChannelService;
+import com.voyageone.service.model.admin.PageModel;
+import com.voyageone.service.model.admin.TmOrderChannelModel;
 import com.voyageone.web2.admin.bean.channel.ChannelFormBean;
 import com.voyageone.web2.admin.views.AdminController;
 import com.voyageone.web2.admin.views.AdminUrlConstants;
@@ -23,14 +28,20 @@ import com.voyageone.web2.base.ajax.AjaxResponse;
 @RequestMapping(value = AdminUrlConstants.Channel.Self.ROOT, method = RequestMethod.POST)
 public class ChannelController extends AdminController {
 	
+	@Resource(name = "AdminChannelService")
+	private ChannelService channelService;
+	
 	@RequestMapping(AdminUrlConstants.Channel.Self.SEARCH_CHANNEL)
 	public AjaxResponse searchChannel(@RequestBody ChannelFormBean form) {
-		Map<String, Object> result = new HashMap<String, Object>();
+		// 验证参数
+		Preconditions.checkNotNull(form.getPageNum());
+		Preconditions.checkNotNull(form.getPageSize());
+
 		// 检索渠道信息
-		String channelList = "";
-		result.put("channelList", channelList);
+		PageModel<TmOrderChannelModel> channelPage = channelService.searchChannel(form.getChannelId(), form.getName(),
+				form.getFullName(), form.getIsUsjoi(), form.getPageNum(), form.getPageSize());
 		
-		return success(result);
+		return success(channelPage);
 	}
 	
 	@RequestMapping(AdminUrlConstants.Channel.Self.ADD_OR_UPDATE_CHANNEL)
