d821c030b085e6bb092cf66569054dd2c6a0d884
desmond <desmond.li@voyageone.cn>
2016-08-11 19:15:23 +0800

    1.新增商品时，当自动设置产品图片images2的flg=1时，把feed.image同时设给images1,images2, 更新产品时不需要更新产品图images2

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsSetMainPropMongoService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsSetMainPropMongoService.java
@@ -1247,6 +1247,7 @@ public class CmsSetMainPropMongoService extends BaseTaskService {
 //            {
 //                if (newFlg) {
             List<Map<String, Object>> multiComplex = new LinkedList<>();
+            List<Map<String, Object>> multiComplex2 = new LinkedList<>();
 
             // jeff 2016/05 change start
            //  List<String> lstImageOrg = feed.getImage();
@@ -1265,18 +1266,24 @@ public class CmsSetMainPropMongoService extends BaseTaskService {
             if (lstImageOrg != null && lstImageOrg.size() > 0) {
                 for (String imgOrg : lstImageOrg) {
                     Map<String, Object> multiComplexChildren = new HashMap<>();
+                    Map<String, Object> multiComplexChildren2 = new HashMap<>();
                     // jeff 2016/04 change start
                     // multiComplexChildren.put("image1", imgOrg);
-                    multiComplexChildren.put("image1", doUpdateImage(feed.getChannelId(), feed.getCode(), imgOrg));
+                    String picName = doUpdateImage(feed.getChannelId(), feed.getCode(), imgOrg);
+                    multiComplexChildren.put("image1", picName);
+                    multiComplexChildren2.put("image2", picName);
                     // jeff 2016/04 add end
                     multiComplex.add(multiComplexChildren);
+                    multiComplex2.add(multiComplexChildren2);
                 }
             }
 
 //            productField.put("images1", multiComplex);
             productCommonField.put("images1", multiComplex);
             // 新增商品时，根据设置决定是否同时设置产品图images2,更新商品时不更新images2(老的数据里面本来就没有images2的时候更新)
-            if (newFlg || ListUtils.isNull(productCommonField.getImages2()) || "1".equals(feed.getIsFeedReImport())) {
+            if (newFlg
+                    || (ListUtils.isNull(productCommonField.getImages2()) || StringUtils.isEmpty(productCommonField.getImages2().get(0).getName()))
+                    || "1".equals(feed.getIsFeedReImport())) {
                 // 从cms_mt_channel_config表从取得新建product时是否自动设置产品图images2(1:自动设置  空，0:不设置)
                 String autoSetImages2Flg = "0";    // 0:不设置产品图images2
                 CmsChannelConfigBean productTypeChannelConfigBean = CmsChannelConfigs.getConfigBeanNoCode(this.channel.getOrder_channel_id(),
@@ -1287,7 +1294,7 @@ public class CmsSetMainPropMongoService extends BaseTaskService {
 
                 if ("1".equals(autoSetImages2Flg)) {
                     // 设置产品图images2
-                    productCommonField.put("images2", multiComplex);
+                    productCommonField.put("images2", multiComplex2);
                 }
             }
 
