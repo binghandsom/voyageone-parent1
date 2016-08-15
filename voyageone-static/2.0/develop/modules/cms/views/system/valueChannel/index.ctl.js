0367c0f9ee790a3d570f42b7824070f6282c8e8f
james <james>
2016-08-15 19:55:52 +0800

    添加加税号页面

---
+++ voyageone-static/2.0/develop/modules/cms/views/system/valueChannel/index.ctl.js
@@ -0,0 +1,26 @@
+/**
+ * Created by 123 on 2016/4/13.
+ */
+define([
+    'underscore',
+    'modules/cms/controller/popup.ctl'
+], function (_) {
+
+
+    function valueChannelController($scope, $valueChannelService,$translate, confirm,notify) {
+
+
+
+        $scope.hsCodeInfo="";
+        $scope.addHsCode = addHsCode;
+        function addHsCode(){
+            $valueChannelService.addHsCodes({"typeId":43,"hsCodes":$scope.hsCodeInfo}).then(function(){
+                notify.success($translate.instant('TXT_MSG_UPDATE_SUCCESS'));
+                $scope.hsCodeInfo = "";
+            })
+        }
+    }
+
+    valueChannelController.$inject = ['$scope', "$valueChannelService",  '$translate', 'confirm','notify'];
+    return valueChannelController;
+});
