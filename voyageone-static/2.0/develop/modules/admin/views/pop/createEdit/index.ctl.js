98898076a9eea661936ec4ff4a0471939983caf1
sofia <sofia.yu@voyageone.cn>
2016-08-16 13:07:27 +0800

    update the channelInfo

--- voyageone-static/2.0/develop/modules/admin/views/pop/createEdit/index.ctl.js
+++ voyageone-static/2.0/develop/modules/admin/views/pop/createEdit/index.ctl.js
@@ -6,33 +6,34 @@ define([
 ], function (admin) {
     admin.controller('CreateEditController', (function () {
         function CreateEditController(context, $uibModalInstance, AdminChannelService) {
-            this.sourceData = context ? context : {};
+            this.sourceData = context;
             this.$uibModalInstance = $uibModalInstance;
             this.AdminChannelService = AdminChannelService;
-            this.append = context == 'add' ? true : false;
+            this.append = context.type == 'add' ? true : false;
             this.popType = '修改';
+            this.configType = 'Channel';
         }
 
         CreateEditController.prototype = {
             init: function () {
                 var self = this;
-                if (self.sourceData == 'add') {
-                    self.popType = '添加';
-                    self.sourceData = {}
-                }
+                if (self.sourceData.type == 'add') self.popType = '添加';
             },
             cancel: function () {
                 this.$uibModalInstance.dismiss();
             },
             save: function () {
                 var self = this;
-                console.log(self.sourceData);
-                _.extend(self.sourceData, {'cartList': self.cartList});
+                _.extend(self.sourceData, {'configType': self.configType, 'channelId': self.sourceData.channelId});
                 if (self.append == true) {
-                    self.AdminChannelService.addConfig().then(function () {
+                    self.AdminChannelService.addConfig(self.sourceData).then(function (res) {
+                        if (res.data.success == false)self.confirm(res.data.message);
+                        self.$uibModalInstance.close('success');
                     })
-                }else{
-                    self.AdminChannelService.updateConfig().then(function () {
+                } else {
+                    self.AdminChannelService.updateConfig(self.sourceData).then(function (res) {
+                        if (res.data.success == false)self.confirm(res.data.message);
+                        self.$uibModalInstance.close('success');
                     })
                 }
             }
