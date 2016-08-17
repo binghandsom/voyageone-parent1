c2d9c0ce56d6b08692aa26ad4f2fd46519212d66
jiangjusheng <s@fed.com>
2016-08-17 14:20:42 +0800

    高级检索画面修改

--- voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/bean/cms/CmsBtDataAmount/EnumPlatformInfoSum.java
+++ voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/bean/cms/CmsBtDataAmount/EnumPlatformInfoSum.java
@@ -28,6 +28,7 @@ public enum EnumPlatformInfoSum implements IEnumDataAmountSum{
     }),//
     CMS_PLATFORM_PUBLISH_FAILD("CMS_PLATFORM_PUBLISH_FAILD", "{'platforms.P%s.pPublishError':{$nin:[null,'']}}", "/search/advanceSearch", "", "上新失败数"),
     CMS_PLATFORM_pStatus_pReallyStatus_notEqual("CMS_PLATFORM_pStatus_pReallyStatus_notEqual", "{platforms.P%s:{$exists:true},$where:'this.platforms.P%s.pStatus!=this.platforms.P%s.pReallyStatus'}", "/search/advanceSearch", "", "商品平台状态与实际相异数",(m)->{
+// TODO-- "{$or:[{'platforms.P#.pReallyStatus':'OnSale','platforms.P#.pStatus':{$ne:'OnSale'}},{'platforms.P#.pReallyStatus':'InStock','platforms.P#.pStatus':{$ne:'InStock'}}]}
         return String.format(m.getQueryStr(),m.getCartId(),m.getCartId(),m.getCartId());
     }),// priceSale
     CMS_PLATFORM_priceSale_Equal_minus1("CMS_PLATFORM_priceSale_Equal_minus1", "{platforms.P%s:{$exists:true},'platforms.P%s.skus.priceSale':-1}", "/search/advanceSearch", "", "中国最终售价价格为-1",(m)->{
