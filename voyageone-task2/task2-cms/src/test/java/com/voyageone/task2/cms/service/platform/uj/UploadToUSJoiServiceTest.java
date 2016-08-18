e752118e5c42d4bc2fbfefcad3a21fb7ba54a937
desmond <desmond.li@voyageone.cn>
2016-08-18 15:35:49 +0800

    修改测试代码

--- voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/platform/uj/UploadToUSJoiServiceTest.java
+++ voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/platform/uj/UploadToUSJoiServiceTest.java
@@ -1,6 +1,13 @@
 package com.voyageone.task2.cms.service.platform.uj;
 
 import com.voyageone.base.dao.mongodb.model.BaseMongoMap;
+import com.voyageone.common.Constants;
+import com.voyageone.common.configs.Enums.CacheKeyEnums;
+import com.voyageone.common.configs.TypeChannels;
+import com.voyageone.common.configs.beans.TypeChannelBean;
+import com.voyageone.common.redis.CacheHelper;
+import com.voyageone.common.util.ListUtils;
+import com.voyageone.common.util.StringUtils;
 import com.voyageone.service.bean.cms.product.ProductUpdateBean;
 import com.voyageone.service.dao.cms.mongo.CmsMtPlatformCategorySchemaDao;
 import com.voyageone.service.impl.cms.product.ProductService;
@@ -14,6 +21,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author james.li on 2016/4/7.
@@ -35,13 +45,69 @@ public class UploadToUSJoiServiceTest {
     @Test
     public void testUpload() throws Exception {
 
+// 清除缓存（这样在synship.com_mt_value_channel表中刚追加的brand，productType，sizeType等初始化mapping信息就能立刻取得了）
+        CacheHelper.delete(CacheKeyEnums.KeyEnum.ConfigData_TypeChannel.toString());
+
+        String usjoiChannelId = "929";
+
+        // --------------------------------------------------------------------------------------------
+        // 品牌mapping表
+        Map<String, String> mapBrandMapping = new HashMap<>();
+        // 产品分类mapping表
+        Map<String, String> mapProductTypeMapping = new HashMap<>();
+        // 适用人群mapping表
+        Map<String, String> mapSizeTypeMapping = new HashMap<>();
+
+        // 品牌mapping作成
+        List<TypeChannelBean> brandTypeChannelBeanList = TypeChannels.getTypeList(Constants.comMtTypeChannel.BRAND_41, usjoiChannelId);
+        if (ListUtils.notNull(brandTypeChannelBeanList)) {
+            for (TypeChannelBean typeChannelBean : brandTypeChannelBeanList) {
+                if (!StringUtils.isEmpty(typeChannelBean.getAdd_name1())
+                        && !StringUtils.isEmpty(typeChannelBean.getName())
+                        && Constants.LANGUAGE.EN.equals(typeChannelBean.getLang_id())
+                        ) {
+                    // 品牌mapping表中key,value都设为小写(feed进来的brand不区分大小写)
+                    mapBrandMapping.put(typeChannelBean.getAdd_name1().toLowerCase().trim(), typeChannelBean.getName().toLowerCase().trim());
+                }
+            }
+        }
+
+        // 产品分类mapping作成
+        List<TypeChannelBean> productTypeChannelBeanList = TypeChannels.getTypeList(Constants.comMtTypeChannel.PROUDCT_TYPE_57, usjoiChannelId);
+        if (ListUtils.notNull(productTypeChannelBeanList)) {
+            for (TypeChannelBean typeChannelBean : productTypeChannelBeanList) {
+                if (!StringUtils.isEmpty(typeChannelBean.getValue())
+                        && !StringUtils.isEmpty(typeChannelBean.getName())
+                        && Constants.LANGUAGE.EN.equals(typeChannelBean.getLang_id())
+                        ) {
+                    // 产品分类mapping表(value是key,name和add_name1是值)
+                    mapProductTypeMapping.put(typeChannelBean.getValue().trim(), typeChannelBean.getName().trim());
+                }
+            }
+        }
+
+        // 适用人群mapping作成
+        List<TypeChannelBean> sizeTypeChannelBeanList = TypeChannels.getTypeList(Constants.comMtTypeChannel.PROUDCT_TYPE_58, usjoiChannelId);
+        if (ListUtils.notNull(sizeTypeChannelBeanList)) {
+            for (TypeChannelBean typeChannelBean : sizeTypeChannelBeanList) {
+                if (!StringUtils.isEmpty(typeChannelBean.getValue())
+                        && !StringUtils.isEmpty(typeChannelBean.getName())
+                        && Constants.LANGUAGE.EN.equals(typeChannelBean.getLang_id())
+                        ) {
+                    // 适用人群mapping作成(value是key,name和add_name1是值)
+                    mapSizeTypeMapping.put(typeChannelBean.getValue().trim(), typeChannelBean.getName().trim());
+                }
+            }
+        }
+        // --------------------------------------------------------------------------------------------
+
         CmsBtSxWorkloadModel sxWorkLoadBean = new CmsBtSxWorkloadModel();
         sxWorkLoadBean.setChannelId("017");
-        sxWorkLoadBean.setGroupId(662789L);
+        sxWorkLoadBean.setGroupId(12922L);
         sxWorkLoadBean.setModifier("james");
-        sxWorkLoadBean.setCartId(929);
+        sxWorkLoadBean.setCartId(Integer.parseInt(usjoiChannelId)); // "929"
 
-        uploadToUSJoiService.upload(sxWorkLoadBean);
+        uploadToUSJoiService.upload(sxWorkLoadBean, mapBrandMapping, mapProductTypeMapping, mapSizeTypeMapping);
     }
 
     @Test
