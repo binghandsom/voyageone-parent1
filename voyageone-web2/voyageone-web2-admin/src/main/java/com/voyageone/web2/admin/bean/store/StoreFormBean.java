64798246baf8ef3f246a3175492372664ca1ae4e
wangtiedao <tiedao.wang@voyageone.cn>
2016-08-18 13:37:16 +0800

    update store

--- voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/bean/store/StoreFormBean.java
+++ voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/bean/store/StoreFormBean.java
@@ -36,7 +36,7 @@ public class StoreFormBean extends AdminFormBean {
 	
 	private String storeArea;
 	
-	private Integer areaId;
+	private Long areaId;
 	
 	private String storeComment;
 	
@@ -154,11 +154,11 @@ public class StoreFormBean extends AdminFormBean {
 		this.storeArea = storeArea;
 	}
 
-	public Integer getAreaId() {
+	public Long getAreaId() {
 		return areaId;
 	}
 
-	public void setAreaId(Integer areaId) {
+	public void setAreaId(Long areaId) {
 		this.areaId = areaId;
 	}
 
