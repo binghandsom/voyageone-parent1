0cbe7e8fc19d238d8df3a6ec5e83e38c7e6f0a4a
ethan.shi <ethan.shi@vovageone.cn>
2016-08-18 10:15:15 +0800

    Add Res Controller Service

--- voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/views/user/AdminResController.java
+++ voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/views/user/AdminResController.java
@@ -1,4 +1,6 @@
 package com.voyageone.web2.admin.views.user;
+
+import com.google.common.base.Preconditions;
 import com.voyageone.security.model.ComResourceModel;
 import com.voyageone.service.impl.com.user.AdminResService;
 import com.voyageone.web2.admin.AdminController;
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -38,7 +40,7 @@ public class AdminResController extends AdminController {
 
         String application = requestBean.getOrDefault("application", "").toString();
         Map<String, Object> response = new HashMap<>();
-        response.put("treeList",  adminResService.searchRes(application));
+        response.put("treeList", adminResService.searchRes(application));
 
         return success(response);
     }
@@ -54,10 +56,35 @@ public class AdminResController extends AdminController {
 
         Map<String, Object> response = new HashMap<>();
 
+        adminResService.addRes(model);
+
+        return success(response);
+    }
+
+    @RequestMapping(AdminUrlConstants.User.Res.UPDATE_RES)
+    public AjaxResponse updateRes(@RequestBody Map requestBean) throws Exception {
+
+        ComResourceModel model = new ComResourceModel();
+
+        BeanUtils.populate(model, requestBean);
+
+        model.setCreater(getUser().getUserName());
+
+        Map<String, Object> response = new HashMap<>();
+
+        adminResService.updateRes(model);
+
         return success(response);
     }
 
 
+    @RequestMapping(AdminUrlConstants.User.Res.DELETE_RES)
+    public AjaxResponse deleteUser(@RequestBody List<Integer> bean)  {
+
+        String username = getUser().getUserName();
+        adminResService.deleteUser(bean, username);
+        return success(true);
+    }
 
 
 }
