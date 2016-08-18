b534f5d4c066247b46d6e487af4128ace265ff43
Jonas Gao <gaopinsong@live.cn>
2016-08-18 14:23:21 +0800

    format(propertyMapping.ctl.js)

--- voyageone-static/2.0/develop/modules/cms/views/pop/platformMapping/propertyMapping.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/platformMapping/propertyMapping.ctl.js
@@ -1,11 +1,11 @@
 define([
     'cms',
     'modules/cms/enums/Carts'
-], function (cms,carts) {
+], function (cms, carts) {
     'use strict';
     return cms.controller('propertyMappingController', (function () {
 
-        function PropertyMappingController(context,$uibModalInstance,popups) {
+        function PropertyMappingController(context, $uibModalInstance, popups) {
             this.context = context;
             this.uibModalInstance = $uibModalInstance;
             this.popups = popups;
@@ -13,53 +13,53 @@ define([
         }
 
         PropertyMappingController.prototype = {
-          init:function(){
-              var self = this;
-              if(self.context.cartId)
-                self.context.cartName = carts.valueOf(+self.context.cartId).desc;
+            init: function () {
+                var self = this;
+                if (self.context.cartId)
+                    self.context.cartName = carts.valueOf(+self.context.cartId).desc;
 
-              self.valueArr = JSON.parse(self.context.value);
-          },
-          openPpPropertySetting:function(){
-              var self = this;
-              self.popups.openPropertySetting(self.context).then(function(context){
-                  self.valueArr.push(context);
-              });
-          },
-          order:function(arrow,index){
+                self.valueArr = JSON.parse(self.context.value);
+            },
+            openPpPropertySetting: function () {
+                var self = this;
+                self.popups.openPropertySetting(self.context).then(function (context) {
+                    self.valueArr.push(context);
+                });
+            },
+            order: function (arrow, index) {
 
                 var curr = this.valueArr[index];
-                var repIndex = arrow == "up" ? index - 1 : index+1;
+                var repIndex = arrow == "up" ? index - 1 : index + 1;
 
-                if(repIndex < 0 || repIndex > this.valueArr.length - 1)
+                if (repIndex < 0 || repIndex > this.valueArr.length - 1)
                     return;
 
-                var tmp = this.valueArr.splice(repIndex,1,curr);
-                this.valueArr.splice(index,1,tmp[0]);
-          },
-          update:function(item){
-              var self = this;
-              _.extend(item,{
-                  cartPath:self.context.cartPath,
-                  cartName:self.context.cartName,
-                  name:self.context.name
-              });
-              self.popups.openPropertySetting(item).then(function(context){
-                  item.type = context.type;
-                  item.value = context.value;
-              });
-          },
-          remove:function(index){
-              this.valueArr.splice(index, 1)
-          },
-          confirm:function(){
-              var valueList = _.map(this.valueArr,function(item){
-                  return {type:item.type,append:item.append,value:item.value};
-              });
+                var tmp = this.valueArr.splice(repIndex, 1, curr);
+                this.valueArr.splice(index, 1, tmp[0]);
+            },
+            update: function (item) {
+                var self = this;
+                _.extend(item, {
+                    cartPath: self.context.cartPath,
+                    cartName: self.context.cartName,
+                    name: self.context.name
+                });
+                self.popups.openPropertySetting(item).then(function (context) {
+                    item.type = context.type;
+                    item.value = context.value;
+                });
+            },
+            remove: function (index) {
+                this.valueArr.splice(index, 1)
+            },
+            confirm: function () {
+                var valueList = _.map(this.valueArr, function (item) {
+                    return {type: item.type, append: item.append, value: item.value};
+                });
 
-              this.context.value = JSON.stringify(valueList);
-              this.uibModalInstance.close();
-          }
+                this.context.value = JSON.stringify(valueList);
+                this.uibModalInstance.close();
+            }
         };
 
         return PropertyMappingController;
