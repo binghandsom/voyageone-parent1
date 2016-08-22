bf4e5e08ea8ec650910783538016adeb806092d4
Jonas Gao <gaopinsong@live.cn>
2016-08-22 15:19:48 +0800

    format(propertySetting.controller)

--- voyageone-static/2.0/develop/modules/cms/views/pop/platformMapping/propertySetting.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/platformMapping/propertySetting.ctl.js
@@ -4,7 +4,7 @@ define([
     'use strict';
     return cms.controller('propertySettingController', (function () {
 
-        function PropertySettingController(context,$uibModalInstance,platformMappingService,$q) {
+        function PropertySettingController(context, $uibModalInstance, platformMappingService, $q) {
             this.context = context;
             this.uibModalInstance = $uibModalInstance;
             this.platformMappingService = platformMappingService;
@@ -12,51 +12,51 @@ define([
         }
 
         PropertySettingController.prototype = {
-            init:function(){
+            init: function () {
                 var self = this,
                     platformMappingService = self.platformMappingService;
 
                 self.cat = 'MASTER';
 
-                self.$q.all(platformMappingService.getCommonSchema().then(function(res){
-                    self.mastOpts = res.data;
-                    if (self.optionSource === "mastOpts")
-                        self.options = self.mastOpts;
-                }),
-                platformMappingService.getFeedCustomProps().then(function(res){
-                    self.feedOpts = res.data;
-                    if (self.optionSource === "feedOpts")
-                        self.options = self.feedOpts;
-                })
-                ).then(function(){
+                self.$q.all(platformMappingService.getCommonSchema().then(function (res) {
+                        self.mastOpts = res.data;
+                        if (self.optionSource === "mastOpts")
+                            self.options = self.mastOpts;
+                    }),
+                    platformMappingService.getFeedCustomProps().then(function (res) {
+                        self.feedOpts = res.data;
+                        if (self.optionSource === "feedOpts")
+                            self.options = self.feedOpts;
+                    })
+                ).then(function () {
                     //判断是否是编辑入口进来
-                    if(self.context.type)
+                    if (self.context.type)
                         self.cat = self.context.type;
-                        self.value = self.context.value;
+                    self.value = self.context.value;
                     self.changeCat();
                 });
 
             },
-            changeCat:function(){
+            changeCat: function () {
                 var self = this,
                     optionSource;
 
-                switch (self.cat){
+                switch (self.cat) {
                     case "MASTER":
-                            self.isFixValue = false;
-                            self.optionSource = "mastOpts";
-                            break;
+                        self.isFixValue = false;
+                        self.optionSource = "mastOpts";
+                        break;
                     case "FEED_ORG":
 
 
                     case "FEED_CN":
-                            self.isFixValue = false;
-                            self.optionSource = "feedOpts";
-                            break;
+                        self.isFixValue = false;
+                        self.optionSource = "feedOpts";
+                        break;
                     case "FIXED":
-                            self.optionSource = null;
-                            self.isFixValue = true;
-                            break
+                        self.optionSource = null;
+                        self.isFixValue = true;
+                        break
                 }
 
                 if (!self.optionSource)
@@ -69,13 +69,13 @@ define([
 
                 self.options = optionSource;
             },
-            save:function(){
+            save: function () {
                 var self = this;
                 var newValue = {
-                    $id:Math.random().toString().substr(2,6),
-                    type:self.cat,
-                    append:"",
-                    value:self.value
+                    $id: Math.random().toString().substr(2, 6),
+                    type: self.cat,
+                    append: "",
+                    value: self.value
                 };
 
                 self.uibModalInstance.close(newValue);
