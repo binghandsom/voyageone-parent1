88e53fbf0a4316f66050cd0cbcdb1116f11b3761
edward.lin <edward.lin@voyageone.cn>
2016-08-24 16:34:47 +0800

    ajax.service.js中追加 httpProvider

--- voyageone-static/2.0/develop/components/angular/services/ajax.service.js
+++ voyageone-static/2.0/develop/components/angular/services/ajax.service.js
@@ -3,7 +3,11 @@
  * @User:    Jonas
  * @Version: 0.2.1
  */
-angular.module("voyageone.angular.services").service("$ajax", $Ajax).service("ajaxService", AjaxService);
+angular.module("voyageone.angular.services").service("$ajax", $Ajax).service("ajaxService", AjaxService).config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.headers.common = {
+        'x_requested_with': 'xmlhttprequest'
+    };
+}]);
 
 function $Ajax($http, blockUI, $q) {
     this.$http = $http;
