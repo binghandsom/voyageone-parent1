1ecf74c104745000af8529e18dbd1a3a7585e45c
desmond <desmond.li@voyageone.cn>
2016-08-26 18:54:42 +0800

    天猫同购对应

---
+++ voyageone-components/voyageone-components-tmall/src/main/java/com/voyageone/components/tmall/service/TbSimpleItemService.java
@@ -0,0 +1,75 @@
+package com.voyageone.components.tmall.service;
+
+import com.taobao.api.ApiException;
+import com.taobao.api.request.TmallItemSimpleschemaAddRequest;
+import com.taobao.api.request.TmallItemSimpleschemaUpdateRequest;
+import com.taobao.api.response.TmallItemSimpleschemaAddResponse;
+import com.taobao.api.response.TmallItemSimpleschemaUpdateResponse;
+import com.voyageone.common.configs.beans.ShopBean;
+import com.voyageone.components.tmall.TbBase;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * 天猫官网同购上新接口调用（简化方式发布商品）
+ * Created by desmond on 8/25/2016.
+ */
+@Component
+public class TbSimpleItemService extends TbBase {
+
+    /**
+     * 天猫官网同购新增Item
+     * 淘宝接口名：tmall.item.simpleschema.add
+     * 文档地址：http://open.taobao.com/docs/api.htm?apiId=25455
+     *
+     * @param shopBean 店铺
+     * @param schemaXml xml
+     * @return numIId
+     * @throws ApiException
+     */
+    public String addSimpleItem(ShopBean shopBean, String schemaXml) throws ApiException {
+
+        TmallItemSimpleschemaAddRequest request = new TmallItemSimpleschemaAddRequest();
+        request.setSchemaXmlFields(schemaXml);
+
+        TmallItemSimpleschemaAddResponse response = reqTaobaoApi(shopBean, request);
+
+        // 调用淘宝API未成功或者errCode不为空的时候
+        if (!response.isSuccess() || !StringUtils.isEmpty(response.getErrorCode())) {
+            logger.error("addSimpleItem [Msg = " + response.getMsg() + "] [SubMsg = " + response.getSubMsg() + "]");
+            return "ERROR:"+ response.getErrorCode() + ":" + response.getMsg();
+        }
+
+        return response.getResult();
+    }
+
+    /**
+     * 天猫官网同购更新Item
+     * 淘宝接口名：tmall.item.simpleschema.update
+     * 文档地址：http://open.taobao.com/docs/api.htm?apiId=26072
+     *
+     * @param shopBean 店铺
+     * @param numIId numIId
+     * @param schemaXml xml
+     * @return numIId
+     * @throws ApiException
+     */
+    public String updateSimpleItem(ShopBean shopBean, Long numIId, String schemaXml) throws ApiException {
+
+        TmallItemSimpleschemaUpdateRequest request = new TmallItemSimpleschemaUpdateRequest();
+
+        request.setItemId(numIId);
+        request.setSchemaXmlFields(schemaXml);
+
+        TmallItemSimpleschemaUpdateResponse response = reqTaobaoApi(shopBean, request);
+
+        // 调用淘宝API未成功或者errCode不为空的时候
+        if (!response.isSuccess() || !StringUtils.isEmpty(response.getErrorCode())) {
+            logger.error("updateSimpleItem [Msg = " + response.getMsg() + "] [SubMsg = " + response.getSubMsg() + "]");
+            return "ERROR:"+ response.getErrorCode() + ":" + response.getMsg();
+        }
+
+        return response.getUpdateItemResult();
+    }
+
+}
