ff45e0507ac7381dc681db175a896415680c01bd
wenjie.piao@voyageone.cn <wj7405049>
2016-08-26 13:34:00 +0800

    新建尺码图和尺码表时的优化操作

--- voyageone-static/2.0/develop/modules/cms/views/pop/store/listing/sizechart.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/store/listing/sizechart.ctl.js
@@ -6,9 +6,9 @@ define([
     'modules/cms/controller/popup.ctl'
 ], function (angularAMD) {
 
-    angularAMD.controller('popSizeChartCtl', function ($scope, context,sizeChartService,$translate) {
+    angularAMD.controller('popSizeChartCtl', function ($scope, context, sizeChartService, imageGroupService) {
         $scope.vm = {
-            saveInfo:{sizeChartName: "",brandNameList:[],productTypeList:[],sizeTypeList:[]}
+            saveInfo: {sizeChartName: "", brandNameList: [], productTypeList: [], sizeTypeList: []}
         };
 
         $scope.dropdown = context;
@@ -17,10 +17,23 @@ define([
             if ($scope.vm == undefined) {
                 $scope.vm = {};
             }
+
+            /**获取未匹配的尺码图组*/
+            imageGroupService.getNoMatchSizeImageGroupList().then(function (res) {
+                $scope.noMathOpt = res.data;
+            });
         };
 
-        $scope.save = function(){
-           sizeChartService.editSave($scope.vm.saveInfo).then(function(){
+        $scope.save = function () {
+            var _sizeChart = $scope.vm.sizeChart,
+                upEntity;
+
+            if (_.isObject(_sizeChart))
+                upEntity = _.extend($scope.vm.saveInfo, _sizeChart);
+            else
+                upEntity = _.extend($scope.vm.saveInfo, {imageGroupName: _sizeChart});
+
+            sizeChartService.editSave(upEntity).then(function () {
                 $scope.$close();
                 context.search();
             });
