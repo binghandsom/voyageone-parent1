5a1202cc12d74732cba8c6d2b6f49d1b1c7fda35
james <james>
2016-08-29 17:32:11 +0800

    DOC-92-1:导入时，如果该主商品已经存在并且相关信息已经匹配，则copy主商品的信息设置

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsSetMainPropMongoService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsSetMainPropMongoService.java
@@ -3687,12 +3687,15 @@ public class CmsSetMainPropMongoService extends BaseTaskService {
             common.getFields().setHsCodeStatus(mainProduct.getCommon().getFields().getHsCodeStatus());
             common.getFields().setHsCodeSetTime(mainProduct.getCommon().getFields().getHsCodeSetTime());
 
+            common.getFields().setCategorySetTime(mainProduct.getCommon().getFields().getCategorySetTime());
+            common.getFields().setCategoryStatus(mainProduct.getCommon().getFields().getCategoryStatus());
+            common.getFields().setCategorySetter(mainProduct.getCommon().getFields().getCategorySetter());
+
             common.getFields().setShortDesCn(mainProduct.getCommon().getFields().getShortDesCn());
             common.getFields().setLongDesCn(mainProduct.getCommon().getFields().getLongDesCn());
             common.getFields().setOriginalTitleCn(mainProduct.getCommon().getFields().getOriginalTitleCn());
             common.getFields().setMaterialCn(mainProduct.getCommon().getFields().getMaterialCn());
             common.getFields().setUsageCn(mainProduct.getCommon().getFields().getUsageCn());
-            common.getFields().setOrigin(mainProduct.getCommon().getFields().getOrigin());
         }
     }
 
