ff2373161db0734c4b881d3be9ac2c6432ca1b21
sofia <sofia.yu@voyageone.cn>
2016-08-31 17:28:40 +0800

    add resource pages

--- voyageone-static/2.0/develop/adminResetPass.js
+++ voyageone-static/2.0/develop/adminResetPass.js
@@ -1,3 +1,16 @@
 /**
  * Created by sofia on 2016/8/30.
  */
+define(['components/dist/voyageone.angular.com'], function () {
+    angular.module('voyageone.admin.adminResetPass', [
+        'blockUI',
+        'voyageone.angular'
+    ]).controller('resetPassController', function ($scope, adminUserService) {
+        $scope.submit = function () {
+            adminUserService.editPass().then(function (res) {
+                console.log(res);
+            })
+        }
+    });
+    return angular.bootstrap(document, ['voyageone.admin.adminResetPass']);
+});
