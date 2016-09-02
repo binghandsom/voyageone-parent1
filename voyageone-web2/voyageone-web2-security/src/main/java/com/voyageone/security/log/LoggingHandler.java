ef07c505536ac323937357f91d08b0fb5dd6e7c8
ethan.shi <ethan.shi@vovageone.cn>
2016-09-02 16:42:05 +0800

    AdminUserService

--- voyageone-web2/voyageone-web2-security/src/main/java/com/voyageone/security/log/LoggingHandler.java
+++ voyageone-web2/voyageone-web2-security/src/main/java/com/voyageone/security/log/LoggingHandler.java
@@ -1,7 +1,9 @@
 package com.voyageone.security.log;
 
 import com.voyageone.common.util.JacksonUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.MDC;
+import org.apache.shiro.SecurityUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,8 +15,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Aspect
 @Component
@@ -37,6 +38,22 @@ public class LoggingHandler {
     protected void initXXX() {
     }
 
+    private String getApp(String url)
+    {
+        if(StringUtils.isEmpty(url))
+            return "";
+
+        String [] a = url.split("/+");
+
+        for (String str : a) {
+            if(!StringUtils.isEmpty(url))
+            {
+                return str;
+            }
+        }
+        return "";
+    }
+
     @Around("controller() && !getXXX() && !searchXXX() && !initXXX()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
 
@@ -44,8 +61,8 @@ public class LoggingHandler {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         String ip = request.getRemoteAddr();
         String url = request.getRequestURI();
-        String application = "admin";
-        String user = "admin";
+        String application = getApp(url);
+        String user = SecurityUtils.getSubject().getPrincipal().toString();
         Object[] arguments = joinPoint.getArgs();
         String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
         String methodName = joinPoint.getSignature().getName();
