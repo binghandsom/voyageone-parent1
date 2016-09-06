2ee88f3b73f0a5f58b5b0d84a8e743e84641ef15
sofia <sofia.yu@voyageone.cn>
2016-09-06 10:07:53 +0800

    add new page [ newShop ]

--- voyageone-static/2.0/develop/modules/admin/views/newShopManagement/history/index.ctl.js
+++ voyageone-static/2.0/develop/modules/admin/views/newShopManagement/history/index.ctl.js
@@ -6,78 +6,70 @@ define([
     'modules/admin/controller/popup.ctl'
 ], function (admin) {
     admin.controller('HistoryConfigController', (function () {
-        function HistoryConfigController(popups, alert, confirm, channelService, carrierConfigService, selectRowsFactory) {
+        function HistoryConfigController(popups, alert, confirm, newShopService, selectRowsFactory) {
             this.popups = popups;
             this.alert = alert;
             this.confirm = confirm;
             this.selectRowsFactory = selectRowsFactory;
-            this.channelService = channelService;
-            this.carrierConfigService = carrierConfigService;
-            this.channelPageOption = {curr: 1, size: 10, total: 0, fetch: this.search.bind(this)};
-            this.carrierList = [];
-            this.carrierSelList = {selList: []};
-            this.tempChannelSelect = null;
+            this.newShopService = newShopService;
+            this.pageOption = {curr: 1, size: 10, total: 0, fetch: this.search.bind(this)};
+            this.historyList = [];
+            this.historySelList = {selList: []};
+            this.tempSelect = null;
             this.searchInfo = {
-                orderChannelId: '',
-                carrier: '',
-                usekd100Flg: '',
-                active: '',
-                pageInfo: this.channelPageOption
+                channelId: '',
+                channelName: '',
+                modifiedFrom: '',
+                modifiedTo: '',
+                pageInfo: this.pageOption
             }
         }
 
         HistoryConfigController.prototype = {
             init: function () {
                 var self = this;
-                self.activeList = [{active: true, value: '启用'}, {active: false, value: '禁用'}];
-                self.channelService.getAllChannel().then(function (res) {
-                    self.channelAllList = res.data;
-                });
                 self.search();
             },
             search: function (page) {
                 var self = this;
                 page == 1 ? self.searchInfo.pageInfo.curr = 1 : page;
-                self.carrierConfigService.searchCarrierConfigByPage({
+                self.newShopService.searchNewShopByPage({
                         'pageNum': self.searchInfo.pageInfo.curr,
                         'pageSize': self.searchInfo.pageInfo.size,
-                        'orderChannelId': self.searchInfo.orderChannelId,
-                        'carrier': self.searchInfo.carrier,
-                        'active': self.searchInfo.active,
-                        'usekd100Flg': self.searchInfo.usekd100Flg
+                        'channelId': self.searchInfo.channelId,
+                        'channelName': self.searchInfo.channelName,
+                        'modifiedFrom': self.searchInfo.modifiedFrom,
+                        'modifiedTo': self.searchInfo.modifiedTo
                     })
                     .then(function (res) {
-                        self.carrierList = res.data.result;
-                        self.channelPageOption.total = res.data.count;
+                        self.historyList = res.data.result;
+                        self.pageOption.total = res.data.count;
 
                         // 设置勾选框
-                        if (self.tempChannelSelect == null) {
-                            self.tempChannelSelect = new self.selectRowsFactory();
+                        if (self.tempSelect == null) {
+                            self.tempSelect = new self.selectRowsFactory();
                         } else {
-                            self.tempChannelSelect.clearCurrPageRows();
-                            self.tempChannelSelect.clearSelectedList();
+                            self.tempSelect.clearCurrPageRows();
+                            self.tempSelect.clearSelectedList();
                         }
-                        _.forEach(self.carrierList, function (channelInfo, index) {
-                            if (channelInfo.updFlg != 8) {
-                                _.extend(channelInfo, {"mainKey": index});
-                                self.tempChannelSelect.currPageRows({
-                                    "id": channelInfo.mainKey,
-                                    "code": channelInfo.carrier,
-                                    "orderChannelId": channelInfo.orderChannelId
+                        _.forEach(self.historyList, function (Info) {
+                            if (Info.updFlg != 8) {
+                                self.tempSelect.currPageRows({
+                                    "id": Info.id
                                 });
                             }
                         });
-                        self.carrierSelList = self.tempChannelSelect.selectRowsInfo;
+                        self.historySelList = self.tempSelect.selectRowsInfo;
                     })
             },
             clear: function () {
                 var self = this;
                 self.searchInfo = {
-                    pageInfo: this.channelPageOption,
-                    orderChannelId: '',
-                    carrier: '',
-                    active: '',
-                    usekd100Flg: ''
+                    pageInfo: this.pageOption,
+                    channelId: '',
+                    channelName: '',
+                    modifiedFrom: '',
+                    modifiedTo: ''
                 }
             },
             edit: function (type) {
@@ -87,12 +79,12 @@ define([
                         self.search(1);
                     });
                 } else {
-                    if (self.carrierSelList.selList.length <= 0) {
+                    if (self.historySelList.selList.length <= 0) {
                         self.alert('TXT_MSG_NO_ROWS_SELECT');
                         return;
                     } else {
-                        _.forEach(self.carrierList, function (Info) {
-                            if (Info.mainKey == self.carrierSelList.selList[0].id) {
+                        _.forEach(self.historyList, function (Info) {
+                            if (Info.mainKey == self.historySelList.selList[0].id) {
                                 self.popups.openChannelCarrier(Info).then(function () {
                                     self.search(1);
                                 });
@@ -102,14 +94,10 @@ define([
                 }
 
             },
-            delete: function () {
+            delete: function (list) {
                 var self = this;
                 self.confirm('TXT_CONFIRM_DELETE_MSG').then(function () {
-                        var delList = [];
-                        _.forEach(self.carrierSelList.selList, function (delInfo) {
-                            delList.push({'orderChannelId': delInfo.orderChannelId, 'carrier': delInfo.code});
-                        });
-                        self.carrierConfigService.deleteCarrierConfig(delList).then(function (res) {
+                        self.newShopService.deleteNewShop(list.id).then(function (res) {
                             if (res.data.success == false)self.confirm(res.data.message);
                             self.search(1);
                         })
