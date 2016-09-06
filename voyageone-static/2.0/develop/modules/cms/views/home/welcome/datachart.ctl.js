47f99ad99bb0cd10549a1a5753d134abd98162d4
wenjie.piao@voyageone.cn <wj7405049>
2016-09-06 14:15:34 +0800

    首页提示信息

--- voyageone-static/2.0/develop/modules/cms/views/home/welcome/datachart.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/home/welcome/datachart.ctl.js
@@ -1,18 +1,18 @@
-
 define([
-    'angularAMD',
     'underscore'
-], function (angularAMD) {
-    function dController($scope, $menuService, $location, cRoutes, $sessionStorage) {
-        $scope.vm={sumData:{}};
+], function () {
+    function dController($scope, $menuService, $location, cRoutes, $sessionStorage , alert) {
+        $scope.vm = {sumData: {}};
         $scope.init = function () {
-            $menuService.getHomeSumData().then(function (res) {
 
+            alert("首页<span class='text-u-red'>【每小时刷新一次】</span>，如与其它反馈有些许差异，请稍后重新确认。");
+
+            $menuService.getHomeSumData().then(function (res) {
                 $scope.vm.sumData = res.data;
             });
         };
-        $scope.jump=jump;
-        function jump(url,param){
+        $scope.jump = jump;
+        function jump(url, param) {
             $sessionStorage.feedSearch = param;
             $location.path(url);
         }
@@ -20,12 +20,14 @@ define([
         /**
          * search by input value.
          */
-        $scope.goSearchPage = function(bizType, value) {
+        $scope.goSearchPage = function (bizType, value) {
             $sessionStorage.feedSearch = value;
             $location.path(cRoutes.search_advance_param.url + "4/" + bizType + '/0/0');
         }
 
     }
-    dController.$inject = ['$scope', '$menuService', '$location', 'cRoutes', '$sessionStorage'];
+
+    dController.$inject = ['$scope', '$menuService', '$location', 'cRoutes', '$sessionStorage','alert'];
+
     return dController;
 });
