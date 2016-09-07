2997e92f0ac5a732338ea945380e3c28a4ec0f49
Jonas Gao <gaopinsong@live.cn>
2016-09-07 15:13:06 +0800

    update(CmsBtBrandBlockService): update isBlocked

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/CmsBtBrandBlockService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/CmsBtBrandBlockService.java
@@ -1,5 +1,6 @@
 package com.voyageone.service.impl.cms;
 
+import com.voyageone.common.util.MapUtil;
 import com.voyageone.service.dao.cms.CmsBtBrandBlockDao;
 import com.voyageone.service.impl.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +17,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class CmsBtBrandBlockService extends BaseService {
 
-    private final CmsBtBrandBlockDao btBrandBlockDao;
+    private final static int BRAND_TYPE_FEED = 0;
+    private final static int BRAND_TYPE_MASTER = 1;
+    private final static int BRAND_TYPE_PLATFORM = 2;
+
+    private final CmsBtBrandBlockDao brandBlockDao;
 
     @Autowired
-    public CmsBtBrandBlockService(CmsBtBrandBlockDao btBrandBlockDao) {
-        this.btBrandBlockDao = btBrandBlockDao;
+    public CmsBtBrandBlockService(CmsBtBrandBlockDao brandBlockDao) {
+        this.brandBlockDao = brandBlockDao;
     }
 
-    public boolean isBlocked(String channelId, int cartIt, String feedBrand, int masterBrandId, String platformBrand) {
-        // TODO 后续实现
-        return false;
+    public boolean isBlocked(String channelId, int cartId, String feedBrand, String masterBrand, int platformBrandId) {
+
+        int count = brandBlockDao.selectCount(MapUtil.toMap("channelId", channelId,
+                "cartId", cartId,
+                "type", BRAND_TYPE_FEED,
+                "brand", feedBrand));
+
+        if (count > 0)
+            return true;
+
+        count = brandBlockDao.selectCount(MapUtil.toMap("channelId", channelId,
+                "cartId", cartId,
+                "type", BRAND_TYPE_MASTER,
+                "brand", masterBrand));
+
+        if (count > 0)
+            return true;
+
+        count = brandBlockDao.selectCount(MapUtil.toMap("channelId", channelId,
+                "cartId", cartId,
+                "type", BRAND_TYPE_PLATFORM,
+                "brand", platformBrandId));
+
+        return count > 0;
     }
 }
