39e3a97cb4b11d61b5ab3e60860fdb05e39b0c32
sofia <sofia.yu@voyageone.cn>
2016-09-08 09:40:10 +0800

    update validation

--- voyageone-static/2.0/develop/components/dist/voyageone.angular.com.js
+++ voyageone-static/2.0/develop/components/dist/voyageone.angular.com.js
@@ -2615,6 +2615,11 @@ angular.module("voyageone.angular.factories").factory("interceptorFactory", func
     var CODE_SYS_REDIRECT = "SYS_REDIRECT";
     // 和 JAVA 同步,回话过期的信息
     var MSG_TIMEOUT = "300001";
+    var MSG_LOCKED = "A003";
+    var MSG_MANYFAILS = "A004";
+    var MSG_MISSAUTHENTICATION = "A005";
+    var MSG_CHANGEPASS = "A006";
+    var MSG_LOGINAGAIN = "A001";
 
     /**
      * 对系统自动跳转的响应,执行跳转
@@ -2645,6 +2650,35 @@ angular.module("voyageone.angular.factories").factory("interceptorFactory", func
     }
 
     /**
+     * 对会话超时和未登录进行特殊处理(Admin)
+     */
+    function adminSessionTimeout(res) {
+        if (res.code != MSG_MISSAUTHENTICATION) {
+            return false;
+        }
+        // 会话超时,默认跳转到登陆页
+        location.href = "/adminLogin.html";
+        return true;
+    }
+
+    function adminResetPassword(res) {
+        if (res.code != MSG_CHANGEPASS) {
+            return false;
+        }
+        // 密码输入错误,默认跳转到重置密码界面
+        location.href = "/adminResetPass.html";
+        return true;
+    }
+    function adminReLogin(res) {
+        if (res.code != MSG_LOGINAGAIN) {
+            return false;
+        }
+        // 密码输入错误,默认跳转到重置密码界面
+        location.href = "/adminLogin.html";
+        return true;
+    }
+
+    /**
      * 处理位置的异常
      * @param response
      */
@@ -2668,7 +2702,7 @@ angular.module("voyageone.angular.factories").factory("interceptorFactory", func
         response: function (res) {
             var result = res.data;
             // 特殊处理部分内容
-            if (autoRedirect(result) || sessionTimeout(result)) {
+            if (autoRedirect(result) || sessionTimeout(result) || adminSessionTimeout(result) || adminResetPassword(result)||adminReLogin(result)) {
                 return res;
             }
             unknownException(res);
@@ -2683,6 +2717,7 @@ angular.module("voyageone.angular.factories").factory("interceptorFactory", func
 }).config(function ($httpProvider) {
     $httpProvider.interceptors.push("interceptorFactory");
 });
+
 
 
 /*****************************/
