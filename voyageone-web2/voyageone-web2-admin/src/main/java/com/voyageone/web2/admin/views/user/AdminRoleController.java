f84dcaeb370558759b4e5280cf1f04c165571043
ethan.shi <ethan.shi@vovageone.cn>
2016-09-08 15:57:25 +0800

    AdminUserService

--- voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/views/user/AdminRoleController.java
+++ voyageone-web2/voyageone-web2-admin/src/main/java/com/voyageone/web2/admin/views/user/AdminRoleController.java
@@ -1,8 +1,5 @@
 package com.voyageone.web2.admin.views.user;
 
-import com.google.common.base.Preconditions;
-import com.voyageone.base.dao.mysql.paginator.MySqlPageHelper;
-import com.voyageone.security.model.ComResourceModel;
 import com.voyageone.security.model.ComRoleModel;
 import com.voyageone.service.bean.com.AdminResourceBean;
 import com.voyageone.service.bean.com.AdminRoleBean;
@@ -19,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -36,23 +32,31 @@ public class AdminRoleController extends AdminController {
     @Autowired
     AdminRoleService adminRoleService;
 
+    /**
+     * 初始化页面
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.INIT)
     public AjaxResponse searchUser() {
-        PageModel<AdminRoleBean> result  = adminRoleService.searchRole(1, DEFAULT_PAGE_SIZE );
+        PageModel<AdminRoleBean> result = adminRoleService.searchRole(1, DEFAULT_PAGE_SIZE);
         return success(result);
     }
 
+    /**
+     * 检索角色
+     * @param form
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.SEARCH_ROLE)
     public AjaxResponse searchUser(@RequestBody UserFormBean form) {
         int pageNum = 1;
         int pageSize = DEFAULT_PAGE_SIZE;
 
-        if(form.getPageNum() != null  && form.getPageNum() > 1)
-        {
+        if (form.getPageNum() != null && form.getPageNum() > 1) {
             pageNum = form.getPageNum();
         }
 
-        if(form.getPageSize() != null  && form.getPageNum() > 0) {
+        if (form.getPageSize() != null && form.getPageNum() > 0) {
             pageSize = form.getPageSize();
         }
 
@@ -63,11 +67,19 @@ public class AdminRoleController extends AdminController {
         Integer storeId = form.getStoreId();
         String application = form.getApplication();
 
-        PageModel<AdminRoleBean> result  = adminRoleService.searchRole(roleName, roleType, channelId,  active, storeId, application , pageNum, pageSize );
+        PageModel<AdminRoleBean> result = adminRoleService.searchRole(roleName, roleType, channelId, active, storeId, application, pageNum, pageSize);
 
         return success(result);
     }
 
+
+    /**
+     * 添加角色
+     *
+     * @param requestMap
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(AdminUrlConstants.User.Role.ADD_ROLE)
     public AjaxResponse addRole(@RequestBody Map requestMap) throws Exception {
 
@@ -77,19 +89,26 @@ public class AdminRoleController extends AdminController {
         BeanUtils.populate(model, requestMap);
         model.setCreater(getUser().getUserName());
 
-        List<String>  applications = (List<String>) requestMap.getOrDefault("applications", "");
+        List<String> applications = (List<String>) requestMap.getOrDefault("applications",new ArrayList<>());
         List<String> channelIds = (List<String>) requestMap.getOrDefault("channelIds", new ArrayList<>());
         List<String> storeIds = (List<String>) requestMap.getOrDefault("storeIds", new ArrayList<>());
         String allChannel = requestMap.getOrDefault("allChannel", "0").toString();
         String allStore = requestMap.getOrDefault("allStore", "0").toString();
 
-        adminRoleService.addRole(model ,applications, channelIds,  storeIds , allChannel ,allStore );
+        adminRoleService.addRole(model, applications, channelIds, storeIds, allChannel, allStore);
 
 
         return success(true);
     }
 
 
+    /**
+     * 更新角色
+     *
+     * @param requestMap
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(AdminUrlConstants.User.Role.UPDATE_ROLE)
     public AjaxResponse updateRole(@RequestBody Map requestMap) throws Exception {
 
@@ -99,77 +118,135 @@ public class AdminRoleController extends AdminController {
         BeanUtils.populate(model, requestMap);
         model.setCreater(getUser().getUserName());
 
-        List<String>  applications = (List<String>) requestMap.getOrDefault("applications", "");
+        List<String> applications = (List<String>) requestMap.getOrDefault("applications", "");
         List<String> channelIds = (List<String>) requestMap.getOrDefault("channelIds", new ArrayList<>());
         List<String> storeIds = (List<String>) requestMap.getOrDefault("storeIds", new ArrayList<>());
         String allChannel = requestMap.getOrDefault("allChannel", "0").toString();
         String allStore = requestMap.getOrDefault("allStore", "0").toString();
-        adminRoleService.updateRole(model ,applications, channelIds,  storeIds , allChannel ,allStore );
+        adminRoleService.updateRole(model, applications, channelIds, storeIds, allChannel, allStore);
         return success(true);
     }
 
 
+    /**
+     * 软删除角色
+     *
+     * @param bean
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.DELETE_ROLE)
-    public AjaxResponse deleteUser(@RequestBody List<Integer> bean)  {
+    public AjaxResponse deleteUser(@RequestBody List<Integer> bean) {
         String username = getUser().getUserName();
         adminRoleService.deleteRole(bean, username);
         return success(true);
     }
 
+
+    /**
+     * 取所有角色
+     *
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.GET_ALL_ROLE)
-    public AjaxResponse getAllRole()  {
+    public AjaxResponse getAllRole() {
         return success(adminRoleService.getAllRole());
     }
 
+
+    /**
+     * 取所有角色类型
+     *
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.GET_ALL_ROLE_TYPE)
-    public AjaxResponse getAllRoleType()  {
+    public AjaxResponse getAllRoleType() {
         List<Map<String, Object>> list = new ArrayList<>();
-        list.add(new HashMap(){{put("id", 1); put("name", "IT管理员"); } });
-        list.add(new HashMap(){{put("id", 2);put("name", "运营人员"); } });
-        list.add(new HashMap(){{put("id", 3);put("name", "客服人员"); } });
-        list.add(new HashMap(){{put("id", 4); put("name", "财务人员");  } });
+        list.add(new HashMap() {
+            {
+                put("id", 1);
+                put("name", "IT管理员");
+            } });
+        list.add(new HashMap() {
+            {
+                put("id", 2);
+                put("name", "运营人员");
+            } });
+        list.add(new HashMap() {
+            {
+                put("id", 3);
+                put("name", "客服人员");
+            } });
+        list.add(new HashMap() {
+            {
+                put("id", 4);
+                put("name", "财务人员");
+            } });
         return success(list);
     }
 
 
+    /**
+     * 授权
+     *
+     * @param requestMap
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.SET_AUTH)
-    public AjaxResponse setAuth(@RequestBody Map requestMap)  {
+    public AjaxResponse setAuth(@RequestBody Map requestMap) {
 
-        List<Integer>  roleIds = (List<Integer>) requestMap.get("roleIds");
-        List<Integer>  resIds = (List<Integer>) requestMap.get("resIds");
-        List<String>  apps = (List<String>) requestMap.get("applications");
+        List<Integer> roleIds = (List<Integer>) requestMap.get("roleIds");
+        List<Integer> resIds = (List<Integer>) requestMap.get("resIds");
+        List<String> apps = (List<String>) requestMap.get("applications");
         String username = getUser().getUserName();
         adminRoleService.setAuth(apps, roleIds, resIds, username);
         return success(true);
     }
 
+    /**
+     * 增加权限
+     *
+     * @param requestMap
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.ADD_AUTH)
-    public AjaxResponse addAuth(@RequestBody Map requestMap)  {
+    public AjaxResponse addAuth(@RequestBody Map requestMap) {
 
-        List<Integer>  roleIds = (List<Integer>) requestMap.get("roleIds");
-        List<Integer>  resIds = (List<Integer>) requestMap.get("resIds");
+        List<Integer> roleIds = (List<Integer>) requestMap.get("roleIds");
+        List<Integer> resIds = (List<Integer>) requestMap.get("resIds");
         String username = getUser().getUserName();
         adminRoleService.addAuth(roleIds, resIds, username);
         return success(true);
     }
 
 
+    /**
+     * 移除权限
+     *
+     * @param requestMap
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.REMOVE_AUTH)
-    public AjaxResponse removeAuth(@RequestBody Map requestMap)  {
+    public AjaxResponse removeAuth(@RequestBody Map requestMap) {
 
-        List<Integer>  roleIds = (List<Integer>) requestMap.get("roleIds");
-        List<Integer>  resIds = (List<Integer>) requestMap.get("resIds");
+        List<Integer> roleIds = (List<Integer>) requestMap.get("roleIds");
+        List<Integer> resIds = (List<Integer>) requestMap.get("resIds");
         String username = getUser().getUserName();
         adminRoleService.removeAuth(roleIds, resIds, username);
         return success(true);
     }
 
+
+    /**
+     * 按角色取所有权限
+     *
+     * @param requestMap
+     * @return
+     */
     @RequestMapping(AdminUrlConstants.User.Role.GET_AUTH_BY_ROLES)
-    public AjaxResponse getAuthByRoles(@RequestBody Map requestMap)  {
-        List<Integer>  roleIds = (List<Integer>) requestMap.get("roleIds");
-        if(roleIds.size() ==0)
-        {
-            UserFormBean form = (UserFormBean)requestMap.get("form");
+    public AjaxResponse getAuthByRoles(@RequestBody Map requestMap) {
+        List<Integer> roleIds = (List<Integer>) requestMap.get("roleIds");
+        if (roleIds.size() == 0) {
+            UserFormBean form = (UserFormBean) requestMap.get("form");
             String roleName = form.getRoleName();
             Integer roleType = form.getRoleType();
             String channelId = form.getChannelId();
@@ -177,9 +254,8 @@ public class AdminRoleController extends AdminController {
             Integer storeId = form.getStoreId();
             String application = form.getApplication();
 
-            List<AdminRoleBean> result  = adminRoleService.searchRole(roleName, roleType, channelId,  active, storeId, application , null, null ).getResult();
-            if(result != null && result.size() >0)
-            {
+            List<AdminRoleBean> result = adminRoleService.searchRole(roleName, roleType, channelId, active, storeId, application, null, null).getResult();
+            if (result != null && result.size() > 0) {
                 result.stream().forEach(w -> roleIds.add(w.getId()));
             }
         }
