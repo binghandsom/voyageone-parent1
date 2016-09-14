1e99c9b7fde33bf24bd7bec778ff3b26f0784027
Jonas Gao <gaopinsong@live.cn>
2016-09-14 15:59:25 +0800

    fix(PRODUCTBUG-79)

--- voyageone-service2/voyageone-service2-impl/src/test/java/com/voyageone/service/impl/cms/prices/CmsMtFeeCommissionServiceTest.java
+++ voyageone-service2/voyageone-service2-impl/src/test/java/com/voyageone/service/impl/cms/prices/CmsMtFeeCommissionServiceTest.java
@@ -1,6 +1,10 @@
 package com.voyageone.service.impl.cms.prices;
 
-import com.voyageone.Service2TestBase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by jonas on 9/14/16.
@@ -8,6 +12,21 @@ import com.voyageone.Service2TestBase;
  * @version 2.6.0
  * @since 2.6.0
  */
-public class CmsMtFeeCommissionServiceTest implements Service2TestBase {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:test-context-service2.xml")
+public class CmsMtFeeCommissionServiceTest {
 
+    @Autowired
+    private CmsMtFeeCommissionService feeCommissionService;
+
+    @Test
+    public void test() {
+        CmsMtFeeCommissionService.CommissionQueryBuilder queryBuilder = feeCommissionService.new CommissionQueryBuilder();
+        queryBuilder.withCategory("???");
+        queryBuilder.withCart(23);
+        queryBuilder.withChannel("010");
+        queryBuilder.withPlatform(1);
+        Double commission = queryBuilder.getCommission(CmsMtFeeCommissionService.COMMISSION_TYPE_PLATFORM);
+        System.out.println("\n\n"+commission+"\n\n");
+    }
 }
