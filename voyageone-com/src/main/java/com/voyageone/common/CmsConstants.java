6be8f6f946967f8a5f6be29a8221bea294e7b59e
morse.lu <morse.lu@voyageone.cn>
2016-09-19 10:49:42 +0800

    独立域名 上新 临时中途保存一版

--- voyageone-com/src/main/java/com/voyageone/common/CmsConstants.java
+++ voyageone-com/src/main/java/com/voyageone/common/CmsConstants.java
@@ -243,6 +243,7 @@ public class CmsConstants {
         int errorNum = 2;       // 上新失败
         int uploadingNum = 3;   // 上新中(上新完成后回写状态的时候用)
         int review = 4;         // 达尔文产品审核中
+        int waitCnUpload = 5;  // 独立域名等待xml上传
     }
 
     public interface FeedUpdFlgStatus{
