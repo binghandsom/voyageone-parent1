75a7cd196ce0a61554aed2d30368ab3a44bcb50b
gjl <gump.gan@voyageone.cn>
2016-09-19 14:29:29 +0800

    overStore Bug

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/feed/OverStockAnalysisService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/feed/OverStockAnalysisService.java
@@ -79,7 +79,9 @@ public class OverStockAnalysisService extends BaseAnalysisService {
         }
         $info("产品信息插入完成 共" + cnt + "条数据");
         if (cnt > 0) {
-            transformer.new Context(channel, this).transform();
+            if(!"1".equalsIgnoreCase(TaskControlUtils.getVal1(taskControlList, TaskControlEnums.Name.feed_full_copy_temp))) {
+                transformer.new Context(channel, this).transform();
+            }
             postNewProduct();
         }
     }
