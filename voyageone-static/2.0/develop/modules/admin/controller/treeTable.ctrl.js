e3d106647dfbb7856dd6a67ad175f2fcec721e90
sofia <sofia.yu@voyageone.cn>
2016-09-28 14:25:06 +0800

    fix bugs

--- voyageone-static/2.0/develop/modules/admin/controller/treeTable.ctrl.js
+++ voyageone-static/2.0/develop/modules/admin/controller/treeTable.ctrl.js
@@ -8,6 +8,23 @@ define([
 ], function (admin, _, MappingTypes) {
     admin.controller('treeTable', ['$scope', '$filter',
         function ($scope, $filter) {
+            $scope.toggleAllCheckboxes = function ($event, list, selectedList) {
+                var i, item, len, ref, results, selected;
+                selected = $event.target.checked;
+                ref = list;
+                results = [];
+                for (i = 0, len = ref.length; i < len; i++) {
+                    item = ref[i];
+                    item.selected = selected;
+                    if (item.children != null && item.children.length > 0) {
+                        $scope.toggleCheckbox(item, selectedList);
+                        results.push($scope.$broadcast('changeChildren', item, selectedList));
+                    } else {
+                        results.push(void 0);
+                    }
+                }
+                return results;
+            };
             $scope.initCheckbox = function (item, parentItem, flatList) {
                 if (!item.opened) item.opened = true;
                 if (item.children.length < 1) item.showArrow = false;
@@ -15,10 +32,29 @@ define([
                 return item.selected = parentItem && parentItem.selected || item.selected || false;
             };
             $scope.toggleCheckbox = function (item, list) {
-                if (item.selected===true) {
+                if (item.selected === true && list != undefined) {
                     list.push(item);
                 }
+                if (item.children != null) {
+                    $scope.$broadcast('changeChildren', item);
+                }
             };
+            $scope.$on('changeChildren', function (event, parentItem, selectedList) {
+                var child, i, len, ref, results;
+                ref = parentItem.children;
+                results = [];
+                for (i = 0, len = ref.length; i < len; i++) {
+                    child = ref[i];
+                    child.selected = parentItem.selected;
+                    $scope.toggleCheckbox(child, selectedList);
+                    if (child.children != null) {
+                        results.push($scope.$broadcast('changeChildren', child, selectedList));
+                    } else {
+                        results.push(void 0);
+                    }
+                }
+                return results;
+            });
         }
     ]);
     admin.filter('selected', [
