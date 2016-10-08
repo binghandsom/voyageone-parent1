3c4c8a275bb4c8e219fea680fd13e6ae23e15a55
wenjie.piao@voyageone.cn <wj7405049>
2016-10-08 15:35:29 +0800

    添加sku价格刷新页面

---
+++ voyageone-static/2.0/develop/modules/cms/views/pop/promotion/setSkuPrice.ctl.js
@@ -0,0 +1,20 @@
+/**
+ * @author piao
+ * @date 2016-10-08
+ * @version V2.8.0
+ */
+define([
+    'cms'
+],function(cms){
+
+    return cms.controller('setSkuPriceCtl',(function(){
+
+        function SetSkuPriceController(){
+
+        }
+
+        return SetSkuPriceController;
+
+    })());
+
+});
