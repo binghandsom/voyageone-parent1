b68fbf58d1ad24b09c07bc72f396f8eff1957078
gjl <gump.gan@voyageone.cn>
2016-10-11 18:36:36 +0800

    target

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/tools/common/CmsFeedToMasterBrandMappingController.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/tools/common/CmsFeedToMasterBrandMappingController.java
@@ -20,18 +20,18 @@ import java.util.Map;
 @RequestMapping(value = CmsUrlConstants.TOOLS.COMMON.ROOT, method = RequestMethod.POST)
 public class CmsFeedToMasterBrandMappingController extends CmsController {
 
-    @Autowired
-    private CmsMasterBrandMappingService cmsMasterBrandMappingService;
-
-    /**
-     * Master品牌匹配初始化
-     *
-     * @param param
-     * @return resultBean
-     */
-    @RequestMapping(value = CmsUrlConstants.TOOLS.COMMON.INIT_MASTER_BRAND_INFO)
-    public AjaxResponse doSearch(@RequestBody Map param) {
-        Map<String, Object> result = new HashMap<>();
-        return success(result);
-    }
+//    @Autowired
+//    private CmsMasterBrandMappingService cmsMasterBrandMappingService;
+//
+//    /**
+//     * Master品牌匹配初始化
+//     *
+//     * @param param
+//     * @return resultBean
+//     */
+//    @RequestMapping(value = CmsUrlConstants.TOOLS.COMMON.INIT_MASTER_BRAND_INFO)
+//    public AjaxResponse doSearch(@RequestBody Map param) {
+//        Map<String, Object> result = new HashMap<>();
+//        return success(result);
+//    }
 }
