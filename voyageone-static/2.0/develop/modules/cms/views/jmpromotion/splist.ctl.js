147f6d7b9d21981563837a61d213e1e61640f610
jiangjusheng <!@1.com>
2016-10-11 11:50:33 +0800

    聚美专场

--- voyageone-static/2.0/develop/modules/cms/views/jmpromotion/splist.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/jmpromotion/splist.ctl.js
@@ -23,10 +23,11 @@ define([
         };
 
         $scope.openOtherDownload = function (promotion) {
-
             $.download.post(cActions.cms.promotion.promotionService.root + "/" + cActions.cms.promotion.promotionService.exportPromotion, {"promotionId": promotion.id,"promotionName":promotion.promotionName});
         };
+
         $scope.dataPageOption = {curr: 1, total: 0, fetch: goPage.bind(this)};
+
         $scope.search = function () {
             var pageParameter=getPageParameter();
             $scope.dataPageOption.setPageIndex(1);//查询第一页
@@ -49,6 +50,7 @@ define([
         //获取分页参数及其条件
         function getPageParameter() {
             var pageParameter={};
+            $scope.searchInfo.cartId = 27;
             pageParameter.parameters=angular.copy($scope.searchInfo);;
             return pageParameter;
         }
@@ -66,20 +68,6 @@ define([
             });
             $scope.groupPageOption.total = $scope.vm.promotionList.length;
         }
-        //$scope.search = function () {
-        //    promotionService.getPromotionList($scope.searchInfo).then(function (res) {
-        //        $scope.vm.promotionList = _.where(res.data, {isAllPromotion: 0});
-        //        _.each($scope.vm.promotionList,function(item){
-        //            if(item.prePeriodStart) item.prePeriodStart = new Date(item.prePeriodStart);
-        //            if(item.prePeriodEnd) item.prePeriodEnd = new Date(item.prePeriodEnd);
-        //            if(item.activityStart) item.activityStart = new Date(item.activityStart);
-        //            if(item.activityEnd) item.activityEnd = new Date(item.activityEnd);
-        //            if(item.preSaleStart) item.preSaleStart = new Date(item.preSaleStart);
-        //            if(item.preSaleEnd) item.preSaleEnd = new Date(item.preSaleEnd);
-        //        });
-        //        $scope.groupPageOption.total = $scope.vm.promotionList.length;
-        //    })
-        //};
 
         $scope.del = function (data) {
             confirm($translate.instant('TXT_MSG_PROMOTION_DELETE').replace("%s",data.promotionName)).then(function () {
