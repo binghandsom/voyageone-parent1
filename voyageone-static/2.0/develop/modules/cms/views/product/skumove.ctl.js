d42d6706681835071464060a17019c94d9d746a2
sofia <sofia.yu@voyageone.cn>
2016-10-11 09:30:54 +0800

    add new tab [ Sku property] on the product detail

---
+++ voyageone-static/2.0/develop/modules/cms/views/product/skumove.ctl.js
@@ -0,0 +1,18 @@
+/**
+ * Created by sofia on 2016/10/10.
+ */
+define([
+    'cms',
+    'modules/cms/controller/popup.ctl'
+], function (cms) {
+    cms.controller('SkuMoveController', (function () {
+        function SkuMoveController() {
+        }
+
+        SkuMoveController.prototype = {
+            init: function () {
+            }
+        };
+        return SkuMoveController;
+    })())
+});
