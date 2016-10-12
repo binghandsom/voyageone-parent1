c21bd06d384ef23e7d52cf1fb8708787098ae958
wenjie.piao@voyageone.cn <wj7405049>
2016-10-12 14:31:58 +0800

    master品牌匹配页面

--- voyageone-static/2.0/develop/modules/cms/views/pop/system/brandMatching.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/system/brandMatching.ctl.js
@@ -1,3 +1,23 @@
 /**
  * Created by 123 on 2016/10/12.
  */
+
+define([
+    'cms'
+],function(cms){
+
+    cms.controller('brandMatchingCtl',(function(){
+
+        function BrandMatchingCtl(){
+
+        }
+
+        BrandMatchingCtl.prototype.init = function(){
+
+        };
+
+        return BrandMatchingCtl;
+
+    })());
+
+});
