ecad96cc09c9d105f8f2e449533c5f222582dad3
wenjie.piao@voyageone.cn <wj7405049>
2016-10-16 14:44:43 +0800

    聚美活动    图片详情

--- voyageone-static/2.0/develop/modules/cms/views/jmpromotion/sp.images.directive.js
+++ voyageone-static/2.0/develop/modules/cms/views/jmpromotion/sp.images.directive.js
@@ -3,27 +3,30 @@ define([
     'modules/cms/controller/popup.ctl'
 ], function (cms) {
 
-    function SpImagesDirectiveController($routeParams, jmPromotionService, alert, confirm, $translate, $filter) {
+    function SpImagesDirectiveController($routeParams, jmPromotionService, popups) {
         this.$routeParams = $routeParams;
         this.jmPromotionService = jmPromotionService;
-        this.alert = alert;
-        this.confirm = confirm;
-        this.$translate = $translate;
-        this.$filter = $filter;
-        this.vm = {"jmMasterBrandList": []};
-        this.editModel = {model: {}};
-        this.datePicker = [];
+        this.popups = popups;
     }
 
     SpImagesDirectiveController.prototype.init = function () {
 
-
     };
+
+    SpImagesDirectiveController.prototype.popImageSuit = function(){
+        var self = this,
+            popups = self.popups;
+
+        popups.openImageSuit({}).then(function(){
+
+        });
+    };
+
 
     cms.directive('spImages', [function spImagesDirectiveFactory() {
         return {
             restrict: 'E',
-            controller: ['$routeParams', 'jmPromotionService', 'alert', 'confirm', '$translate', '$filter', SpImagesDirectiveController],
+            controller: ['$routeParams', 'jmPromotionService', 'popups', SpImagesDirectiveController],
             controllerAs: 'ctrl',
             templateUrl: '/modules/cms/views/jmpromotion/sp.images.directive.html'
         }
