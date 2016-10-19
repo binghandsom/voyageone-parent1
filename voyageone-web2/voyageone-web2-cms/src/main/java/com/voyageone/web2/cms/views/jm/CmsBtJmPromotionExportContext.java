0b5f9e394af7e3452b4207971e9092565771f439
james <james.li@voyageone.cn>
2016-10-19 19:14:09 +0800

    jm promotion export

---
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/jm/CmsBtJmPromotionExportContext.java
@@ -0,0 +1,105 @@
+package com.voyageone.web2.cms.views.jm;
+
+import com.voyageone.common.masterdate.schema.utils.StringUtil;
+import com.voyageone.common.util.JacksonUtil;
+import com.voyageone.service.bean.cms.jumei.CmsBtJmPromotionSaveBean;
+import com.voyageone.service.impl.cms.jumei.CmsMtJmConfigService;
+import com.voyageone.service.model.cms.CmsBtJmPromotionModel;
+import com.voyageone.service.model.cms.CmsBtJmPromotionSpecialExtensionModel;
+import com.voyageone.service.model.cms.mongo.jm.promotion.CmsMtJmConfigModel;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by james on 2016/10/17.
+ */
+public class CmsBtJmPromotionExportContext {
+
+    private CmsBtJmPromotionModel model;
+    private CmsBtJmPromotionSpecialExtensionModel extModel;
+
+    private final CmsMtJmConfigService cmsMtJmConfigService;
+
+    @Autowired
+    public CmsBtJmPromotionExportContext(CmsBtJmPromotionSaveBean cmsBtJmPromotionSaveBean, CmsMtJmConfigService cmsMtJmConfigService) {
+        this.model = cmsBtJmPromotionSaveBean.getModel();
+        this.extModel = cmsBtJmPromotionSaveBean.getExtModel();
+        this.cmsMtJmConfigService = cmsMtJmConfigService;
+    }
+
+    public CmsBtJmPromotionModel getModel() {
+        return model;
+    }
+
+    public void setModel(CmsBtJmPromotionModel model) {
+        this.model = model;
+    }
+
+    public CmsBtJmPromotionSpecialExtensionModel getExtModel() {
+        return extModel;
+    }
+
+    public void setExtModel(CmsBtJmPromotionSpecialExtensionModel extModel) {
+        this.extModel = extModel;
+    }
+
+    public String getDisplayPlatform() {
+        StringBuffer stringBuffer = new StringBuffer();
+        if (!StringUtil.isEmpty(this.extModel.getDisplayPlatform())) {
+            List<Integer> displayPllatforms = JacksonUtil.jsonToBeanList(this.extModel.getDisplayPlatform(), Integer.class);
+            CmsMtJmConfigModel cmsMtJmConfigModel = cmsMtJmConfigService.getCmsMtJmConfigById(CmsMtJmConfigService.JmCofigTypeEnum.displayPlatform);
+            displayPllatforms.forEach(displayPllatform -> {
+                Map<String, Object> value = cmsMtJmConfigModel.getValues().stream().filter(objectObjectMap -> objectObjectMap.get("value") == displayPllatform).findFirst().orElse(null);
+                if(value != null){
+                    stringBuffer.append(value.get("name"));
+                    stringBuffer.append(" ");
+                }
+            });
+        }
+        return stringBuffer.toString();
+    }
+
+    public String getPreDisplayChannel(){
+        StringBuffer stringBuffer = new StringBuffer();
+        if (!StringUtil.isEmpty(this.extModel.getDisplayPlatform())) {
+            List<Integer> displayPllatforms = JacksonUtil.jsonToBeanList(this.extModel.getPreDisplayChannel(), Integer.class);
+            CmsMtJmConfigModel cmsMtJmConfigModel = cmsMtJmConfigService.getCmsMtJmConfigById(CmsMtJmConfigService.JmCofigTypeEnum.preDisplayChannel);
+            displayPllatforms.forEach(displayPllatform -> {
+                Map<String, Object> value = cmsMtJmConfigModel.getValues().stream().filter(objectObjectMap -> objectObjectMap.get("value") == displayPllatform).findFirst().orElse(null);
+                if(value != null){
+                    stringBuffer.append(value.get("name"));
+                    stringBuffer.append(" ");
+                }
+            });
+        }
+        return stringBuffer.toString();
+    }
+
+    public String getBrandString() {
+        return String.format("%s (%s)", this.model.getCmsBtJmMasterBrandId(), this.model.getBrand());
+    }
+
+    public String getSyncMobile() {
+        return getBooleanString(this.extModel.getSyncMobile());
+    }
+
+    public String getShowHiddenDeal() {
+        return getBooleanString(this.extModel.getShowHiddenDeal());
+    }
+
+    public String getShowSoldOutDeal() {
+        return getBooleanString(this.extModel.getShowSoldOutDeal());
+    }
+
+    private String getBooleanString(Boolean value) {
+        if (value) {
+            return "是";
+        } else if (value) {
+            return "否";
+        } else {
+            return "";
+        }
+    }
+}
