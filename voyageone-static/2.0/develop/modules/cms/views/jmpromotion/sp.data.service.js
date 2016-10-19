35bff0db381b10316fe7421fa5ace1e91d2bc0a7
Jonas Gao <gaopinsong@live.cn>
2016-10-19 14:07:01 +0800

    update(sp.data.service): remove notify

--- voyageone-static/2.0/develop/modules/cms/views/jmpromotion/sp.data.service.js
+++ voyageone-static/2.0/develop/modules/cms/views/jmpromotion/sp.data.service.js
@@ -1,14 +1,13 @@
 define(['cms'], function (cms) {
 
-    function SpDataService(jmPromotionService, jmPromotionDetailService, JmPromotionImagesService,$routeParams,$q,notify) {
+    function SpDataService(jmPromotionService, jmPromotionDetailService, JmPromotionImagesService, $routeParams, $q) {
         var self = this;
         self.promotionId = parseInt($routeParams['promId']);
         self.jmPromotionId = parseInt($routeParams['jmpromId']);
         self.commonUpEntity = {
-            promotionId:self.promotionId,
-            jmPromotionId:self.jmPromotionId
+            promotionId: self.promotionId,
+            jmPromotionId: self.jmPromotionId
         };
-        self.notify = notify;
         self.$q = $q;
         self.jmPromotionService = jmPromotionService;
         self.jmPromotionDetailService = jmPromotionDetailService;
@@ -65,28 +64,25 @@ define(['cms'], function (cms) {
         });
     };
 
-    SpDataService.prototype.initPromotionImages = function initPromotionImages(){
-       var self = this,
-           defer = self.$q.defer(),
-           JmPromotionImagesService = self.JmPromotionImagesService;
+    SpDataService.prototype.initPromotionImages = function initPromotionImages() {
+        var self = this,
+            defer = self.$q.defer(),
+            JmPromotionImagesService = self.JmPromotionImagesService;
 
-        JmPromotionImagesService.init(self.commonUpEntity).then(function(res){
+        JmPromotionImagesService.init(self.commonUpEntity).then(function (res) {
             defer.resolve(res);
-        },function(res){
+        }, function (res) {
             defer.reject(res);
         });
 
         return defer.promise;
     };
 
-    SpDataService.prototype.savePromotionImages = function savePromotionImages(upEntity){
-      var self = this,
-          notify = self.notify,
-          JmPromotionImagesService = self.JmPromotionImagesService;
+    SpDataService.prototype.savePromotionImages = function savePromotionImages(upEntity) {
+        var self = this,
+            JmPromotionImagesService = self.JmPromotionImagesService;
 
-        JmPromotionImagesService.save(_.extend(upEntity,self.commonUpEntity)).then(function(){
-            notify.success("SUCCESS");
-        });
+        return JmPromotionImagesService.save(_.extend(upEntity, self.commonUpEntity));
     };
 
     SpDataService.prototype.getPromotionProducts = function (tagId) {
