cb433deb7b3cbaf432d2afa4fa8d8667c23bbcbc
Jonas Gao <gaopinsong@live.cn>
2016-10-19 16:15:06 +0800

    update(sort.product))

--- voyageone-static/2.0/develop/modules/cms/views/jmpromotion/sort.product.controller.js
+++ voyageone-static/2.0/develop/modules/cms/views/jmpromotion/sort.product.controller.js
@@ -1,11 +1,16 @@
 define(['cms'], function (cms) {
-    function JmSortProductPopupController(spDataService, $scope) {
+    function JmSortProductPopupController(spDataService, $scope, $element, notify) {
+        this.destroy = function () {
+            $element.remove();
+            $scope.$destroy();
+        };
         this.tagId = $scope.tagId;
         this.spDataService = spDataService;
+        this.notify = notify;
         this.loadProduct();
     }
 
-    JmSortProductPopupController.prototype.loadProduct = function () {
+    JmSortProductPopupController.prototype.loadProduct = function loadProduct() {
         var self = this,
             spDataService = self.spDataService;
 
@@ -14,5 +19,21 @@ define(['cms'], function (cms) {
         });
     };
 
-    cms.controller('JmSortProductPopupController', ['spDataService', '$scope', JmSortProductPopupController]);
+    JmSortProductPopupController.prototype.remove = function (index) {
+        this.productList.splice(index, 1);
+    };
+
+    JmSortProductPopupController.prototype.save = function () {
+        var self = this,
+            spDataService = self.spDataService,
+            notify = self.notify,
+            destroy = self.destroy;
+
+        spDataService.saveProductSort(self.tagId, self.productList).then(function () {
+            notify.success('保存成功');
+            destroy();
+        });
+    };
+
+    cms.controller('JmSortProductPopupController', ['spDataService', '$scope', '$element', 'notify', JmSortProductPopupController]);
 });
