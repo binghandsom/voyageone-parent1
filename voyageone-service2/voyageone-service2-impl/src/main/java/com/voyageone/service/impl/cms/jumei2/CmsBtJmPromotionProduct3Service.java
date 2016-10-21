1b645ff1904caf5334196d8db0760891cf5e64c1
sunpeitao <peitaosun@voyageone.cn>
2016-10-21 14:47:31 +0800

    修改tag界面

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/jumei2/CmsBtJmPromotionProduct3Service.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/jumei2/CmsBtJmPromotionProduct3Service.java
@@ -167,6 +167,51 @@ public class CmsBtJmPromotionProduct3Service {
         return result;
     }
 
+
+    @VOTransactional
+    public CallResult batchUpdateSkuDealPrice(BatchUpdateSkuPriceParameterBean parameter) {
+        CallResult result = new CallResult();
+//        <option value="0">中国官网价格</option> <!--msrp_rmb-->
+//        <option value="1">中国指导价格</option> <!--retail_price-->
+//        <option value="2">中国最终售价</option> <!--sale_price-->
+
+        if (parameter.getListPromotionProductId().isEmpty()) return result;
+        //价格类型    1 建议售价   2指导售价  3最终售价  4固定售价
+        double price = 0;
+        if (parameter.getPriceTypeId() == 4) {//价格
+            price = parameter.getPriceValue();
+        } else//折扣 0：市场价 1：团购价
+        {
+            if (parameter.getPriceTypeId() == 1)//团购价 deal_price
+            {
+                // price = "a.msrp_rmb*" + Double.toString(parameter.getDiscount());//中国官网价格
+            } else if (parameter.getPriceTypeId() == 2) //市场价 market_price
+            {
+                // price = "a.retail_price*" + Double.toString(parameter.getDiscount());//中国指导价格
+            } else if (parameter.getPriceTypeId() == 3) {
+                //price = "a.sale_price*" + Double.toString(parameter.getDiscount());//中国最终售价
+            }
+        }
+        switch (parameter.getOptType()) {
+            case "+":
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "=":
+                break;
+            default:
+                break;
+        }
+        //price = "CEIL(" + price + ")";//向上取整
+        //         向下取整 floor
+
+         return result;
+    }
+
+
+
     //批量同步价格  1. then price_status=1
     public void batchSynchPrice(BatchSynchPriceParameter parameter) {
 
--- voyageone-static/2.0/develop/modules/cms/views/pop/jm/pricemodify.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/jm/pricemodify.ctl.js
@@ -3,8 +3,7 @@ define([
     'modules/cms/controller/popup.ctl'
 ], function (angularAMD) {
     angularAMD.controller('popPriceModifyCtl', function ($scope, $translate, jmPromotionDetailService,alert,context, $routeParams) {
-
-        $scope.model = {priceValueType:1,priceType:"1",discount:0,price:0};
+        // $scope.model = {priceValueType:1,priceType:"1",discount:0,price:0};
         var listPromotionProduct=[];
         var jmPromotionId=undefined;
         var isBegin=false;
@@ -32,6 +31,8 @@ define([
         };
 
         $scope.ok = function () {
+            console.log($scope.vm);
+            return;
             if (listPromotionProduct.length == 0) {
                 alert("请选择修改价格的商品!");
                 return;
