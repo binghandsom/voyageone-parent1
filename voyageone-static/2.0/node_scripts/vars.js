497d2abc7ef18668ace8d38820a30b9155a4298c
edward.lin <edward.lin@voyageone.cn>
2016-10-26 14:04:53 +0800

    取消静态内容的版本号

--- voyageone-static/2.0/node_scripts/vars.js
+++ voyageone-static/2.0/node_scripts/vars.js
@@ -96,15 +96,15 @@ var publish = {
     },
     release: {
         static: {
-            path: 'publish/release/' + versions.publish + '/static',
-            fonts: 'publish/release/' + versions.publish + '/static/fonts',
-            img: 'publish/release/' + versions.publish + '/static/img',
-            css: 'publish/release/' + versions.publish + '/static/css'
+            path: 'publish/release/static',
+            fonts: 'publish/release/static/fonts',
+            img: 'publish/release/static/img',
+            css: 'publish/release/static/css'
         },
-        components: 'publish/release/' + versions.publish + '/components/dist',
-        libs: 'publish/release/' + versions.publish + '/libs',
-        modules: 'publish/release/' + versions.publish + '/modules',
-        loginAndChannel: 'publish/release/' + versions.publish
+        components: 'publish/release/components/dist',
+        libs: 'publish/release/libs',
+        modules: 'publish/release/modules',
+        loginAndChannel: 'publish/release/'
     },
     replace_value: {
         voyageone_angular_com: 'components/dist/voyageone.angular.com',
