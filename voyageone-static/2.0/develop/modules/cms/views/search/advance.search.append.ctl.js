7769c9f4e6d03a11bb83fabd2b137d17598a4bbd
wenjie.piao@voyageone.cn <wj7405049>
2016-11-03 16:31:44 +0800

    高级检索排序

--- voyageone-static/2.0/develop/modules/cms/views/search/advance.search.append.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/search/advance.search.append.ctl.js
@@ -3,18 +3,51 @@
  */
 define([
     'cms'
-],function(cms){
+], function (cms) {
 
-    cms.controller('adSearchAppendCtl',(function(){
+    cms.controller('adSearchAppendCtl', (function () {
 
-        function AdSearchAppendCtl($scope){
+        function AdSearchAppendCtl($scope, $translate) {
             this.parentScope = $scope.$parent;
+            this.$translate = $translate;
+            this.columnArrow = {};
         }
 
-        AdSearchAppendCtl.prototype.test = function(){
-            var self = this;
+        AdSearchAppendCtl.prototype.columnOrder = function (columnName) {
+            var self = this,
+                $translate = self.$translate,
+                columnArrow = self.columnArrow;
 
+            columnName = $translate.instant(columnName);
 
+            var column = columnArrow[columnName];
+
+            if(!column){
+                column = {};
+                column.mark = 'unsorted';
+                column.count = 0;
+            }
+
+            column.count++;
+            if (column.count % 2 === 0)
+                column.mark = 'sort-up';
+            else
+                column.mark = 'sort-desc';
+
+            columnArrow[columnName] = column;
+
+        };
+
+        AdSearchAppendCtl.prototype.getArrowName = function(columnName){
+            var self = this,
+                $translate = self.$translate,
+                name = $translate.instant(columnName),
+                columnArrow = self.columnArrow;
+
+            if(!columnArrow || !columnArrow[name])
+                return 'unsorted';
+
+            return columnArrow[name].mark;
         };
 
         return AdSearchAppendCtl;
