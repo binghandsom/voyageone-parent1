e722fb82dfe64d74c5c41b460c5d76a202b64a1f
jeff.duan <jeff.duan@voyageone.cn>
2016-11-03 18:44:26 +0800

    BUGID-17

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/product/CmsProductMoveService.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/product/CmsProductMoveService.java
@@ -829,8 +829,8 @@ public class CmsProductMoveService extends BaseViewService {
         // 处理platforms.P0
         CmsBtProductModel_Platform_Cart newP0Model = newProductModel.getPlatform(0);
         if ("new".equals(destGroupType)) {
-            // 新Group的场合，P0下的主商品=源Code
-            newP0Model.setMainProductCode(sourceCode);
+            // 新Group的场合，P0下的主商品=新Code
+            newP0Model.setMainProductCode(newFieldModel.getCode());
         } else {
             if ("old".equals(destGroupType)) {
                 // 原Group的场合,P0下的主商品不变
