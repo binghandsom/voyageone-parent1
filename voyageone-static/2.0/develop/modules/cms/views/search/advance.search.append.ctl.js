62b06e5637013da69babfbd0fa14f6743fb71bfb
wenjie.piao@voyageone.cn <wj7405049>
2016-11-04 15:43:35 +0800

    高级检索排序

--- voyageone-static/2.0/develop/modules/cms/views/search/advance.search.append.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/search/advance.search.append.ctl.js
@@ -7,28 +7,38 @@ define([
 
     cms.controller('adSearchAppendCtl', (function () {
 
-        function AdSearchAppendCtl($scope, $translate) {
+        function AdSearchAppendCtl($scope,notify) {
+            //高级检索中的scope
             this.parentScope = $scope.$parent;
-            this.$translate = $translate;
+            this.notify = notify;
             this.columnArrow = {};
         }
 
-        AdSearchAppendCtl.prototype.columnOrder = function (columnName) {
+        AdSearchAppendCtl.prototype.columnOrder = function (columnName,cartId) {
             var self = this,
-                $translate = self.$translate,
+                column,
                 columnArrow = self.columnArrow;
 
-            columnName = $translate.instant(columnName);
+            if(cartId){
+                columnName = columnName.replace('✓',cartId);
+            }
 
-            var column = columnArrow[columnName];
+            _.forEach(columnArrow, function (value, key) {
+                if (key != columnName)
+                    columnArrow[key] = null;
+            });
 
-            if(!column){
+            column = columnArrow[columnName];
+
+            if (!column) {
                 column = {};
                 column.mark = 'unsorted';
                 column.count = 0;
             }
 
             column.count++;
+
+            //偶数升序，奇数降序
             if (column.count % 2 === 0)
                 column.mark = 'sort-up';
             else
@@ -36,19 +46,60 @@ define([
 
             columnArrow[columnName] = column;
 
+            self.searchByOrder(columnName, column.mark);
         };
 
-        AdSearchAppendCtl.prototype.getArrowName = function(columnName){
+        AdSearchAppendCtl.prototype.searchByOrder = function (columnName, sortOneType) {
             var self = this,
-                $translate = self.$translate,
-                name = $translate.instant(columnName),
+                parentScope = self.parentScope,
+                searchInfo = parentScope.vm.searchInfo;
+
+            searchInfo.sortOneName = columnName;
+            searchInfo.sortOneType = sortOneType == 'sort-up' ? '1' : '-1';
+
+            parentScope.search();
+
+        };
+
+        AdSearchAppendCtl.prototype.getArrowName = function (columnName,cartId) {
+            var self = this,
                 columnArrow = self.columnArrow;
 
-            if(!columnArrow || !columnArrow[name])
+            if(cartId){
+                columnName = columnName.replace('✓',cartId);
+            }
+
+            if (!columnArrow || !columnArrow[columnName])
                 return 'unsorted';
 
-            return columnArrow[name].mark;
+            return columnArrow[columnName].mark;
         };
+
+        AdSearchAppendCtl.prototype.orderStep1 = function () {
+            var self = this,
+                parentScope = self.parentScope,
+                searchInfo = parentScope.vm.searchInfo,
+                column,
+                notify = self.notify,
+                columnArrow = self.columnArrow;
+
+            if(!searchInfo.sortOneName){
+                searchInfo.sortOneName = 0;
+                notify.warning("Warning： 请选择排序条件1");
+                return;
+            }
+
+            _.forEach(columnArrow, function (value, key) {
+                columnArrow[key] = null;
+            });
+
+            column = {};
+            column.mark = searchInfo.sortOneType == 1 ? 'sort-up' : 'sort-desc';
+            column.count = searchInfo.sortOneType == 1 ? 2 : 1;
+
+            columnArrow[searchInfo.sortOneName] = column;
+        };
+
 
         return AdSearchAppendCtl;
 
--- voyageone-static/2.0/develop/modules/cms/views/search/advanceSearch.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/search/advanceSearch.ctl.js
@@ -266,7 +266,7 @@ define([
                 $scope.vm.fstShowGrpFlg = true;
                 // 计算表格宽度
                 $scope.vm.tblWidth = (($scope.vm.commonProps.length + $scope.vm.sumCustomProps.length) * 120 + $scope.vm.selSalesType.length * 100 + $scope.vm.selBiDataList.length * 100 + 900) + 'px';
-                $scope.vm.tblWidth2 = (($scope.vm.commonProps.length + $scope.vm.sumCustomProps.length) * 120 + $scope.vm.selSalesType.length * 100 + $scope.vm.selBiDataList.length * 100 + 1300) + 'px';
+                $scope.vm.tblWidth2 = (($scope.vm.commonProps.length + $scope.vm.sumCustomProps.length) * 120 + $scope.vm.selSalesType.length * 100 + $scope.vm.selBiDataList.length * 100 + 1360) + 'px';
             })
         }
 
