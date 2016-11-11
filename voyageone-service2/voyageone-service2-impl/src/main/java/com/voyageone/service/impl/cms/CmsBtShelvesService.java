e3b2841639385f9c9c84d366df02e9c157ef9dc1
james <james.li@voyageone.cn>
2016-11-11 14:44:44 +0800

    doc263

---
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/CmsBtShelvesService.java
@@ -0,0 +1,30 @@
+package com.voyageone.service.impl.cms;
+
+import com.voyageone.service.dao.cms.CmsBtShelvesDao;
+import com.voyageone.service.impl.BaseService;
+import com.voyageone.service.model.cms.CmsBtShelvesModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by james on 2016/11/11.
+ */
+@Service
+public class CmsBtShelvesService extends BaseService {
+
+    @Autowired
+    private CmsBtShelvesDao cmsBtShelvesDao;
+
+    public CmsBtShelvesModel getId(Integer id){
+        return cmsBtShelvesDao.select(id);
+    }
+
+    public int update(CmsBtShelvesModel cmsBtShelvesModel){
+        return cmsBtShelvesDao.update(cmsBtShelvesModel);
+    }
+
+    public Integer insert(CmsBtShelvesModel cmsBtShelvesModel){
+        cmsBtShelvesDao.insert(cmsBtShelvesModel);
+        return cmsBtShelvesModel.getId();
+    }
+}
