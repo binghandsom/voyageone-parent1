083860ddc194bb2faa9307ab4cf76e3c3fa8848a
jonas <jonas.gao@voyageone.cn>
2016-11-14 15:20:23 +0800

    add(shelvesFields)

---
+++ voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/fields/cms/CmsBtShelvesTemplateModelTemplateType.java
@@ -0,0 +1,19 @@
+package com.voyageone.service.fields.cms;
+
+/**
+ * （平台页面）货架模板的模板类型
+ * Created by jonas on 2016/11/14.
+ *
+ * @version 2.10.0
+ * @since 2.10.0
+ */
+public interface CmsBtShelvesTemplateModelTemplateType {
+    /**
+     * 布局模板
+     */
+    int LAYOUT = 0;
+    /**
+     * 单品模板
+     */
+    int ITEM = 1;
+}
