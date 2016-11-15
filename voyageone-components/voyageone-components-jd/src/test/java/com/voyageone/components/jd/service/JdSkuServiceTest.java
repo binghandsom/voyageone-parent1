07863816ad32f0cd31b67ca174a3bdf179772c0d
desmond <desmond.li@voyageone.cn>
2016-11-15 18:49:58 +0800

    追加取得sku信息共通方法

--- voyageone-components/voyageone-components-jd/src/test/java/com/voyageone/components/jd/service/JdSkuServiceTest.java
+++ voyageone-components/voyageone-components-jd/src/test/java/com/voyageone/components/jd/service/JdSkuServiceTest.java
@@ -1,11 +1,16 @@
 package com.voyageone.components.jd.service;
 
+import com.google.common.base.Joiner;
+import com.jd.open.api.sdk.domain.ware.Sku;
 import com.voyageone.common.configs.beans.ShopBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 京东商品sku相关API(更新sku价格等)调用服务测试
@@ -27,7 +32,7 @@ public class JdSkuServiceTest {
         shopBean.setAppSecret("");
         shopBean.setSessionKey(""); // 京东国际匠心界全球购专营店(SessionKey)
         shopBean.setOrder_channel_id("928");
-        shopBean.setCart_id("29");
+        shopBean.setCart_id("28");
         shopBean.setShop_name("京东国际匠心界全球购专营店");
 
         String outerId = "skucode001";
@@ -44,7 +49,7 @@ public class JdSkuServiceTest {
         shopBean.setAppSecret("");
         shopBean.setSessionKey(""); // 京东国际匠心界全球购专营店(SessionKey)
         shopBean.setOrder_channel_id("928");
-        shopBean.setCart_id("29");
+        shopBean.setCart_id("28");
         shopBean.setShop_name("京东国际匠心界全球购专营店");
 
         String skuId = "";
@@ -55,5 +60,74 @@ public class JdSkuServiceTest {
         String jdPrice = "9301";
         String result = jdSkuService.updateSkuPrice(shopBean, skuId, outerId, price, null, marketPrice, jdPrice);
         System.out.println("result = " + (result == null ? "" : result));
+    }
+
+    @Test
+    public void testGetSkusByWareId() throws Exception {
+        ShopBean shopBean = new ShopBean();
+        shopBean.setApp_url("https://api.jd.com/routerjson");
+        shopBean.setAppKey("");
+        shopBean.setAppSecret("");
+        shopBean.setSessionKey(""); // 京东国际匠心界全球购专营店(SessionKey)
+        shopBean.setOrder_channel_id("928");
+        shopBean.setCart_id("28");
+        shopBean.setShop_name("京东国际匠心界全球购专营店");
+
+        String wareId = "1955593691";
+        StringBuffer failCause = new StringBuffer("");
+        List<Sku> skus = jdSkuService.getSkusByWareId(shopBean, wareId, failCause);
+        System.out.println("result skuId = " + (skus == null ? "no skus" : Joiner.on(",").join(skus.stream().map(Sku::getSkuId).toArray())));
+        System.out.println("failCause = " + failCause.toString());
+    }
+
+    @Test
+    public void testGetSkusByWareIds() throws Exception {
+        ShopBean shopBean = new ShopBean();
+        shopBean.setApp_url("https://api.jd.com/routerjson");
+        shopBean.setAppKey("");
+        shopBean.setAppSecret("");
+        shopBean.setSessionKey(""); // 京东国际匠心界全球购专营店(SessionKey)
+        shopBean.setOrder_channel_id("928");
+        shopBean.setCart_id("28");
+        shopBean.setShop_name("京东国际匠心界全球购专营店");
+
+        List<String> wareIds = new ArrayList() {{
+            add("1955593691");
+            add("1955182318");
+            add("1955593098");
+            add("1955208725");
+            add("1955173805");
+            add("1955602904");
+            add("1955174905");
+            add("1956471339");
+            add("1955178904");
+            add("1955126566");
+            add("abcddddddtest");
+//            add("9999888899");
+            add("1955172111");
+        }};
+
+        StringBuffer failCause = new StringBuffer("");
+        List<Sku> skus = jdSkuService.getSkusByWareIds(shopBean, wareIds, failCause);
+        System.out.println("result skuId = " + (skus == null ? "no skus" : Joiner.on(",").join(skus.stream().map(Sku::getSkuId).toArray())));
+        System.out.println("failCause = " + failCause.toString());
+    }
+
+    @Test
+    public void testGetSkuByOuterId() throws Exception {
+        ShopBean shopBean = new ShopBean();
+        shopBean.setApp_url("https://api.jd.com/routerjson");
+        shopBean.setAppKey("");
+        shopBean.setAppSecret("");
+        shopBean.setSessionKey(""); // 京东国际匠心界全球购专营店(SessionKey)
+        shopBean.setOrder_channel_id("928");
+        shopBean.setCart_id("28");
+        shopBean.setShop_name("京东国际匠心界全球购专营店");
+
+        String outerId = "022-0410S0RAW0862";
+        StringBuffer failCause = new StringBuffer("");
+        Sku sku = jdSkuService.getSkuByOuterId(shopBean, outerId, failCause);
+        System.out.println("result skuId = " + (sku == null ? "no sku" : sku.getSkuId()));
+        System.out.println("failCause = " + failCause.toString());
     }
 }
