8e341877f9449ab61189cc4172edb4905e178ea7
desmond <desmond.li@voyageone.cn>
2016-11-15 18:49:01 +0800

    追加取得sku信息共通方法

--- voyageone-components/voyageone-components-jd/src/main/java/com/voyageone/components/jd/service/JdSkuService.java
+++ voyageone-components/voyageone-components-jd/src/main/java/com/voyageone/components/jd/service/JdSkuService.java
@@ -1,16 +1,28 @@
 package com.voyageone.components.jd.service;
 
+import com.google.common.base.Joiner;
+import com.jd.open.api.sdk.domain.ware.Sku;
+import com.jd.open.api.sdk.request.ware.SkuCustomGetRequest;
 import com.jd.open.api.sdk.request.ware.WareSkuPriceUpdateRequest;
+import com.jd.open.api.sdk.request.ware.WareSkusGetRequest;
+import com.jd.open.api.sdk.response.ware.SkuCustomGetResponse;
 import com.jd.open.api.sdk.response.ware.WareSkuPriceUpdateResponse;
+import com.jd.open.api.sdk.response.ware.WareSkusGetResponse;
 import com.voyageone.base.exception.BusinessException;
 import com.voyageone.common.configs.beans.ShopBean;
+import com.voyageone.common.util.CommonUtil;
+import com.voyageone.common.util.ListUtils;
 import com.voyageone.common.util.StringUtils;
 import com.voyageone.components.jd.JdBase;
 import com.voyageone.components.jd.JdConstants;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 京东商品sku相关API(更新sku价格等)调用服务
+ * 京东商品sku相关API调用服务
+ * 包括更新sku价格,取得sku信息等服务
  * <p/>
  * @author desmond on 2016/11/14.
  * @version 2.8.0
@@ -106,6 +118,122 @@ public class JdSkuService extends JdBase {
             logger.error(errMsg);
             throw new BusinessException(errMsg);
         }
+    }
+
+    /**
+     * 根据商品ID列表获取商品SKU信息
+     * 通过api(360buy.ware.skus.get) 获取多个商品下的所有sku信息
+     *
+     * @param shop ShopBean  店铺信息
+     * @param wareId String  京东商品id列表，sku所属商品id，必选。ware_ids个数不能超过10个
+     * @return skus  指定商品列表的所有sku信息列表
+     */
+    public List<Sku> getSkusByWareId(ShopBean shop, String wareId, StringBuffer failCause) throws BusinessException {
+        List<String> wareIds = new ArrayList<>();
+        wareIds.add(wareId);
+
+        return getSkusByWareIds(shop, wareIds, failCause);
+    }
+
+    /**
+     * 根据商品ID列表获取商品SKU信息
+     * 通过api(360buy.ware.skus.get) 获取多个商品下的所有sku信息
+     *
+     * @param shop ShopBean  店铺信息
+     * @param wareIds List<String>  京东商品id列表，sku所属商品id，必选。ware_ids个数不能超过10个
+     * @return skus  指定商品列表的所有sku信息列表
+     */
+    public List<Sku> getSkusByWareIds(ShopBean shop, List<String> wareIds, StringBuffer failCause) throws BusinessException {
+        List<Sku> skus = new ArrayList<>();
+        if (ListUtils.isNull(wareIds)) return null;
+
+        // 批量取得商品wareId对应的sku信息时，ware_ids个数不能超过10个
+        List<List<String>> pageList = CommonUtil.splitList(wareIds, 10);
+        WareSkusGetRequest request = new WareSkusGetRequest();
+        String strWareIds;
+        for(List<String> page : pageList) {
+            // sku所属商品id
+            strWareIds = Joiner.on(",").join(page);
+            try {
+                // sku所属商品id，必选。ware_ids个数不能超过10个
+                request.setWareIds(strWareIds);
+                // 需返回的字段列表(现在选的是返回全部字段)。可选值：Sku结构体中的所有字段；字段之间用“,”分隔
+                request.setFields("shop_id,cost_price,status,outer_id,color_value,stock_num,modified,jd_price,market_price,ware_id,created,sku_id,attributes,size_value");
+
+                // 调用京东根据商品ID列表获取商品SKU信息API(360buy.ware.skus.get)
+                WareSkusGetResponse response = reqApi(shop, request);
+                if (response != null) {
+                    // 京东返回正常的场合
+                    if (JdConstants.C_JD_RETURN_SUCCESS_OK.equals(response.getCode())) {
+                        // 返回图片列表
+                        skus.addAll(response.getSkus());
+                    } else {
+                        // 京东返回失败的场合
+                        throw new BusinessException(response.getZhDesc());
+                    }
+                } else {
+                    // response = null（https://api.jd.com/routerjson）不能访问的可能原因是服务器禁掉了https端口
+                    // 或app_url,app_key等不正确
+                    throw new BusinessException("京东根据商品ID列表获取商品SKU信息API返回应答为空(response = null)");
+                }
+            } catch (Exception e) {
+                String errMsg = String.format(shop.getShop_name() + "调用京东API根据商品ID列表获取商品SKU信息失败! [channelId:%s] [cartId:%s] [wareIds:%s] " +
+                        "[errMsg:%s]", shop.getOrder_channel_id(), shop.getCart_id(), strWareIds, e.getMessage());
+                logger.error(errMsg);
+                failCause.append(errMsg);
+            }
+        }
+
+        return skus;
+    }
+
+    /**
+     * 根据外部商家ID(skuCode)获取商品SKU信息
+     * 根据商家设定的sku的外部id获取所对应的sku数据，一个sku的外部id对应一个sku数据
+     * 通过api(360buy.sku.custom.get) 获取多个商品下的所有sku信息
+     *
+     * @param shop ShopBean  店铺信息
+     * @param outerId String  sku的外部商家ID(skuCode)
+     * @return sku  外部商家ID对应的sku信息
+     */
+    public Sku getSkuByOuterId(ShopBean shop, String outerId, StringBuffer failCause) {
+        if (StringUtils.isEmpty(outerId)) {
+            failCause.append("根据外部商家ID(skuCode)获取商品SKU信息时，参数传入的outerid为空！");
+            return null;
+        }
+
+        SkuCustomGetRequest request = new SkuCustomGetRequest();
+        // sku的外部商家ID 对应商家后台“商家SKU”字段(skuCode)
+        request.setOuterId(outerId);
+        // 需返回的字段列表(现在选的是返回全部字段)。可选值：Sku结构体中的所有字段；字段之间用“,”分隔
+        request.setFields("shop_id,cost_price,status,outer_id,color_value,stock_num,modified,jd_price,market_price,ware_id,created,sku_id,attributes,size_value");
+
+        Sku resultSku = null;
+        try {
+            // 调用京东根据外部商家ID(skuCode)获取商品SKU信息API(360buy.ware.skus.get)
+            SkuCustomGetResponse response = reqApi(shop, request);
+            if (response != null) {
+                // 京东返回正常的场合
+                if (JdConstants.C_JD_RETURN_SUCCESS_OK.equals(response.getCode())) {
+                    // 返回图片列表
+                    resultSku = response.getSku();
+                } else {
+                    // 京东返回失败的场合
+                    throw new BusinessException(response.getZhDesc());
+                }
+            } else {
+                // response = null（https://api.jd.com/routerjson）不能访问的可能原因是服务器禁掉了https端口
+                // 或app_url,app_key等不正确
+                throw new BusinessException("京东根据根据外部商家ID(skuCode)获取商品SKU信息API返回应答为空(response = null)");
+            }
+        } catch (Exception e) {
+            String errMsg = String.format(shop.getShop_name() + "调用京东API根据外部商家ID(skuCode)获取商品SKU信息失败! [channelId:%s] [cartId:%s] [outerId:%s] " +
+                    "[errMsg:%s]", shop.getOrder_channel_id(), shop.getCart_id(), outerId, e.getMessage());
+            logger.error(errMsg);
+            failCause.append(errMsg);
+        }
+
+        return resultSku;
     }
 
 }
