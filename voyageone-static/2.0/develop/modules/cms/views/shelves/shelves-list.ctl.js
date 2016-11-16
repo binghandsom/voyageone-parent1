00681bc3e720117e1dd81dc48b65d9c929cd0dce
Jonas <jonas.gao@voyageone.cn>
2016-11-16 16:00:53 +0800

    add(shelves-list): shelves search

--- voyageone-static/2.0/develop/modules/cms/views/shelves/shelves-list.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/shelves/shelves-list.ctl.js
@@ -2,20 +2,50 @@ define([
     'cms',
     'modules/cms/controller/popup.ctl'
 ], function (cms) {
-    function ShelvesListController(shelvesService, popups) {
-        // shelvesService.search();
-        // this.
+    function ShelvesListController(shelvesService, popups, menuService) {
         var self = this;
 
+        self.shelvesService = shelvesService;
         self.popups = popups;
+
+        menuService.getPlatformType().then(function (data) {
+            self.cartList = data.filter(function (i) {
+                return i.value >= 20 && i .value < 900;
+            });
+
+            self.cart = self.cartList[0].value;
+            self.clientType = self.clientTypes.PC;
+
+            self.getShelves();
+        });
     }
 
     ShelvesListController.prototype = {
+        clientTypes: {
+            PC: 1,
+            APP: 2
+        },
+        getShelves: function () {
+            var self = this;
+            var shelvesService = self.shelvesService;
+            var params = {
+                cartId: +self.cart,
+                clientType: self.clientType
+            };
+
+            if (!params.cartId && !params.clientType) {
+                return;
+            }
+
+            shelvesService.search(params).then(function (resp) {
+                self.shelves = resp.data;
+            });
+        },
         addShelves: function () {
             this.popups.popNewShelves();
         }
     };
 
-    cms.controller('ShelvesListController', ['shelvesService', 'popups', ShelvesListController]);
+    cms.controller('ShelvesListController', ['shelvesService', 'popups', 'menuService', ShelvesListController]);
 });
 
