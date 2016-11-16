7ec5ea7626407e146d4becdd81437693f0bcca6f
Jonas <jonas.gao@voyageone.cn>
2016-11-16 21:31:48 +0800

    add(shelves-list): format & add select by example model

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/CmsBtShelvesService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/CmsBtShelvesService.java
@@ -4,7 +4,6 @@ import com.voyageone.service.dao.cms.CmsBtShelvesDao;
 import com.voyageone.service.fields.cms.CmsBtShelvesModelActive;
 import com.voyageone.service.impl.BaseService;
 import com.voyageone.service.model.cms.CmsBtShelvesModel;
-import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,27 +25,31 @@ public class CmsBtShelvesService extends BaseService {
         this.cmsBtShelvesDao = cmsBtShelvesDao;
     }
 
-    public CmsBtShelvesModel getId(Integer id){
+    public CmsBtShelvesModel getId(Integer id) {
         return cmsBtShelvesDao.select(id);
     }
 
-    public int update(CmsBtShelvesModel cmsBtShelvesModel){
+    public int update(CmsBtShelvesModel cmsBtShelvesModel) {
         return cmsBtShelvesDao.update(cmsBtShelvesModel);
     }
 
-    public Integer insert(CmsBtShelvesModel cmsBtShelvesModel){
+    public Integer insert(CmsBtShelvesModel cmsBtShelvesModel) {
         cmsBtShelvesDao.insert(cmsBtShelvesModel);
         return cmsBtShelvesModel.getId();
     }
 
-    public List<CmsBtShelvesModel>selectList(Map map){
+    public List<CmsBtShelvesModel> selectList(Map map) {
         return cmsBtShelvesDao.selectList(map);
     }
 
-    public List<CmsBtShelvesModel>selectByChannelId(String channelId){
-        Map<String, Object> params = new HashedMap();
-        params.put("channelId",channelId);
-        params.put("active", CmsBtShelvesModelActive.ACTIVATE);
-        return cmsBtShelvesDao.selectList(params);
+    public List<CmsBtShelvesModel> selectList(CmsBtShelvesModel example) {
+        return cmsBtShelvesDao.selectList(example);
+    }
+
+    public List<CmsBtShelvesModel> selectByChannelId(String channelId) {
+        CmsBtShelvesModel byChannelId = new CmsBtShelvesModel();
+        byChannelId.setChannelId(channelId);
+        byChannelId.setActive(CmsBtShelvesModelActive.ACTIVATE);
+        return selectList(byChannelId);
     }
 }
