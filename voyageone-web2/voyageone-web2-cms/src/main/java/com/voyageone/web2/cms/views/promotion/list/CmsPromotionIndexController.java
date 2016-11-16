448f1eac42c5139edd5626ef88a5f6d706f01c6d
Jonas <jonas.gao@voyageone.cn>
2016-11-16 21:51:03 +0800

    format(CmsPromotionIndexController)

--- voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/promotion/list/CmsPromotionIndexController.java
+++ voyageone-web2/voyageone-web2-cms/src/main/java/com/voyageone/web2/cms/views/promotion/list/CmsPromotionIndexController.java
@@ -2,13 +2,9 @@ package com.voyageone.web2.cms.views.promotion.list;
 
 import com.voyageone.common.PageQueryParameters;
 import com.voyageone.common.util.DateTimeUtil;
-import com.voyageone.service.bean.cms.CallResult;
 import com.voyageone.service.bean.cms.CmsBtPromotion.EditCmsBtPromotionBean;
 import com.voyageone.service.bean.cms.CmsBtPromotion.SetPromotionStatusParameter;
-import com.voyageone.service.bean.cms.CmsBtPromotionBean;
 import com.voyageone.service.impl.cms.promotion.PromotionService;
-import com.voyageone.service.model.cms.CmsBtTagModel;
-import com.voyageone.service.model.util.MapModel;
 import com.voyageone.web2.base.ajax.AjaxResponse;
 import com.voyageone.web2.cms.CmsController;
 import com.voyageone.web2.cms.CmsUrlConstants.PROMOTION;
@@ -18,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,38 +26,46 @@ import java.util.Map;
         method = RequestMethod.POST
 )
 public class CmsPromotionIndexController extends CmsController {
+    private final CmsPromotionIndexService cmsPromotionService;
+    private final PromotionService promotionService;
 
     @Autowired
-    private CmsPromotionIndexService cmsPromotionService;
-    @Autowired
-    private PromotionService promotionService;
+    public CmsPromotionIndexController(PromotionService promotionService, CmsPromotionIndexService cmsPromotionService) {
+        this.promotionService = promotionService;
+        this.cmsPromotionService = cmsPromotionService;
+    }
 
     @RequestMapping(PROMOTION.LIST.INDEX.INIT)
     public AjaxResponse init() {
         return success(cmsPromotionService.init(getUser().getSelChannelId(), getLang()));
     }
+
     @RequestMapping(PROMOTION.LIST.INDEX.InitByPromotionId)
     public AjaxResponse initByPromotionId(@RequestBody int PromotionId) {
         return success(cmsPromotionService.initByPromotionId(PromotionId, getUser().getSelChannelId(), getLang()));
     }
+
     //重构 begin
     //获取指定页数据
     @RequestMapping(PROMOTION.LIST.INDEX.GetPage)
     public AjaxResponse getPage(@RequestBody PageQueryParameters parameters) {
-        parameters.put("channelId",getUser().getSelChannelId());
+        parameters.put("channelId", getUser().getSelChannelId());
         return success(promotionService.getPage(parameters));
     }
+
     //获取数量
     @RequestMapping(PROMOTION.LIST.INDEX.GetCount)
     public AjaxResponse getCount(@RequestBody PageQueryParameters parameters) {
-        parameters.put("channelId",getUser().getSelChannelId());
+        parameters.put("channelId", getUser().getSelChannelId());
         return success(promotionService.getCount(parameters));
     }
+
     //获取编辑数据
     @RequestMapping(PROMOTION.LIST.INDEX.GetEditModel)
-    public  AjaxResponse getEditModel(@RequestBody int promotionId) {
-       return success(promotionService.getEditModel(promotionId));
+    public AjaxResponse getEditModel(@RequestBody int promotionId) {
+        return success(promotionService.getEditModel(promotionId));
     }
+
     /**
      * 保存
      */
@@ -75,15 +78,18 @@ public class CmsPromotionIndexController extends CmsController {
         promotionService.saveEditModel(editModel);
         return success(null);
     }
+
     //删除
     @RequestMapping(PROMOTION.LIST.INDEX.DeleteByPromotionId)
-    public AjaxResponse deleteByPromotionId(@RequestBody int promotionId ) {
-     return success(promotionService.deleteByPromotionId(promotionId));
+    public AjaxResponse deleteByPromotionId(@RequestBody int promotionId) {
+        return success(promotionService.deleteByPromotionId(promotionId));
     }
+
     @RequestMapping(PROMOTION.LIST.INDEX.SetPromotionStatus)
-    public  AjaxResponse  setPromotionStatus(@RequestBody SetPromotionStatusParameter parameter) {
-        return  success(promotionService.setPromotionStatus(parameter));
+    public AjaxResponse setPromotionStatus(@RequestBody SetPromotionStatusParameter parameter) {
+        return success(promotionService.setPromotionStatus(parameter));
     }
+
     //重构 end
     @RequestMapping(PROMOTION.LIST.INDEX.GET_PROMOTION_LIST)
     public AjaxResponse queryList(@RequestBody Map<String, Object> params) {
@@ -98,6 +104,7 @@ public class CmsPromotionIndexController extends CmsController {
 //        cmsBtPromotionBean.setModifier(getUser().getUserName());
 //        return success(cmsPromotionService.addOrUpdate(cmsBtPromotionBean));
 //    }
+//
 //    @RequestMapping(PROMOTION.LIST.INDEX.DEL_PROMOTION)
 //    public AjaxResponse delPromotion(@RequestBody CmsBtPromotionBean cmsBtPromotionBean) {
 //        String channelId = getUser().getSelChannelId();
@@ -106,11 +113,16 @@ public class CmsPromotionIndexController extends CmsController {
 //        cmsBtPromotionBean.setModifier(getUser().getUserName());
 //        return success(cmsPromotionService.delete(cmsBtPromotionBean));
 //    }
+
     @RequestMapping(PROMOTION.LIST.INDEX.PROMOTION_EXPORT)
-    public ResponseEntity<byte[]> doExport(HttpServletRequest request, HttpServletResponse response, @RequestParam Integer promotionId, @RequestParam String promotionName)
-            throws Exception {
+    public ResponseEntity<byte[]> doExport(HttpServletRequest request, HttpServletResponse response,
+                                           @RequestParam Integer promotionId,
+                                           @RequestParam String promotionName) throws Exception {
 
         byte[] data = cmsPromotionService.getCodeExcelFile(promotionId, getUser().getSelChannelId());
-        return genResponseEntityFromBytes(String.format("%s(%s).xlsx",promotionName , DateTimeUtil.getLocalTime(getUserTimeZone(), "yyyyMMddHHmmss") , ".xlsx"), data);
+
+        String filename = String.format("%s(%s).xlsx", promotionName, DateTimeUtil.getLocalTime(getUserTimeZone(), "yyyyMMddHHmmss"));
+
+        return genResponseEntityFromBytes(filename, data);
     }
 }
