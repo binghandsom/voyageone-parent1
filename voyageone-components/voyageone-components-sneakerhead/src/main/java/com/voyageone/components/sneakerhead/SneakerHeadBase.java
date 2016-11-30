00ab117959c87fbb1e82c19449013598f49d2e3d
vantis <vantis.zhang@voyageone.cn>
2016-11-30 15:14:28 +0800

    ADDED cmsSneakerheadUsCategorySyncService 相关配置;

--- voyageone-components/voyageone-components-sneakerhead/src/main/java/com/voyageone/components/sneakerhead/SneakerHeadBase.java
+++ voyageone-components/voyageone-components-sneakerhead/src/main/java/com/voyageone/components/sneakerhead/SneakerHeadBase.java
@@ -5,11 +5,12 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * sneakerhead 提供的 api 路径等基本信息
  * Created by gjl on 2016/11/15.
  */
 public class SneakerHeadBase extends ComponentBase {
 
-    protected static final String SNEAKERHEAD_BASE_URL = "http://127.0.0.1:52233";
+    protected static final String SNEAKERHEAD_BASE_URL = "http://47.180.64.158:52233";
 
     protected static final String APPLICATION_JSON_UTF_8 = "application/json;charset=UTF-8";
 
