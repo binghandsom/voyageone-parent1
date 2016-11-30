e2d083408f709a67f052dc339194d8f6f11147cf
morse.lu <morse.lu@voyageone.cn>
2016-11-30 14:07:52 +0800

    独立域名类目-产品关系 排序条件修正，暂时没做分页处理，看看内存支持的住不=。=

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsBuildPlatformProductSellercatCnService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsBuildPlatformProductSellercatCnService.java
@@ -10,7 +10,9 @@ import com.voyageone.common.masterdate.schema.field.InputField;
 import com.voyageone.common.util.ListUtils;
 import com.voyageone.components.cn.service.CnSchemaService;
 import com.voyageone.service.dao.cms.mongo.CmsBtProductDao;
+import com.voyageone.service.dao.cms.mongo.CmsBtProductTopDao;
 import com.voyageone.service.impl.cms.sx.CnCategoryService;
+import com.voyageone.service.model.cms.mongo.product.CmsBtProductTopModel;
 import com.voyageone.task2.base.BaseCronTaskService;
 import com.voyageone.task2.base.Enums.TaskControlEnums;
 import com.voyageone.task2.base.modelbean.TaskControlBean;
@@ -38,6 +40,8 @@ public class CmsBuildPlatformProductSellercatCnService extends BaseCronTaskServi
 
     @Autowired
     private CmsBtProductDao cmsBtProductDao;
+    @Autowired
+    private CmsBtProductTopDao cmsBtProductTopDao;
 
     @Override
     public SubSystem getSubSystem() {
@@ -97,7 +101,13 @@ public class CmsBuildPlatformProductSellercatCnService extends BaseCronTaskServi
 
         List<List<Field>> result = new ArrayList<>();
         for (String catId : listCatIds) {
-            List<String> codes = cmsBtProductDao.selectListCodeBySellerCat(channelId, cartId, catId);
+            // modified by morse.lu 2016/11/30 start
+//            List<String> codes = cmsBtProductDao.selectListCodeBySellerCat(channelId, cartId, catId);
+            List<String> codes = new ArrayList<>();
+            CmsBtProductTopModel topModel = cmsBtProductTopDao.selectByCatId(catId, channelId);
+            codes.addAll(topModel.getProductCodeList()); // 置顶列表
+            codes.addAll(cmsBtProductDao.selectListCodeBySellerCat(channelId, cartId, catId, topModel.getSortColumnName(), topModel.getSortType(), topModel.getProductCodeList())); // 普通code排序
+            // modified by morse.lu 2016/11/30 end
             if (ListUtils.isNull(codes)) {
                 $warn("类目[%s]不存在一个上新过的code!", catId);
                 continue;
