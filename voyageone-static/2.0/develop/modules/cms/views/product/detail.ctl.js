d4b699a70f6e3dc63326e31299ec963dc29284c8
edward <edward.lin@voyageone.cn>
2016-12-02 10:36:08 +0800

    追加天猫 tab 显示

--- voyageone-static/2.0/develop/modules/cms/views/product/detail.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/product/detail.ctl.js
@@ -53,7 +53,7 @@ define([
 
             self.menuService.getPlatformType().then(function (resp) {
                 self.platformTypes = _.filter(resp, function (element) {
-                    return element.value > 20;
+                    return element.value >= 20;
                 });
             });
 
