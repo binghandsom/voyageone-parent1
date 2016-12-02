1370f84cdb675313679adddfc2076f8f46b8fbcd
wenjie.piao@voyageone.cn <wj7405049>
2016-12-02 10:53:12 +0800

    组合套装页面调整

--- voyageone-static/2.0/develop/modules/cms/views/pop/product/combined-product-edit.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/product/combined-product-edit.ctl.js
@@ -9,12 +9,12 @@ define([
     ], function (cms) {
         cms.controller("CombinedProductEditController", (function () {
 
-               function CombinedProductEditController($scope, context, combinedProductService, $compile, $templateRequest, $document) {
-                   $scope.vm = {
-                       carts:{},
-                       product:{}
-                   };
-                   $scope.vm.carts = context.carts;
+            function CombinedProductEditController($scope, context, combinedProductService, $compile, $templateRequest, $document) {
+                $scope.vm = {
+                    carts: {},
+                    product: {}
+                };
+                $scope.vm.carts = context.carts;
 
                 $scope.initialize = function () {
                     combinedProductService.getCombinedProductDetail(context.product).then(function (resp) {
@@ -29,21 +29,25 @@ define([
                 };
 
 
-                   $scope.getCombinedProductInfo = function () {
-                       var cartId = $scope.vm.product.cartId;
-                       var numID = $scope.vm.product.numID;
-                       if(!cartId || !numID) {
-                           return;
-                       }
-                       combinedProductService.getCombinedProductDetail({"cartId" : cartId, "numID" : numID}).then(function (resp) {
-                           $scope.vm.product = resp.data.product == null ? {} : resp.data.product;
-                           // carts集合中cart为string, product为int
-                           $scope.vm.product.cartId = $scope.vm.product.cartId == null ? "" : $scope.vm.product.cartId + "";
-                           _.each($scope.vm.product.skus, function (element) {
-                               _.extend(element, {'tempSuitSellingPriceCn': element.suitSellingPriceCn}, {'tempSuitPreferentialPrice':element.suitPreferentialPrice})
-                           });
-                       });
-                   };
+                $scope.getCombinedProductInfo = function () {
+                    var cartId = $scope.vm.product.cartId,
+                        numID = $scope.vm.product.numID;
+
+                    if (!cartId || !numID) {
+                        return;
+                    }
+                    combinedProductService.getCombinedProductDetail({
+                        "cartId": cartId,
+                        "numID": numID
+                    }).then(function (resp) {
+                        $scope.vm.product = resp.data.product == null ? {} : resp.data.product;
+                        // carts集合中cart为string, product为int
+                        $scope.vm.product.cartId = $scope.vm.product.cartId == null ? "" : $scope.vm.product.cartId + "";
+                        _.each($scope.vm.product.skus, function (element) {
+                            _.extend(element, {'tempSuitSellingPriceCn': element.suitSellingPriceCn}, {'tempSuitPreferentialPrice': element.suitPreferentialPrice})
+                        });
+                    });
+                };
 
                 $scope.getSkuDetail = function (sku, skuItem) {
                     if (!skuItem || !skuItem.skuCode) {
@@ -62,6 +66,7 @@ define([
                 $scope.copySkuItem = function (skuItems) {
                     if (!skuItems || skuItems.length == 0)
                         return;
+
                     skuItems.push({});
                 };
 
@@ -69,6 +74,7 @@ define([
                     if (!sku || !sku.skuItems || sku.skuItems.length <= 1) {
                         return;
                     }
+
                     sku.skuItems.splice(index, 1);
                     dynamicSkuPrice(sku);
                 };
@@ -76,6 +82,7 @@ define([
                 function dynamicSkuPrice(sku) {
                     // 动态统计套装组合SKU【组合套装优惠售价 合计】
                     var tempSuitPreferentialPrice = 0;
+
                     if (!sku || !sku.skuItems || sku.skuItems.length == 0) {
                         tempSuitPreferentialPrice = 0;
                     } else {
@@ -99,7 +106,7 @@ define([
                 };
 
                 $scope.editSubmit = function (status) {
-                    combinedProductService.edit(_.extend($scope.vm.product, {"status": status})).then(function (resp) {
+                    combinedProductService.edit(_.extend($scope.vm.product, {"status": status})).then(function () {
                         $scope.$close();
                     });
                 };
