4d804458dd74de1d7d87d93f44e2701b841b48e9
edward <edward.lin@voyageone.cn>
2016-12-06 20:00:03 +0800

    filter修改导致异常

--- voyageone-static/2.0/develop/components/angular/filter/stringCutter.filter.js
+++ voyageone-static/2.0/develop/components/angular/filter/stringCutter.filter.js
@@ -1,23 +1,22 @@
 /**
  * Created by sofia on 2016/7/22.
  */
-(function() {
-    angular.module("voyageone.angular.filter", []).filter("stringCutter", function() {
-        return function (value, wordWise, max, tail) {
-            if (!value) return '';
 
-            max = parseInt(max, 10);
-            if (!max) return value;
-            if (value.length <= max) return value;
+angular.module("voyageone.angular.filter", []).filter("stringCutter", function() {
+    return function (value, wordWise, max, tail) {
+        if (!value) return '';
 
-            value = value.substr(0, max);
-            if (wordWise) {
-                var lastSpace = value.lastIndexOf(' ');
-                if (lastSpace != -1) {
-                    value = value.substr(0, lastSpace);
-                }
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordWise) {
+            var lastSpace = value.lastIndexOf(' ');
+            if (lastSpace != -1) {
+                value = value.substr(0, lastSpace);
             }
-            return value + (tail || ' …');
-        };
-    });
-})();
+        }
+        return value + (tail || ' …');
+    };
+});
