284806d8b1107589b69486ca12279e1644ac63c3
wenjie.piao@voyageone.cn <wj7405049>
2016-12-06 19:10:37 +0800

    高级检索   添加美国平台类目显示

--- voyageone-static/2.0/develop/modules/cms/directives/platFormStatus.directive.js
+++ voyageone-static/2.0/develop/modules/cms/directives/platFormStatus.directive.js
@@ -23,19 +23,23 @@ define([
             this.statusData = null;
             //cms对应状态
             this.statusDes = {
-                "WaitingPublish":"等待上新",
-                "InStock":"在库",
-                "OnSale":"在售"
+                "WaitingPublish":"待",
+                "InStock":"库",
+                "OnSale":"售"
             },
             //平台对应状态
             this.pStatusDes = {
-                "InStock":"未上架",
-                "OnSale":"已上架"
+                "InStock":"未",
+                "OnSale":"已"
             }
         }
 
         StatusController.prototype.init = function(){
             this.statusData = this.$scope.data;
+            var _numberId = this.statusData.numberId,
+                _cartId = +this.statusData.cartId,
+                _cartInfo = carts.valueOf(_cartId);
+
             //对Error进行处理
             if(!this.statusData)
                 return;
@@ -43,23 +47,22 @@ define([
             if(this.statusData.pPublishError)
                 this.statusData.pStatus = "Error";
 
-            var _numberId = this.statusData.numberId,
-                _cartId = +this.statusData.cartId;
-
             if(_numberId){
-
                 if(_cartId != 27){
-                    this.statusData.detailUrl = carts.valueOf(_cartId).pUrl + _numberId;
+                    this.statusData.detailUrl = _cartInfo.pUrl + _numberId;
                 }else{
-                    this.statusData.detailUrl =carts.valueOf(_cartId).pUrl + _numberId + ".html";
+                    this.statusData.detailUrl =_cartInfo.pUrl + _numberId + ".html";
                 }
 
             }
+
+            this._cartInfo = _cartInfo;
 
             /**由于cms的商品状态和平台的商品状态可能不一致
              * 条件1：两者状态相同时只显示cms的商品状态
              * 条件2：两者状态不一致时两者状态都要显示*/
             this.pStatusDesc = this.statusDes[this.statusData.pStatus];
+
             if(this.statusData.pReallyStatus && this.statusData.pStatus != this.statusData.pReallyStatus){
                 this.pReallyStatusDesc = this.pStatusDes[this.statusData.pReallyStatus];
             }
@@ -73,13 +76,13 @@ define([
             controllerAs: 'ctrl',
             template: "<span class='plateform-status' ng-if='ctrl.statusData.status != \"Approved\"'>"
                     + "<span class='label' ng-class='{\"pending\": ctrl.statusData.status == \"Pending\",\"ready\":ctrl.statusData.status == \"Ready\" }' title='{{ctrl.statusData.status}}'>"
-                    + "<span style='color:black'>{{ctrl.statusData.cartName}}</span>"
+                    + "<span style='color:black'>{{ctrl._cartInfo.name}}</span>"
                     + "</span>"
                     + "</span>"
                     + "<span class='plateform-status' ng-if='ctrl.statusData.status == \"Approved\"'>"
                     + "<span class='label' ng-class='{\"waiting-publish\": ctrl.statusData.pStatus == \"WaitingPublish\",\"in-stock\": ctrl.statusData.pStatus == \"InStock\",\"on-sale\":ctrl.statusData.pStatus == \"OnSale\",\"error\":ctrl.statusData.pStatus == \"Error\"}'  title='{{ctrl.statusData.pStatus}}'>"
                     + "<span ng-if='ctrl.statusData.numberId'><a ng-href='{{ctrl.statusData.detailUrl}}' target='_blank' style='color: #000;text-decoration: none;'>{{ctrl.statusData.cartName}}:{{ctrl.pStatusDesc}}{{ctrl.pReallyStatusDesc?'-'+ctrl.pReallyStatusDesc:''}}</a></span>"
-                    + "<span ng-if='!ctrl.statusData.numberId'>{{ctrl.statusData.cartName}}:{{ctrl.pStatusDesc}}{{ctrl.pReallyStatusDesc?'-'+ctrl.pReallyStatusDesc:''}}</span>"
+                    + "<span ng-if='!ctrl.statusData.numberId'>{{ctrl._cartInfo.name}}:{{ctrl.pStatusDesc}}{{ctrl.pReallyStatusDesc?'-'+ctrl.pReallyStatusDesc:''}}</span>"
                     + "</span>"
                     + "</span>",
             link: function ($scope) {
