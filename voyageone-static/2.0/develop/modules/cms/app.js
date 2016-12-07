9dbb4b72d3a4197523643e30e461bfe4e44af3cc
sunpeitao <peitaosun@voyageone.cn>
2016-12-07 19:39:46 +0800

    菜单

--- voyageone-static/2.0/develop/modules/cms/app.js
+++ voyageone-static/2.0/develop/modules/cms/app.js
@@ -459,6 +459,7 @@ define([
                 $rootScope.platformType = {cTypeId: cType.add_name2, cartId: cType.value};
                 $scope.menuInfo.categoryTreeList = data.categoryTreeList;
                 $rootScope.productUrl = data.productUrl;
+                console.log($scope.menuInfo.categoryTreeList);
             });
         }
 
