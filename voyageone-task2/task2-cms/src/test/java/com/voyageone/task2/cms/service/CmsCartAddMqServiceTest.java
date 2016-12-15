f7270b9c002067d7f4fb9aa7ee808717bbc46567
james <james.li@voyageone.cn>
2016-12-15 10:24:25 +0800

    改名

--- voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/CmsCartAddMqServiceTest.java
+++ voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/CmsCartAddMqServiceTest.java
@@ -9,17 +9,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by james on 2016/12/9.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:context-cms-test.xml")
-public class CmsCartAppMqServiceTest {
+public class CmsCartAddMqServiceTest {
 
     @Autowired
-    CmsCartAppMqService cmsCartAppMqService;
+    CmsCartAddMqService cmsCartAddMqService;
     @Test
     public void onStartup(){
         Map<String,Object> map = new HashMap<>();
@@ -27,7 +25,7 @@ public class CmsCartAppMqServiceTest {
         map.put("cartId",928);
 
         try {
-            cmsCartAppMqService.onStartup(map);
+            cmsCartAddMqService.onStartup(map);
         } catch (Exception e) {
             e.printStackTrace();
         }
