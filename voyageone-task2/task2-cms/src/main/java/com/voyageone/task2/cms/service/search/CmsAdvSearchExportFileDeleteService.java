6295012d760abeef56b904cb875370cc23a2c5de
rex.wu <rex.wu@voyageone.cn>
2016-12-15 15:24:59 +0800

    CMSDOC-336

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/search/CmsAdvSearchExportFileDeleteService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/search/CmsAdvSearchExportFileDeleteService.java
@@ -66,17 +66,21 @@ public class CmsAdvSearchExportFileDeleteService extends BaseCronTaskService {
                     }else if (task.getTaskType() != null && task.getTaskType().intValue() == CmsBtExportTaskService.ADV_SEARCH) {
                         filePath = advSearchExportPath + task.getFileName();
                     }
+                    boolean exists = true;
                     File file = new File(filePath);
                     if (file.isFile() && file.exists()) {
                         boolean deleted = file.delete();
-                        if (deleted) {
-                            CmsBtExportTaskModel target = new CmsBtExportTaskModel();
-                            target.setStatus(-1); // 导出文件已被系统定期删除
-                            target.setId(task.getId());
-                            target.setComment("文件过期，系统删除。");
-                            target.setModifier("SYSTEM");
-                            cmsBtExportTaskDao.update(target);
-                        }
+                        exists = !deleted;
+                    }else {
+                        exists = false;
+                    }
+                    if (!exists) {
+                        CmsBtExportTaskModel target = new CmsBtExportTaskModel();
+                        target.setStatus(-1); // 导出文件已被系统定期删除
+                        target.setId(task.getId());
+                        target.setComment("文件过期");
+                        target.setModifier("SYSTEM");
+                        cmsBtExportTaskDao.update(target);
                     }
                 }
             }
