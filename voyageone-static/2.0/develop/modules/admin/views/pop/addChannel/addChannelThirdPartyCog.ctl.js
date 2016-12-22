ae8d9c4bf8f450cdd27ef97c233ba81b69ef04a4
sofia <sofia.yu@voyageone.cn>
2016-12-22 17:30:30 +0800

    refreash list

--- voyageone-static/2.0/develop/modules/admin/views/pop/addChannel/addChannelThirdPartyCog.ctl.js
+++ voyageone-static/2.0/develop/modules/admin/views/pop/addChannel/addChannelThirdPartyCog.ctl.js
@@ -54,11 +54,19 @@ define([
                 _.extend(self.context, self.sourceData);
                 if (self.append == true) {
                     self.thirdPartyConfigService.addThirdPartyConfig(self.sourceData).then(function (res) {
+                        if (res.data == false) {
+                            self.confirm(res.data.message);
+                            return;
+                        }
                         _.extend(result, {'res': 'success', 'sourceData': self.context});
                         self.$uibModalInstance.close(result);
                     })
                 } else {
                     self.thirdPartyConfigService.updateThirdPartyConfig(self.sourceData).then(function (res) {
+                        if (res.data == false) {
+                            self.confirm(res.data.message);
+                            return;
+                        }
                         _.extend(result, {'res': 'success', 'sourceData': self.context});
                         self.$uibModalInstance.close(result);
                     })
