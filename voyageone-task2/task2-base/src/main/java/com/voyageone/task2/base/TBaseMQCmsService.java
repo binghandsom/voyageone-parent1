7f891142066d395e937fb5bd0051c0b425f0e629
sunpeitao <peitaosun@voyageone.cn>
2016-12-26 17:22:41 +0800

    重构项目 CmsRoutingKey

--- voyageone-task2/task2-base/src/main/java/com/voyageone/task2/base/TBaseMQCmsService.java
+++ voyageone-task2/task2-base/src/main/java/com/voyageone/task2/base/TBaseMQCmsService.java
@@ -3,6 +3,8 @@ package com.voyageone.task2.base;
 import com.voyageone.common.components.issueLog.enums.SubSystem;
 import com.voyageone.components.rabbitmq.bean.IMQMessageBody;
 import com.voyageone.components.rabbitmq.service.IMQJobLog;
+import com.voyageone.task2.base.Enums.TaskControlEnums;
+import com.voyageone.task2.base.util.TaskControlUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Date;
 /**
@@ -21,30 +23,33 @@ public abstract class TBaseMQCmsService<TMQMessageBody  extends IMQMessageBody> 
         return getClass().getSimpleName();
     }
 
-    @Autowired(required=false)
+    //是否记录Job日志
+    public boolean isMQJobLog() {
+      //  cfg_name = 'run_flg'  cfg_val2=1 全量记录mq处理日志   默认只记录异常日志
+        String val2 = TaskControlUtils.getVal2(taskControlList, TaskControlEnums.Name.run_flg, "1");
+        return "1".equals(val2);
+    }
+    @Autowired()
     IMQJobLog logService;
 
     @Override
     public void startup(TMQMessageBody messageBody) throws Exception {
-
         Date beginDate = new Date();
-
         try {
             onStartup(messageBody);
+            if (isMQJobLog()) {
+                log(messageBody, null, beginDate, new Date());
+            }
         }
         //特殊异常 抛出处理
         catch (Exception e) {
-            Date endDate = new Date();
-            e.getMessage();
-            e.getStackTrace();
-            if(logService!=null) {
-                logService.log(messageBody, e, beginDate, new Date());
-            }
-            //e.toString();
-            /*
-  1.基类异常处理
-  2. cfg_name = 'run_flg'  cfg_val2=1 全量记录mq处理日志   默认只记录异常日志
-  * */
+            log(messageBody, e, beginDate, new Date());
+        }
+    }
+
+    private void log(IMQMessageBody messageBody, Exception ex, Date beginDate, Date endDate) {
+        if (logService != null) {
+            logService.log(messageBody, null, beginDate, new Date());
         }
     }
 }
