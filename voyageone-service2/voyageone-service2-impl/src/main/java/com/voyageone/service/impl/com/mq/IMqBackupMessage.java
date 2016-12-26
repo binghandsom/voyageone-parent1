2f4f341bdf8351c3715c99da890b51447f3e685b
sunpeitao <peitaosun@voyageone.cn>
2016-12-26 10:04:05 +0800

    重构项目 voyageone-components-rabbitmq

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/com/mq/IMqBackupMessage.java
+++
@@ -1,8 +0,0 @@
-package com.voyageone.service.impl.com.mq;
-
-/**
- * Created by dell on 2016/12/23.
- */
-public interface IMqBackupMessage {
-     void addBackMessage(String routingKey, Object messageMap);
-}
