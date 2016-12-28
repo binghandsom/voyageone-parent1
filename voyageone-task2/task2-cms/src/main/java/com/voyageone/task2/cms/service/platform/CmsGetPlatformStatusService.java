568f501dff46c163956991375b4c084d845f4050
james <james.li@voyageone.cn>
2016-12-28 15:48:33 +0800

    doc-410

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/platform/CmsGetPlatformStatusService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/platform/CmsGetPlatformStatusService.java
@@ -144,7 +144,7 @@ public class CmsGetPlatformStatusService extends BaseCronTaskService {
                     rsList = null;
                     try {
                         // 查询下架
-                        rsList = tbSaleService.getInventoryProduct(channelId, cartIdStr, pageNo++, 200L);
+                        rsList = tbSaleService.getInventoryProduct(channelId, cartIdStr, "for_shelved,sold_out", pageNo++, 200L);
                     } catch (ApiException apiExp) {
                         $error(String.format("调用淘宝API获取下架商品时API出错 channelId=%s, cartId=%s", channelId, cartIdStr), apiExp);
                         break;
@@ -155,6 +155,25 @@ public class CmsGetPlatformStatusService extends BaseCronTaskService {
                     if (rsList != null && rsList.size() > 0) {
                         List<String> numIIdList = rsList.stream().map(tmItem -> tmItem.getNumIid().toString()).collect(Collectors.toList());
                         savePlatfromSts(channelId, cartId, numIIdList, CmsConstants.PlatformStatus.InStock.name());
+                    }
+                } while (rsList != null && rsList.size() == 200);
+
+                pageNo = 1;
+                do {
+                    rsList = null;
+                    try {
+                        // 查询下架
+                        rsList = tbSaleService.getInventoryProduct(channelId, cartIdStr, "violation_off_shelf", pageNo++, 200L);
+                    } catch (ApiException apiExp) {
+                        $error(String.format("调用淘宝API获取下架商品时API出错 channelId=%s, cartId=%s", channelId, cartIdStr), apiExp);
+                        break;
+                    } catch (Exception exp) {
+                        $error(String.format("调用淘宝API获取下架商品时出错 channelId=%s, cartId=%s", channelId, cartIdStr), exp);
+                        break;
+                    }
+                    if (rsList != null && rsList.size() > 0) {
+                        List<String> numIIdList = rsList.stream().map(tmItem -> tmItem.getNumIid().toString()).collect(Collectors.toList());
+                        savePlatfromSts(channelId, cartId, numIIdList, CmsConstants.PlatformStatus.Violation.name());
                     }
                 } while (rsList != null && rsList.size() == 200);
 
