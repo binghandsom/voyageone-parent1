83df18a9452ef7948fdd34ea1cfa86c88c0de9f2
rex.wu <rex.wu@voyageone.cn>
2016-12-28 10:30:30 +0800

    CMSDOC-397

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/CmsBtCombinedProductService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/CmsBtCombinedProductService.java
@@ -106,11 +106,11 @@ public class CmsBtCombinedProductService extends BaseService {
      */
     public CmsBtCombinedProductModel getCombinedProductPlatformDetail(String numId, String channelId, Integer cartId, boolean local) {
         ShopBean shopBean = Shops.getShop(channelId, cartId);
-        shopBean.setAppKey("21008948");
+        /*shopBean.setAppKey("21008948");
         shopBean.setApp_url("http://gw.api.taobao.com/router/rest");
         shopBean.setAppSecret("0a16bd08019790b269322e000e52a19f");
         shopBean.setSessionKey("620230429acceg4103a72932e22e4d53856b145a192140b2854639042");
-        shopBean.setShop_name("Target海外旗舰店");
+        shopBean.setShop_name("Target海外旗舰店");*/
         if (shopBean != null) {
             long threadNo = Thread.currentThread().getId();
             $info("threadNo:" + threadNo + " numiid:" + numId );
@@ -237,7 +237,7 @@ public class CmsBtCombinedProductService extends BaseService {
                             for (TmallItemCombineSubSku subSku:tmallItemCombineSku.getSubSkuList()) {
                                 CmsBtCombinedProductModel_Sku_Item skuItem = new CmsBtCombinedProductModel_Sku_Item();
                                 skuItem.setSkuCode(subSku.getOuterId());
-                                //skuItem.setPreferentialPrice(Double.valueOf(subSku.getPrice()));
+                                skuItem.setPreferentialPrice(Double.valueOf(subSku.getPrice()));
                                 // 循环subItemList
                                 String ratioVal = "";
                                 for (TmallItemCombinedSubItem subItem:tmallItemCombine.getSubItemList()) {
@@ -265,7 +265,7 @@ public class CmsBtCombinedProductService extends BaseService {
                 for (TmallItemCombineSubSku subSku:tmallItemCombineSku.getSubSkuList()) {
                     CmsBtCombinedProductModel_Sku_Item skuItem = new CmsBtCombinedProductModel_Sku_Item();
                     skuItem.setSkuCode(subSku.getOuterId()); // 真实skuCode
-                    //skuItem.setPreferentialPrice(Double.valueOf(subSku.getPrice()));
+                    skuItem.setPreferentialPrice(Double.valueOf(subSku.getPrice()));
                     // 循环subItemList
                     String ratioVal = "";
                     for (TmallItemCombinedSubItem subItem:tmallItemCombine.getSubItemList()) {
