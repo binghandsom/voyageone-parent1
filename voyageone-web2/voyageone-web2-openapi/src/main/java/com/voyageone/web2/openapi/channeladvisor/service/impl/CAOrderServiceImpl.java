f19eddbb8f5d48d85849f61e8f10183ffb877e21
aooer <bingbing.gao@voyageone.cn>
2016-12-30 14:05:23 +0800

    fix bugs

--- voyageone-web2/voyageone-web2-openapi/src/main/java/com/voyageone/web2/openapi/channeladvisor/service/impl/CAOrderServiceImpl.java
+++ voyageone-web2/voyageone-web2-openapi/src/main/java/com/voyageone/web2/openapi/channeladvisor/service/impl/CAOrderServiceImpl.java
@@ -513,7 +513,7 @@ public class CAOrderServiceImpl extends CAOpenApiBaseService implements CAOrderS
         }
 
         if (!orderID.equals(request.getOrderId())) {
-            throw new CAApiException(ErrorIDEnum.InvalidRequiredParameter, "OrderID is inconsistent.");
+            throw new CAApiException(ErrorIDEnum.InvalidRequiredParameter, "OrderID in the request endpoint and the request body are not the same. Please double check the OrderID.");
         }
 
         if (CollectionUtils.isEmpty(request.getItems())) {
@@ -754,7 +754,7 @@ public class CAOrderServiceImpl extends CAOpenApiBaseService implements CAOrderS
         }
 
         if (!orderID.equals(request.getOrderId())) {
-            throw new CAApiException(ErrorIDEnum.InvalidRequiredParameter, "OrderID is inconsistent.");
+            throw new CAApiException(ErrorIDEnum.InvalidRequiredParameter, "OrderID in the request endpoint and the request body are not the same. Please double check the OrderID.");
         }
 
         if (CollectionUtils.isEmpty(request.getItems())) {
