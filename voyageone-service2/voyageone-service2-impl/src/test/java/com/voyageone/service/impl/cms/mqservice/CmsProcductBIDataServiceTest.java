b21e0983baf41bf5fbbfb735bcdf6dd57d326101
sunpeitao <peitaosun@voyageone.cn>
2017-01-04 16:56:47 +0800

    CmsBrandBlockMQJob 重构

---
+++ voyageone-service2/voyageone-service2-impl/src/test/java/com/voyageone/service/impl/cms/mqservice/CmsProcductBIDataServiceTest.java
@@ -0,0 +1,26 @@
+package com.voyageone.service.impl.cms.mqservice;
+
+import com.voyageone.components.rabbitmq.exception.MQMessageRuleException;
+import com.voyageone.service.impl.cms.vomqservice.CmsProcductBIDataService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by dell on 2017/1/4.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:test-context-service2.xml")
+public class CmsProcductBIDataServiceTest {
+
+    @Autowired
+    CmsProcductBIDataService service;
+
+     @Test
+    public  void  test() throws MQMessageRuleException {
+        service.sendMessage("010",23,"test");
+    }
+}
