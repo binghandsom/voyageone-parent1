4beaf6f1d24027929742508334bc8c5f37368d13
desmond <desmond.li@voyageone.cn>
2017-01-05 16:35:56 +0800

    修改税号个人值不对时，报出错误

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsBuildPlatformProductUploadJMService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/CmsBuildPlatformProductUploadJMService.java
@@ -1511,7 +1511,7 @@ public class CmsBuildPlatformProductUploadJMService extends BaseCronTaskService 
             String[] hscodeArray = hscode.split(",");
             // 税号个人(8位)正确的值应该是"04020400,腰带,条"
             if (hscodeArray == null || hscodeArray.length < 3) {
-                String errMsg = String.format("该产品(%s)税号个人(8位)值(%s)的格式不对，正确的格式应该是\"04020400,腰带,条\"，请修改好后再上新!", productCode, hscode);
+                String errMsg = String.format("该产品(%s)\"税号个人(8位)\"值(%s)的格式不对，正确的格式应该是\"04020400,腰带,条\"，请修改好后再上新!", productCode, hscode);
                 $error(errMsg);
                 throw new BusinessException(errMsg);
             }
