95c930be9a40a9c449f315c6f5c3210d3af48024
rex.wu <rex.wu@voyageone.cn>
2017-01-11 16:49:17 +0800

    CMSDOC-395

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/mqjob/CmsAdvSearchExportMQJob.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/mqjob/CmsAdvSearchExportMQJob.java
@@ -1,14 +1,20 @@
 package com.voyageone.task2.cms.mqjob;
 
+import com.voyageone.base.exception.BusinessException;
+import com.voyageone.common.util.JacksonUtil;
 import com.voyageone.service.enums.cms.OperationLog_Type;
 import com.voyageone.service.impl.cms.CmsBtExportTaskService;
 import com.voyageone.service.impl.cms.vomq.vomessage.body.AdvSearchExportMQMessageBody;
 import com.voyageone.service.model.cms.CmsBtExportTaskModel;
 import com.voyageone.task2.cms.service.product.batch.CmsAdvSearchExportFileService;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 高级检索文件导出job
@@ -37,6 +43,18 @@ public class CmsAdvSearchExportMQJob extends TBaseMQCmsService<AdvSearchExportMQ
             this.cmsLog(messageBody, OperationLog_Type.parameterException, String.format("cms.bt.export.task(id=%s)不存在", cmsBtExportTaskId));
             return;
         }
-        cmsAdvSearchExportFileService.export(messageBody);
+        try {
+            List<Map<String, String>> failList = cmsAdvSearchExportFileService.export(messageBody);
+            if (CollectionUtils.isNotEmpty(failList)) {
+                cmsLog(messageBody, OperationLog_Type.successIncludeFail, JacksonUtil.bean2Json(failList));
+            }
+        } catch (Exception e) {
+            if (e instanceof BusinessException) {
+                cmsLog(messageBody, OperationLog_Type.businessException, e.getMessage());
+            } else {
+                cmsLog(messageBody, OperationLog_Type.unknownException, e.getMessage());
+            }
+        }
+
     }
 }
