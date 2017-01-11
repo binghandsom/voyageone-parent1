b75d4e761189e9cb2d28d8d4ccdf08b12e7b43e6
gjl <gump.gan@voyageone.cn>
2017-01-11 11:01:07 +0800

    395

--- voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/enums/cms/OperationLog_Type.java
+++ voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/enums/cms/OperationLog_Type.java
@@ -4,14 +4,24 @@ package com.voyageone.service.enums.cms;
  * Created by dell on 2016/12/27.
  */
 public enum OperationLog_Type {
-    success(1,"成功"),
+    success(1, "成功"),
     successIncludeFail(2),
     parameterException(3),
     configException(4),
     businessException(5),
     unknownException(6);
+
     short id;
+
     String name;
+
+    public short getId() {
+        return id;
+    }
+
+    public void setId(short id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -23,28 +33,20 @@ public enum OperationLog_Type {
 
     OperationLog_Type(int id, String name) {
         this.id = (short) id;
-        this.name=name;
+        this.name = name;
     }
+
     OperationLog_Type(int id) {
         this.id = (short) id;
     }
 
-    public short getId() {
-        return id;
-    }
+    public static OperationLog_Type get(short id) {
 
-    public void setId(short id) {
-        this.id = id;
-    }
-
-    public static OperationLog_Type get(short id)
-    {
         for (OperationLog_Type operationLog_type : OperationLog_Type.values()) {
-            if(operationLog_type.getId()==id)
-            {
-                return  operationLog_type;
+            if (operationLog_type.getId() == id) {
+                return operationLog_type;
             }
         }
-        return  null;
+        return null;
     }
 }
