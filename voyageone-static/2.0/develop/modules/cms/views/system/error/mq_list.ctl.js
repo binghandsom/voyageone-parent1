672cb0af8ea940b5703208ab31fe0c98e40ffead
gjl <gump.gan@voyageone.cn>
2017-01-13 11:31:37 +0800

    395

--- voyageone-static/2.0/develop/modules/cms/views/system/error/mq_list.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/system/error/mq_list.ctl.js
@@ -3,12 +3,14 @@
  */
 
 define(['cms'], function (cms) {
+
     cms.controller("mqListSetController", (function () {
         function mqListSetController($mqErrorListService) {
             this.$mqErrorListService = $mqErrorListService;
             this.mqErrorPageOption = {curr: 1, total: 0, size: 10, fetch: this.search.bind(this)};
             this.name = "";
             this.title = "";
+            this.typeStatus = {};
         }
 
         /**
@@ -19,16 +21,51 @@ define(['cms'], function (cms) {
             self.search();
         };
 
+        mqListSetController.prototype.clear = function () {
+            this.name = '';
+            this.title = '';
+            this.typeStatus = {};
+        };
+
         mqListSetController.prototype.search = function () {
             var self = this,
-                data = this.mqErrorPageOption;
-            _.extend(data, {"name": this.name});
-            _.extend(data, {"title": this.title});
+                _typeValue = [],
+                data = self.mqErrorPageOption;
+
+            _.forEach(self.typeStatus, function (value, key) {
+                if (value) {
+                    _typeValue.push(key);
+                }
+            });
+
+            _.extend(data, {
+                "name": this.name,
+                "title": this.title,
+                "type": this.type,
+                "typeValue": _typeValue
+            });
+
             self.$mqErrorListService.search(data).then(function (res) {
                 self.mqErrorList = res.data.mqErrorList;
                 self.mqErrorPageOption.total = res.data.mqErrorCnt;
+                self.type = res.data.type;
+            });
+
+        };
+
+        mqListSetController.prototype.getStatusName = function(id){
+            var self = this,
+                stateList = self.type;
+
+            if(!id)
+                return;
+
+            return _.find(stateList,function(ele){
+                    return ele.id === id;
             });
         };
+
         return mqListSetController;
+
     })());
 });
