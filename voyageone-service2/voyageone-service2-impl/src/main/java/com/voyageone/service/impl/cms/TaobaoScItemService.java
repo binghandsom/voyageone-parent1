858b26db4d8bead82eaed89d7de7d3612927eecf
tom <tom.zhu@voyageone.cn>
2017-02-10 13:48:25 +0800

    创建货品关联之前， 一定要睡一会儿， 因为如果商品都已经创建成功并初始化过的话， 就会连续创建关联， 可能会导致IC_OPTIMISTIC_LOCKING_CONFLICT

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/TaobaoScItemService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/TaobaoScItemService.java
@@ -382,7 +382,7 @@ public class TaobaoScItemService extends BaseService {
 		// 创建关联
 		String outerCodeResult;
 		try {
-			Thread.sleep(3000); // 一定要睡一会儿， 因为如果商品都已经创建成功并初始化过的话， 就会连续创建关联， 可能会导致IC_OPTIMISTIC_LOCKING_CONFLICT
+			Thread.sleep(1000); // 一定要睡一会儿， 因为如果商品都已经创建成功并初始化过的话， 就会连续创建关联， 可能会导致IC_OPTIMISTIC_LOCKING_CONFLICT
 			if (StringUtils.isEmpty(sku_id)) {
 				outerCodeResult = tbScItemService.addScItemMap(shopBean, numIId, null, sku_outerId);
 			} else {
