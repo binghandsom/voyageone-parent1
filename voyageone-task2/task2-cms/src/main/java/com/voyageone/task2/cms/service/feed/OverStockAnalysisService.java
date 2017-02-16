eb3985bd56f100656afcfed5d7b236826b8194df
james <james.li@voyageone.cn>
2017-02-16 12:11:06 +0800

    bug fix

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/feed/OverStockAnalysisService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/feed/OverStockAnalysisService.java
@@ -41,7 +41,7 @@ public class OverStockAnalysisService extends BaseAnalysisService {
     @Autowired
     private OverstockProductService overstockProductService;
 
-    private Integer pageIndex = 0;
+    private Integer pageIndex = 1990;
 
     private long lastExecuteTime = 0;
 
