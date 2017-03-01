fbec2926e8a3b8dfd92053be43e9b0d3b8231244
wenjie.piao@voyageone.cn <wj7405049>
2017-03-01 13:58:52 +0800

    产品详情页   feed-tab修改

--- voyageone-static/2.0/develop/modules/cms/views/product/feed.component.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/product/feed.component.ctl.js
@@ -4,53 +4,63 @@
  */
 define([
     'cms'
-],function(cms) {
-    cms.directive("feedSchema", function (productDetailService,notify,alert) {
+], function (cms) {
+    cms.directive("feedSchema", function () {
+
+        function feedComponentCtl($scope, $attrs, $element, productDetailService, notify, alert) {
+            this.$scope = $scope;
+            this.$attrs = $attrs;
+            this.$element = $element;
+            this.productDetailService = productDetailService;
+            this.notify = notify;
+            this.alert = alert;
+        }
+
+        feedComponentCtl.prototype.updateFeedInfo = function () {
+            var self = this,
+                productDetailService = self.productDetailService,
+                $scope = self.$scope;
+
+            if ($scope.feedFrom.$invalid) {
+                self.alert("保存失败，请查看已匹配属性是否填写正确！");
+                self.focusError();
+                return;
+            }
+
+            productDetailService.updateProductAtts({
+                prodId: self.scope.productInfo.productId,
+                feedInfo: self.scope.productInfo.feedInfo
+            }).then(function () {
+                self.notify.success("更新成功!");
+            }, function () {
+                self.alert("更新失败！");
+            });
+        };
+
+        /**
+         * 右侧导航栏
+         * @param index div的index
+         * @param speed 导航速度 ms为单位
+         */
+        feedComponentCtl.prototype.pageAnchor = function (area, speed) {
+            var offsetTop = 0;
+            if (area != "attribute")
+                offsetTop = element.find("#" + area).offset().top;
+            $("body").animate({scrollTop: offsetTop - 70}, speed);
+        };
+
+        feedComponentCtl.prototype.focusError = function () {
+            var firstError = element.find("input.ng-invalid:first");
+            firstError.focus();
+            firstError.addClass("focus-error");
+        };
+
         return {
             restrict: "E",
-            templateUrl : "views/product/feed.component.tpl.html",
-            scope: {productInfo: "=productInfo"},
-            link: function (scope,element) {
-                scope.pageAnchor = pageAnchor;
-                scope.updateFeedInfo = updateFeedInfo;
-
-                /**
-                 * feed更新操作  updateProductAtts
-                 */
-
-                function updateFeedInfo(){
-                    if(scope.feedFrom.$invalid){
-                        alert("保存失败，请查看已匹配属性是否填写正确！");
-                        focusError();
-                        return;
-                    }
-
-                    productDetailService.updateProductAtts({prodId:scope.productInfo.productId,feedInfo:scope.productInfo.feedInfo}).then(function(){
-                        notify.success("更新成功!");
-                    },function(){
-                        alert("更新失败！");
-                    });
-                }
-
-                /**
-                 * 右侧导航栏
-                 * @param index div的index
-                 * @param speed 导航速度 ms为单位
-                 */
-                function pageAnchor(area,speed){
-                    var offsetTop = 0;
-                    if(area != "attribute")
-                        offsetTop = element.find("#" + area).offset().top;
-                    $("body").animate({ scrollTop:  offsetTop-70}, speed);
-                }
-
-                function focusError(){
-                    var firstError = element.find("input.ng-invalid:first");
-                    firstError.focus();
-                    firstError.addClass("focus-error");
-                }
-
-            }
+            controller: feedComponentCtl,
+            controllerAs: 'ctrl',
+            templateUrl: "views/product/feed.component.tpl.html",
+            scope: {productInfo: "=productInfo"}
         };
     });
 });
