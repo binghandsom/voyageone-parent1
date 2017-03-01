993df2e49786255f8b8ad3971b3b7e557910c8e8
rongbin.xie <rongbin.xie@voyageone.cn>
2017-03-01 12:51:25 +0800

    控制生成文件的时间选择

--- voyageone-static/2.0/develop/modules/cms/views/bi_report/bi_report_list.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/bi_report/bi_report_list.ctl.js
@@ -3,22 +3,29 @@ define([
     'modules/cms/controller/popup.ctl'
 ], function (_) {
 
-    function indexController($scope, alert, confirm, $translate, cActions, notify, $location, cRoutes, cookieService, biReportService) {
+    function indexController($scope, alert, confirm, $translate, cActions, notify, $location, cRoutes, cookieService, biReportService, $filter) {
         $scope.datePicker = [];
-        $scope.selectFileButton="全选";
-        $scope.vm =
-        {
+        $scope.AllfileShow = true;
+        $scope.selectFileButton = "全选";
+        $scope.fromMinDate = new Date("2015-01-01");
+        $scope.fromMaxDate = new Date();
+        $scope.toMinDate = new Date("2015-01-01");
+        $scope.toMaxDate = new Date();
+        $scope.tips=null;
+        $scope.vm = {
             downloadTaskList: [],
             searchInfo: {
-                channels : [],
-                channelCodeList:[],
-                fileTypes: []
+                channels: [],
+                channelCodeList: [],
+                fileTypes: [],
+                fileTypesTempt: [],
+                endDate: undefined,
+                staDate: undefined
             },
             "message": "successfully!",
             status: {open: true}
         };
-        // $scope.channels = [];
-        $scope.fileTypes = [
+        $scope.fileTypesSource = [
             {fileTypeCode: "1", fileTypeName: "店铺月报"},
             {fileTypeCode: "2", fileTypeName: "店铺周报"},
             {fileTypeCode: "3", fileTypeName: "店铺日报"},
@@ -32,18 +39,46 @@ define([
             {fileTypeCode: "11", fileTypeName: "sku周报"},
             {fileTypeCode: "12", fileTypeName: "sku日报"}
         ];
+
+        $scope.toDate = function (date) {
+            if (!date) return date;
+            if (date instanceof Date) return date;
+            return new Date(date);
+        };
+
+        $scope.getMinDate = function (firstDate, secondDate) {
+            if (!firstDate && !secondDate) return new Date();
+            if (!firstDate) return secondDate;
+            if (!secondDate) return firstDate;
+            if (firstDate < secondDate)
+                return firstDate;
+            return secondDate;
+        };
+
+        $scope.getMaxDate = function (firstDate, secondDate) {
+            if (!firstDate && !secondDate) return new Date();
+            if (!firstDate) return secondDate;
+            if (!secondDate) return firstDate;
+            if (firstDate < secondDate)
+                return secondDate;
+            return firstDate;
+        };
+
+        $scope.setDateFilterConfig = function () {
+        };
         $scope.clear = function () {
-                $scope.vm.searchInfo={};
-                $scope.vm.searchInfo.channelCodeList=[];
+            $scope.vm.searchInfo = {};
+            $scope.vm.searchInfo.channelCodeList = [];
         };
         $scope.initialize = function () {
-            $scope.vm.minDate = new Date(2015,1,1);
+            $scope.AllfileShow = true;
+            $scope.vm.minDate = new Date(2015, 1, 1);
             $scope.vm.maxDate = new Date();
-            biReportService.init().then(function (res) {
+            biReportService.init().then(function () {
                 $scope.search();
             });
             biReportService.get_channel_list().then(function (res) {
-                $scope.channels=res.data;
+                $scope.channels = res.data;
             })
         };
         //分页
@@ -53,7 +88,6 @@ define([
             $scope.dataPageOption.setPageIndex(1);//查询第一页
             //获取页数量,这里需要跨域请求
             biReportService.getCount(pageParameter).then(function (res) {
-                console.log(res.data);
                 $scope.dataPageOption.total = res.data;
             }, function (res) {
             });
@@ -68,80 +102,92 @@ define([
             }, function (res) {
             })
         }
+
         //获取分页参数及其条件
         function getPageParameter() {
             var pageParameter = angular.copy($scope.vm.searchInfo);
-            pageParameter.fileTypes = _.filter(pageParameter.fileTypes, function (subFileType) {
-                return (null != subFileType || false != subFileType || true !=subFileType);
-            });
+            var files = pageParameter.fileTypesTempt;
+            var finalFiles = [];
+            for (var i = 0; i < files.length; i++) {
+                if ($scope.AllfileShow) {
+                    if (false != files[i]  && null != files[i] && undefined != files[i]) {
+                        finalFiles.push(files[i]);
+                    }
+                } else {
+                    if (false != files[i]  && null != files[i] && undefined != files[i] &&  files[i] < 4) {
+                        finalFiles.push(files[i]);
+                    }
+                }
+            }
+            pageParameter.fileTypes = finalFiles;
             return pageParameter;
         }
+
         //绑定grid数据源
         function loadGridDataSource(data) {
             $scope.vm.downloadTaskList = data;
         }
+
         function getDownloadTaskList() {
             biReportService.getDownloadTaskList().then(function (res) {
                 $scope.vm.downloadTaskList = res.data.list;
             });
         }
+
         /* 分页以下*/
         $scope.openOtherDownload = function (item) {
-                if (item.taskStatus != 3) {
-                    alert("文件无法下载，可能是尚未生成，或者已失效");
-                    return;
+            if (item.taskStatus != 3) {
+                alert("文件无法下载，可能是尚未生成，或者已失效");
+                return;
+            }
+            function _exportFileCallback(res) {
+                var obj = JSON.parse(res);
+                if (obj.code == '4004') {
+                    $scope.initialize();
+                    alert("此文件不存在");
                 }
-                function _exportFileCallback(res) {
-                    var obj = JSON.parse(res);
-                    if (obj.code == '4004') {
-                        $scope.initialize();
-                        alert("此文件不存在");
-                    }
-                }
+            }
+
             $.download.post(cActions.cms.biReportService.root + cActions.cms.biReportService.biRepDownload,
                 {
-                    "taskId":item.id,
-                    "fileName":item.fileName,
-                    "exportPath":item.filePath
+                    "taskId": item.id,
+                    "fileName": item.fileName,
+                    "exportPath": item.filePath
                 },
                 _exportFileCallback);
 
-            };
-        $scope.createXlsFileTask = function()
-        {
-            var parameter=getPageParameter();
-            if( parameter.fileTypes.length == 0)
-            {
+        };
+        $scope.createXlsFileTask = function () {
+            var parameter = getPageParameter();
+            if (parameter.fileTypes.length == 0) {
                 alert("请选择文件类型");
                 return;
             }
-            if(!(validateDate(parameter.staDate)&&validateDate(parameter.endDate)))
-            {
+            if (!(validateDate(parameter.staDate) && validateDate(parameter.endDate))) {
                 alert("请选择时间！");
-                return ;
+                return;
             }
-            if(parameter.channelCodeList.length == 0)
-            {
+            if (parameter.channelCodeList.length == 0) {
                 alert("请选择渠道");
-                return ;
+                return;
             }
             confirm("确认生成此文件?").then(function () {
                 // var index = _.indexOf($scope.vm.promotionList, data);
+                console.log(parameter);
                 biReportService.createXlsFileTask(parameter).then(function (res) {
-                    var ecd = res.data.ecd ;
-                    switch (ecd)
-                    {
+                    var ecd = res.data.ecd;
+                    switch (ecd) {
                         case "0":
-                            alert("文件正在生成！");
+                            alert($translate.instant('TXT_BI_DOWNLOAD_LIST_FILE_CREATING'));
                             break;
                         case "4400":
-                            alert("无法找到文件路径！错误代码：" + res.data.ecd );
+                            alert($translate.instant('TXT_BI_DOWNLOAD_LIST_NO_FILEPATH') + res.data.ecd);
                             break;
                         case "4100":
-                            alert("无法连接远程API！错误代码："+ res.data.ecd );
+                            alert($translate.instant('TXT_BI_DOWNLOAD_LIST_NO_CONNECTION') + res.data.ecd);
                             break;
                         default:
-                            alert("生成文件失败，错误代码:" + res.data.ecd );
+                            alert($translate.instant('TXT_BI_DOWNLOAD_LIST_ERROR_CODE') + res.data.ecd);
                             break;
                     }
                     $scope.initialize();
@@ -154,41 +200,106 @@ define([
         }
 
         $scope.del = function (data) {
-            confirm("确认删除此文件？").then(function () {
+            confirm($translate.instant('TXT_BI_DOWNLOAD_LIST_SURE_TO_DELETE')).then(function () {
                 // var index = _.indexOf($scope.vm.promotionList, data);
-                biReportService.deleteTask(data.id).then(function(res){
-                    if(res.data.result) {
-                        alert("删除成功！");
+                biReportService.deleteTask(data.id).then(function (res) {
+                    alert("删除成功！");
+                    if (res.data.result) {
                         $scope.search();
-                    }
-                    else
-                    {
-                        alert("删除成功！");
                     }
                 });
             })
         };
         $scope.selectAllFileTypes = function () {
-            if($scope.selectFileButton == "全选")
-            {
-                if( $scope.vm.searchInfo.channelCodeList.length <= 1)
-                {
-                    $scope.vm.searchInfo.fileTypes = [1,2,3,4,5,6,7,8,9,10,11,12];
+            if ($scope.selectFileButton == "全选") {
+                if ($scope.AllfileShow == true) {
+                    $scope.vm.searchInfo.fileTypesTempt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
                 }
                 else {
-                    $scope.vm.searchInfo.fileTypes = [1,2,3];
+                    $scope.vm.searchInfo.fileTypesTempt = [1, 2, 3];
                 }
                 $scope.selectFileButton = "全不选";
             }
             else {
-                $scope.vm.searchInfo.fileTypes = [];
+                $scope.vm.searchInfo.fileTypesTempt = [];
                 $scope.selectFileButton = "全选";
             }
 
+        };
+        function compareDate(x, sDate, eDate) {
+            if (!(sDate && eDate && x)) return true;
+            var compareSDate = getXMonthChange(x, sDate, eDate);
+            var compareEDate = stringToDate($scope.vm.searchInfo.endDate);
+            return compareEDate < compareSDate;
+        }
+
+        function dateToString(date) {
+            if (date && date instanceof Date) {
+                return $filter("date")(date, "yyyy-MM-dd");
+            } else {
+                if (date)    return date;
+                return "";
+            }
+        }
+
+        function stringToDate(date) {
+            if (date) {
+                return new Date(date);
+            } else {
+                return null;
+            }
+        }
+
+        function getXMonthChange(xMonth, val) {
+            var array = val.split("-");
+            var month = parseInt(array[1]) + xMonth;
+            var date = new Date();
+            var realMonth, realYear;
+            var realDay = array [2];
+            if (month < 1) {
+                realMonth = month + 12;
+                realYear = parseInt(array[0]) - 1;
+                date.setYear(realYear);
+                date.setMonth(realMonth - 1);
+                date.setDate(realDay);
+                return date;
+            } else if (month > 12) {
+                realMonth = month - 12;
+                realYear = parseInt(array[0]) + 1;
+                date.setYear(realYear);
+                date.setMonth(realMonth - 1);
+                date.setDate(realDay);
+                return date;
+            } else {
+                realMonth = month;
+                realYear = array[0];
+                date.setYear(realYear);
+                date.setMonth(realMonth - 1);
+                date.setDate(realDay);
+                return date;
+            }
+        }
+        $scope.fileTypeViewManager = function () {
+            //获取结束时间和开始时间
+            var sDate = $scope.vm.searchInfo.staDate;
+            var eDate = $scope.vm.searchInfo.endDate;
+            var channelsNumber = $scope.vm.searchInfo.channelCodeList.length;
+            var timeMtThreeMonth = compareDate(3, sDate, eDate);
+            if ((channelsNumber == 1 && timeMtThreeMonth == true)|| channelsNumber ==0) {
+                $scope.AllfileShow = true;
+                $scope.tips = null;
+            } else{
+                $scope.AllfileShow = false;
+                $scope.tips=$translate.instant('TXT_BI_DOWNLOAD_LIST_FILE_MAYBE_TOO_BIG_CONTINUE');
+            }
+            var tempEndDate = $scope.toDate($scope.vm.searchInfo.endDate);
+            var tempStaDate = $scope.toDate($scope.vm.searchInfo.staDate);
+            $scope.fromMaxDate = $scope.getMinDate(tempEndDate, new Date());
+            $scope.toMinDate = $scope.getMaxDate(tempStaDate, new Date("2015-01-01"));
         }
     }
 
-    indexController.$inject = ['$scope',"alert", 'confirm', '$translate', 'cActions','notify','$location','cRoutes', 'cookieService','biReportService'];
+    indexController.$inject = ['$scope', "alert", 'confirm', '$translate', 'cActions', 'notify', '$location', 'cRoutes', 'cookieService', 'biReportService', '$filter'];
 
     return indexController;
 });
