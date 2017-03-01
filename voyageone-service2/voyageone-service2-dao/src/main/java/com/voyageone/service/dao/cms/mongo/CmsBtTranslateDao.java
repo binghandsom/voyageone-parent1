15682091aecfb05ce950341c00dbdd632b2a7a8f
james <james.li@voyageone.cn>
2017-03-01 19:07:06 +0800

    chg

--- voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/mongo/CmsBtTranslateDao.java
+++ voyageone-service2/voyageone-service2-dao/src/main/java/com/voyageone/service/dao/cms/mongo/CmsBtTranslateDao.java
@@ -10,36 +10,36 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 /**
- *
  * Created by james on 2017/2/27.
  */
 @Repository
 public class CmsBtTranslateDao extends BaseMongoDao<CmsBtTranslateModel> {
 
-    public CmsBtTranslateModel get(String channelId, Integer customPropType, String name, String valueEn){
+    public CmsBtTranslateModel get(String channelId, Integer customPropType, String name, String valueEn) {
         JongoQuery jongoQuery = new JongoQuery();
         jongoQuery.setQuery(new Criteria("channelId").is(channelId).and("type").is(customPropType).and("name").is(name).and("valueEn").is(valueEn));
         return selectOneWithQuery(jongoQuery);
     }
 
-    public void save(CmsBtTranslateModel cmsBtTranslateModel){
+    public void save(CmsBtTranslateModel cmsBtTranslateModel) {
         mongoTemplate.save(cmsBtTranslateModel);
     }
 
     public List<CmsBtTranslateModel> select(String channelId, Integer customPropType, String name, String valueEn, Integer skip, Integer limit) {
+
         JongoQuery jongoQuery = new JongoQuery();
         Criteria criteria = new Criteria("channelId").is(channelId);
-        if(customPropType != null){
+        if (customPropType != null) {
             criteria.and("type").is(customPropType);
         }
-        if(name != null){
+        if (name != null) {
             criteria.and("name").regex(name);
         }
-        if(valueEn != null){
+        if (valueEn != null) {
             criteria.and("valueEn").regex(valueEn);
         }
         jongoQuery.setQuery(criteria);
-        jongoQuery.setSkip(skip);
+        jongoQuery.setSkip((skip - 1) * limit);
         jongoQuery.setLimit(limit);
         return select(jongoQuery);
     }
@@ -47,13 +47,13 @@ public class CmsBtTranslateDao extends BaseMongoDao<CmsBtTranslateModel> {
     public Long selectCnt(String channelId, Integer customPropType, String name, String valueEn) {
         JongoQuery jongoQuery = new JongoQuery();
         Criteria criteria = new Criteria("channelId").is(channelId);
-        if(customPropType != null){
+        if (customPropType != null) {
             criteria.and("type").is(customPropType);
         }
-        if(name != null){
+        if (name != null) {
             criteria.and("name").regex(name);
         }
-        if(valueEn != null){
+        if (valueEn != null) {
             criteria.and("valueEn").regex(valueEn);
         }
         jongoQuery.setQuery(criteria);
