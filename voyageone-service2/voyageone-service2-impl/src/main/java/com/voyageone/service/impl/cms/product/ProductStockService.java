e409674fb6a3a14c4b5308349cb9b09b83fffa63
wenjie.piao@voyageone.cn <wj7405049>
2017-03-24 18:51:25 +0800

    接收WMS推送过来的渠道库存

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/ProductStockService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/ProductStockService.java
@@ -1,25 +1,60 @@
 package com.voyageone.service.impl.cms.product;
 
+import com.voyageone.common.CmsConstants;
 import com.voyageone.service.impl.BaseService;
+import com.voyageone.service.model.cms.mongo.product.CmsBtProductModel;
 import com.voyageone.task2.cms.bean.stock.CartChangedStockBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 /**
- * @description 处理WMS =》 CMS 的库存
  * @author piao
+ * @description 处理WMS =》 CMS 的库存
  * @date 2017-3-24
  */
 public class ProductStockService extends BaseService {
 
+    @Autowired
+    ProductService productService;
     /**
-     *channelId
-     cartId
-     sku
-     itemCode
-     qty
+     * channelId
+     * cartId
+     * sku
+     * itemCode
+     * qty
      */
-    public void updateProductStock(List<CartChangedStockBean> stockList){
+    public void updateProductStock(List<CartChangedStockBean> stockList) {
+
+        for (CartChangedStockBean stockInfo : stockList) {
+
+            CmsBtProductModel productInfo = productService.getProductByCode(stockInfo.getChannelId(), stockInfo.getItemCode());
+
+            if(productInfo != null){
+
+                StringBuffer updateStr = new StringBuffer();
+
+                /**cartId = 0 : 表示全平台更新,
+                 * cartId != 0 : 表示更新具体平台*/
+                if(stockInfo.getCartId() == 0){
+
+                    productInfo.getPlatforms().forEach((_cart, platform) -> {
+                        Integer cartId = platform.getCartId();
+
+                        if (cartId < CmsConstants.ACTIVE_CARTID_MIN)
+                            return;
+
+                        //updateStr.append(String.format("platforms.P%s.lock", cartId), lock);
+
+                    });
+
+                }else{
+
+                }
+
+            }
+
+        }
 
     }
 
