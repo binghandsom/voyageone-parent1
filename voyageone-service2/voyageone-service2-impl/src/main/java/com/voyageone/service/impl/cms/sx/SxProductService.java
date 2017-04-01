df8b84192f528fb47109dc80c4c5a6014a8e9c38
tom <tom.zhu@voyageone.cn>
2017-04-01 16:40:26 +0800

    京东上新， 让程序更健壮了一点点

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/sx/SxProductService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/sx/SxProductService.java
@@ -4025,7 +4025,7 @@ public class SxProductService extends BaseService {
                     CmsBtProductModel_Field_Image img = new CmsBtProductModel_Field_Image();
 
                     Object v = tmp.get(i);
-                    String s = ((LinkedHashMap<String, String>)v).get(CmsBtProductConstants.FieldImageType.CUSTOM_PRODUCT_IMAGE.name());
+                    String s = ((LinkedHashMap<String, String>)v).get(CmsBtProductConstants.FieldImageType.CUSTOM_PRODUCT_IMAGE.getName());
                     img.put(imageType.getName(), s);
 
                     productImages.add(img);
