9c7f01706006c4a54dca6d2ed99e02704fba3b10
wenjie.piao@voyageone.cn <wj7405049>
2017-04-20 13:24:07 +0800

    master详情tab产品名称长度影响平台tab产品名称长度问题

--- voyageone-static/2.0/develop/components/angular/directives/bytelength.js
+++ voyageone-static/2.0/develop/components/angular/directives/bytelength.js
@@ -32,13 +32,10 @@
                 scope: false,
                 link: function (scope, element, attrs, ngModelController) {
                     var length = attrs[attrName];
-                    if (!length) return;
-
-                    console.log('result:',getByteLength(scope.field.value) + '|' + length);
-                    console.log('real-result:',checkLength(getByteLength(scope.field.value), length));
+                    if (!length)
+                        return;
 
                     if(checkLength(getByteLength(scope.field.value), length)){
-                        console.log(attrName + "=>" + false);
                         ngModelController.$setValidity(attrName,true);
                     }else{
                         ngModelController.$setValidity(attrName,false);
