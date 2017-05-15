2f28f1aa71ede2e2c26dbfc36ab3b88e2ceff29c
james <james.li@voyageone.cn>
2017-05-15 15:41:20 +0800

    chg

--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/SetMainPropService.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/service/SetMainPropService.java
@@ -2703,6 +2703,9 @@ public class SetMainPropService extends VOAbsIssueLoggable {
 
                 return newModel.getImgName();
             } else {
+                if(!StringUtil.isEmpty(findImage.getOriginalUrl())){
+                    findImage.setOriginalUrl(findImage.getOriginalUrl().trim());
+                }
                 // 如果原始图片的地址发生变更则做更新操作
                 if (!originalUrl.equals(findImage.getOriginalUrl())) {
                     findImage.setOriginalUrl(originalUrl);
