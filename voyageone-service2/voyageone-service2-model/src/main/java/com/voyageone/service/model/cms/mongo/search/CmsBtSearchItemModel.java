820a3f14f24bb199709f65fb15a5c5eefdc55040
rex.wu <rex.wu@voyageone.cn>
2017-05-15 14:04:21 +0800

    CMS-2更改

---
+++ voyageone-service2/voyageone-service2-model/src/main/java/com/voyageone/service/model/cms/mongo/search/CmsBtSearchItemModel.java
@@ -0,0 +1,65 @@
+package com.voyageone.service.model.cms.mongo.search;
+
+import com.voyageone.base.dao.mongodb.model.BaseMongoModel;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * CMS检索和下载项分析Model
+ *
+ * @Author rex.wu
+ * @Create 2017-05-12 11:33
+ */
+public class CmsBtSearchItemModel extends BaseMongoModel {
+
+    /**
+     * 渠道
+     */
+    private String channelId;
+    /**
+     * 检索/下载条件项
+     */
+    private List<CmsBtSearchItemModel_SearchItem> searchItems;
+    /**
+     * 排序条件项
+     */
+    private List<String> sortItems;
+    /**
+     * 自定义列项
+     */
+    private Map<String, List<String>> custColumnItems;
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
+    public List<CmsBtSearchItemModel_SearchItem> getSearchItems() {
+        return searchItems;
+    }
+
+    public void setSearchItems(List<CmsBtSearchItemModel_SearchItem> searchItems) {
+        this.searchItems = searchItems;
+    }
+
+    public List<String> getSortItems() {
+        return sortItems;
+    }
+
+    public void setSortItems(List<String> sortItems) {
+        this.sortItems = sortItems;
+    }
+
+    public Map<String, List<String>> getCustColumnItems() {
+        return custColumnItems;
+    }
+
+    public void setCustColumnItems(Map<String, List<String>> custColumnItems) {
+        this.custColumnItems = custColumnItems;
+    }
+}
