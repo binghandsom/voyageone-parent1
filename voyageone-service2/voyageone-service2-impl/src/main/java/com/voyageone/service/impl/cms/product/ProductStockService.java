f9b7b8026fd43b9b481dc5d9821a847c2ec79113
rex.wu <rex.wu@voyageone.cn>
2017-06-19 19:02:13 +0800

    CMS-98

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/ProductStockService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/product/ProductStockService.java
@@ -3,13 +3,21 @@ package com.voyageone.service.impl.cms.product;
 import com.mongodb.BulkWriteResult;
 import com.voyageone.base.dao.mongodb.model.BaseMongoMap;
 import com.voyageone.base.dao.mongodb.model.BulkUpdateModel;
+import com.voyageone.common.CmsConstants;
+import com.voyageone.common.configs.ChannelConfigs;
+import com.voyageone.common.configs.CmsChannelConfigs;
+import com.voyageone.common.configs.beans.CmsChannelConfigBean;
+import com.voyageone.common.util.DateTimeUtil;
 import com.voyageone.common.util.JacksonUtil;
+import com.voyageone.service.bean.cms.product.EnumProductOperationType;
 import com.voyageone.service.bean.cms.stock.CartChangedStockBean;
 import com.voyageone.service.dao.cms.mongo.CmsBtOperationLogDao;
 import com.voyageone.service.dao.cms.mongo.CmsBtProductDao;
 import com.voyageone.service.impl.BaseService;
 import com.voyageone.service.model.cms.mongo.CmsBtOperationLogModel_Msg;
+import com.voyageone.service.model.cms.mongo.product.CmsBtProductGroupModel;
 import com.voyageone.service.model.cms.mongo.product.CmsBtProductModel;
+import com.voyageone.service.model.cms.mongo.product.CmsBtProductModel_Platform_Cart;
 import com.voyageone.service.model.cms.mongo.product.CmsBtProductModel_Sku;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -17,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author piao
@@ -28,6 +37,12 @@ public class ProductStockService extends BaseService {
 
     @Autowired
     ProductService productService;
+    @Autowired
+    private ProductGroupService productGroupService;
+    @Autowired
+    private ProductPlatformService productPlatformService;
+    @Autowired
+    private ProductStatusHistoryService productStatusHistoryService;
     @Autowired
     CmsBtProductDao cmsBtProductDao;
 
@@ -87,7 +102,8 @@ public class ProductStockService extends BaseService {
                         queryMap.put("common.skus.skuCode", stockBean.getSku());
 
                     } else {
-                        if(productModel.getPlatform(stockBean.getCartId()) == null || productModel.getPlatform(stockBean.getCartId()).getSkus() == null){
+                        CmsBtProductModel_Platform_Cart platformCart = productModel.getPlatform(stockBean.getCartId());
+                        if(platformCart == null || platformCart.getSkus() == null){
                             continue;
                         }
                         // cartId不为0，表示更新具体某个平台某个店铺的库存
@@ -104,13 +120,100 @@ public class ProductStockService extends BaseService {
                         updateMap.put(String.format("platforms.P%s.skus.$.qty", stockBean.getCartId()), stockBean.getQty());
                         updateMap.put(String.format("platforms.P%s.quantity", stockBean.getCartId()), quantity);
                         queryMap.put(String.format("platforms.P%s.skus.skuCode", stockBean.getCartId()), stockBean.getSku());
-
                     }
                     try {
 
-                        BulkWriteResult writeResult = cmsBtProductDao.bulkUpdateWithMap(channelId, Arrays.asList(createBulkUpdateModel(updateMap, queryMap)), modifier, "$set");
+                        BulkWriteResult writeResult = cmsBtProductDao.bulkUpdateWithMap(channelId, Collections.singletonList(createBulkUpdateModel(updateMap, queryMap)), modifier, "$set");
 //                        $info(String.format("(channelId=%s, cartId=%d, code=%s, sku=%s)库存更新结果：%s",
 //                                stockBean.getChannelId(), stockBean.getCartId(), stockBean.getItemCode(), stockBean.getSku(), JacksonUtil.bean2Json(writeResult)));
+                        CmsBtProductModel_Platform_Cart platform = null;
+                        if (cartId != null && cartId != 0
+                                && CmsConstants.ProductStatus.Approved.name().equalsIgnoreCase((platform = productModel.getPlatform(cartId)).getStatus())
+                                && quantity == 0) {
+                            // 从cms_mt_channel_config查询AUTO_SWITCH_MASTER_PRODUCT设置，如果config_value1为1则自动切换平台主商品
+                            CmsChannelConfigBean channelConfig = CmsChannelConfigs.getConfigBeanNoCode(channelId, CmsConstants.ChannelConfig.AUTO_SWITCH_MASTER_PRODUCT);
+                            if(channelConfig != null && "1".equals(channelConfig.getConfigValue1())) {
+                                // 查询当前Code所在的Group下cartId库存最大值且Approved
+                                CmsBtProductGroupModel cmsBtProductGroup = productGroupService.selectProductGroupByCode(channelId, stockBean.getItemCode(), cartId);
+                                if (cmsBtProductGroup == null) {
+                                    continue;
+                                }
+
+                                // Group下非当前Code的其他产品Code集合
+                                List<String> codes = cmsBtProductGroup.getProductCodes().stream().filter(code -> !code.equalsIgnoreCase(stockBean.getItemCode())).collect(Collectors.toList());
+                                // 当前Group下只有一个Code
+                                if (codes.isEmpty()) {
+                                    continue;
+                                }
+
+                                // Group下非当前Code的其他产品Map集合,KV分别是Code和产品Model
+                                Map<String, CmsBtProductModel> productMap = new HashMap<>();
+                                // 待切换成的主商品Code
+                                String newMastCode = null;
+                                CmsBtProductModel newMastProduct = null;
+                                CmsBtProductModel_Platform_Cart newPlatFormCart = null;
+                                int platformQuantity = 0;
+                                for (String code : codes) {
+                                    CmsBtProductModel product = productService.getProductByCode(channelId, code);
+                                    if (product != null) {
+                                        productMap.put(code, product);
+
+                                        if ((newPlatFormCart = product.getPlatform(cartId)) != null
+                                                && CmsConstants.ProductStatus.Approved.name().equals(newPlatFormCart.getStatus())
+                                                && newPlatFormCart.getIntAttribute("quantity") > platformQuantity) {
+                                            newMastCode = code;
+                                            newMastProduct = product;
+                                            platformQuantity = newPlatFormCart.getIntAttribute("quantity");
+                                        }
+                                    }
+                                }
+
+                                if (newMastCode != null) {
+
+                                    if (!newMastCode.equalsIgnoreCase(platform.getMainProductCode())) {
+                                        String comment = "取消主商品 主商品：" + platform.getMainProductCode();
+                                        productStatusHistoryService.insert(channelId, stockBean.getItemCode(), platform.getStatus(), cartId, EnumProductOperationType.ChangeMastProduct, comment, modifier);
+
+                                        platform.setpIsMain(0);// 把product表中对应的平台的pIsMain设0
+                                        platform.setMainProductCode(newMastCode);
+                                        productPlatformService.updateProductPlatformWithSx(channelId, productModel.getProdId(), platform, modifier, "切换主商品", false);
+
+                                    }
+
+                                    if (!newMastCode.equalsIgnoreCase(newPlatFormCart.getMainProductCode())) {
+                                        newPlatFormCart.setpIsMain(1);//把productCode的所对应的product表中对应的平台的pIsMain设1
+                                        newPlatFormCart.setMainProductCode(newMastCode);
+                                        productPlatformService.updateProductPlatformWithSx(channelId, productMap.get(newMastCode).getProdId(), newPlatFormCart, modifier, "切换主商品", false);
+
+                                        String newComment = "设置为主商品 主商品：" + newMastCode;
+                                        productStatusHistoryService.insert(channelId, newMastCode, newPlatFormCart.getStatus(), cartId, EnumProductOperationType.ChangeMastProduct, newComment, modifier);
+                                    }
+
+                                    String oldMainProductCode = cmsBtProductGroup.getMainProductCode();
+                                    if (!newMastCode.equalsIgnoreCase(oldMainProductCode)) {
+                                        cmsBtProductGroup.setMainProductCode(newMastCode);//把group表中的mainProduct替换成productCode
+                                        cmsBtProductGroup.setModifier(modifier);
+                                        cmsBtProductGroup.setModified(DateTimeUtil.getNowTimeStamp());
+                                        productGroupService.update(cmsBtProductGroup);
+                                    }
+
+                                    // 新主商品Code已经处理过了
+                                    codes.remove(newMastCode);
+
+                                    if (!codes.isEmpty()) {
+                                        for (String code : codes) {
+                                            CmsBtProductModel product = productMap.get(code);
+                                            CmsBtProductModel_Platform_Cart pform = product.getPlatform(cartId);
+                                            pform.setMainProductCode(newMastCode);
+                                            productPlatformService.updateProductPlatformWithSx(channelId, product.getProdId(), pform, modifier, "切换主商品", false);
+                                            String comment = "主商品发生变化 主商品：" + code;
+                                            productStatusHistoryService.insert(channelId, code, pform.getStatus(), cartId, EnumProductOperationType.ChangeMastProduct, comment, modifier);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
                     } catch (Exception e) {
                         CmsBtOperationLogModel_Msg failMsg = new CmsBtOperationLogModel_Msg();
                         failMsg.setSkuCode(stockBean.getSku());
