cf24048f09e67f0fc2a331653eb2eaebe2da0fa0
wenjie.piao@voyageone.cn <wj7405049>
2017-07-05 19:52:52 +0800

    add template

--- voyageone-static/2.0/develop/modules/cms-us/routes.js
+++ voyageone-static/2.0/develop/modules/cms-us/routes.js
@@ -1,10 +1,17 @@
 define(function () {
     return {
-        "home": {
-            "hash": "/home",
-            "templateUrl": "views/home/welcome/datachart.tpl.html",
-            "controllerUrl": "modules/cms-us/views/home/welcome/datachart.ctl",
-            "controller": 'datachartController as ctrl'
+        home: {
+            hash: "/home",
+            templateUrl: "views/home/welcome/datachart.tpl.html",
+            controllerUrl: "modules/cms-us/views/home/welcome/datachart.ctl",
+            controller: 'datachartController as ctrl'
+        },
+        newItems:{
+            hash: "/items/newItems",
+            templateUrl: "views/items/newItems/index.tpl.html",
+            controllerUrl: "modules/cms-us/views/items/newItems/index.ctl",
+            controller: 'newItemController as ctrl'
         }
+
     };
 });
