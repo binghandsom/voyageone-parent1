48dbe8b1cd512f2f2ce0ba805351016921dda4b0
JonasGao <jonas.gao@voyageone.cn>
2017-07-12 21:55:07 +0800

    add: ImageServer helper

---
+++ voyageone-com/src/main/java/com/voyageone/common/ImageServer.java
@@ -0,0 +1,121 @@
+package com.voyageone.common;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 提供图片服务器的配置
+ */
+public class ImageServer {
+
+    private static final String FILE_NAME = "image-server.properties";
+
+    private static ImageServer imageServer;
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private String domain = "image.voyageone.com.cn";
+
+    private ImageServer() {
+        init();
+    }
+
+    /**
+     * 上传文件到图片服务
+     */
+    public static void uploadImage(String channel, String imageName, InputStream inputStream) {
+        HttpEntity entity = MultipartEntityBuilder.create()
+                .addTextBody("imageName", imageName)
+                .addBinaryBody("file", inputStream, ContentType.DEFAULT_BINARY, "image")
+                .build();
+        try {
+            HttpResponse response = Request.Post(imageServerUploadFilePath(channel))
+                    .body(entity)
+                    .execute()
+                    .returnResponse();
+            StatusLine statusLine = response.getStatusLine();
+            int code = statusLine.getStatusCode();
+            if (code == 200) {
+                return;
+            }
+            String content = IOUtils.toString(response.getEntity().getContent());
+            throw new FailUploadingException(code, statusLine.getReasonPhrase(), content);
+        } catch (IOException e) {
+            throw new FailUploadingException(e);
+        }
+    }
+
+    /**
+     * 获取经过包装的 s7 图片的主题路径部分
+     */
+    public static String imageBasePath(String channel) {
+        return imageServerUrl(channel, "is/image/sneakerhead/");
+    }
+
+    public static String imageUrl(String channel, String imageName) {
+        return imageBasePath(channel) + imageName;
+    }
+
+    public static String imageServerUrl(String path) {
+        return "http://" + imageServerDomain() + "/" + path;
+    }
+
+    private static String imageServerUploadFilePath(String channel) {
+        return imageServerUrl(channel, "file");
+    }
+
+    private static String imageServerUrl(String channel, String path) {
+        return "http://" + imageServerDomain() + "/" + channel + "/" + path;
+    }
+
+    /**
+     * 获取当前配置的图片服务器地址
+     */
+    private static String imageServerDomain() {
+        if (imageServer == null)
+            imageServer = new ImageServer();
+
+        return imageServer.domain;
+    }
+
+    private void init() {
+        Resource resource = new ClassPathResource(FILE_NAME, this.getClass());
+
+        if (!resource.exists()) {
+            return;
+        }
+
+        try {
+            Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+            this.domain = properties.getProperty("domain");
+        } catch (IOException e) {
+            logger.warn("尝试读取 ImageServer 的配置，出现了未知异常，所以使用了默认配置。Cause by IOException: {}",
+                    e.getMessage());
+        }
+    }
+
+    public static class FailUploadingException extends RuntimeException {
+        FailUploadingException(Throwable cause) {
+            super(cause);
+        }
+
+        FailUploadingException(int code, String reasonPhrase, String content) {
+            super(String.format("%s %s; %s", code, reasonPhrase, content));
+        }
+    }
+}
+
