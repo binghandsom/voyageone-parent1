a7eecb3198760d17cf7ec514bdda8f6496b4c3f4
JonasGao <jonas.gao@voyageone.cn>
2017-07-13 17:42:48 +0800

    update: direct get domain from codes

--- voyageone-com/src/main/java/com/voyageone/common/ImageServer.java
+++ voyageone-com/src/main/java/com/voyageone/common/ImageServer.java
@@ -2,19 +2,13 @@ package com.voyageone.common;
 
 import com.voyageone.common.configs.Codes;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,24 +16,6 @@ import java.io.InputStream;
  * 提供图片服务器的配置
  */
 public class ImageServer {
-
-    private static ImageServer imageServer;
-
-    private static Logger logger = LoggerFactory.getLogger(ImageServer.class);
-
-    private String domain;
-
-    @PostConstruct
-    public void afterPropertiesSet() {
-        String domain = Codes.getCodeName("IMAGE_SERVER", "domain");
-        if (StringUtils.isNotBlank(domain)) {
-            this.domain = domain;
-            return;
-        }
-        this.domain = "image.voyageone.com.cn";
-        logger.warn("没有从 Codes 配置中获取到图片服务的配置，将使用默认值 {}", this.domain);
-    }
-
 
     /**
      * 上传文件到图片服务
@@ -93,17 +69,7 @@ public class ImageServer {
      * 获取当前配置的图片服务器地址
      */
     private static String domain() {
-        if (imageServer == null) {
-            throw new TooEarlyException();
-        }
-
-        return imageServer.domain;
-    }
-
-    public static class TooEarlyException extends RuntimeException {
-        TooEarlyException() {
-            super("尝试获取图片服务的域名太早了。Spring 还没有初始化 ImageServer 的示例");
-        }
+        return Codes.getCodeName("IMAGE_SERVER", "domain");
     }
 
     public static class FailUploadingException extends RuntimeException {
@@ -115,16 +81,4 @@ public class ImageServer {
             super(String.format("%s %s; %s", code, reasonPhrase, content));
         }
     }
-
-    /**
-     * 自动初始化一个 ImageServer 的实例，并保存到静态单例变量中
-     */
-    @Configuration
-    public static class ImageServerAutoConfiguration {
-        @Bean
-        public ImageServer imageServer() {
-            return imageServer = new ImageServer();
-        }
-    }
 }
-
