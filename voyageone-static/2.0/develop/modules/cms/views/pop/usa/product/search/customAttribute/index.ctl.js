0c1548a9d84645d67bdadbed8dabfc5cbe4c636e
rex.wu <rex.wu@voyageone.cn>
2017-07-18 11:25:58 +0800

    Advance search add some query terms

--- voyageone-static/2.0/develop/modules/cms/views/pop/usa/product/search/customAttribute/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/pop/usa/product/search/customAttribute/index.ctl.js
@@ -4,8 +4,18 @@ define([
 
     cms.controller('usaCustomAttributeController',class usaCustomAttributeController{
 
-        constructor(){
+        constructor(advanceSearch){
+            this.advanceSearch = advanceSearch;
+            this.init();
+        }
 
+        init() {
+            let self = this;
+            this.advanceSearch.getCustomColumns().then(res => {
+               if (res.data) {
+                   console.log(res.data);
+               }
+            });
         }
 
     });
--- voyageone-static/2.0/develop/modules/cms/views/usa/product/search/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/usa/product/search/index.ctl.js
@@ -9,16 +9,56 @@ define([
 
     cms.controller('usProductSearchController', class UsProductSearchController {
 
-        constructor(popups) {
+        constructor(popups,advanceSearch) {
             let self = this;
 
             self.popups = popups;
+            self.advanceSearch = advanceSearch;
+
+            self.pageOption = {curr: 1, total: 0, size: 10, fetch: self.search},
+            self.searchInfo = {};
+            self.searchResult = {
+                productList:[]
+            };
+
+            self.masterData = {
+                platforms:[],
+                brandList:[]
+            };
         }
 
         init(){
-
+            let self = this;
+            // 查询masterData,包括platforms、brandList
+            this.advanceSearch.init().then(res => {
+               if (res.data) {
+                    self.masterData.platforms = res.data.platforms;
+                    self.masterData.brandList = res.data.brandList;
+               }
+            });
+            this.search();
         }
 
+        search() {
+            let self = this;
+            let searchInfo = angular.copy(self.searchInfo);
+            // codeList 换行符分割
+            if (searchInfo.codeList) {
+                let codeList = searchInfo.codeList.split("\n");
+                searchInfo.codeList = codeList;
+            }
+            console.log(searchInfo.codeList);
+            _.extend(searchInfo, self.pageOption);
+            // console.log(searchInfo);
+            self.advanceSearch.search(searchInfo).then(res => {
+                if (res.data) {
+                    self.searchResult.productList = res.data.productList;
+                }
+                console.log(self.searchResult.productList);
+            });
+        }
+
+        // 自定义列弹出
         popCustomAttributes(){
             let self = this;
 
