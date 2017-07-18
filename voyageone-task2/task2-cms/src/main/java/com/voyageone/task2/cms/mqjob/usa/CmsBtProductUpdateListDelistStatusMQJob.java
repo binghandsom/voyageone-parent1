a5525053dc8a31f328e017040c9cafa68fa3cbe5
xusong <song.xu@voyageone.cn>
2017-07-18 10:30:01 +0800

    修改价格

---
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/mqjob/usa/CmsBtProductUpdateListDelistStatusMQJob.java
@@ -0,0 +1,7 @@
+package com.voyageone.task2.cms.mqjob.usa;
+
+/**
+ * Created by dell on 2017/7/18.
+ */
+public class CmsBtProductUpdateListDelistStatusMQJob {
+}
--- voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/mqjob/usa/CmsBtProductUpdatePriceMQJob.java
+++ voyageone-task2/task2-cms/src/main/java/com/voyageone/task2/cms/mqjob/usa/CmsBtProductUpdatePriceMQJob.java
@@ -1,5 +1,6 @@
 package com.voyageone.task2.cms.mqjob.usa;
 
+import com.voyageone.base.dao.mongodb.JongoQuery;
 import com.voyageone.base.dao.mongodb.JongoUpdate;
 import com.voyageone.base.dao.mongodb.model.BaseMongoMap;
 import com.voyageone.base.dao.mongodb.model.BulkUpdateModel;
@@ -9,6 +10,7 @@ import com.voyageone.service.dao.cms.mongo.CmsBtProductDao;
 import com.voyageone.service.impl.cms.product.ProductService;
 import com.voyageone.service.impl.cms.sx.SxProductService;
 import com.voyageone.service.impl.cms.vomq.vomessage.body.CmsPlatformCategoryUpdateMQMessageBody;
+import com.voyageone.service.impl.cms.vomq.vomessage.body.usa.CmsBtProductUpdateCategoryMQMessageBody;
 import com.voyageone.service.impl.cms.vomq.vomessage.body.usa.CmsBtProductUpdatePriceMQMessageBody;
 import com.voyageone.service.model.cms.mongo.product.CmsBtProductModel;
 import com.voyageone.service.model.cms.mongo.product.CmsBtProductModel_Platform_Cart;
@@ -18,10 +20,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by dell on 2017/7/17.
@@ -41,12 +40,14 @@ public class CmsBtProductUpdatePriceMQJob extends TBaseMQCmsService<CmsBtProduct
     @Override
     public void onStartup(CmsBtProductUpdatePriceMQMessageBody messageBody) throws Exception {
         if (messageBody != null){
+            String channelId = messageBody.getChannelId();
             Map<String, Object> params = messageBody.getParams();
             String changedPriceType = (String) params.get("changedPriceType");
             String basePriceType = (String) params.get("basePriceType");
             String optionType = (String) params.get("optionType");
-            String value = (String) params.get("value");
-
+            Double value = Double.parseDouble((String)params.get("value"));
+            //"1":取整,"0":不取整
+            String flag = (String) params.get("flag");
             List<String> productCodes = messageBody.getProductCodes();
             if (ListUtils.notNull(productCodes)){
                 super.count = productCodes.size();
@@ -54,7 +55,8 @@ public class CmsBtProductUpdatePriceMQJob extends TBaseMQCmsService<CmsBtProduct
                 for (String productCode : productCodes) {
                     CmsBtProductModel cmsBtProductModel = productService.getProductByCode(messageBody.getChannelId(), productCode);
                     if (cmsBtProductModel != null){
-                        CmsBtProductModel_Platform_Cart platform = cmsBtProductModel.getPlatform(cartId);
+                        Map<String, CmsBtProductModel_Platform_Cart> usPlatforms = cmsBtProductModel.getUsPlatforms();
+                        CmsBtProductModel_Platform_Cart platform = usPlatforms.get("P" + cartId);
                         if (platform != null){
                             List<BaseMongoMap<String, Object>> skus = platform.getSkus();
                             if (skus != null){
@@ -62,74 +64,42 @@ public class CmsBtProductUpdatePriceMQJob extends TBaseMQCmsService<CmsBtProduct
                                     //获取到对应的skuCode
                                     String skuCode = (String) sku.get("skuCode");
                                     Double newPrice = null;
-                                    Double basePrice = (Double) sku.get(basePriceType);
-                                    Double value1 = (Double) sku.get(value);
-                                    if ("*".equals(optionType)){
-                                        newPrice = basePrice * value1;
+                                    Double basePrice = null;
+                                    if ("clientMsrpPrice".equals(basePriceType)||"clientRetailPrice".equals(basePriceType)){
+                                        basePrice = (Double) sku.get(basePriceType);
+                                        if ("*".equals(optionType)){
+                                            newPrice = basePrice * value;
+                                        }
+                                        if ("/".equals(optionType)){
+                                            newPrice = basePrice / value;
+                                        }
+                                        if ("+".equals(optionType)){
+                                            newPrice = basePrice + value;
+                                        }
+                                        if ("-".equals(optionType)){
+                                            newPrice = basePrice - value;
+                                        }
+                                    }else {
+                                        //固定值类型
+                                        newPrice = value;
                                     }
-                                    if ("/".equals(optionType)){
-                                        newPrice = basePrice / value1;
-                                    }
-                                    if ("+".equals(optionType)){
-                                        newPrice = basePrice + value1;
-                                    }
-                                    if ("-".equals(optionType)){
-                                        newPrice = basePrice - value1;
+                                    if (flag == "1"){
+                                        //将价格取整
+                                        Math.round(newPrice);
                                     }
                                     JongoUpdate jongoUpdate = new JongoUpdate();
-                                    jongoUpdate.setQuery("{\"platforms.P23.skus.skuCode\":#}");
-                                    jongoUpdate.setQueryParameters(skuCode.toLowerCase());
-                                    jongoUpdate.setUpdate("{$set:{\"skus.$.qty\":#}}");
-                                    //jongoUpdate.setUpdateParameters(skuTotal);
-
-
+                                    jongoUpdate.setQuery("{\"usPlatforms.P"+ cartId +".skus.skuCode\":#}");
+                                    jongoUpdate.setQueryParameters(skuCode);
+                                    jongoUpdate.setUpdate("{$set:{\"usPlatforms.P"+ cartId +".skus.$."+ changedPriceType+"\":#}}");
+                                    jongoUpdate.setUpdateParameters(newPrice);
+                                    cmsBtProductDao.bulkUpdateWithJongo(channelId, Collections.singletonList(jongoUpdate));
                                 }
                             }
-
                         }
                     }
                 }
             }
         }
-
-
-      /*  for (String productCode : productCodes) {
-            CmsBtProductModel cmsBtProductModel = productService.getProductByCode(messageBody.getChannelId(), productCode);
-            if(cmsBtProductModel != null && cmsBtProductModel.getPlatform(cartId) != null) {
-                CmsBtProductModel_Platform_Cart platform = cmsBtProductModel.getPlatform(cartId);
-                if (!messageBody.getpCatId().equalsIgnoreCase(platform.getpCatId()) || !messageBody.getpCatPath().equalsIgnoreCase(platform.getpCatPath())) {
-                    if (CartEnums.Cart.TT.getValue() != cartId
-                            && CartEnums.Cart.LTT.getValue() != cartId
-                            && CartEnums.Cart.JM.getValue() != cartId) {
-                        if (!"Approved".equalsIgnoreCase(platform.getStatus())) {
-                            update(productCode, messageBody);
-                        }
-                    } else {
-                        update(productCode, messageBody);
-                        if("Approved".equalsIgnoreCase(platform.getStatus())){
-                            sxProductService.insertSxWorkLoad(messageBody.getChannelId(), productCode, cartId, getTaskName());
-                        }
-                    }
-                }
-            }
-        }*/
     }
-
-    private void update(String productCode, CmsPlatformCategoryUpdateMQMessageBody messageBody){
-        Integer cartId = messageBody.getCartId();
-        List<BulkUpdateModel> bulkList = new ArrayList<>(1);
-        HashMap<String, Object> updateMap = new HashMap<>();
-        updateMap.put("platforms.P" + cartId + ".pCatPath", messageBody.getpCatPath());
-        updateMap.put("platforms.P" + cartId + ".pCatId", messageBody.getpCatId());
-        updateMap.put("platforms.P" + cartId + ".pCatStatus", 1);
-        HashMap<String, Object> queryMap = new HashMap<>();
-        queryMap.put("common.fields.code", productCode);
-        BulkUpdateModel model = new BulkUpdateModel();
-        model.setUpdateMap(updateMap);
-        model.setQueryMap(queryMap);
-        bulkList.add(model);
-        productService.bulkUpdateWithMap(messageBody.getChannelId(), bulkList, messageBody.getSender(), "$set");
-    }
-
     }
 
