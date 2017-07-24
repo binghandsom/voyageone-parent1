a0ebebdf21ef87c5dca61e30eb59e495d4405436
rex.wu <rex.wu@voyageone.cn>
2017-07-24 20:21:45 +0800

    product detail update

--- voyageone-static/2.0/develop/modules/cms/views/usa/feed/detail/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/usa/feed/detail/index.ctl.js
@@ -42,7 +42,6 @@ define([
             this.topFeedList = null; // 同Model查询结果
             this.imageUrl = "http://image.sneakerhead.com/is/image/sneakerhead/";
             this.init();
-            this.flag = '';
         }
 
         init() {
