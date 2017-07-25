ebd073e67819adbc91b0d3c873f80b38b1976eee
rex.wu <rex.wu@voyageone.cn>
2017-07-25 16:21:19 +0800

    check bugs

--- voyageone-static/2.0/develop/modules/cms/views/usa/product/search/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/usa/product/search/index.ctl.js
@@ -548,6 +548,7 @@ define([
                             // notify.success($translate.instant('TXT_MSG_SET_SUCCESS'));
                             self.notify.success("Set free tags succeeded.");
                             self.clearSelList();
+                            self._selall = false;
                             self.search();
                         })
                     });
