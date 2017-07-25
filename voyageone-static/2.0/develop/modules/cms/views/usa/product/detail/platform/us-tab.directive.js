7be903c5147b73522fcb567f9e563ab2f509c184
rex.wu <rex.wu@voyageone.cn>
2017-07-25 10:31:48 +0800

    product detail update

--- voyageone-static/2.0/develop/modules/cms/views/usa/product/detail/platform/us-tab.directive.js
+++ voyageone-static/2.0/develop/modules/cms/views/usa/product/detail/platform/us-tab.directive.js
@@ -12,10 +12,18 @@ define([
             this.detailDataService = detailDataService;
             this.productInfo = $scope.productInfo;
             this.cartInfo = $scope.cartInfo;
+
+            console.log(this.cartInfo);
+
+            // 平台信息
             this.platform = {};
+
+            // SKU
+            this.selAllSkuFlag = false;
 
             // 平台状态
             this.platformStatus = this.detailDataService.platformStatus;
+
         }
 
         init() {
@@ -25,9 +33,44 @@ define([
                 cartId: Number(self.cartInfo.value),
                 prodId: self.productInfo.productId
             }).then(res => {
-                self.platform.platformFields = res.data.platformFields;
-                self.platform.fields = res.data.fields;
+
+                if (res.data) {
+                    self.platform = res.data;
+
+                    // SKU 是否全选
+                    let flag = true;
+                    _.each(self.platform.skus, sku => {
+                        let isSale = sku.isSale;
+                        if (!isSale) {
+                            flag = false;
+                        }
+                    });
+                    self.selAllSkuFlag = flag;
+                    console.log(self.platform);
+                }
+                // self.platform.platformFields = res.data.platformFields;
+                // self.platform.fields = res.data.fields;
             })
+        }
+
+        // SKU可售选择
+        selAllSku() {
+            let self = this;
+            _.each(self.platform.skus, sku => {
+                sku.isSale = self.selAllSkuFlag;
+            });
+        }
+        checkSelAllSku(sku) {
+            let self = this;
+            let isSale = sku.isSale;
+            if (!isSale) {
+                self.selAllSkuFlag = false;
+            } else {
+                let notSelOne = _.find(self.platform.skus, sku => {
+                    return !sku.isSale;
+                });
+                self.selAllSkuFlag = !notSelOne;
+            }
         }
 
     }
