f3ba2e35c49ccb1959e3b46406e639b08988ad2d
JonasGao <jonas.gao@voyageone.cn>
2017-07-28 12:43:48 +0800

    frontend/update: gulp build task, add debug title

--- voyageone-static/2.0/gulp/build.task.js
+++ voyageone-static/2.0/gulp/build.task.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp');
 const glob = require('glob');
 const babel = require('gulp-babel');
-const debug = require('gulp-debug');
+const gulpDebug = require('gulp-debug');
+const debug = (title) => gulpDebug({title});
 const concat = require('gulp-concat');
 const header = require('gulp-header');
 const footer = require('gulp-footer');
@@ -23,7 +24,7 @@ const tryMin = require('./gulp-try-min');
 
 function copy(path) {
     gulp.src(path.src)
-        .pipe(debug())
+        .pipe(debug('copy'))
         .pipe(gulp.dest(path.dist));
 }
 
@@ -37,12 +38,12 @@ function buildResources() {
     // 复制第三方库文件
     copy(paths.libs);
     // 复制 json 文件
-    copy({src:entry.modules.json, dist: paths.modules});
+    copy({src: entry.modules.json, dist: paths.modules});
 
     // 处理全局样式
     _.forEach(entry.css, (v, k) => {
         gulp.src(v)
-            .pipe(debug())
+            .pipe(debug('global css'))
             .pipe(concat(`${k}.min.css`))
             .pipe(production(cleanCss()))
             .pipe(gulp.dest(paths.css.dist));
@@ -50,7 +51,7 @@ function buildResources() {
 
     // 处理页面的样式
     gulp.src(entry.modules.css)
-        .pipe(debug())
+        .pipe(debug('modules css'))
         .pipe(production(cleanCss()))
         .pipe(gulp.dest(paths.modules));
 }
@@ -59,7 +60,7 @@ function buildAppJS() {
 
     // 处理顶层页面的脚本
     gulp.src(entry.loginAndChannel.js)
-        .pipe(debug())
+        .pipe(debug('login/channel'))
         .pipe(ngAnnotate())
         .pipe(production(uglify()))
         .pipe(production(rename({suffix: ".min"})))
@@ -67,8 +68,8 @@ function buildAppJS() {
 
     // 处理应用模块的脚本
     gulp.src(entry.modules.js)
-        .pipe(debug())
-        .pipe(replace('version=', 'v='+ Date.parse(new Date())))
+        .pipe(debug('module'))
+        .pipe(replace('version=', 'v=' + Date.parse(new Date())))
         .pipe(ngAnnotate())
         .pipe(production(tryMin([
             'develop',
@@ -87,13 +88,13 @@ function buildAppJS() {
     };
 
     gulp.src(entry.common.src)
-        .pipe(debug())
+        .pipe(debug('common'))
         .pipe(babel())
         .pipe(ngAnnotate())
-        .pipe(iife())
+        // .pipe(iife())
         .pipe(concat(paths.common.output))
-        .pipe(header(definePrefix))
-        .pipe(footer(defineSuffix))
+        // .pipe(header(definePrefix))
+        // .pipe(footer(defineSuffix))
         .pipe(development(uglify(formatOpt)))   // 开发环境: 格式化
         .pipe(production(uglify()))             // 生产环境: 压缩
         .pipe(gulp.dest(paths.common.dist));
@@ -104,7 +105,7 @@ function buildAppHtml() {
     // 处理顶层页面
     // 压缩，并替换引用的 min 脚本
     gulp.src(entry.loginAndChannel.html)
-        .pipe(debug())
+        .pipe(debug('login/channel html'))
         .pipe(production(replace(/require\(\['(\w+?)'\]\)/g, 'require(["$1.min"])')))
         .pipe(production(replace(/<script src="(libs\/.+?)js"><\/script>/g, '<script src="$1min.js"></script>')))
         .pipe(production(minifyHtml({empty: true})))
@@ -112,13 +113,15 @@ function buildAppHtml() {
 
     // 处理模块页面
     gulp.src(entry.modules.html)
-        .pipe(debug())
-        .pipe(replace('version=', 'v='+ Date.parse(new Date())))
+        .pipe(debug('module html'))
+        .pipe(replace('version=', 'v=' + Date.parse(new Date())))
         .pipe(production(replace(/libs\/require\.js\/(.+?)\/require.js/, 'libs/require.js/$1/require.min.js')))
         .pipe(production(minifyHtml({empty: true})))
         .pipe(gulp.dest(paths.modules));
 }
 
 gulp.task(taskName.build, function () {
-
+    buildResources();
+    buildAppJS();
+    buildAppHtml();
 });
