15962078ee988bbf3e8d4f91319bc9daa51f3c0e
rex.wu <rex.wu@voyageone.cn>
2017-07-28 15:14:13 +0800

    usa - js 误删代码恢复

--- voyageone-static/2.0/develop/modules/cms/views/usa/product/search/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/usa/product/search/index.ctl.js
@@ -62,6 +62,30 @@ define([
             };
             self.columnArrow = {};
             self.cartEntity = cartEntity;
+
+            if (self.$routeParams.code) {
+                let routePrams = eval('(' + self.$routeParams.code + ')');
+                let cartId = routePrams.cartId;
+                let platformStatus = routePrams.platformStatus;
+                if (cartId && platformStatus) {
+                    self.searchInfo.cartId = cartId;
+                    if (!self.searchInfo.platformStatus) {
+                        self.searchInfo.platformStatus = {};
+                    }
+                    // 如果状态在self.masterData不存在(all)则默认查询平台下所有状态数据
+                    let platformStatusObj = _.find(self.masterData.platformStatus, item => {
+                        return platformStatus === item.status;
+                    });
+                    if (!platformStatusObj) {
+                        _.each(self.masterData.platformStatus, item => {
+                            let tempPlatformStatus = item.status;
+                            self.searchInfo.platformStatus[tempPlatformStatus] = true;
+                        });
+                    } else {
+                        self.searchInfo.platformStatus[platformStatus] = true;
+                    }
+                }
+            }
             self.sort = {
                 sName:'Newest',
                 sortType:-1
