ada91c51c172718243d7be9b5176ef6d223001cc
edward <edward.lin@voyageone.cn>
2017-07-31 23:29:56 +0800

    修正高级检索产品图片显示不正确

--- voyageone-static/2.0/develop/modules/cms/views/searchSolr/advanceSolrSearch.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/searchSolr/advanceSolrSearch.ctl.js
@@ -289,7 +289,7 @@ define([
                 //     :productInfo.common.fields.images1[0].image1
                 $scope.vm.productList.forEach(function (p) {
                     var f = p.common.fields;
-                    p.thumb = f.images6 && f.images6[0].image6
+                    p.thumb = f.images6 && f.images6[0] && f.images6[0].image6
                         ?f.images6[0].image6
                         :f.images1[0].image1
                 });
