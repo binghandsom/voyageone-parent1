43404f366298ecbc42d79ece7f134a05781c867f
piaowenjie <wenjie.piao@voyageone.cn>
2017-07-31 14:41:55 +0800

    add shop cattegory page html

---
+++ voyageone-static/2.0/develop/modules/cms/views/usa/channel/shopCategory/index.ctl.js
@@ -0,0 +1,20 @@
+/**
+ * @description 店铺内分类管理
+ */
+define([
+    'cms'
+],function (cms) {
+
+    cms.controller('shopCategoryController',class ShopCategoryController{
+
+        constructor(){
+
+        }
+
+        init(){
+
+        }
+
+    })
+
+});
