668abd4501579c549c313a1cd7d1e844713bccff
piaowenjie <wenjie.piao@voyageone.cn>
2017-07-31 20:32:41 +0800

    add edit category popup page

--- voyageone-static/2.0/develop/modules/cms/views/usa/channel/shopCategory/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/usa/channel/shopCategory/index.ctl.js
@@ -7,12 +7,50 @@ define([
 
     cms.controller('shopCategoryController',class ShopCategoryController{
 
-        constructor(){
-
+        constructor(sellerCatService,popups){
+            this.sellerCatService = sellerCatService;
+            this.popups = popups;
+            this.totalCategory = [];
         }
 
         init(){
+            let self = this;
 
+            self.sellerCatService.getCat({cartId: 8}).then(res => {
+                let len = self.totalCategory.length;
+                self.totalCategory.push({index: len, children: res.data.catTree});
+
+            });
+        }
+
+        /**
+         * @param category 子节点
+         * @param categoryItem 父节点
+         */
+        openCategory(category, categoryItem) {
+            const self = this,
+                totalCategory = self.totalCategory;
+
+            totalCategory.splice(categoryItem.index + 1);
+
+            if (category && category.children.length > 0) {
+                let len = totalCategory.length;
+                totalCategory.push({index: len, children: category.children});
+            }
+
+            categoryItem.selectedCat = category;
+            self.selected = category;
+
+        }
+
+        popEditCategory(model,$event){
+            let self = this;
+
+            self.popups.openEditCategory().then(res => {
+
+            });
+
+            $event.stopPropagation();
         }
 
     })
