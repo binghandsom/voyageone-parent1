7c82c59a920d5479b2cdcf0afa6174a0d483ce88
piaowenjie <wenjie.piao@voyageone.cn>
2017-08-03 14:13:49 +0800

    product detail add issale shadow

--- voyageone-static/2.0/bs-config.js
+++ voyageone-static/2.0/bs-config.js
@@ -1,7 +1,7 @@
 var morgan = require('morgan'),
     proxy = require('http-proxy-middleware');
 
-var proxyUrl = "http://localhost:8080";
+var proxyUrl = "http://10.0.1.143:8080";
 var proxyMiddleware = proxy(proxyUrl);
 
 var jsonPlaceholderProxy = proxy(['/cms','/core'], {
