fb1f529038ee462aa536bb2c2cc622bb62454dd0
james <james.li@voyageone.cn>
2017-08-04 16:38:31 +0800

    liking 官网共同图片

--- voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/CmsImagePostScene7ServiceTest.java
+++ voyageone-task2/task2-cms/src/test/java/com/voyageone/task2/cms/service/CmsImagePostScene7ServiceTest.java
@@ -3,12 +3,17 @@ package com.voyageone.task2.cms.service;
 import com.voyageone.service.model.cms.CmsBtImagesModel;
 import com.voyageone.task2.base.modelbean.TaskControlBean;
 import com.voyageone.task2.cms.service.CmsImagePostScene7Service;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.fluent.Request;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,20 +47,28 @@ public class CmsImagePostScene7ServiceTest {
 
     @Test
     public void testGetAndSendImage() {
-        String orderChannelId = "024";
+        String orderChannelId = "045";
         List<CmsBtImagesModel> imageUrlList = new ArrayList<>();
 
         CmsBtImagesModel model = new CmsBtImagesModel();
-        model.setOriginalUrl("http://p0.jmstatic.com/partner_product/2015new/09/23/11/38/14429794817080.jpg");
-        model.setImgName("14429794817080.jpg");
+        model.setOriginalUrl("http://d3d71ba2asa5oz.cloudfront.net/12016207/images/bb0189-parent__1.jpg");
+        model.setImgName("045-045-844656-006-PARENT-CoolGreyStadiumGreen-1");
         imageUrlList.add(model);
 
         List<CmsBtImagesModel> successImageUrlList = new ArrayList<>();
         List<CmsBtImagesModel> urlErrorList = new ArrayList<>();
 
+//        try (InputStream stream = Request.Get(model.getOriginalUrl()).execute().returnContent().asStream()) {
+            try (InputStream stream = new URL(model.getOriginalUrl()).openStream()) {
+                FileOutputStream aa = new FileOutputStream("h://cc.jpg");
+                IOUtils.copy(stream,aa);
+                aa.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         int threadNo = 0;
 
-        cmsImagePostScene7Service.getAndSendImage(orderChannelId, imageUrlList, successImageUrlList, urlErrorList, threadNo);
+//        cmsImagePostScene7Service.getAndSendImage(orderChannelId, imageUrlList, successImageUrlList, urlErrorList, threadNo);
     }
 
     @Autowired
