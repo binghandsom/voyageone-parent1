e33c2030d1cdc9ff31f865940fc8dbff847720be
xusong <song.xu@voyageone.cn>
2017-08-10 13:54:04 +0800

    修改價格的時候同步傷心表

--- voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/usa/UsaProductDetailService.java
+++ voyageone-service2/voyageone-service2-impl/src/main/java/com/voyageone/service/impl/cms/usa/UsaProductDetailService.java
@@ -682,7 +682,8 @@ public class UsaProductDetailService extends BaseService {
                         productService.bulkUpdateWithMap(channelId, bulkList, userName, "$set");
                         productService.bulkUpdateWithMap(channelId, minMaxPrice, userName, "$set");
                     }
-                    if (CmsConstants.ProductStatus.Approved.name().equals(usPlatform.getStatus()) && (CmsConstants.PlatformStatus.OnSale.name().equals(usPlatform.getpStatus()) || CmsConstants.PlatformStatus.InStock.name().equals(usPlatform.getpStatus()))) {
+
+                    if (CmsConstants.ProductStatus.Approved.name().equals(usPlatform.getStatus()) && (CmsConstants.PlatformStatus.OnSale == usPlatform.getpStatus() || CmsConstants.PlatformStatus.InStock == usPlatform.getpStatus() )) {
                         platformProductUploadService.saveCmsBtUsWorkloadModel(channelId, cartId, productCode, null, 0, userName);
                     }
                 }
@@ -743,10 +744,9 @@ public class UsaProductDetailService extends BaseService {
                             productService.bulkUpdateWithMap(channelId, bulkList1, userName, "$set");
                             productService.bulkUpdateWithMap(channelId, minMaxPrice1, userName, "$set");
                         }
-                        if (CmsConstants.ProductStatus.Approved.name().equals(platform.getStatus()) && (CmsConstants.PlatformStatus.OnSale.name().equals(platform.getpStatus()) || CmsConstants.PlatformStatus.InStock.name().equals(platform.getpStatus()))) {
+                        if (CmsConstants.ProductStatus.Approved.name().equals(platform.getStatus()) && (CmsConstants.PlatformStatus.OnSale == platform.getpStatus() || CmsConstants.PlatformStatus.InStock == platform.getpStatus() )) {
                             platformProductUploadService.saveCmsBtUsWorkloadModel(channelId, cartId, productCode, null, 0, userName);
                         }
-
                         //更新价格履历
                         List<String> skus1 = new ArrayList<>();
                         skus.forEach(sku -> skus1.add(sku.getStringAttribute("skuCode")));
