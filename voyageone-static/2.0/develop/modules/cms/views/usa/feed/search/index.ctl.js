2727f1b87b003f1d54a37b4781b032c7afce9ceb
rex.wu <rex.wu@voyageone.cn>
2017-08-10 17:44:46 +0800

    5555

--- voyageone-static/2.0/develop/modules/cms/views/usa/feed/search/index.ctl.js
+++ voyageone-static/2.0/develop/modules/cms/views/usa/feed/search/index.ctl.js
@@ -177,6 +177,7 @@ define([
 
             self.itemDetailService.updateOne(requestMap).then(() => {
                 self.notify.success('update success!');
+                self.feed.usPlatforms.P8[key] = value;
 
                 feed.editMsrp = false;
                 feed.editRetai = false;
